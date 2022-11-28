a693f459bf9d6494697ae2f21919155cd9c1d0d0
Kielan <ki.lemons@gmail.com>
2022-11-27 20:54:08 -0500

    Create GHOST_NDOFManagerUnix.h

---
+++ internal/ghost/test/intern/GHOST_NDOFManagerUnix.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "GHOST_NDOFManager.h"
+
+/* Event capture is handled within the NDOF manager on Linux,
+ * so there's no need for SystemX11 to look for them. */
+
+class GHOST_NDOFManagerUnix : public GHOST_NDOFManager {
+ public:
+  GHOST_NDOFManagerUnix(GHOST_System &);
+  ~GHOST_NDOFManagerUnix();
+  bool available();
+  bool processEvents();
+
+ private:
+  bool available_;
+};
