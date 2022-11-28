f0deb118089f823d76325ca6ecb01f15c5a20cf5
Kielan <ki.lemons@gmail.com>
2022-11-27 20:47:36 -0500

    Create GHOST_NDOFManagerCocoa.h

---
+++ internal/ghost/test/intern/GHOST_NDOFManagerCocoa.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "GHOST_NDOFManager.h"
+
+// Event capture is handled within the NDOF manager on Macintosh,
+// so there's no need for SystemCocoa to look for them.
+
+class GHOST_NDOFManagerCocoa : public GHOST_NDOFManager {
+ public:
+  GHOST_NDOFManagerCocoa(GHOST_System &);
+  ~GHOST_NDOFManagerCocoa();
+
+  bool available();
+};
