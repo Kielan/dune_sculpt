09d898a77fb14a7a87053b7239c1f0aa83d37602
Kielan <ki.lemons@gmail.com>
2022-12-06 09:18:07 -0700

    Update LIB_assert.h

--- source/dune/lib/LIB_assert.h
+++ source/dune/lib/LIB_assert.h
@@ -1,3 +1,21 @@
+#pragma once
+
+/**
+ *
+ * Defines:
+ * - #LIB_assert
+ * - #LIB_STATIC_ASSERT
+ * - #LIB_STATIC_ASSERT_ALIGN
+ */
+
+/* Utility functions. */
+
+void _LIB_assert_print_pos(const char *file, int line, const char *function, const char *id);
+void _LIB_assert_print_extra(const char *str);
+void _LIB_assert_print_backtrace(void);
+void _LIB_assert_abort(void);
+void _LIB_assert_unreachable_print(const char *file, int line, const char *function);
+
 #ifndef NDEBUG
 /* _LIB_ASSERT_PRINT_POS */
 #  if defined(__GNUC__)
