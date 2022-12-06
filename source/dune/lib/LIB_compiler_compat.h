0b3489b0774c8daa903eccc1fae57da2b0da2299
Kielan <ki.lemons@gmail.com>
2022-12-06 16:43:29 -0700

    Create LIB_compiler_compat.h

---
+++ source/dune/lib/LIB_compiler_compat.h
@@ -0,0 +1,29 @@
+/* #define typeof() triggers a bug in some clang-format versions, disable format
+ * for entire file to keep results consistent. */
+
+#pragma once
+
+/**
+ * Use to help with cross platform portability.
+ */
+
+#if defined(_MSC_VER)
+#  define alloca _alloca
+#endif
+
+#define typeof(x) decltype(decltype_helper(x))
+}
+#endif
+
+/* little macro so inline keyword works */
+#if defined(_MSC_VER)
+#  define LIB_INLINE static __forceinline
+#else
+#  define LIB_INLINE static inline __attribute__((always_inline)) __attribute__((__unused__))
+#endif
+
+#if defined(__GNUC__)
+#  define LIB_NOINLINE __attribute__((noinline))
+#else
+#  define LIB_NOINLINE
+#endif
