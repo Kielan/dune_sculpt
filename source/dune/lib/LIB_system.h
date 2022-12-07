c544d4769eaebe73957c69727126311ae5932798
Kielan <ki.lemons@gmail.com>
2022-12-06 18:04:32 -0700

    Update LIB_system.h

--- source/dune/lib/LIB_system.h
+++ source/dune/lib/LIB_system.h
@@ -1,1 +1,10 @@
+#pragma once
 
+#include <stdio.h>
+
+int LIB_cpu_support_sse2(void);
+int LIB_cpu_support_sse41(void);
+void LIB_system_backtrace(FILE *fp);
+
+/** Get CPU brand, result is to be MEM_freeN()-ed. */
+char *LIB_cpu_brand_string(void);
