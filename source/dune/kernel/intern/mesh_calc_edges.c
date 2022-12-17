0c8ca821b0e9c5d902bef0429b5734dc210c6471
Kielan <ki.lemons@gmail.com>
2022-12-17 12:34:33 -0700

    Create mesh_calc_edges.c

---
+++ source/dune/kernel/intern/mesh_calc_edges.c
@@ -0,0 +1,11 @@
+#include "STRUCTS_mesh_types.h"
+#include "STRUCTS_meshdata_types.h"
+#include "STRUCTS_object_types.h"
+
+#include "LIB_map.hh"
+#include "LIB_task.hh"
+#include "LIB_threads.h"
+#include "LIB_timeit.hh"
+
+#include "KERNEL_customdata.h"
+#include "KERNEL_mesh.h"
