6f18b4dbcc006282514af15711ca33e7bc03ee24
Kielan <ki.lemons@gmail.com>
2022-12-26 08:03:34 -0700

    Update modifier.c

--- source/dune/kernel/intern/modifier.c
+++ source/dune/kernel/intern/modifier.c
@@ -1,11 +1,10 @@
-/** \file
- * \ingroup bke
+/**
  * Modifier stack implementation.
- * BKE_modifier.h contains the function prototypes for this file.
+ * KERNEL_modifier.h contains the function prototypes for this file.
  */
 
 /* Allow using deprecated functionality for .blend file I/O. */
-#define DNA_DEPRECATED_ALLOW
+#define STRUCTS_DEPRECATED_ALLOW
 
 #include <float.h>
 #include <math.h>
@@ -16,48 +15,48 @@
 
 #include "MEM_guardedalloc.h"
 
-#include "DNA_armature_types.h"
-#include "DNA_cloth_types.h"
-#include "DNA_dynamicpaint_types.h"
-#include "DNA_fluid_types.h"
-#include "DNA_gpencil_modifier_types.h"
-#include "DNA_mesh_types.h"
-#include "DNA_object_fluidsim_types.h"
-#include "DNA_object_force_types.h"
-#include "DNA_object_types.h"
-#include "DNA_scene_types.h"
-#include "DNA_screen_types.h"
+#include "STRUCTS_armature_types.h"
+#include "STRUCTS_cloth_types.h"
+#include "STRUCTS_dynamicpaint_types.h"
+#include "STRUCTS_fluid_types.h"
+#include "STRUCTS_gpencil_modifier_types.h"
+#include "STRUCTS_mesh_types.h"
+#include "STRUCTS_object_fluidsim_types.h"
+#include "STRUCTS_object_force_types.h"
+#include "STRUCTS_object_types.h"
+#include "STRUCTS_scene_types.h"
+#include "STRUCTS_screen_types.h"
 
-#include "BLI_linklist.h"
-#include "BLI_listbase.h"
-#include "BLI_path_util.h"
-#include "BLI_session_uuid.h"
-#include "BLI_string.h"
-#include "BLI_string_utils.h"
-#include "BLI_utildefines.h"
+#include "LI_linklist.h"
+#include "LI_listbase.h"
+#include "LI_path_util.h"
+#include "LI_session_uuid.h"
+#include "LI_string.h"
+#include "LI_string_utils.h"
+#include "LI_utildefines.h"
 
-#include "BLT_translation.h"
+#include "TRANSLATION_translation.h"
 
-#include "BKE_DerivedMesh.h"
-#include "BKE_appdir.h"
-#include "BKE_editmesh.h"
-#include "BKE_editmesh_cache.h"
-#include "BKE_effect.h"
-#include "BKE_fluid.h"
-#include "BKE_global.h"
-#include "BKE_gpencil_modifier.h"
-#include "BKE_idtype.h"
-#include "BKE_key.h"
-#include "BKE_lib_id.h"
-#include "BKE_lib_query.h"
-#include "BKE_mesh.h"
-#include "BKE_mesh_wrapper.h"
-#include "BKE_multires.h"
-#include "BKE_object.h"
-#include "BKE_pointcache.h"
+#include "KE_DerivedMesh.h"
+#include "KE_appdir.h"
+#include "KE_editmesh.h"
+#include "KE_editmesh_cache.h"
+#include "KE_effect.h"
+#include "KE_fluid.h"
+#include "KE_global.h"
+#include "KE_gpencil_modifier.h"
+#include "KE_idtype.h"
+#include "KE_key.h"
+#include "KE_lib_id.h"
+#include "KE_lib_query.h"
+#include "KE_mesh.h"
+#include "KE_mesh_wrapper.h"
+#include "KE_multires.h"
+#include "KE_object.h"
+#include "KE_pointcache.h"
 
-/* may move these, only for BKE_modifier_path_relbase */
-#include "BKE_main.h"
+/* may move these, only for KE_modifier_path_relbase */
+#include "KERNEL_main.h"
 /* end */
 
 #include "DEG_depsgraph.h"
@@ -65,7 +64,7 @@
 
 #include "MOD_modifiertypes.h"
 
-#include "BLO_read_write.h"
+#include "LOADER_read_write.h"
 
 #include "CLG_log.h"
 
@@ -73,7 +72,7 @@ static CLG_LogRef LOG = {"bke.modifier"};
 static ModifierTypeInfo *modifier_types[NUM_MODIFIER_TYPES] = {NULL};
 static VirtualModifierData virtualModifierCommonData;
 
-void BKE_modifier_init(void)
+void KERNEL_modifier_init(void)
 {
   ModifierData *md;
 
@@ -81,21 +80,21 @@ void BKE_modifier_init(void)
   modifier_type_init(modifier_types); /* MOD_utils.c */
 
   /* Initialize global common storage used for virtual modifier list. */
-  md = BKE_modifier_new(eModifierType_Armature);
+  md = KERNEL_modifier_new(eModifierType_Armature);
   virtualModifierCommonData.amd = *((ArmatureModifierData *)md);
-  BKE_modifier_free(md);
+  KERNEL_modifier_free(md);
 
-  md = BKE_modifier_new(eModifierType_Curve);
+  md = KERNEL_modifier_new(eModifierType_Curve);
   virtualModifierCommonData.cmd = *((CurveModifierData *)md);
-  BKE_modifier_free(md);
+  KERNEL_modifier_free(md);
 
-  md = BKE_modifier_new(eModifierType_Lattice);
+  md = KERNEL_modifier_new(eModifierType_Lattice);
   virtualModifierCommonData.lmd = *((LatticeModifierData *)md);
-  BKE_modifier_free(md);
+  KERNEL_modifier_free(md);
 
-  md = BKE_modifier_new(eModifierType_ShapeKey);
+  md = KERNEL_modifier_new(eModifierType_ShapeKey);
   virtualModifierCommonData.smd = *((ShapeKeyModifierData *)md);
-  BKE_modifier_free(md);
+  KERNEL_modifier_free(md);
 
   virtualModifierCommonData.amd.modifier.mode |= eModifierMode_Virtual;
   virtualModifierCommonData.cmd.modifier.mode |= eModifierMode_Virtual;
@@ -103,7 +102,7 @@ void BKE_modifier_init(void)
   virtualModifierCommonData.smd.modifier.mode |= eModifierMode_Virtual;
 }
 
-const ModifierTypeInfo *BKE_modifier_get_info(ModifierType type)
+const ModifierTypeInfo *KERNEL_modifier_get_info(ModifierType type)
 {
   /* type unsigned, no need to check < 0 */
   if (type < NUM_MODIFIER_TYPES && modifier_types[type] && modifier_types[type]->name[0] != '\0') {
@@ -113,15 +112,15 @@ const ModifierTypeInfo *BKE_modifier_get_info(ModifierType type)
   return NULL;
 }
 
-void BKE_modifier_type_panel_id(ModifierType type, char *r_idname)
+void KERNEL_modifier_type_panel_id(ModifierType type, char *r_idname)
 {
-  const ModifierTypeInfo *mti = BKE_modifier_get_info(type);
+  const ModifierTypeInfo *mti = KERNEL_modifier_get_info(type);
 
   strcpy(r_idname, MODIFIER_TYPE_PANEL_PREFIX);
   strcat(r_idname, mti->name);
 }
 
-void BKE_modifier_panel_expand(ModifierData *md)
+void KERNEL_modifier_panel_expand(ModifierData *md)
 {
   md->ui_expand_flag |= UI_PANEL_DATA_EXPAND_ROOT;
 }
@@ -130,11 +129,11 @@ void BKE_modifier_panel_expand(ModifierData *md)
 
 static ModifierData *modifier_allocate_and_init(int type)
 {
-  const ModifierTypeInfo *mti = BKE_modifier_get_info(type);
+  const ModifierTypeInfo *mti = KERNEL_modifier_get_info(type);
   ModifierData *md = MEM_callocN(mti->structSize, mti->structName);
 
   /* NOTE: this name must be made unique later. */
-  BLI_strncpy(md->name, DATA_(mti->name), sizeof(md->name));
+  LIB_strncpy(md->name, DATA_(mti->name), sizeof(md->name));
 
   md->type = type;
   md->mode = eModifierMode_Realtime | eModifierMode_Render;
@@ -152,11 +151,11 @@ static ModifierData *modifier_allocate_and_init(int type)
   return md;
 }
 
-ModifierData *BKE_modifier_new(int type)
+ModifierData *KERNEL_modifier_new(int type)
 {
   ModifierData *md = modifier_allocate_and_init(type);
 
-  BKE_modifier_session_uuid_generate(md);
+  KERNEL_modifier_session_uuid_generate(md);
 
   return md;
 }
@@ -172,9 +171,9 @@ static void modifier_free_data_id_us_cb(void *UNUSED(userData),
   }
 }
 
-void BKE_modifier_free_ex(ModifierData *md, const int flag)
+void KERNEL_modifier_free_ex(ModifierData *md, const int flag)
 {
-  const ModifierTypeInfo *mti = BKE_modifier_get_info(md->type);
+  const ModifierTypeInfo *mti = KERNEL_modifier_get_info(md->type);
 
   if ((flag & LIB_ID_CREATE_NO_USER_REFCOUNT) == 0) {
     if (mti->foreachIDLink) {
@@ -192,37 +191,37 @@ void BKE_modifier_free_ex(ModifierData *md, const int flag)
   MEM_freeN(md);
 }
 
-void BKE_modifier_free(ModifierData *md)
+void KERNEL_modifier_free(ModifierData *md)
 {
-  BKE_modifier_free_ex(md, 0);
+  KERNEL_modifier_free_ex(md, 0);
 }
 
-void BKE_modifier_remove_from_list(Object *ob, ModifierData *md)
+void KERNEL_modifier_remove_from_list(Object *ob, ModifierData *md)
 {
-  BLI_assert(BLI_findindex(&ob->modifiers, md) != -1);
+  LIB_assert(LIB_findindex(&ob->modifiers, md) != -1);
 
   if (md->flag & eModifierFlag_Active) {
     /* Prefer the previous modifier but use the next if this modifier is the first in the list. */
     if (md->next != NULL) {
-      BKE_object_modifier_set_active(ob, md->next);
+      KERNEL_object_modifier_set_active(ob, md->next);
     }
     else if (md->prev != NULL) {
-      BKE_object_modifier_set_active(ob, md->prev);
+      KERNEL_object_modifier_set_active(ob, md->prev);
     }
   }
 
-  BLI_remlink(&ob->modifiers, md);
+  LIB_remlink(&ob->modifiers, md);
 }
 
-void BKE_modifier_session_uuid_generate(ModifierData *md)
+void KERNEL_modifier_session_uuid_generate(ModifierData *md)
 {
-  md->session_uuid = BLI_session_uuid_generate();
+  md->session_uuid = LIB_session_uuid_generate();
 }
 
-bool BKE_modifier_unique_name(ListBase *modifiers, ModifierData *md)
+bool KERNEL_modifier_unique_name(ListBase *modifiers, ModifierData *md)
 {
   if (modifiers && md) {
-    const ModifierTypeInfo *mti = BKE_modifier_get_info(md->type);
+    const ModifierTypeInfo *mti = KERNEL_modifier_get_info(md->type);
 
     return BLI_uniquename(
         modifiers, md, DATA_(mti->name), '.', offsetof(ModifierData, name), sizeof(md->name));
