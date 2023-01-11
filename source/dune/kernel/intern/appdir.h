7fdc67a157f6c558e94fb98e507eb399d1978034
Kielan <ki.lemons@gmail.com>
2023-01-11 16:40:40 -0700

    Update appdir.h

--- source/dune/kernel/intern/appdir.h
+++ source/dune/kernel/intern/appdir.h
@@ -59,115 +59,100 @@ bool KERNEL_appdir_folder_documents(char *dir);
  * returns True if the path is valid. It doesn't create or checks format
  * if the `dune` folder exists. It does check if the parent of the path exists.
  */
-bool BKE_appdir_folder_caches(char *r_path, size_t path_len);
+bool KERNEL_appdir_folder_caches(char *r_path, size_t path_len);
 /**
- * Get a folder out of the \a folder_id presets for paths.
+ * Get a folder out of the folder_id presets for paths.
  *
- * \param subfolder: The name of a directory to check for,
+ * param subfolder: The name of a directory to check for,
  * this may contain path separators but must resolve to a directory, checked with #BLI_is_dir.
- * \return The path if found, NULL string if not.
+ * return The path if found, NULL string if not.
  */
-bool BKE_appdir_folder_id_ex(int folder_id, const char *subfolder, char *path, size_t path_len);
-const char *BKE_appdir_folder_id(int folder_id, const char *subfolder);
+bool KERNEL_appdir_folder_id_ex(int folder_id, const char *subfolder, char *path, size_t path_len);
+const char *KERNEL_appdir_folder_id(int folder_id, const char *subfolder);
 /**
  * Returns the path to a folder in the user area, creating it if it doesn't exist.
  */
-const char *BKE_appdir_folder_id_create(int folder_id, const char *subfolder);
+const char *KERNEL_appdir_folder_id_create(int folder_id, const char *subfolder);
 /**
  * Returns the path to a folder in the user area without checking that it actually exists first.
  */
-const char *BKE_appdir_folder_id_user_notest(int folder_id, const char *subfolder);
+const char *KERNEL_appdir_folder_id_user_notest(int folder_id, const char *subfolder);
 /**
  * Returns the path of the top-level version-specific local, user or system directory.
  * If check_is_dir, then the result will be NULL if the directory doesn't exist.
  */
-const char *BKE_appdir_folder_id_version(int folder_id, int version, bool check_is_dir);
+const char *KERNEL_appdir_folder_id_version(int folder_id, int version, bool check_is_dir);
 
 /**
  * Check if this is an install with user files kept together
- * with the Blender executable and its installation files.
+ * with the Dune executable and its installation files.
  */
-bool BKE_appdir_app_is_portable_install(void);
+bool KERNEL_appdir_app_is_portable_install(void);
 /**
  * Return true if templates exist
  */
-bool BKE_appdir_app_template_any(void);
-bool BKE_appdir_app_template_id_search(const char *app_template, char *path, size_t path_len);
-bool BKE_appdir_app_template_has_userpref(const char *app_template);
-void BKE_appdir_app_templates(struct ListBase *templates);
+bool KERNEL_appdir_app_template_any(void);
+bool KERNEL_appdir_app_template_id_search(const char *app_template, char *path, size_t path_len);
+bool KERNEL_appdir_app_template_has_userpref(const char *app_template);
+void KERNEL_appdir_app_templates(struct ListBase *templates);
 
 /**
  * Initialize path to program executable.
  */
-void BKE_appdir_program_path_init(const char *argv0);
+void KERNEL_appdir_program_path_init(const char *argv0);
 
 /**
  * Path to executable
  */
-const char *BKE_appdir_program_path(void);
-/**
- * Path to directory of executable
- */
-const char *BKE_appdir_program_dir(void);
+const char *KERNEL_appdir_program_path(void);
+/** Path to directory of executable **/
+const char *KERNEL_appdir_program_dir(void);
 
-/**
- * Gets a good default directory for fonts.
- */
-bool BKE_appdir_font_folder_default(char *dir);
+/** Gets a good default directory for fonts. */
+bool KERNEL_appdir_font_folder_default(char *dir);
 
-/**
- * Find Python executable.
- */
-bool BKE_appdir_program_python_search(char *fullpath,
+/** Find Python executable. */
+bool KERNEL_appdir_program_python_search(char *fullpath,
                                       size_t fullpath_len,
                                       int version_major,
                                       int version_minor);
+/** Initialize path to temporary directory. */
+void KERNEL_tempdir_init(const char *userdir);
 
-/**
- * Initialize path to temporary directory.
- */
-void BKE_tempdir_init(const char *userdir);
-
-/**
- * Path to persistent temporary directory (with trailing slash)
- */
-const char *BKE_tempdir_base(void);
-/**
- * Path to temporary directory (with trailing slash)
- */
-const char *BKE_tempdir_session(void);
-/**
- * Delete content of this instance's temp dir.
- */
-void BKE_tempdir_session_purge(void);
+/** Path to persistent temporary directory (with trailing slash) */
+const char *KERNEL_tempdir_base(void);
+/** Path to temporary directory (with trailing slash) */
+const char *KERNEL_tempdir_session(void);
+/** Delete content of this instance's temp dir. */
+void KERNEL_tempdir_session_purge(void);
 
 /* folder_id */
 enum {
   /* general, will find based on user/local/system priority */
-  BLENDER_DATAFILES = 2,
+  DUNE_DATAFILES = 2,
 
   /* user-specific */
-  BLENDER_USER_CONFIG = 31,
-  BLENDER_USER_DATAFILES = 32,
-  BLENDER_USER_SCRIPTS = 33,
-  BLENDER_USER_AUTOSAVE = 34,
+  DUNE_USER_CONFIG = 31,
+  DUNE_USER_DATAFILES = 32,
+  DUNE_USER_SCRIPTS = 33,
+  DUNE_USER_AUTOSAVE = 34,
 
   /* system */
-  BLENDER_SYSTEM_DATAFILES = 52,
-  BLENDER_SYSTEM_SCRIPTS = 53,
-  BLENDER_SYSTEM_PYTHON = 54,
+  DUNE_SYSTEM_DATAFILES = 52,
+  DUNE_SYSTEM_SCRIPTS = 53,
+  DUNE_SYSTEM_PYTHON = 54,
 };
 
-/* for BKE_appdir_folder_id_version only */
+/* for KERNEL_appdir_folder_id_version only */
 enum {
-  BLENDER_RESOURCE_PATH_USER = 0,
-  BLENDER_RESOURCE_PATH_LOCAL = 1,
-  BLENDER_RESOURCE_PATH_SYSTEM = 2,
+  DUNE_RESOURCE_PATH_USER = 0,
+  DUNE_RESOURCE_PATH_LOCAL = 1,
+  DUNE_RESOURCE_PATH_SYSTEM = 2,
 };
 
-#define BLENDER_STARTUP_FILE "startup.blend"
-#define BLENDER_USERPREF_FILE "userpref.blend"
-#define BLENDER_QUIT_FILE "quit.blend"
-#define BLENDER_BOOKMARK_FILE "bookmarks.txt"
-#define BLENDER_HISTORY_FILE "recent-files.txt"
-#define BLENDER_PLATFORM_SUPPORT_FILE "platform_support.txt"
+#define DUNE_STARTUP_FILE "startup.dune"
+#define DUNE_USERPREF_FILE "userpref.dune"
+#define DUNE_QUIT_FILE "quit.dune"
+#define DUNE_BOOKMARK_FILE "bookmarks.txt"
+#define DUNE_HISTORY_FILE "recent-files.txt"
+#define DUNE_PLATFORM_SUPPORT_FILE "platform_support.txt"
