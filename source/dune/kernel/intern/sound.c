c62d3776c808413627458294279d9c21dfd742eb
Kielan <ki.lemons@gmail.com>
2023-01-25 08:55:40 -0700

    Update sound.c

--- source/dune/kernel/intern/sound.c
+++ source/dune/kernel/intern/sound.c
@@ -3,25 +3,25 @@
 
 #include "MEM_guardedalloc.h"
 
-#include "BLI_blenlib.h"
-#include "BLI_iterator.h"
-#include "BLI_math.h"
-#include "BLI_threads.h"
+#include "LIB_dunelib.h"
+#include "LIB_iterator.h"
+#include "LIB_math.h"
+#include "LIB_threads.h"
 
-#include "BLT_translation.h"
+#include "LANG_translation.h"
 
 /* Allow using deprecated functionality for .blend file I/O. */
 #define DNA_DEPRECATED_ALLOW
 
-#include "DNA_anim_types.h"
-#include "DNA_object_types.h"
-#include "DNA_packedFile_types.h"
-#include "DNA_scene_types.h"
-#include "DNA_screen_types.h"
-#include "DNA_sequence_types.h"
-#include "DNA_sound_types.h"
-#include "DNA_speaker_types.h"
-#include "DNA_windowmanager_types.h"
+#include "structs_anim_types.h"
+#include "structs_object_types.h"
+#include "structs_packedFile_types.h"
+#include "structs_scene_types.h"
+#include "structs_screen_types.h"
+#include "structs_sequence_types.h"
+#include "structs_sound_types.h"
+#include "structs_speaker_types.h"
+#include "structs_windowmanager_types.h"
 
 #ifdef WITH_AUDASPACE
 #  include "../../../intern/audaspace/intern/AUD_Set.h"
@@ -31,67 +31,67 @@
 #  include <AUD_Special.h>
 #endif
 
-#include "BKE_bpath.h"
-#include "BKE_global.h"
-#include "BKE_idtype.h"
-#include "BKE_lib_id.h"
-#include "BKE_main.h"
-#include "BKE_packedFile.h"
-#include "BKE_scene.h"
-#include "BKE_sound.h"
+#include "KE_bpath.h"
+#include "KE_global.h"
+#include "KE_idtype.h"
+#include "KE_lib_id.h"
+#include "KE_main.h"
+#include "KE_packedFile.h"
+#include "KE_scene.h"
+#include "KE_sound.h"
 
 #include "DEG_depsgraph.h"
 #include "DEG_depsgraph_query.h"
 
-#include "BLO_read_write.h"
+#include "LOADER_read_write.h"
 
 #include "SEQ_sequencer.h"
 #include "SEQ_sound.h"
 
 static void sound_free_audio(bSound *sound);
 
-static void sound_copy_data(Main *UNUSED(bmain),
+static void sound_copy_data(Main *UNUSED(dunemain),
                             ID *id_dst,
                             const ID *id_src,
                             const int UNUSED(flag))
 {
-  bSound *sound_dst = (bSound *)id_dst;
-  const bSound *sound_src = (const bSound *)id_src;
+  duneSound *sound_dst = (duneSound *)id_dst;
+  const bSound *sound_src = (const duneSound *)id_src;
 
   sound_dst->handle = NULL;
   sound_dst->cache = NULL;
   sound_dst->waveform = NULL;
   sound_dst->playback_handle = NULL;
   sound_dst->spinlock = MEM_mallocN(sizeof(SpinLock), "sound_spinlock");
-  BLI_spin_init(sound_dst->spinlock);
+  LIB_spin_init(sound_dst->spinlock);
 
   /* Just to be sure, should not have any value actually after reading time. */
   sound_dst->ipo = NULL;
   sound_dst->newpackedfile = NULL;
 
   if (sound_src->packedfile != NULL) {
-    sound_dst->packedfile = BKE_packedfile_duplicate(sound_src->packedfile);
+    sound_dst->packedfile = KERNEL_packedfile_duplicate(sound_src->packedfile);
   }
 
-  BKE_sound_reset_runtime(sound_dst);
+  KERNEL_sound_reset_runtime(sound_dst);
 }
 
 static void sound_free_data(ID *id)
 {
-  bSound *sound = (bSound *)id;
+  bSound *sound = (duneSound *)id;
 
   /* No animation-data here. */
 
   if (sound->packedfile) {
-    BKE_packedfile_free(sound->packedfile);
+    KERNEL_packedfile_free(sound->packedfile);
     sound->packedfile = NULL;
   }
 
   sound_free_audio(sound);
-  BKE_sound_free_waveform(sound);
+  KERNEL_sound_free_waveform(sound);
 
   if (sound->spinlock) {
-    BLI_spin_end(sound->spinlock);
+    LIB_spin_end(sound->spinlock);
     MEM_freeN(sound->spinlock);
     sound->spinlock = NULL;
   }
@@ -101,31 +101,31 @@ static void sound_foreach_cache(ID *id,
                                 IDTypeForeachCacheFunctionCallback function_callback,
                                 void *user_data)
 {
-  bSound *sound = (bSound *)id;
+  duneSound *sound = (duneSound *)id;
   IDCacheKey key = {
       .id_session_uuid = id->session_uuid,
-      .offset_in_ID = offsetof(bSound, waveform),
+      .offset_in_ID = offsetof(duneSound, waveform),
       .cache_v = sound->waveform,
   };
 
   function_callback(id, &key, &sound->waveform, 0, user_data);
 }
 
-static void sound_foreach_path(ID *id, BPathForeachPathData *bpath_data)
+static void sound_foreach_path(ID *id, DunePathForeachPathData *dunepath_data)
 {
-  bSound *sound = (bSound *)id;
-  if (sound->packedfile != NULL && (bpath_data->flag & BKE_BPATH_FOREACH_PATH_SKIP_PACKED) != 0) {
+  duneSound *sound = (duneSound *)id;
+  if (sound->packedfile != NULL && (dunepath_data->flag & KERNEL_DUNEPATH_FOREACH_PATH_SKIP_PACKED) != 0) {
     return;
   }
 
   /* FIXME: This does not check for empty path... */
-  BKE_bpath_foreach_path_fixed_process(bpath_data, sound->filepath);
+  KERNEL_dunepath_foreach_path_fixed_process(dunepath_data, sound->filepath);
 }
 
-static void sound_blend_write(BlendWriter *writer, ID *id, const void *id_address)
+static void sound_dune_write(DuneWriter *writer, ID *id, const void *id_address)
 {
-  bSound *sound = (bSound *)id;
-  const bool is_undo = BLO_write_is_undo(writer);
+  duneSound *sound = (duneSound *)id;
+  const bool is_undo = LOADER_write_is_undo(writer);
 
   /* Clean up, important in undo case to reduce false detection of changed datablocks. */
   sound->tags = 0;
@@ -139,8 +139,8 @@ static void sound_blend_write(BlendWriter *writer, ID *id, const void *id_addres
   }
 
   /* write LibData */
-  BLO_write_id_struct(writer, bSound, id_address, &sound->id);
-  BKE_id_blend_write(writer, &sound->id);
+  LOADER_write_id_struct(writer, duneSound, id_address, &sound->id);
+  KERNEL_id_dune_write(writer, &sound->id);
 
-  BKE_packedfile_blend_write(writer, sound->packedfile);
+  KERNEL_packedfile_dune_write(writer, sound->packedfile);
 }
