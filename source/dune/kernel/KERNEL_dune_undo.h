0e8aa1e6f0b5d29384e8ad2d1896a68c449b2ba5
Kielan <ki.lemons@gmail.com>
2023-01-28 00:20:15 -0700

    Update KERNEL_dune_undo.h

--- source/dune/kernel/KERNEL_dune_undo.h
+++ source/dune/kernel/KERNEL_dune_undo.h
@@ -6,12 +6,12 @@ struct bContext;
 	
 enum eUndoStepDir;
 
-#define KERNEL_UNDO_STR_MAX 64
+#define DUNE_UNDO_STR_MAX 64
 
-struct MemFileUndoData *KERNEL_memfile_undo_encode(struct Main *bmain,
+struct MemFileUndoData *DUNE_memfile_undo_encode(struct Main *duneMain,
                                                 struct MemFileUndoData *mfu_prev);
-bool KERNEL_memfile_undo_decode(struct MemFileUndoData *mfu,
+bool DUNE_memfile_undo_decode(struct MemFileUndoData *mfu,
                              enum eUndoStepDir undo_direction,
-                             bool use_old_bmain_data,
-                             struct bContext *C);
-void KERNEL_memfile_undo_free(struct MemFileUndoData *mfu);
+                             bool use_old_duneMain_data,
+                             struct duneContext *C);
+void DUNE_memfile_undo_free(struct MemFileUndoData *mfu);
