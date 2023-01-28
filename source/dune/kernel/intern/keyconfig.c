ce08a57be50d54ba31683db9f5f81f0699928f73
Kielan <ki.lemons@gmail.com>
2023-01-28 01:39:56 -0700

    Update keyconfig.c

--- source/dune/kernel/intern/keyconfig.c
+++ source/dune/kernel/intern/keyconfig.c
@@ -2,36 +2,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "RNA_types.h"
+#include "API_types.h"
 
-#include "BLI_ghash.h"
-#include "BLI_listbase.h"
-#include "BLI_string.h"
-#include "BLI_utildefines.h"
+#include "LIB_ghash.h"
+#include "LIB_listbase.h"
+#include "LIB_string.h"
+#include "LIB_utildefines.h"
 
-#include "DNA_listBase.h"
-#include "DNA_userdef_types.h"
-#include "DNA_windowmanager_types.h"
+#include "TYPES_listBase.h"
+#include "TYPES_userdef.h"
+#include "TYPES_windowmanager.h"
 
-#include "BKE_idprop.h"
-#include "BKE_keyconfig.h" /* own include */
+#include "DUNE_idprop.h"
+#include "DUNE_keyconfig.h" /* own include */
 
 #include "MEM_guardedalloc.h"
 
 /* -------------------------------------------------------------------- */
-/** \name Key-Config Preference (UserDef) API
+/** Key-Config Preference (UserDef) API
  *
- * \see #BKE_addon_pref_type_init for logic this is bases on.
- * \{ */
+ * DUNE_addon_pref_type_init for logic this is bases on.
+ **/
 
-wmKeyConfigPref *BKE_keyconfig_pref_ensure(UserDef *userdef, const char *kc_idname)
+wmKeyConfigPref *DUNE_keyconfig_pref_ensure(UserDef *userdef, const char *kc_idname)
 {
-  wmKeyConfigPref *kpt = BLI_findstring(
+  wmKeyConfigPref *kpt = LIB_findstring(
       &userdef->user_keyconfig_prefs, kc_idname, offsetof(wmKeyConfigPref, idname));
   if (kpt == NULL) {
     kpt = MEM_callocN(sizeof(*kpt), __func__);
     STRNCPY(kpt->idname, kc_idname);
-    BLI_addtail(&userdef->user_keyconfig_prefs, kpt);
+    LIB_addtail(&userdef->user_keyconfig_prefs, kpt);
   }
   if (kpt->prop == NULL) {
     IDPropertyTemplate val = {0};
@@ -40,22 +40,20 @@ wmKeyConfigPref *BKE_keyconfig_pref_ensure(UserDef *userdef, const char *kc_idna
   return kpt;
 }
 
-/** \} */
-
 /* -------------------------------------------------------------------- */
-/** \name Key-Config Preference (RNA Type) API
+/** Key-Config Preference (RNA Type) API
  *
- * \see #BKE_addon_pref_type_init for logic this is bases on.
- * \{ */
+ * see DUNE_addon_pref_type_init for logic this is bases on.
+ **/
 
 static GHash *global_keyconfigpreftype_hash = NULL;
 
-wmKeyConfigPrefType_Runtime *BKE_keyconfig_pref_type_find(const char *idname, bool quiet)
+wmKeyConfigPrefType_Runtime *DUNE_keyconfig_pref_type_find(const char *idname, bool quiet)
 {
   if (idname[0]) {
     wmKeyConfigPrefType_Runtime *kpt_rt;
 
-    kpt_rt = BLI_ghash_lookup(global_keyconfigpreftype_hash, idname);
+    kpt_rt = LIB_ghash_lookup(global_keyconfigpreftype_hash, idname);
     if (kpt_rt) {
       return kpt_rt;
     }
@@ -73,37 +71,34 @@ wmKeyConfigPrefType_Runtime *BKE_keyconfig_pref_type_find(const char *idname, bo
   return NULL;
 }
 
-void BKE_keyconfig_pref_type_add(wmKeyConfigPrefType_Runtime *kpt_rt)
+void DUNE_keyconfig_pref_type_add(wmKeyConfigPrefType_Runtime *kpt_rt)
 {
-  BLI_ghash_insert(global_keyconfigpreftype_hash, kpt_rt->idname, kpt_rt);
+  LIB_ghash_insert(global_keyconfigpreftype_hash, kpt_rt->idname, kpt_rt);
 }
 
-void BKE_keyconfig_pref_type_remove(const wmKeyConfigPrefType_Runtime *kpt_rt)
+void DUNE_keyconfig_pref_type_remove(const wmKeyConfigPrefType_Runtime *kpt_rt)
 {
-  BLI_ghash_remove(global_keyconfigpreftype_hash, kpt_rt->idname, NULL, MEM_freeN);
+  LIB_ghash_remove(global_keyconfigpreftype_hash, kpt_rt->idname, NULL, MEM_freeN);
 }
 
-void BKE_keyconfig_pref_type_init(void)
+void DUNE_keyconfig_pref_type_init(void)
 {
-  BLI_assert(global_keyconfigpreftype_hash == NULL);
+  LIB_assert(global_keyconfigpreftype_hash == NULL);
   global_keyconfigpreftype_hash = BLI_ghash_str_new(__func__);
 }
 
-void BKE_keyconfig_pref_type_free(void)
+void DUNE_keyconfig_pref_type_free(void)
 {
-  BLI_ghash_free(global_keyconfigpreftype_hash, NULL, MEM_freeN);
+  LIB_ghash_free(global_keyconfigpreftype_hash, NULL, MEM_freeN);
   global_keyconfigpreftype_hash = NULL;
 }
 
-/** \} */
-
 /* -------------------------------------------------------------------- */
-/** \name Key-Config Versioning
- * \{ */
+/** Key-Config Versioning **/
 
-void BKE_keyconfig_pref_set_select_mouse(UserDef *userdef, int value, bool override)
+void DUNE_keyconfig_pref_set_select_mouse(UserDef *userdef, int value, bool override)
 {
-  wmKeyConfigPref *kpt = BKE_keyconfig_pref_ensure(userdef, WM_KEYCONFIG_STR_DEFAULT);
+  wmKeyConfigPref *kpt = DUNE_keyconfig_pref_ensure(userdef, WM_KEYCONFIG_STR_DEFAULT);
   IDProperty *idprop = IDP_GetPropertyFromGroup(kpt->prop, "select_mouse");
   if (!idprop) {
     IDPropertyTemplate tmp = {
@@ -136,7 +131,7 @@ static void keymap_diff_item_free(wmKeyMapDiffItem *kmdi)
   MEM_freeN(kmdi);
 }
 
-void BKE_keyconfig_keymap_filter_item(wmKeyMap *keymap,
+void DUNE_keyconfig_keymap_filter_item(wmKeyMap *keymap,
                                       const struct wmKeyConfigFilterItemParams *params,
                                       bool (*filter_fn)(wmKeyMapItem *kmi, void *user_data),
                                       void *user_data)
@@ -163,7 +158,7 @@ void BKE_keyconfig_keymap_filter_item(wmKeyMap *keymap,
       }
 
       if (remove) {
-        BLI_remlink(&keymap->diff_items, kmdi);
+        LIB_remlink(&keymap->diff_items, kmdi);
         keymap_diff_item_free(kmdi);
       }
     }
@@ -173,14 +168,14 @@ void BKE_keyconfig_keymap_filter_item(wmKeyMap *keymap,
     for (wmKeyMapItem *kmi = keymap->items.first, *kmi_next; kmi; kmi = kmi_next) {
       kmi_next = kmi->next;
       if (filter_fn(kmi, user_data)) {
-        BLI_remlink(&keymap->items, kmi);
+        LIB_remlink(&keymap->items, kmi);
         keymap_item_free(kmi);
       }
     }
   }
 }
 
-void KERNEL_keyconfig_pref_filter_items(struct UserDef *userdef,
+void DUNE_keyconfig_pref_filter_items(struct UserDef *userdef,
                                      const struct wmKeyConfigFilterItemParams *params,
                                      bool (*filter_fn)(wmKeyMapItem *kmi, void *user_data),
                                      void *user_data)
