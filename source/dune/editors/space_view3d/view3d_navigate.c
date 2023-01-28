b464c9dc870a3c549cbb80426307a24e56877ccc
Kielan <ki.lemons@gmail.com>
2023-01-28 12:39:35 -0700

    Update view3d_navigate.c

--- source/dune/editors/space_view3d/view3d_navigate.c
+++ source/dune/editors/space_view3d/view3d_navigate.c
@@ -3,20 +3,20 @@
 
 #include "MEM_guardedalloc.h"
 
-#include "BLI_math.h"
-#include "BLI_rect.h"
+#include "LIB_math.h"
+#include "LIB_rect.h"
 
-#include "BLT_translation.h"
+#include "LANG_translation.h"
 
-#include "BKE_armature.h"
-#include "BKE_context.h"
-#include "BKE_gpencil_geom.h"
-#include "BKE_layer.h"
-#include "BKE_object.h"
-#include "BKE_paint.h"
-#include "BKE_scene.h"
-#include "BKE_screen.h"
-#include "BKE_vfont.h"
+#include "DUNE_armature.h"
+#include "DUNE_context.h"
+#include "DUNE_gpencil_geom.h"
+#include "DUNE_layer.h"
+#include "DUNE_object.h"
+#include "DUNE_paint.h"
+#include "DUNE_scene.h"
+#include "DUNE_screen.h"
+#include "DUNE_vfont.h"
 
 #include "DEG_depsgraph_query.h"
 
@@ -28,8 +28,8 @@
 #include "WM_api.h"
 #include "WM_message.h"
 
-#include "RNA_access.h"
-#include "RNA_define.h"
+#include "API_access.h"
+#include "API_define.h"
 
 #include "UI_resources.h"
 
@@ -38,10 +38,9 @@
 #include "view3d_navigate.h" /* own include */
 
 /* -------------------------------------------------------------------- */
-/** \name Navigation Polls
- * \{ */
+/** Navigation Polls **/
 
-static bool view3d_navigation_poll_impl(bContext *C, const char viewlock)
+static bool view3d_navigation_poll_impl(duneContext *C, const char viewlock)
 {
   if (!ED_operator_region_view3d_active(C)) {
     return false;
@@ -51,17 +50,17 @@ static bool view3d_navigation_poll_impl(bContext *C, const char viewlock)
   return !(RV3D_LOCK_FLAGS(rv3d) & viewlock);
 }
 
-bool view3d_location_poll(bContext *C)
+bool view3d_location_poll(duneContext *C)
 {
   return view3d_navigation_poll_impl(C, RV3D_LOCK_LOCATION);
 }
 
-bool view3d_rotation_poll(bContext *C)
+bool view3d_rotation_poll(duneContext *C)
 {
   return view3d_navigation_poll_impl(C, RV3D_LOCK_ROTATION);
 }
 
-bool view3d_zoom_or_dolly_poll(bContext *C)
+bool view3d_zoom_or_dolly_poll(dContext *C)
 {
   return view3d_navigation_poll_impl(C, RV3D_LOCK_ZOOM_AND_DOLLY);
 }
