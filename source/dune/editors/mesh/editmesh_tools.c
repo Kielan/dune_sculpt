ce43b4763262468e03c98e55bbcf935df9443dd7
Kielan <ki.lemons@gmail.com>
2023-01-29 22:41:28 -0700

    Create editmesh_tools.c

---
+++ source/dune/editors/mesh/editmesh_tools.c
@@ -0,0 +1,341 @@
+#include <stddef.h>
+
+#include "MEM_guardedalloc.h"
+
+#include "DNA_key_types.h"
+#include "DNA_material_types.h"
+#include "DNA_mesh_types.h"
+#include "DNA_meshdata_types.h"
+#include "DNA_modifier_types.h"
+#include "DNA_object_types.h"
+#include "DNA_scene_types.h"
+
+#include "BLI_bitmap.h"
+#include "BLI_heap_simple.h"
+#include "BLI_linklist.h"
+#include "BLI_linklist_stack.h"
+#include "BLI_listbase.h"
+#include "BLI_math.h"
+#include "BLI_rand.h"
+#include "BLI_sort_utils.h"
+#include "BLI_string.h"
+
+#include "BKE_context.h"
+#include "BKE_customdata.h"
+#include "BKE_deform.h"
+#include "BKE_editmesh.h"
+#include "BKE_key.h"
+#include "BKE_layer.h"
+#include "BKE_lib_id.h"
+#include "BKE_main.h"
+#include "BKE_material.h"
+#include "BKE_mesh.h"
+#include "BKE_report.h"
+#include "BKE_texture.h"
+
+#include "DEG_depsgraph.h"
+#include "DEG_depsgraph_build.h"
+
+#include "BLT_translation.h"
+
+#include "RNA_access.h"
+#include "RNA_define.h"
+#include "RNA_enum_types.h"
+#include "RNA_prototypes.h"
+
+#include "WM_api.h"
+#include "WM_types.h"
+
+#include "ED_mesh.h"
+#include "ED_object.h"
+#include "ED_outliner.h"
+#include "ED_screen.h"
+#include "ED_select_utils.h"
+#include "ED_transform.h"
+#include "ED_uvedit.h"
+#include "ED_view3d.h"
+
+#include "RE_texture.h"
+
+#include "UI_interface.h"
+#include "UI_resources.h"
+
+#include "mesh_intern.h" /* own include */
+
+#include "bmesh_tools.h"
+
+#define USE_FACE_CREATE_SEL_EXTEND
+
+/* -------------------------------------------------------------------- */
+/** \name Subdivide Operator
+ * \{ */
+
+static int edbm_subdivide_exec(bContext *C, wmOperator *op)
+{
+  const int cuts = RNA_int_get(op->ptr, "number_cuts");
+  const float smooth = RNA_float_get(op->ptr, "smoothness");
+  const float fractal = RNA_float_get(op->ptr, "fractal") / 2.5f;
+  const float along_normal = RNA_float_get(op->ptr, "fractal_along_normal");
+  const bool use_quad_tri = !RNA_boolean_get(op->ptr, "ngon");
+
+  if (use_quad_tri && RNA_enum_get(op->ptr, "quadcorner") == SUBD_CORNER_STRAIGHT_CUT) {
+    RNA_enum_set(op->ptr, "quadcorner", SUBD_CORNER_INNERVERT);
+  }
+  const int quad_corner_type = RNA_enum_get(op->ptr, "quadcorner");
+  const int seed = RNA_int_get(op->ptr, "seed");
+
+  ViewLayer *view_layer = CTX_data_view_layer(C);
+  uint objects_len = 0;
+  Object **objects = BKE_view_layer_array_from_objects_in_edit_mode_unique_data(
+      view_layer, CTX_wm_view3d(C), &objects_len);
+
+  for (uint ob_index = 0; ob_index < objects_len; ob_index++) {
+    Object *obedit = objects[ob_index];
+    BMEditMesh *em = BKE_editmesh_from_object(obedit);
+
+    if (!(em->bm->totedgesel || em->bm->totfacesel)) {
+      continue;
+    }
+
+    BM_mesh_esubdivide(em->bm,
+                       BM_ELEM_SELECT,
+                       smooth,
+                       SUBD_FALLOFF_LIN,
+                       false,
+                       fractal,
+                       along_normal,
+                       cuts,
+                       SUBDIV_SELECT_ORIG,
+                       quad_corner_type,
+                       use_quad_tri,
+                       true,
+                       false,
+                       seed);
+
+    EDBM_update(obedit->data,
+                &(const struct EDBMUpdate_Params){
+                    .calc_looptri = true,
+                    .calc_normals = false,
+                    .is_destructive = true,
+                });
+  }
+
+  MEM_freeN(objects);
+
+  return OPERATOR_FINISHED;
+}
+
+/* NOTE: these values must match delete_mesh() event values. */
+static const EnumPropertyItem prop_mesh_cornervert_types[] = {
+    {SUBD_CORNER_INNERVERT, "INNERVERT", 0, "Inner Vert", ""},
+    {SUBD_CORNER_PATH, "PATH", 0, "Path", ""},
+    {SUBD_CORNER_STRAIGHT_CUT, "STRAIGHT_CUT", 0, "Straight Cut", ""},
+    {SUBD_CORNER_FAN, "FAN", 0, "Fan", ""},
+    {0, NULL, 0, NULL, NULL},
+};
+
+void MESH_OT_subdivide(wmOperatorType *ot)
+{
+  PropertyRNA *prop;
+
+  /* identifiers */
+  ot->name = "Subdivide";
+  ot->description = "Subdivide selected edges";
+  ot->idname = "MESH_OT_subdivide";
+
+  /* api callbacks */
+  ot->exec = edbm_subdivide_exec;
+  ot->poll = ED_operator_editmesh;
+
+  /* flags */
+  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
+
+  /* properties */
+  prop = RNA_def_int(ot->srna, "number_cuts", 1, 1, 100, "Number of Cuts", "", 1, 10);
+  /* avoid re-using last var because it can cause
+   * _very_ high poly meshes and annoy users (or worse crash) */
+  RNA_def_property_flag(prop, PROP_SKIP_SAVE);
+
+  RNA_def_float(
+      ot->srna, "smoothness", 0.0f, 0.0f, 1e3f, "Smoothness", "Smoothness factor", 0.0f, 1.0f);
+
+  WM_operatortype_props_advanced_begin(ot);
+
+  RNA_def_boolean(ot->srna,
+                  "ngon",
+                  true,
+                  "Create N-Gons",
+                  "When disabled, newly created faces are limited to 3 and 4 sided faces");
+  RNA_def_enum(
+      ot->srna,
+      "quadcorner",
+      prop_mesh_cornervert_types,
+      SUBD_CORNER_STRAIGHT_CUT,
+      "Quad Corner Type",
+      "How to subdivide quad corners (anything other than Straight Cut will prevent n-gons)");
+
+  RNA_def_float(ot->srna,
+                "fractal",
+                0.0f,
+                0.0f,
+                1e6f,
+                "Fractal",
+                "Fractal randomness factor",
+                0.0f,
+                1000.0f);
+  RNA_def_float(ot->srna,
+                "fractal_along_normal",
+                0.0f,
+                0.0f,
+                1.0f,
+                "Along Normal",
+                "Apply fractal displacement along normal only",
+                0.0f,
+                1.0f);
+  RNA_def_int(ot->srna,
+              "seed",
+              0,
+              0,
+              INT_MAX,
+              "Random Seed",
+              "Seed for the random number generator",
+              0,
+              255);
+}
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Edge Ring Subdivide Operator
+ *
+ * Bridge code shares props.
+ *
+ * \{ */
+
+struct EdgeRingOpSubdProps {
+  int interp_mode;
+  int cuts;
+  float smooth;
+
+  int profile_shape;
+  float profile_shape_factor;
+};
+
+static void mesh_operator_edgering_props(wmOperatorType *ot,
+                                         const int cuts_min,
+                                         const int cuts_default)
+{
+  /* NOTE: these values must match delete_mesh() event values. */
+  static const EnumPropertyItem prop_subd_edgering_types[] = {
+      {SUBD_RING_INTERP_LINEAR, "LINEAR", 0, "Linear", ""},
+      {SUBD_RING_INTERP_PATH, "PATH", 0, "Blend Path", ""},
+      {SUBD_RING_INTERP_SURF, "SURFACE", 0, "Blend Surface", ""},
+      {0, NULL, 0, NULL, NULL},
+  };
+
+  PropertyRNA *prop;
+
+  prop = RNA_def_int(
+      ot->srna, "number_cuts", cuts_default, 0, 1000, "Number of Cuts", "", cuts_min, 64);
+  RNA_def_property_flag(prop, PROP_SKIP_SAVE);
+
+  RNA_def_enum(ot->srna,
+               "interpolation",
+               prop_subd_edgering_types,
+               SUBD_RING_INTERP_PATH,
+               "Interpolation",
+               "Interpolation method");
+
+  RNA_def_float(
+      ot->srna, "smoothness", 1.0f, 0.0f, 1e3f, "Smoothness", "Smoothness factor", 0.0f, 2.0f);
+
+  /* profile-shape */
+  RNA_def_float(ot->srna,
+                "profile_shape_factor",
+                0.0f,
+                -1e3f,
+                1e3f,
+                "Profile Factor",
+                "How much intermediary new edges are shrunk/expanded",
+                -2.0f,
+                2.0f);
+
+  prop = RNA_def_property(ot->srna, "profile_shape", PROP_ENUM, PROP_NONE);
+  RNA_def_property_enum_items(prop, rna_enum_proportional_falloff_curve_only_items);
+  RNA_def_property_enum_default(prop, PROP_SMOOTH);
+  RNA_def_property_ui_text(prop, "Profile Shape", "Shape of the profile");
+  RNA_def_property_translation_context(prop,
+                                       BLT_I18NCONTEXT_ID_CURVE_LEGACY); /* Abusing id_curve :/ */
+}
+
+static void mesh_operator_edgering_props_get(wmOperator *op, struct EdgeRingOpSubdProps *op_props)
+{
+  op_props->interp_mode = RNA_enum_get(op->ptr, "interpolation");
+  op_props->cuts = RNA_int_get(op->ptr, "number_cuts");
+  op_props->smooth = RNA_float_get(op->ptr, "smoothness");
+
+  op_props->profile_shape = RNA_enum_get(op->ptr, "profile_shape");
+  op_props->profile_shape_factor = RNA_float_get(op->ptr, "profile_shape_factor");
+}
+
+static int edbm_subdivide_edge_ring_exec(bContext *C, wmOperator *op)
+{
+
+  ViewLayer *view_layer = CTX_data_view_layer(C);
+  uint objects_len = 0;
+  Object **objects = BKE_view_layer_array_from_objects_in_edit_mode_unique_data(
+      view_layer, CTX_wm_view3d(C), &objects_len);
+  struct EdgeRingOpSubdProps op_props;
+
+  mesh_operator_edgering_props_get(op, &op_props);
+
+  for (uint ob_index = 0; ob_index < objects_len; ob_index++) {
+    Object *obedit = objects[ob_index];
+    BMEditMesh *em = BKE_editmesh_from_object(obedit);
+
+    if (em->bm->totedgesel == 0) {
+      continue;
+    }
+
+    if (!EDBM_op_callf(em,
+                       op,
+                       "subdivide_edgering edges=%he interp_mode=%i cuts=%i smooth=%f "
+                       "profile_shape=%i profile_shape_factor=%f",
+                       BM_ELEM_SELECT,
+                       op_props.interp_mode,
+                       op_props.cuts,
+                       op_props.smooth,
+                       op_props.profile_shape,
+                       op_props.profile_shape_factor)) {
+      continue;
+    }
+
+    EDBM_update(obedit->data,
+                &(const struct EDBMUpdate_Params){
+                    .calc_looptri = true,
+                    .calc_normals = false,
+                    .is_destructive = true,
+                });
+  }
+
+  MEM_freeN(objects);
+  return OPERATOR_FINISHED;
+}
+
+void MESH_OT_subdivide_edgering(wmOperatorType *ot)
+{
+  /* identifiers */
+  ot->name = "Subdivide Edge-Ring";
+  ot->description = "Subdivide perpendicular edges to the selected edge-ring";
+  ot->idname = "MESH_OT_subdivide_edgering";
+
+  /* api callbacks */
+  ot->exec = edbm_subdivide_edge_ring_exec;
+  ot->poll = ED_operator_editmesh;
+
+  /* flags */
+  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
+
+  /* properties */
+  mesh_operator_edgering_props(ot, 1, 10);
+}
