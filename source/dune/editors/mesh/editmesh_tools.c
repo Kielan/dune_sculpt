051f6d415b6c5c57adb867ab06161e1d37a4b3ef
Kielan <ki.lemons@gmail.com>
2023-01-31 07:57:10 -0700

    Update editmesh_tools.c

--- source/dune/editors/mesh/editmesh_tools.c
+++ source/dune/editors/mesh/editmesh_tools.c
@@ -1970,3 +1970,174 @@ void MESH_OT_edge_split(wmOperatorType *ot)
   ot->prop = RNA_def_enum(
       ot->srna, "type", merge_type_items, BM_EDGE, "Type", "Method to use for splitting");
 }
+
+/* -------------------------------------------------------------------- */
+/** \name Duplicate Operator
+ * \{ */
+
+static int edbm_duplicate_exec(bContext *C, wmOperator *op)
+{
+  ViewLayer *view_layer = CTX_data_view_layer(C);
+  uint objects_len = 0;
+  Object **objects = BKE_view_layer_array_from_objects_in_edit_mode_unique_data(
+      view_layer, CTX_wm_view3d(C), &objects_len);
+
+  for (uint ob_index = 0; ob_index < objects_len; ob_index++) {
+    Object *obedit = objects[ob_index];
+    BMEditMesh *em = BKE_editmesh_from_object(obedit);
+    if (em->bm->totvertsel == 0) {
+      continue;
+    }
+
+    BMOperator bmop;
+    BMesh *bm = em->bm;
+
+    EDBM_op_init(em,
+                 &bmop,
+                 op,
+                 "duplicate geom=%hvef use_select_history=%b use_edge_flip_from_face=%b",
+                 BM_ELEM_SELECT,
+                 true,
+                 true);
+
+    BMO_op_exec(bm, &bmop);
+
+    /* de-select all would clear otherwise */
+    BM_SELECT_HISTORY_BACKUP(bm);
+
+    EDBM_flag_disable_all(em, BM_ELEM_SELECT);
+
+    BMO_slot_buffer_hflag_enable(
+        bm, bmop.slots_out, "geom.out", BM_ALL_NOLOOP, BM_ELEM_SELECT, true);
+
+    /* rebuild editselection */
+    BM_SELECT_HISTORY_RESTORE(bm);
+
+    if (!EDBM_op_finish(em, &bmop, op, true)) {
+      continue;
+    }
+    EDBM_update(obedit->data,
+                &(const struct EDBMUpdate_Params){
+                    .calc_looptri = true,
+                    .calc_normals = false,
+                    .is_destructive = true,
+                });
+  }
+  MEM_freeN(objects);
+
+  return OPERATOR_FINISHED;
+}
+
+static int edbm_duplicate_invoke(bContext *C, wmOperator *op, const wmEvent *UNUSED(event))
+{
+  WM_cursor_wait(true);
+  edbm_duplicate_exec(C, op);
+  WM_cursor_wait(false);
+
+  return OPERATOR_FINISHED;
+}
+
+void MESH_OT_duplicate(wmOperatorType *ot)
+{
+  /* identifiers */
+  ot->name = "Duplicate";
+  ot->description = "Duplicate selected vertices, edges or faces";
+  ot->idname = "MESH_OT_duplicate";
+
+  /* api callbacks */
+  ot->invoke = edbm_duplicate_invoke;
+  ot->exec = edbm_duplicate_exec;
+
+  ot->poll = ED_operator_editmesh;
+
+  /* to give to transform */
+  RNA_def_int(ot->srna, "mode", TFM_TRANSLATION, 0, INT_MAX, "Mode", "", 0, INT_MAX);
+}
+
+static BMLoopNorEditDataArray *flip_custom_normals_init_data(BMesh *bm)
+{
+  BMLoopNorEditDataArray *lnors_ed_arr = NULL;
+  if (CustomData_has_layer(&bm->ldata, CD_CUSTOMLOOPNORMAL)) {
+    /* The mesh has custom normal data, update these too.
+     * Otherwise they will be left in a mangled state.
+     */
+    BM_lnorspace_update(bm);
+    lnors_ed_arr = BM_loop_normal_editdata_array_init(bm, true);
+  }
+
+  return lnors_ed_arr;
+}
+
+static bool flip_custom_normals(BMesh *bm, BMLoopNorEditDataArray *lnors_ed_arr)
+{
+  if (!lnors_ed_arr) {
+    return false;
+  }
+
+  if (lnors_ed_arr->totloop == 0) {
+    /* No loops normals to flip, exit early! */
+    return false;
+  }
+
+  bm->spacearr_dirty |= BM_SPACEARR_DIRTY_ALL;
+  BM_lnorspace_update(bm);
+
+  /* We need to recreate the custom normal array because the clnors_data will
+   * be mangled because we swapped the loops around when we flipped the faces. */
+  BMLoopNorEditDataArray *lnors_ed_arr_new_full = BM_loop_normal_editdata_array_init(bm, true);
+
+  {
+    /* We need to recalculate all loop normals in the affected area. Even the ones that are not
+     * going to be flipped because the clnors data is mangled. */
+
+    BMLoopNorEditData *lnor_ed_new_full = lnors_ed_arr_new_full->lnor_editdata;
+    for (int i = 0; i < lnors_ed_arr_new_full->totloop; i++, lnor_ed_new_full++) {
+
+      BMLoopNorEditData *lnor_ed =
+          lnors_ed_arr->lidx_to_lnor_editdata[lnor_ed_new_full->loop_index];
+
+      BLI_assert(lnor_ed != NULL);
+
+      BKE_lnor_space_custom_normal_to_data(
+          bm->lnor_spacearr->lspacearr[lnor_ed_new_full->loop_index],
+          lnor_ed->nloc,
+          lnor_ed_new_full->clnors_data);
+    }
+  }
+
+  BMFace *f;
+  BMLoop *l, *l_start;
+  BMIter iter_f;
+  BM_ITER_MESH (f, &iter_f, bm, BM_FACES_OF_MESH) {
+    /* Flip all the custom loop normals on the selected faces. */
+    if (!BM_elem_flag_test(f, BM_ELEM_SELECT)) {
+      continue;
+    }
+
+    /* Because the winding has changed, we need to go the reverse way around the face to get the
+     * correct placement of the normals. However we need to derive the old loop index to get the
+     * correct data. Note that the first loop index is the same though. So the loop starts and ends
+     * in the same place as before the flip.
+     */
+
+    l_start = l = BM_FACE_FIRST_LOOP(f);
+    int old_index = BM_elem_index_get(l);
+    do {
+      int loop_index = BM_elem_index_get(l);
+
+      BMLoopNorEditData *lnor_ed = lnors_ed_arr->lidx_to_lnor_editdata[old_index];
+      BMLoopNorEditData *lnor_ed_new = lnors_ed_arr_new_full->lidx_to_lnor_editdata[loop_index];
+      BLI_assert(lnor_ed != NULL && lnor_ed_new != NULL);
+
+      negate_v3(lnor_ed->nloc);
+
+      BKE_lnor_space_custom_normal_to_data(
+          bm->lnor_spacearr->lspacearr[loop_index], lnor_ed->nloc, lnor_ed_new->clnors_data);
+
+      old_index++;
+      l = l->prev;
+    } while (l != l_start);
+  }
+  BM_loop_normal_editdata_array_free(lnors_ed_arr_new_full);
+  return true;
+}
