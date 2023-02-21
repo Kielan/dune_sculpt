cfc1c7b1422070bab3ff7f24953a4758cfc8b4c1
Kielan <ki.lemons@gmail.com>
2023-02-20 19:19:47 -0700

    Update dpen_undo.c

--- source/dune/editors/dpencil/dpen_undo.c
+++ source/dune/editors/dpencil/dpen_undo.c
@@ -3,119 +3,119 @@
 
 #include "MEM_guardedalloc.h"
 
-#include "DNA_gpencil_types.h"
-#include "DNA_listBase.h"
-#include "DNA_object_types.h"
-#include "DNA_windowmanager_types.h"
+#include "types_dpen.h"
+#include "types_listBase.h"
+#include "types_object.h"
+#include "types_windowmanager.h"
 
-#include "BLI_listbase.h"
+#include "lib_listbase.h"
 
-#include "BKE_blender_undo.h"
-#include "BKE_context.h"
-#include "BKE_gpencil.h"
-#include "BKE_undo_system.h"
+#include "dune_undo.h"
+#include "dune_context.h"
+#include "dune_dpen.h"
+#include "dune_undo_system.h"
 
-#include "ED_gpencil.h"
+#include "ed_dpen.h"
 
-#include "WM_api.h"
-#include "WM_types.h"
+#include "wm_api.h"
+#include "wm_types.h"
 
 #include "DEG_depsgraph.h"
 
-#include "gpencil_intern.h"
+#include "dpen_intern.h"
 
-typedef struct bGPundonode {
-  struct bGPundonode *next, *prev;
+typedef struct DPenUndoNode {
+  struct DPenUndoNode *next, *prev;
 
-  char name[BKE_UNDO_STR_MAX];
-  struct bGPdata *gpd;
-} bGPundonode;
+  char name[NODE_UNDO_STR_MAX];
+  struct DPenData *dpd;
+} DPenUndoNode;
 
 static ListBase undo_nodes = {NULL, NULL};
-static bGPundonode *cur_node = NULL;
+static DPenUndoNode *cur_node = NULL;
 
-int ED_gpencil_session_active(void)
+int ed_dpen_session_active(void)
 {
-  return (BLI_listbase_is_empty(&undo_nodes) == false);
+  return (lib_listbase_is_empty(&undo_nodes) == false);
 }
 
-int ED_undo_gpencil_step(bContext *C, const int step)
+int ed_undo_dpen_step(dContext *C, const int step)
 {
-  bGPdata **gpd_ptr = NULL, *new_gpd = NULL;
+  DPendata **dpd_ptr = NULL, *new_dpd = NULL;
 
-  gpd_ptr = ED_gpencil_data_get_pointers(C, NULL);
+  fpd_ptr = ed_dpen_data_get_ptrs(C, NULL);
 
   const eUndoStepDir undo_step = (eUndoStepDir)step;
   if (undo_step == STEP_UNDO) {
     if (cur_node->prev) {
       cur_node = cur_node->prev;
-      new_gpd = cur_node->gpd;
+      new_dpd = cur_node->dpd;
     }
   }
   else if (undo_step == STEP_REDO) {
     if (cur_node->next) {
       cur_node = cur_node->next;
-      new_gpd = cur_node->gpd;
+      new_dpd = cur_node->dpd;
     }
   }
 
-  if (new_gpd) {
-    if (gpd_ptr) {
-      if (*gpd_ptr) {
-        bGPdata *gpd = *gpd_ptr;
-        bGPDlayer *gpld;
+  if (new_dpd) {
+    if (dpd_ptr) {
+      if (*dpd_ptr) {
+        DPenData *dpd = *dpd_ptr;
+        DPenLayer *dpld;
 
-        BKE_gpencil_free_layers(&gpd->layers);
+        dune_dpen_free_layers(&dpd->layers);
 
         /* copy layers */
-        BLI_listbase_clear(&gpd->layers);
+        lib_listbase_clear(&dpd->layers);
 
-        LISTBASE_FOREACH (bGPDlayer *, gpl, &gpd->layers) {
+        LISTBASE_FOREACH (DPenLayer *, dpl, &dpd->layers) {
           /* make a copy of source layer and its data */
-          gpld = BKE_gpencil_layer_duplicate(gpl, true, true);
-          BLI_addtail(&gpd->layers, gpld);
+          dpld = dune_dpen_layer_duplicate(dpl, true, true);
+          lib_addtail(&dpd->layers, dpld);
         }
       }
     }
     /* drawing batch cache is dirty now */
-    DEG_id_tag_update(&new_gpd->id, ID_RECALC_TRANSFORM | ID_RECALC_GEOMETRY);
-    new_gpd->flag |= GP_DATA_CACHE_IS_DIRTY;
+    DEG_id_tag_update(&new_dpd->id, ID_RECALC_TRANSFORM | ID_RECALC_GEOMETRY);
+    new_dpd->flag |= DPEN_DATA_CACHE_IS_DIRTY;
   }
 
-  WM_event_add_notifier(C, NC_GPENCIL | NA_EDITED, NULL);
+  wm_event_add_notifier(C, NC_DPEN | NA_EDITED, NULL);
 
-  return OPERATOR_FINISHED;
+  return OP_FINISHED;
 }
 
-void gpencil_undo_init(bGPdata *gpd)
+void dpen_undo_init(DPenData *dpd)
 {
-  gpencil_undo_push(gpd);
+  dpen_undo_push(dpd);
 }
 
-static void gpencil_undo_free_node(bGPundonode *undo_node)
+static void dpen_undo_free_node(DPenUndoNode *undo_node)
 {
   /* HACK: animdata wasn't duplicated, so it shouldn't be freed here,
    * or else the real copy will segfault when accessed
    */
-  undo_node->gpd->adt = NULL;
+  undo_node->dpd->adt = NULL;
 
-  BKE_gpencil_free_data(undo_node->gpd, false);
-  MEM_freeN(undo_node->gpd);
+  dune_dpen_free_data(undo_node->dpd, false);
+  MEM_freeN(undo_node->dpd);
 }
 
-void gpencil_undo_push(bGPdata *gpd)
+void dpen_undo_push(DPenData *dpd)
 {
-  bGPundonode *undo_node;
+  DPenUndoNode *undo_node;
 
   if (cur_node) {
     /* Remove all undone nodes from stack. */
     undo_node = cur_node->next;
 
     while (undo_node) {
-      bGPundonode *next_node = undo_node->next;
+      DPenUndoNode *next_node = undo_node->next;
 
-      gpencil_undo_free_node(undo_node);
-      BLI_freelinkN(&undo_nodes, undo_node);
+      dpen_undo_free_node(undo_node);
+      lib_freelinkN(&undo_nodes, undo_node);
 
       undo_node = next_node;
     }
@@ -127,17 +127,17 @@ void gpencil_undo_push(bGPdata *gpd)
    */
   /* TODO: Undo-memory constraint is not respected yet,
    * but can be added if we have any need for it. */
-  if (U.undosteps && !BLI_listbase_is_empty(&undo_nodes)) {
+  if (U.undosteps && !lib_listbase_is_empty(&undo_nodes)) {
     /* remove anything older than n-steps before cur_node */
     int steps = 0;
 
     undo_node = (cur_node) ? cur_node : undo_nodes.last;
     while (undo_node) {
-      bGPundonode *prev_node = undo_node->prev;
+      DPenUndoNode *prev_node = undo_node->prev;
 
       if (steps >= U.undosteps) {
-        gpencil_undo_free_node(undo_node);
-        BLI_freelinkN(&undo_nodes, undo_node);
+        dpen_undo_free_node(undo_node);
+        lib_freelinkN(&undo_nodes, undo_node);
       }
 
       steps++;
@@ -146,24 +146,24 @@ void gpencil_undo_push(bGPdata *gpd)
   }
 
   /* create new undo node */
-  undo_node = MEM_callocN(sizeof(bGPundonode), "gpencil undo node");
-  undo_node->gpd = BKE_gpencil_data_duplicate(NULL, gpd, true);
+  undo_node = MEM_callocN(sizeof(DPenUndoNode), "dpen undo node");
+  undo_node->dpd = dune_dpen_data_duplicate(NULL, dpd, true);
 
   cur_node = undo_node;
 
-  BLI_addtail(&undo_nodes, undo_node);
+  lib_addtail(&undo_nodes, undo_node);
 }
 
-void gpencil_undo_finish(void)
+void dpen_undo_finish(void)
 {
-  bGPundonode *undo_node = undo_nodes.first;
+  DPenUndonmNode *undo_node = undo_nodes.first;
 
   while (undo_node) {
-    gpencil_undo_free_node(undo_node);
+    dpen_undo_free_node(undo_node);
     undo_node = undo_node->next;
   }
 
-  BLI_freelistN(&undo_nodes);
+  lib_freelistN(&undo_nodes);
 
   cur_node = NULL;
 }
