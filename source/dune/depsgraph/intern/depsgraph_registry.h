c9be1dbb1664392e29144e70876ef41f64336a03
Kielan <ki.lemons@gmail.com>
2023-02-24 16:18:39 -0700

    Create depsgraph_registry.h

---
+++ source/dune/depsgraph/intern/depsgraph_registry.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "intern/depsgraph_type.h"
+
+struct Main;
+
+namespace blender {
+namespace deg {
+
+struct Depsgraph;
+
+void register_graph(Depsgraph *depsgraph);
+void unregister_graph(Depsgraph *depsgraph);
+Span<Depsgraph *> get_all_registered_graphs(Main *bmain);
+
+}  // namespace deg
+}  // namespace dune
