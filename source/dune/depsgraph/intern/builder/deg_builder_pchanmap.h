0d5a7fb5742f0a7bbdd5bc85a3e0943c9937ffef
Kielan <ki.lemons@gmail.com>
2023-02-25 15:41:53 -0700

    Create deg_builder_pchanmap.h

---
+++ source/dune/depsgraph/intern/builder/deg_builder_pchanmap.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "intern/depsgraph_type.h"
+
+namespace dune::deg {
+
+struct RootPChanMap {
+  /** Debug contents of map. */
+  void print_debug();
+
+  /** Add a mapping. */
+  void add_bone(const char *bone, const char *root);
+
+  /** Check if there's a common root bone between two bones. */
+  bool has_common_root(const char *bone1, const char *bone2) const;
+
+ protected:
+  /**
+   * The strings are only referenced by this map. Users of RootPChanMap have to make sure that the
+   * life-time of the strings is long enough.
+   */
+  Map<StringRefNull, Set<StringRefNull>> map_;
+};
+
+}  // namespace dune::deg
