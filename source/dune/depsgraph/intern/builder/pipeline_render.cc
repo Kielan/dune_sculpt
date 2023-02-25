29c3da67061c7c853cb6ba57ea326d2e6dc1d23b
Kielan <ki.lemons@gmail.com>
2023-02-25 16:06:03 -0700

    Create pipeline_render.cc

---
+++ source/dune/depsgraph/intern/builder/pipeline_render.cc
@@ -0,0 +1,24 @@
+#include "pipeline_render.h"
+
+#include "intern/builder/deg_builder_nodes.h"
+#include "intern/builder/deg_builder_relations.h"
+#include "intern/depsgraph.h"
+
+namespace dune::deg {
+
+RenderBuilderPipeline::RenderBuilderPipeline(::Depsgraph *graph) : AbstractBuilderPipeline(graph)
+{
+  deg_graph_->is_render_pipeline_depsgraph = true;
+}
+
+void RenderBuilderPipeline::build_nodes(DepsgraphNodeBuilder &node_builder)
+{
+  node_builder.build_scene_render(scene_, view_layer_);
+}
+
+void RenderBuilderPipeline::build_relations(DepsgraphRelationBuilder &relation_builder)
+{
+  relation_builder.build_scene_render(scene_, view_layer_);
+}
+
+}  // namespace dune::deg
