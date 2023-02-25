5bd5f7905f076b825d812e728c69eb60c7ecc6aa
Kielan <ki.lemons@gmail.com>
2023-02-25 16:07:57 -0700

    Create pipeline_view_layer.h

---
+++ source/dune/depsgraph/intern/builder/pipeline_view_layer.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "pipeline.h"
+
+namespace dune::deg {
+
+class ViewLayerBuilderPipeline : public AbstractBuilderPipeline {
+ public:
+  ViewLayerBuilderPipeline(::Depsgraph *graph);
+
+ protected:
+  virtual void build_nodes(DepsgraphNodeBuilder &node_builder) override;
+  virtual void build_relations(DepsgraphRelationBuilder &relation_builder) override;
+};
+
+}  // namespace dune::deg
