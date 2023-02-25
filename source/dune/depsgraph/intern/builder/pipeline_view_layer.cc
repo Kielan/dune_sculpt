aac97025666b90cd1d48a9a230ac7b8c99ef6e26
Kielan <ki.lemons@gmail.com>
2023-02-25 16:07:30 -0700

    Create pipeline_view_layer.cc

---
+++ source/dune/depsgraph/intern/builder/pipeline_view_layer.cc
@@ -0,0 +1,24 @@
+#include "pipeline_view_layer.h"
+
+#include "intern/builder/deg_builder_nodes.h"
+#include "intern/builder/deg_builder_relations.h"
+#include "intern/depsgraph.h"
+
+namespace dune::deg {
+
+ViewLayerBuilderPipeline::ViewLayerBuilderPipeline(::Depsgraph *graph)
+    : AbstractBuilderPipeline(graph)
+{
+}
+
+void ViewLayerBuilderPipeline::build_nodes(DepsgraphNodeBuilder &node_builder)
+{
+  node_builder.build_view_layer(scene_, view_layer_, DEG_ID_LINKED_DIRECTLY);
+}
+
+void ViewLayerBuilderPipeline::build_relations(DepsgraphRelationBuilder &relation_builder)
+{
+  relation_builder.build_view_layer(scene_, view_layer_, DEG_ID_LINKED_DIRECTLY);
+}
+
+}  // namespace dune::deg
