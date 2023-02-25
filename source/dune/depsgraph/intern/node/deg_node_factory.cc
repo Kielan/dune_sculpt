e2168f46c4c564880ad983902bde1bd9858d3c75
Kielan <ki.lemons@gmail.com>
2023-02-24 18:39:18 -0700

    Create deg_node_factory.cc

---
+++ source/dune/depsgraph/intern/node/deg_node_factory.cc
@@ -0,0 +1,22 @@
+#include "intern/node/deg_node_factory.h"
+
+namespace dune::deg {
+
+/* Global type registry */
+static DepsNodeFactory *node_typeinfo_registry[static_cast<int>(NodeType::NUM_TYPES)] = {nullptr};
+
+void register_node_typeinfo(DepsNodeFactory *factory)
+{
+  lib_assert(factory != nullptr);
+  const int type_as_int = static_cast<int>(factory->type());
+  node_typeinfo_registry[type_as_int] = factory;
+}
+
+DepsNodeFactory *type_get_factory(const NodeType type)
+{
+  /* Look up type - at worst, it doesn't exist in table yet, and we fail. */
+  const int type_as_int = static_cast<int>(type);
+  return node_typeinfo_registry[type_as_int];
+}
+
+}  // namespace dune::deg
