0a24f9dac8750f11f0fa0119cb777c00479b65b5
Kielan <ki.lemons@gmail.com>
2023-02-25 14:36:58 -0700

    Create deg_node_time.h

---
+++ source/dune/depsgraph/intern/node/deg_node_time.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "intern/node/deg_node.h"
+
+namespace dune {
+namespace deg {
+
+/* Time Source Node. */
+struct TimeSourceNode : public Node {
+  bool tagged_for_update = false;
+
+  // TODO: evaluate() operation needed
+
+  virtual void tag_update(Depsgraph *graph, eUpdateSource source) override;
+
+  void flush_update_tag(Depsgraph *graph);
+
+  DEG_DEPSNODE_DECLARE;
+};
+
+}  // namespace deg
+}  // namespace dune
