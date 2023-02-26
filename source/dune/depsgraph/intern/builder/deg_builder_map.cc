de6803336bf00e605d6bd0e4eafa39239797aacf
Kielan <ki.lemons@gmail.com>
2023-02-26 15:33:31 -0700

    Update deg_builder_map.cc

--- source/dune/depsgraph/intern/builder/deg_builder_map.cc
+++ source/dune/depsgraph/intern/builder/deg_builder_map.cc
@@ -1,20 +1,20 @@
 #include "intern/builder/deg_builder_map.h"
 
-#include "DNA_ID.h"
+#include "types_id.h"
 
-namespace blender::deg {
+namespace dune::deg {
 
-bool BuilderMap::checkIsBuilt(ID *id, int tag) const
+bool BuilderMap::checkIsBuilt(Id *id, int tag) const
 {
-  return (getIDTag(id) & tag) == tag;
+  return (getIdTag(id) & tag) == tag;
 }
 
-void BuilderMap::tagBuild(ID *id, int tag)
+void BuilderMap::tagBuild(Id *id, int tag)
 {
   id_tags_.lookup_or_add(id, 0) |= tag;
 }
 
-bool BuilderMap::checkIsBuiltAndTag(ID *id, int tag)
+bool BuilderMap::checkIsBuiltAndTag(Id *id, int tag)
 {
   int &id_tag = id_tags_.lookup_or_add(id, 0);
   const bool result = (id_tag & tag) == tag;
@@ -22,9 +22,9 @@ bool BuilderMap::checkIsBuiltAndTag(ID *id, int tag)
   return result;
 }
 
-int BuilderMap::getIDTag(ID *id) const
+int BuilderMap::getIdTag(Id *id) const
 {
   return id_tags_.lookup_default(id, 0);
 }
 
-}  // namespace blender::deg
+}  // namespace dune::deg
