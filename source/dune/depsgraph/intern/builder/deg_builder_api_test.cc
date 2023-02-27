441416ffa6cb17c31295fde0c895258ef14e8771
Kielan <ki.lemons@gmail.com>
2023-02-27 13:15:27 -0700

    Update deg_builder_api_test.cc

--- source/dune/depsgraph/intern/builder/deg_builder_api_test.cc
+++ source/dune/depsgraph/intern/builder/deg_builder_api_test.cc
@@ -2,32 +2,32 @@
 
 #include "testing/testing.h"
 
-namespace blender::deg::tests {
+namespace dune::deg::tests {
 
-class TestableRNANodeQuery : public RNANodeQuery {
+class TestableApiNodeQuery : public ApiNodeQuery {
  public:
-  static bool contains(const char *prop_identifier, const char *rna_path_component)
+  static bool contains(const char *prop_id, const char *api_path_component)
   {
-    return RNANodeQuery::contains(prop_identifier, rna_path_component);
+    return ApiNodeQuery::contains(prop_id, api_path_component);
   }
 };
 
-TEST(deg_builder_rna, contains)
+TEST(dgraph_builder_api, contains)
 {
-  EXPECT_TRUE(TestableRNANodeQuery::contains("location", "location"));
-  EXPECT_TRUE(TestableRNANodeQuery::contains("location.x", "location"));
-  EXPECT_TRUE(TestableRNANodeQuery::contains("pose.bone[\"blork\"].location", "location"));
-  EXPECT_TRUE(TestableRNANodeQuery::contains("pose.bone[\"blork\"].location.x", "location"));
-  EXPECT_TRUE(TestableRNANodeQuery::contains("pose.bone[\"blork\"].location[0]", "location"));
+  EXPECT_TRUE(TestableApiNodeQuery::contains("location", "location"));
+  EXPECT_TRUE(TestableApiNodeQuery::contains("location.x", "location"));
+  EXPECT_TRUE(TestableApiNodeQuery::contains("pose.bone[\"blork\"].location", "location"));
+  EXPECT_TRUE(TestableApiNodeQuery::contains("pose.bone[\"blork\"].location.x", "location"));
+  EXPECT_TRUE(TestableApiNodeQuery::contains("pose.bone[\"blork\"].location[0]", "location"));
 
-  EXPECT_FALSE(TestableRNANodeQuery::contains("", "location"));
-  EXPECT_FALSE(TestableRNANodeQuery::contains("locatio", "location"));
-  EXPECT_FALSE(TestableRNANodeQuery::contains("locationnn", "location"));
-  EXPECT_FALSE(TestableRNANodeQuery::contains("test_location", "location"));
-  EXPECT_FALSE(TestableRNANodeQuery::contains("location_test", "location"));
-  EXPECT_FALSE(TestableRNANodeQuery::contains("test_location_test", "location"));
-  EXPECT_FALSE(TestableRNANodeQuery::contains("pose.bone[\"location\"].scale", "location"));
-  EXPECT_FALSE(TestableRNANodeQuery::contains("pose.bone[\"location\"].scale[0]", "location"));
+  EXPECT_FALSE(TestableApiNodeQuery::contains("", "location"));
+  EXPECT_FALSE(TestableApiNodeQuery::contains("locatio", "location"));
+  EXPECT_FALSE(TestableApiNodeQuery::contains("locationnn", "location"));
+  EXPECT_FALSE(TestableApiNodeQuery::contains("test_location", "location"));
+  EXPECT_FALSE(TestableApiNodeQuery::contains("location_test", "location"));
+  EXPECT_FALSE(TestableApiNodeQuery::contains("test_location_test", "location"));
+  EXPECT_FALSE(TestableApiNodeQuery::contains("pose.bone[\"location\"].scale", "location"));
+  EXPECT_FALSE(TestableApiNodeQuery::contains("pose.bone[\"location\"].scale[0]", "location"));
 }
 
-}  // namespace blender::deg::tests
+}  // namespace dune::deg::tests
