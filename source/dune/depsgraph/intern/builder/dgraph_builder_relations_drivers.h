894460504e6cb7f2ae6af0ac94ce650d81161af8
Kielan <ki.lemons@gmail.com>
2023-02-28 15:57:43 -0700

    Update and rename deg_builder_relations_drivers.h to dgraph_builder_relations_drivers.h

--- source/dune/depsgraph/intern/builder/dgraph_builder_relations_drivers.h
+++ source/dune/depsgraph/intern/builder/dgraph_builder_relations_drivers.h
@@ -1,14 +1,14 @@
 #pragma once
 
-#include "BLI_string_ref.hh"
+#include "lib_string_ref.hh"
 
-#include "RNA_types.h"
+#include "api_types.h"
 
-#include "intern/builder/deg_builder_relations.h"
+#include "intern/builder/dgraph_builder_relations.h"
 
 struct FCurve;
 
-namespace blender::deg {
+namespace dune::dgraph {
 
 /* Helper class for determining which relations are needed between driver evaluation nodes. */
 class DriverDescriptor {
@@ -17,38 +17,38 @@ class DriverDescriptor {
    * Drivers are grouped by their RNA prefix. The prefix is the part of the RNA
    * path up to the last dot, the suffix is the remainder of the RNA path:
    *
-   * \code{.unparsed}
-   * fcu->rna_path                     rna_prefix              rna_suffix
+   * code'''
+   * fcu->api_path                     rna_prefix              rna_suffix
    * -------------------------------   ----------------------  ----------
    * 'color'                           ''                      'color'
    * 'rigidbody_world.time_scale'      'rigidbody_world'       'time_scale'
    * 'pose.bones["master"].location'   'pose.bones["master"]'  'location'
-   * \endcode
+   * '''
    */
-  StringRef rna_prefix;
-  StringRef rna_suffix;
+  StringRef api_prefix;
+  StringRef api_suffix;
 
  public:
-  DriverDescriptor(PointerRNA *id_ptr, FCurve *fcu);
+  DriverDescriptor(ApiPtr *id_ptr, FCurve *fcu);
 
   bool driver_relations_needed() const;
   bool is_array() const;
-  /** Assumes that 'other' comes from the same RNA group, that is, has the same RNA path prefix. */
+  /** Assumes that 'other' comes from the same Api group, that is, has the same api path prefix. */
   bool is_same_array_as(const DriverDescriptor &other) const;
-  OperationKey depsgraph_key() const;
+  OpKey dgraph_key() const;
 
  private:
-  PointerRNA *id_ptr_;
+  ApiPtr *id_ptr_;
   FCurve *fcu_;
   bool driver_relations_needed_;
 
-  PointerRNA pointer_rna_;
-  PropertyRNA *property_rna_;
+  ApiPtr ptr_api_;
+  ApiProp *prop_api_;
   bool is_array_;
 
   bool determine_relations_needed();
-  void split_rna_path();
-  bool resolve_rna();
+  void split_api_path();
+  bool resolve_api();
 };
 
 }  // namespace dune::deg
