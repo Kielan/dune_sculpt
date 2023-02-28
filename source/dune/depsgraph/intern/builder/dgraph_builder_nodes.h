068e5b68ba72efc393f2da3e29c38a640e880913
Kielan <ki.lemons@gmail.com>
2023-02-28 13:44:01 -0700

    Update and rename deg_builder_nodes.h to dgraph_builder_nodes.h

--- source/dune/depsgraph/intern/builder/dgraph_builder_nodes.h
+++ source/dune/depsgraph/intern/builder/dgraph_builder_nodes.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#include "intern/builder/deg_builder.h"
-#include "intern/builder/deg_builder_key.h"
-#include "intern/builder/deg_builder_map.h"
-#include "intern/depsgraph_type.h"
-#include "intern/node/deg_node_id.h"
-#include "intern/node/deg_node_operation.h"
+#include "intern/builder/dgraph_builder.h"
+#include "intern/builder/dgraph_builder_key.h"
+#include "intern/builder/dgraph_builder_map.h"
+#include "intern/dgraph_type.h"
+#include "intern/node/dgraph_node_id.h"
+#include "intern/node/dgraph_node_op.h"
 
-#include "DEG_depsgraph.h"
+#include "dgraph.h"
 
 struct CacheFile;
 struct Camera;
@@ -15,8 +15,8 @@ struct Collection;
 struct FCurve;
 struct FreestyleLineSet;
 struct FreestyleLineStyle;
-struct ID;
-struct IDProperty;
+struct Id;
+struct IdPropl;
 struct Image;
 struct Key;
 struct LayerCollection;
@@ -35,34 +35,34 @@ struct Speaker;
 struct Tex;
 struct VFont;
 struct World;
-struct bAction;
-struct bArmature;
-struct bConstraint;
-struct bNodeSocket;
-struct bNodeTree;
-struct bPoseChannel;
-struct bSound;
+struct DAction;
+struct DArmature;
+struct DConstraint;
+struct DNodeSocket;
+struct DNodeTree;
+struct DPoseChannel;
+struct DSound;
 
-namespace dune::deg {
+namespace dune::dgraph {
 
 struct ComponentNode;
-struct Depsgraph;
-class DepsgraphBuilderCache;
-struct IDNode;
-struct OperationKey;
-struct OperationNode;
+struct DGraph;
+class DGraphBuilderCache;
+struct IdNode;
+struct OpKey;
+struct OpNode;
 struct TimeSourceNode;
 
-class DepsgraphNodeBuilder : public DepsgraphBuilder {
+class DGraphNodeBuilder : public DGraphBuilder {
  public:
-  DepsgraphNodeBuilder(Main *bmain, Depsgraph *graph, DepsgraphBuilderCache *cache);
+  DepsgraphNodeBuilder(Main *Dmain, DGraph *graph, DGraphBuilderCache *cache);
   ~DepsgraphNodeBuilder();
 
-  /* For given original ID get ID which is created by CoW system. */
-  ID *get_cow_id(const ID *id_orig) const;
+  /* For given original Id get Id which is created by CoW system. */
+  Id *get_cow_id(const Id *id_orig) const;
   /* Similar to above, but for the cases when there is no ID node we create
    * one. */
-  ID *ensure_cow_id(ID *id_orig);
+  Id *ensure_cow_id(Id *id_orig);
 
   /* Helper wrapper function which wraps get_cow_id with a needed type cast. */
   template<typename T> T *get_cow_datablock(const T *orig) const
@@ -80,82 +80,82 @@ class DepsgraphNodeBuilder : public DepsgraphBuilder {
   virtual void end_build();
 
   /**
-   * `id_cow_self` is the user of `id_pointer`,
-   * see also `LibraryIDLinkCallbackData` struct definition.
+   * `id_cow_self` is the user of `id_ptr`,
+   * see also `LibIdLinkCbData` struct definition.
    */
-  int foreach_id_cow_detect_need_for_update_callback(ID *id_cow_self, ID *id_pointer);
+  int foreach_id_cow_detect_need_for_update_cb(Id *id_cow_self, Id *id_ptr);
 
-  IDNode *add_id_node(ID *id);
-  IDNode *find_id_node(const ID *id);
+  IdNode *add_id_node(Id *id);
+  IdNode *find_id_node(const Id *id);
   TimeSourceNode *add_time_source();
 
-  ComponentNode *add_component_node(ID *id, NodeType comp_type, const char *comp_name = "");
-  ComponentNode *find_component_node(const ID *id, NodeType comp_type, const char *comp_name = "");
+  ComponentNode *add_component_node(Id *id, NodeType comp_type, const char *comp_name = "");
+  ComponentNode *find_component_node(const Id *id, NodeType comp_type, const char *comp_name = "");
 
-  OperationNode *add_operation_node(ComponentNode *comp_node,
-                                    OperationCode opcode,
-                                    const DepsEvalOperationCb &op = nullptr,
+  OpNode *add_op_node(ComponentNode *comp_node,
+                                    OpCode opcode,
+                                    const DepsEvalOpCb &op = nullptr,
                                     const char *name = "",
                                     int name_tag = -1);
-  OperationNode *add_operation_node(ID *id,
+  OpNode *add_op_node(Id *id,
                                     NodeType comp_type,
                                     const char *comp_name,
-                                    OperationCode opcode,
-                                    const DepsEvalOperationCb &op = nullptr,
+                                    OpCode opcode,
+                                    const DepsEvalOpCb &op = nullptr,
                                     const char *name = "",
                                     int name_tag = -1);
-  OperationNode *add_operation_node(ID *id,
+  OpNode *add_op_node(Id *id,
                                     NodeType comp_type,
-                                    OperationCode opcode,
-                                    const DepsEvalOperationCb &op = nullptr,
+                                    OpCode opcode,
+                                    const DepsEvalOpCb &op = nullptr,
                                     const char *name = "",
                                     int name_tag = -1);
 
-  OperationNode *ensure_operation_node(ID *id,
+  OpNode *ensure_op_node(Id *id,
                                        NodeType comp_type,
                                        const char *comp_name,
                                        OperationCode opcode,
-                                       const DepsEvalOperationCb &op = nullptr,
+                                       const DepsEvalOpCb &op = nullptr,
                                        const char *name = "",
                                        int name_tag = -1);
-  OperationNode *ensure_operation_node(ID *id,
-                                       NodeType comp_type,
-                                       OperationCode opcode,
-                                       const DepsEvalOperationCb &op = nullptr,
-                                       const char *name = "",
-                                       int name_tag = -1);
+  OperationNode *ensure_op_node(Id *id,
+                                NodeType comp_type,
+                                OpCode opcode,
+                                const DepsEvalOpCb &op = nullptr,
+                                const char *name = "",
+                                int name_tag = -1);
 
-  bool has_operation_node(ID *id,
-                          NodeType comp_type,
-                          const char *comp_name,
-                          OperationCode opcode,
-                          const char *name = "",
-                          int name_tag = -1);
+  bool has_op_node(Id *id,
+                   NodeType comp_type,
+                   const char *comp_name,
+                   OpCode opcode,
+                   const char *name = "",
+                   int name_tag = -1);
 
-  OperationNode *find_operation_node(const ID *id,
-                                     NodeType comp_type,
-                                     const char *comp_name,
-                                     OperationCode opcode,
-                                     const char *name = "",
-                                     int name_tag = -1);
+  OpNode *find_op_node(const Id *id,
+                       NodeType comp_type,
+                       const char *comp_name,
+                       OpCode opcode,
+                       const char *name = "",
+                       int name_tag = -1);
 
-  OperationNode *find_operation_node(const ID *id,
-                                     NodeType comp_type,
-                                     OperationCode opcode,
-                                     const char *name = "",
-                                     int name_tag = -1);
+  OpNode *find_op_node(const Id *id,
+                       NodeType comp_type,
+                       OpCode opcode,
+                       const char *name = "",
+                       int name_tag = -1);
 
-  OperationNode *find_operation_node(const OperationKey &key);
+  OpNode *find_op_node(const OpKey &key);
 
-  virtual void build_id(ID *id);
+  virtual void build_id(Id *id);
 
   /* Build function for ID types that do not need their own build_xxx() function. */
-  virtual void build_generic_id(ID *id);
+  virtual void build_generic_id(Id *id);
 
-  virtual void build_idproperties(IDProperty *id_property);
+  virtual void build_idprops(IdProp *id_prop);
 
   virtual void build_scene_render(Scene *scene, ViewLayer *view_layer);
-  virtual void build_scene_parameters(Scene *scene);
+  virtual void build_scene_params(Scene *scene);
   virtual void build_scene_compositor(Scene *scene);
 
   virtual void build_layer_collections(ListBase *lb);
@@ -206,20 +206,20 @@ class DepsgraphNodeBuilder : public DepsgraphBuilder {
    * \param fcurve: Driver-FCurve
    * \param driver_index: Index in animation data drivers list
    */
-  virtual void build_driver(ID *id, FCurve *fcurve, int driver_index);
-  virtual void build_driver_variables(ID *id, FCurve *fcurve);
-  virtual void build_driver_id_property(ID *id, const char *rna_path);
-  virtual void build_parameters(ID *id);
+  virtual void build_driver(Id *id, FCurve *fcurve, int driver_index);
+  virtual void build_driver_variables(Id *id, FCurve *fcurve);
+  virtual void build_driver_id_prop(Id *id, const char *rna_path);
+  virtual void build_params(Id *id);
   virtual void build_dimensions(Object *object);
   virtual void build_ik_pose(Object *object, bPoseChannel *pchan, bConstraint *con);
   virtual void build_splineik_pose(Object *object, bPoseChannel *pchan, bConstraint *con);
   virtual void build_rig(Object *object);
-  virtual void build_armature(bArmature *armature);
+  virtual void build_armature(DArmature *armature);
   virtual void build_armature_bones(ListBase *bones);
   virtual void build_shapekeys(Key *key);
   virtual void build_camera(Camera *camera);
   virtual void build_light(Light *lamp);
-  virtual void build_nodetree(bNodeTree *ntree);
+  virtual void build_nodetree(DNodeTree *ntree);
   virtual void build_nodetree_socket(bNodeSocket *socket);
   virtual void build_material(Material *ma);
   virtual void build_materials(Material **materials, int num_materials);
@@ -233,7 +233,7 @@ class DepsgraphNodeBuilder : public DepsgraphBuilder {
   virtual void build_movieclip(MovieClip *clip);
   virtual void build_lightprobe(LightProbe *probe);
   virtual void build_speaker(Speaker *speaker);
-  virtual void build_sound(bSound *sound);
+  virtual void build_sound(DSound *sound);
   virtual void build_simulation(Simulation *simulation);
   virtual void build_scene_sequencer(Scene *scene);
   virtual void build_scene_audio(Scene *scene);
@@ -242,41 +242,41 @@ class DepsgraphNodeBuilder : public DepsgraphBuilder {
 
   /* Per-ID information about what was already in the dependency graph.
    * Allows to re-use certain values, to speed up following evaluation. */
-  struct IDInfo {
+  struct IdInfo {
     /* Copy-on-written pointer of the corresponding ID. */
-    ID *id_cow;
+    Id *id_cow;
     /* Mask of visible components from previous state of the
      * dependency graph. */
-    IDComponentsMask previously_visible_components_mask;
+    IdComponentsMask previously_visible_components_mask;
     /* Special evaluation flag mask from the previous depsgraph. */
     uint32_t previous_eval_flags;
     /* Mesh CustomData mask from the previous depsgraph. */
-    DEGCustomDataMeshMasks previous_customdata_masks;
+    DGraphCustomDataMeshMasks previous_customdata_masks;
   };
 
  protected:
   /* Entry tags from the previous state of the dependency graph.
    * Stored before the graph is re-created so that they can be transferred over. */
-  Vector<PersistentOperationKey> saved_entry_tags_;
+  Vector<PersistentOpKey> saved_entry_tags_;
 
   struct BuilderWalkUserData {
-    DepsgraphNodeBuilder *builder;
+    DGraphNodeBuilder *builder;
   };
   static void modifier_walk(void *user_data,
                             struct Object *object,
-                            struct ID **idpoin,
+                            struct Id **idpoint,
                             int cb_flag);
-  static void constraint_walk(bConstraint *constraint,
-                              ID **idpoin,
-                              bool is_reference,
+  static void constraint_walk(DConstraint *constraint,
+                              Id **idpoint,
+                              bool is_ref,
                               void *user_data);
 
   void tag_previously_tagged_nodes();
   /**
-   * Check for IDs that need to be flushed (COW-updated)
+   * Check for Ids that need to be flushed (COW-updated)
    * because the depsgraph itself created or removed some of their evaluated dependencies.
    */
-  void update_invalid_cow_pointers();
+  void update_invalid_cow_ptrs();
 
   /* State which demotes currently built entities. */
   Scene *scene_;
@@ -290,8 +290,8 @@ class DepsgraphNodeBuilder : public DepsgraphBuilder {
    * very root is visible (aka not restricted.). */
   bool is_parent_collection_visible_;
 
-  /* Indexed by original ID.session_uuid, values are IDInfo. */
-  Map<uint, IDInfo *> id_info_hash_;
+  /* Indexed by original Id.session_uuid, values are IDInfo. */
+  Map<uint, IdInfo *> id_info_hash_;
 
   /* Set of IDs which were already build. Makes it easier to keep track of
    * what was already built and what was not. */
