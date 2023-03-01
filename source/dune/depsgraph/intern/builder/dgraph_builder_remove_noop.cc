bf41f76283efd180c2a721576a174472c4ecdb7a
Kielan <ki.lemons@gmail.com>
2023-03-01 10:09:59 -0700

    Update and rename deg_builder_remove_noop.cc to dgraph_builder_remove_noop.cc

--- source/dune/depsgraph/intern/builder/dgraph_builder_remove_noop.cc
+++ source/dune/depsgraph/intern/builder/dgraph_builder_remove_noop.cc
@@ -1,23 +1,23 @@
-#include "intern/builder/deg_builder_remove_noop.h"
+#include "intern/builder/dgraph_builder_remove_noop.h"
 
 #include "MEM_guardedalloc.h"
 
-#include "intern/node/deg_node.h"
-#include "intern/node/deg_node_operation.h"
+#include "intern/node/dgraph_node.h"
+#include "intern/node/dgraph_node_operation.h"
 
-#include "intern/debug/deg_debug.h"
-#include "intern/depsgraph.h"
-#include "intern/depsgraph_relation.h"
-#include "intern/depsgraph_type.h"
+#include "intern/debug/dgraph_debug.h"
+#include "intern/dgraph.h"
+#include "intern/dgraph_relation.h"
+#include "intern/dgraph_type.h"
 
-namespace blender::deg {
+namespace dune::dgraph {
 
-static inline bool is_unused_noop(OperationNode *op_node)
+static inline bool is_unused_noop(OpNode *op_node)
 {
   if (op_node == nullptr) {
     return false;
   }
-  if (op_node->flag & OperationFlag::DEPSOP_FLAG_PINNED) {
+  if (op_node->flag & OpFlag::DEPSOP_FLAG_PINNED) {
     return false;
   }
   return op_node->is_noop() && op_node->outlinks.is_empty();
@@ -29,25 +29,25 @@ static inline bool is_removable_relation(const Relation *relation)
     return true;
   }
 
-  const OperationNode *operation_from = static_cast<OperationNode *>(relation->from);
-  const OperationNode *operation_to = static_cast<OperationNode *>(relation->to);
+  const OpNode *op_from = static_cast<OpNode *>(relation->from);
+  const OpNode *op_to = static_cast<OpNode *>(relation->to);
 
   /* If the relation connects two different IDs there is a high risk that the removal of the
    * relation will make it so visibility flushing is not possible at runtime. This happens with
    * relations like the DoF on camera of custom shape on bines: such relation do not lead to an
-   * actual depsgraph evaluation operation as they are handled on render engine level.
+   * actual dgraph evaluation operation as they are handled on render engine level.
    *
    * The indirectly linked objects could have some of their components invisible as well, so
    * also keep relations which connect different components of the same object so that visibility
    * tracking happens correct in those cases as well. */
-  return operation_from->owner == operation_to->owner;
+  return op_from->owner == op_to->owner;
 }
 
-void deg_graph_remove_unused_noops(Depsgraph *graph)
+void deg_graph_remove_unused_noops(DGraph *graph)
 {
-  deque<OperationNode *> queue;
+  deque<OpNode *> queue;
 
-  for (OperationNode *node : graph->operations) {
+  for (OpNode *node : graph->ops) {
     if (is_unused_noop(node)) {
       queue.push_back(node);
     }
@@ -56,7 +56,7 @@ void deg_graph_remove_unused_noops(Depsgraph *graph)
   Vector<Relation *> relations_to_remove;
 
   while (!queue.empty()) {
-    OperationNode *to_remove = queue.front();
+    OpNode *to_remove = queue.front();
     queue.pop_front();
 
     for (Relation *rel_in : to_remove->inlinks) {
@@ -68,13 +68,13 @@ void deg_graph_remove_unused_noops(Depsgraph *graph)
       relations_to_remove.append(rel_in);
 
       /* Queue parent no-op node that has now become unused. */
-      OperationNode *operation = dependency->get_exit_operation();
-      if (is_unused_noop(operation)) {
-        queue.push_back(operation);
+      OpNode *op = dependency->get_exit_op();
+      if (is_unused_noop(op)) {
+        queue.push_back(op);
       }
     }
 
-    /* TODO(Sybren): Remove the node itself. */
+    /* TODO: Remove the node itself. */
   }
 
   /* Remove the relations. */
@@ -83,10 +83,10 @@ void deg_graph_remove_unused_noops(Depsgraph *graph)
     delete relation;
   }
 
-  DEG_DEBUG_PRINTF((::Depsgraph *)graph,
+  DEG_DEBUG_PRINTF((::DGraph *)graph,
                    BUILD,
                    "Removed %d relations to no-op nodes\n",
                    int(relations_to_remove.size()));
 }
 
-}  // namespace blender::deg
+}  // namespace dune::dgraph
