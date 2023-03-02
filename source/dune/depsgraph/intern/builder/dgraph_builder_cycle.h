168e5eedd7d2d626f9862db3a7586694c8ef3865
Kielan <ki.lemons@gmail.com>
2023-03-01 20:24:27 -0700

    Update dgraph_builder_cycle.h

--- source/dune/depsgraph/intern/builder/dgraph_builder_cycle.h
+++ source/dune/depsgraph/intern/builder/dgraph_builder_cycle.h
@@ -1,12 +1,12 @@
 #pragma once
 
 namespace dune {
-namespace deg {
+namespace dgraph {
 
 struct DGraph;
 
 /* Detect and solve dependency cycles. */
 void dgraph_detect_cycles(DGraph *graph);
 
-}  // namespace deg
+}  // namespace dgraph
 }  // namespace dune
