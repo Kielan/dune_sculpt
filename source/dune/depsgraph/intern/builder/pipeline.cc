58e1089c7c501c40ada9a08ac4a48aa20dc62b12
Kielan <ki.lemons@gmail.com>
2023-03-03 08:50:12 -0700

    Update pipeline.cc

--- source/dune/depsgraph/intern/builder/pipeline.cc
+++ source/dune/depsgraph/intern/builder/pipeline.cc
@@ -6,18 +6,18 @@
 
 #include "types_scene.h"
 
-#include "deg_builder_cycle.h"
-#include "deg_builder_nodes.h"
-#include "deg_builder_relations.h"
-#include "deg_builder_transitive.h"
+#include "dgraph_builder_cycle.h"
+#include "dgraph_builder_nodes.h"
+#include "dgraph_builder_relations.h"
+#include "dgraph_builder_transitive.h"
 
-namespace dune::deg {
+namespace dune::dgraph {
 
-AbstractBuilderPipeline::AbstractBuilderPipeline(::Depsgraph *graph)
-    : deg_graph_(reinterpret_cast<Depsgraph *>(graph)),
-      dmain_(deg_graph_->bmain),
-      scene_(deg_graph_->scene),
-      view_layer_(deg_graph_->view_layer)
+AbstractBuilderPipeline::AbstractBuilderPipeline(::DGraph *graph)
+    : dgraph_(reinterpret_cast<DGraph *>(graph)),
+      dmain_(dgraph_->dmain),
+      scene_(dgraph_->scene),
+      view_layer_(dgraph_->view_layer)
 {
 }
 
@@ -33,7 +33,7 @@ void AbstractBuilderPipeline::build()
   build_step_relations();
   build_step_finalize();
 
-  if (G.debug & (G_DEBUG_DEPSGRAPH_BUILD | G_DEBUG_DEPSGRAPH_TIME)) {
+  if (G.debug & (G_DEBUG_DGRAPH_BUILD | G_DEBUG_DGRAPH_TIME)) {
     printf("Depsgraph built in %f seconds.\n", PIL_check_seconds_timer() - start_time);
   }
 }
@@ -41,14 +41,14 @@ void AbstractBuilderPipeline::build()
 void AbstractBuilderPipeline::build_step_sanity_check()
 {
   lib_assert(lib_findindex(&scene_->view_layers, view_layer_) != -1);
-  lib_assert(deg_graph_->scene == scene_);
-  lib_assert(deg_graph_->view_layer == view_layer_);
+  lib_assert(dgraph_->scene == scene_);
+  lib_assert(dgraph_->view_layer == view_layer_);
 }
 
 void AbstractBuilderPipeline::build_step_nodes()
 {
   /* Generate all the nodes in the graph first */
-  unique_ptr<DepsgraphNodeBuilder> node_builder = construct_node_builder();
+  unique_ptr<DGraphNodeBuilder> node_builder = construct_node_builder();
   node_builder->begin_build();
   build_nodes(*node_builder);
   node_builder->end_build();
@@ -57,7 +57,7 @@ void AbstractBuilderPipeline::build_step_nodes()
 void AbstractBuilderPipeline::build_step_relations()
 {
   /* Hook up relationships between operations - to determine evaluation order. */
-  unique_ptr<DepsgraphRelationBuilder> relation_builder = construct_relation_builder();
+  unique_ptr<DGraphRelationBuilder> relation_builder = construct_relation_builder();
   relation_builder->begin_build();
   build_relations(*relation_builder);
   relation_builder->build_copy_on_write_relations();
@@ -67,37 +67,37 @@ void AbstractBuilderPipeline::build_step_relations()
 void AbstractBuilderPipeline::build_step_finalize()
 {
   /* Detect and solve cycles. */
-  deg_graph_detect_cycles(deg_graph_);
+  dgraph_detect_cycles(dgraph_);
   /* Simplify the graph by removing redundant relations (to optimize
    * traversal later). */
   /* TODO: it would be useful to have an option to disable this in cases where
    *       it is causing trouble. */
   if (G.debug_value == 799) {
-    deg_graph_transitive_reduction(deg_graph_);
+    dgraph_transitive_reduction(dgraph_);
   }
   /* Store pointers to commonly used evaluated datablocks. */
-  deg_graph_->scene_cow = (Scene *)deg_graph_->get_cow_id(&deg_graph_->scene->id);
+  dgraph_->scene_cow = (Scene *)dgraph_->get_cow_id(&dgraph_->scene->id);
   /* Flush visibility layer and re-schedule nodes for update. */
-  deg_graph_build_finalize(dmain_, deg_graph_);
-  deg_graph_tag_on_visible_update(reinterpret_cast<::Depsgraph *>(deg_graph_), false);
+  dgraph_build_finalize(dmain_, dgraph_);
+  dgraph_tag_on_visible_update(reinterpret_cast<::DGraph *>(dgraph_), false);
 #if 0
-  if (!deg_debug_consistency_check(deg_graph_)) {
+  if (!dgraph_debug_consistency_check(dgraph_)) {
     printf("Consistency validation failed, ABORTING!\n");
     abort();
   }
 #endif
   /* Relations are up to date. */
-  deg_graph_->need_update_relations = false;
+  dgraph_->need_update_relations = false;
 }
 
-unique_ptr<DepsgraphNodeBuilder> AbstractBuilderPipeline::construct_node_builder()
+unique_ptr<DGraphNodeBuilder> AbstractBuilderPipeline::construct_node_builder()
 {
-  return std::make_unique<DepsgraphNodeBuilder>(bmain_, deg_graph_, &builder_cache_);
+  return std::make_unique<DGraphNodeBuilder>(dmain_, dgraph_, &builder_cache_);
 }
 
-unique_ptr<DepsgraphRelationBuilder> AbstractBuilderPipeline::construct_relation_builder()
+unique_ptr<DGraphRelationBuilder> AbstractBuilderPipeline::construct_relation_builder()
 {
-  return std::make_unique<DepsgraphRelationBuilder>(bmain_, deg_graph_, &builder_cache_);
+  return std::make_unique<DGraphRelationBuilder>(dmain_, dgraph_, &builder_cache_);
 }
 
-}  // namespace dune::deg
+}  // namespace dune::dgraph
