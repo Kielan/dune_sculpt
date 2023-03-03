bbaa439a89250c44d6d93d4aac8f7f99f949c001
Kielan <ki.lemons@gmail.com>
2023-03-03 16:49:13 -0700

    Update pipeline_render.cc

--- source/dune/depsgraph/intern/builder/pipeline_render.cc
+++ source/dune/depsgraph/intern/builder/pipeline_render.cc
@@ -1,24 +1,24 @@
 #include "pipeline_render.h"
 
-#include "intern/builder/deg_builder_nodes.h"
-#include "intern/builder/deg_builder_relations.h"
-#include "intern/depsgraph.h"
+#include "intern/builder/dgraph_builder_nodes.h"
+#include "intern/builder/dgraph_builder_relations.h"
+#include "intern/dgraph.h"
 
-namespace dune::deg {
+namespace dune::dgraph {
 
-RenderBuilderPipeline::RenderBuilderPipeline(::Depsgraph *graph) : AbstractBuilderPipeline(graph)
+RenderBuilderPipeline::RenderBuilderPipeline(::DGraph *graph) : AbstractBuilderPipeline(graph)
 {
-  deg_graph_->is_render_pipeline_depsgraph = true;
+  dgraph_->is_render_pipeline_dgraph = true;
 }
 
-void RenderBuilderPipeline::build_nodes(DepsgraphNodeBuilder &node_builder)
+void RenderBuilderPipeline::build_nodes(DGraphNodeBuilder &node_builder)
 {
   node_builder.build_scene_render(scene_, view_layer_);
 }
 
-void RenderBuilderPipeline::build_relations(DepsgraphRelationBuilder &relation_builder)
+void RenderBuilderPipeline::build_relations(DGraphRelationBuilder &relation_builder)
 {
   relation_builder.build_scene_render(scene_, view_layer_);
 }
 
-}  // namespace dune::deg
+}  // namespace dune::dgraph
