571c744bc633448be96679f7ddfcd9e9631fdc8a
Kielan <ki.lemons@gmail.com>
2023-03-05 10:38:39 -0700

    Update depsgraph_tag.h

--- source/dune/depsgraph/intern/depsgraph_tag.h
+++ source/dune/depsgraph/intern/depsgraph_tag.h
@@ -1,26 +1,26 @@
-#include "intern/node/deg_node.h"
+#include "intern/node/dgraph_node.h"
 
-struct ID;
+struct Id;
 struct Main;
 
-namespace blender {
-namespace deg {
+namespace dune {
+namespace dgraph {
 
-struct Depsgraph;
+struct DGraph;
 
 /* Get type of a node which corresponds to a ID_RECALC_GEOMETRY tag. */
-NodeType geometry_tag_to_component(const ID *id);
+NodeType geometry_tag_to_component(const Id *id);
 
 /* Tag given ID for an update in all registered dependency graphs. */
-void id_tag_update(Main *bmain, ID *id, int flag, eUpdateSource update_source);
+void id_tag_update(Main *dmain, Id *id, int flag, eUpdateSource update_source);
 
 /* Tag given ID for an update with in a given dependency graph. */
 void graph_id_tag_update(
-    Main *bmain, Depsgraph *graph, ID *id, int flag, eUpdateSource update_source);
+    Main *dmain, DGraph *graph, Id *id, int flag, eUpdateSource update_source);
 
 /* Tag IDs of the graph for the visibility update tags.
  * Will do nothing if the graph is not tagged for visibility update. */
-void graph_tag_ids_for_visible_update(Depsgraph *graph);
+void graph_tag_ids_for_visible_update(DGraph *graph);
 
-}  // namespace deg
-}  // namespace blender
+}  // namespace dgraph
+}  // namespace dune
