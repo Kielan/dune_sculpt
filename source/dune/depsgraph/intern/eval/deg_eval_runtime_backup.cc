483627f75b8db92b87d93ae0e8f5e91c4427344d
Kielan <ki.lemons@gmail.com>
2023-03-06 08:47:58 -0700

    Update deg_eval_runtime_backup.cc

--- source/dune/depsgraph/intern/eval/deg_eval_runtime_backup.cc
+++ source/dune/depsgraph/intern/eval/deg_eval_runtime_backup.cc
@@ -1,31 +1,31 @@
-#include "intern/eval/deg_eval_runtime_backup.h"
+#include "intern/eval/dgraph_eval_runtime_backup.h"
 
-#include "intern/eval/deg_eval_copy_on_write.h"
+#include "intern/eval/dgraph_eval_copy_on_write.h"
 
-#include "BLI_utildefines.h"
+#include "lib_utildefines.h"
 
 #include "DRW_engine.h"
 
-namespace blender::deg {
+namespace dune::dgraph {
 
-RuntimeBackup::RuntimeBackup(const Depsgraph *depsgraph)
+RuntimeBackup::RuntimeBackup(const DGraph *dgraph)
     : have_backup(false),
       id_data({nullptr}),
-      animation_backup(depsgraph),
-      scene_backup(depsgraph),
-      sound_backup(depsgraph),
-      object_backup(depsgraph),
+      animation_backup(dgraph),
+      scene_backup(dgraph),
+      sound_backup(dgraph),
+      object_backup(dgraph),
       drawdata_ptr(nullptr),
-      movieclip_backup(depsgraph),
-      volume_backup(depsgraph),
-      gpencil_backup(depsgraph)
+      movieclip_backup(dgraph),
+      volume_backup(dgraph),
+      dpen_backup(dgraph)
 {
   drawdata_backup.first = drawdata_backup.last = nullptr;
 }
 
-void RuntimeBackup::init_from_id(ID *id)
+void RuntimeBackup::init_from_id(Id *id)
 {
-  if (!deg_copy_on_write_is_expanded(id)) {
+  if (!dgraph_copy_on_write_is_expanded(id)) {
     return;
   }
   have_backup = true;
@@ -36,7 +36,7 @@ void RuntimeBackup::init_from_id(ID *id)
 
   animation_backup.init_from_id(id);
 
-  const ID_Type id_type = GS(id->name);
+  const IdType id_type = GS(id->name);
   switch (id_type) {
     case ID_OB:
       object_backup.init_from_object(reinterpret_cast<Object *>(id));
@@ -45,7 +45,7 @@ void RuntimeBackup::init_from_id(ID *id)
       scene_backup.init_from_scene(reinterpret_cast<Scene *>(id));
       break;
     case ID_SO:
-      sound_backup.init_from_sound(reinterpret_cast<bSound *>(id));
+      sound_backup.init_from_sound(reinterpret_cast<DSound *>(id));
       break;
     case ID_MC:
       movieclip_backup.init_from_movieclip(reinterpret_cast<MovieClip *>(id));
@@ -54,7 +54,7 @@ void RuntimeBackup::init_from_id(ID *id)
       volume_backup.init_from_volume(reinterpret_cast<Volume *>(id));
       break;
     case ID_GD:
-      gpencil_backup.init_from_gpencil(reinterpret_cast<bGPdata *>(id));
+      dpen_backup.init_from_dpen(reinterpret_cast<DPenData *>(id));
       break;
     default:
       break;
@@ -69,7 +69,7 @@ void RuntimeBackup::init_from_id(ID *id)
   }
 }
 
-void RuntimeBackup::restore_to_id(ID *id)
+void RuntimeBackup::restore_to_id(Id *id)
 {
   if (!have_backup) {
     return;
@@ -79,7 +79,7 @@ void RuntimeBackup::restore_to_id(ID *id)
 
   animation_backup.restore_to_id(id);
 
-  const ID_Type id_type = GS(id->name);
+  const IdType id_type = GS(id->name);
   switch (id_type) {
     case ID_OB:
       object_backup.restore_to_object(reinterpret_cast<Object *>(id));
@@ -107,4 +107,4 @@ void RuntimeBackup::restore_to_id(ID *id)
   }
 }
 
-}  // namespace dune::deg
+}  // namespace dune::dgraph
