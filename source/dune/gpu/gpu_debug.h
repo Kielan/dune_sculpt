ac2ff6191cae9b8834d9bb0d92c127efeebd396a
Kielan <ki.lemons@gmail.com>
2023-03-09 06:16:38 -0700

    Update gpu_debug.h

--- source/dune/gpu/gpu_debug.h
+++ source/dune/gpu/gpu_debug.h
@@ -1,12 +1,8 @@
-/** \file
- * \ingroup gpu
- *
- * Helpers for GPU / drawing debugging.
- */
+/** Helpers for GPU / drawing debugging. **/
 
 #pragma once
 
-#include "BLI_sys_types.h"
+#include "lib_sys_types.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -14,17 +10,17 @@ extern "C" {
 
 #define GPU_DEBUG_SHADER_COMPILATION_GROUP "Shader Compilation"
 
-void GPU_debug_group_begin(const char *name);
-void GPU_debug_group_end(void);
+void gpu_debug_group_begin(const char *name);
+void gpu_debug_group_end(void);
 /**
  * Return a formatted string showing the current group hierarchy in this format:
  * "Group1 > Group 2 > Group3 > ... > GroupN : "
  */
-void GPU_debug_get_groups_names(int name_buf_len, char *r_name_buf);
+void gpu_debug_get_groups_names(int name_buf_len, char *r_name_buf);
 /**
  * Return true if inside a debug group with the same name.
  */
-bool GPU_debug_group_match(const char *ref);
+bool gpu_debug_group_match(const char *ref);
 
 #ifdef __cplusplus
 }
