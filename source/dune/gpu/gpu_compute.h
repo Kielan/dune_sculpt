083b042bd2598be20aec12691010634224069cf8
Kielan <ki.lemons@gmail.com>
2023-03-08 22:43:51 -0700

    Create gpu_compute.h

---
+++ source/dune/gpu/gpu_compute.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "BLI_sys_types.h"
+
+#include "GPU_shader.h"
+#include "GPU_storage_buffer.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void GPU_compute_dispatch(GPUShader *shader,
+                          uint groups_x_len,
+                          uint groups_y_len,
+                          uint groups_z_len);
+
+void GPU_compute_dispatch_indirect(GPUShader *shader, GPUStorageBuf *indirect_buf);
+
+#ifdef __cplusplus
+}
+#endif
