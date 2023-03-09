579ce49f330a2ea04a1e52f7f75173b38ef68fe7
Kielan <ki.lemons@gmail.com>
2023-03-09 07:03:20 -0700

    Create gpu_init_exit.h

---
+++ source/dune/gpu/gpu_init_exit.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "BLI_utildefines.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void GPU_init(void);
+void GPU_exit(void);
+bool GPU_is_init(void);
+
+#ifdef __cplusplus
+}
+#endif
