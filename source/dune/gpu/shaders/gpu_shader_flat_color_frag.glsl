fd5351b17942c817948c4a2592404c7253c9197b
Kielan <ki.lemons@gmail.com>
2023-03-10 09:10:23 -0700

    Create gpu_shader_flat_color_frag.glsl

---
+++ source/dune/gpu/shaders/gpu_shader_flat_color_frag.glsl
@@ -0,0 +1,12 @@
+#pragma DUNE_REQUIRE(gpu_shader_colorspace_lib.glsl)
+
+#ifndef USE_GPU_SHADER_CREATE_INFO
+flat in vec4 finalColor;
+out vec4 fragColor;
+#endif
+
+void main()
+{
+  fragColor = finalColor;
+  fragColor = dune_srgb_to_framebuffer_space(fragColor);
+}
