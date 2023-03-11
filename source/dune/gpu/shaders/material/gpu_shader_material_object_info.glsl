f4921bde052fc22627291d5b53dac9416048e043
Kielan <ki.lemons@gmail.com>
2023-03-11 10:34:13 -0700

    Create gpu_shader_material_object_info.glsl

---
+++ source/dune/gpu/shaders/material/gpu_shader_material_object_info.glsl
@@ -0,0 +1,18 @@
+void node_object_info(mat4 obmat,
+                      vec4 obcolor,
+                      vec4 info,
+                      float mat_index,
+                      out vec3 location,
+                      out vec4 color,
+                      out float alpha,
+                      out float object_index,
+                      out float material_index,
+                      out float random)
+{
+  location = obmat[3].xyz;
+  color = obcolor;
+  alpha = obcolor.w;
+  object_index = info.x;
+  material_index = mat_index;
+  random = info.z;
+}
