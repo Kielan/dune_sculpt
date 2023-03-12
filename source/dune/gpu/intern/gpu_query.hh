4856d2fc76ede812d620e4e5e905c72836d71178
Kielan <ki.lemons@gmail.com>
2023-03-12 13:16:49 -0700

    Create gpu_query.hh

---
+++ source/dune/gpu/intern/gpu_query.hh
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "lib_span.hh"
+
+namespace dune::gpu {
+
+#define QUERY_MIN_LEN 16
+
+typedef enum GPUQueryType {
+  GPU_QUERY_OCCLUSION = 0,
+} GPUQueryType;
+
+class QueryPool {
+ public:
+  virtual ~QueryPool(){};
+
+  /**
+   * Will start and end the query at this index inside the pool. The pool will resize
+   * automatically but does not support sparse allocation. So prefer using consecutive indices.
+   */
+  virtual void init(GPUQueryType type) = 0;
+
+  /**
+   * Will start and end the query at this index inside the pool.
+   * The pool will resize automatically.
+   */
+  virtual void begin_query() = 0;
+  virtual void end_query() = 0;
+
+  /**
+   * Must be fed with a buffer large enough to contain all the queries issued.
+   * IMPORTANT: Result for each query can be either binary or represent the number of samples
+   * drawn.
+   */
+  virtual void get_occlusion_result(MutableSpan<uint32_t> r_values) = 0;
+};
+
+}  // namespace dune::gpu
