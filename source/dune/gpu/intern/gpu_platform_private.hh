f6505d581e31c20f4febd3fb14b98b9dfdb43f4f
Kielan <ki.lemons@gmail.com>
2023-03-12 13:12:33 -0700

    Create gpu_platform_private.hh

---
+++ source/dune/gpu/intern/gpu_platform_private.hh
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "gpu_platform.h"
+
+namespace dune::gpu {
+
+class GPUPlatformGlobal {
+ public:
+  bool initialized = false;
+  eGPUDeviceType device;
+  eGPUOSType os;
+  eGPUDriverType driver;
+  eGPUSupportLevel support_level;
+  char *vendor = nullptr;
+  char *renderer = nullptr;
+  char *version = nullptr;
+  char *support_key = nullptr;
+  char *gpu_name = nullptr;
+  eGPUBackendType backend = GPU_BACKEND_NONE;
+
+ public:
+  void init(eGPUDeviceType gpu_device,
+            eGPUOSType os_type,
+            eGPUDriverType driver_type,
+            eGPUSupportLevel gpu_support_level,
+            eGPUBackendType backend,
+            const char *vendor_str,
+            const char *renderer_str,
+            const char *version_str);
+
+  void clear();
+};
+
+extern GPUPlatformGlobal GPG;
+
+}  // namespace dune::gpu
