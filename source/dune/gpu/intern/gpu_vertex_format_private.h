f4db4a6c6d0f03841020091c9758fe4eba251f0b
Kielan <ki.lemons@gmail.com>
2023-03-12 16:47:25 -0700

    Create gpu_vertex_format_private.h

---
+++ source/dune/gpu/intern/gpu_vertex_format_private.h
@@ -0,0 +1,17 @@
+/** GPU vertex format **/
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct GPUVertFormat;
+
+void VertexFormat_pack(struct GPUVertFormat *format);
+uint padding(uint offset, uint alignment);
+uint vertex_buffer_size(const struct GPUVertFormat *format, uint vertex_len);
+
+#ifdef __cplusplus
+}
+#endif
