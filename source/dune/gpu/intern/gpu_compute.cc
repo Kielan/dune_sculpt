63d76655eb57ae4bb5ac2ab7b2fac8b9e6c96f2b
Kielan <ki.lemons@gmail.com>
2023-03-13 08:16:47 -0700

    Update gpu_compute.cc

--- source/dune/gpu/intern/gpu_compute.cc
+++ source/dune/gpu/intern/gpu_compute.cc
@@ -1,24 +1,24 @@
-#include "GPU_compute.h"
+#include "gpu_compute.h"
 
 #include "gpu_backend.hh"
 #include "gpu_storage_buffer_private.hh"
 
-void GPU_compute_dispatch(GPUShader *shader,
+void gpu_compute_dispatch(GPUShader *shader,
                           uint groups_x_len,
                           uint groups_y_len,
                           uint groups_z_len)
 {
-  blender::gpu::GPUBackend &gpu_backend = *blender::gpu::GPUBackend::get();
-  GPU_shader_bind(shader);
+  dune::gpu::GPUBackend &gpu_backend = *dune::gpu::GPUBackend::get();
+  gpu_shader_bind(shader);
   gpu_backend.compute_dispatch(groups_x_len, groups_y_len, groups_z_len);
 }
 
-void GPU_compute_dispatch_indirect(GPUShader *shader, GPUStorageBuf *indirect_buf_)
+void gpu_compute_dispatch_indirect(GPUShader *shader, GPUStorageBuf *indirect_buf_)
 {
-  blender::gpu::GPUBackend &gpu_backend = *blender::gpu::GPUBackend::get();
-  blender::gpu::StorageBuf *indirect_buf = reinterpret_cast<blender::gpu::StorageBuf *>(
+  dune::gpu::GPUBackend &gpu_backend = *dune::gpu::GPUBackend::get();
+  dune::gpu::StorageBuf *indirect_buf = reinterpret_cast<dune::gpu::StorageBuf *>(
       indirect_buf_);
 
-  GPU_shader_bind(shader);
+  gpu_shader_bind(shader);
   gpu_backend.compute_dispatch_indirect(indirect_buf);
 }
