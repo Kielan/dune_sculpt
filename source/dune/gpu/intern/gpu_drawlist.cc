7234eadadd8ef23fb05d06167cd85cf26d01dda0
Kielan <ki.lemons@gmail.com>
2023-03-13 08:25:49 -0700

    Update gpu_drawlist.cc

--- source/dune/gpu/intern/gpu_drawlist.cc
+++ source/dune/gpu/intern/gpu_drawlist.cc
@@ -1,39 +1,35 @@
-/** \file
- * \ingroup gpu
- *
- * Implementation of Multi Draw Indirect.
- */
+/** Implementation of Multi Draw Indirect. */
 
-#include "MEM_guardedalloc.h"
+#include "mem_guardedalloc.h"
 
-#include "GPU_batch.h"
-#include "GPU_drawlist.h"
+#include "gpu_batch.h"
+#include "gpu_drawlist.h"
 
 #include "gpu_backend.hh"
 
 #include "gpu_drawlist_private.hh"
 
-using namespace blender::gpu;
+using namespace dune::gpu;
 
-GPUDrawList *GPU_draw_list_create(int list_length)
+GPUDrawList *gpu_draw_list_create(int list_length)
 {
   DrawList *list_ptr = GPUBackend::get()->drawlist_alloc(list_length);
   return wrap(list_ptr);
 }
 
-void GPU_draw_list_discard(GPUDrawList *list)
+void gpu_draw_list_discard(GPUDrawList *list)
 {
   DrawList *list_ptr = unwrap(list);
   delete list_ptr;
 }
 
-void GPU_draw_list_append(GPUDrawList *list, GPUBatch *batch, int i_first, int i_count)
+void gpu_draw_list_append(GPUDrawList *list, GPUBatch *batch, int i_first, int i_count)
 {
   DrawList *list_ptr = unwrap(list);
   list_ptr->append(batch, i_first, i_count);
 }
 
-void GPU_draw_list_submit(GPUDrawList *list)
+void gpu_draw_list_submit(GPUDrawList *list)
 {
   DrawList *list_ptr = unwrap(list);
   list_ptr->submit();
