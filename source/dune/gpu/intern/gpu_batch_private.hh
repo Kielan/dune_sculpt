60723824910b18db60f6aacc5941734077ceadab
Kielan <ki.lemons@gmail.com>
2023-03-12 18:43:53 -0700

    Update gpu_batch_private.hh

--- source/dune/gpu/intern/gpu_batch_private.hh
+++ source/dune/gpu/intern/gpu_batch_private.hh
@@ -1,25 +1,22 @@
-/** \file
- * \ingroup gpu
- *
- * GPU geometry batch
+/** GPU geometry batch
  * Contains VAOs + VBOs + Shader representing a drawable entity.
  */
 
 #pragma once
 
-#include "GPU_batch.h"
-#include "GPU_context.h"
+#include "gpu_batch.h"
+#include "gpu_context.h"
 
 #include "gpu_index_buffer_private.hh"
 #include "gpu_vertex_buffer_private.hh"
 
-namespace blender {
+namespace dune {
 namespace gpu {
 
 /**
  * Base class which is then specialized for each implementation (GL, VK, ...).
  *
- * \note Extends #GPUBatch as we still needs to expose some of the internals to the outside C code.
+ * note Extends #GPUBatch as we still needs to expose some of the internals to the outside C code.
  */
 class Batch : public GPUBatch {
  public:
