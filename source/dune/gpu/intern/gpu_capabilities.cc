02eaddf074cf15c263604958c2a35723597cf963
Kielan <ki.lemons@gmail.com>
2023-03-12 21:39:39 -0700

    Update gpu_capabilities.cc

--- source/dune/gpu/intern/gpu_capabilities.cc
+++ source/dune/gpu/intern/gpu_capabilities.cc
@@ -70,104 +70,101 @@ int gpu_max_work_group_size(int index)
   return GCaps.max_work_group_size[index];
 }
 
-int GPU_max_uniforms_vert()
+int gpu_max_uniforms_vert()
 {
   return GCaps.max_uniforms_vert;
 }
 
-int GPU_max_uniforms_frag()
+int gpu_max_uniforms_frag()
 {
   return GCaps.max_uniforms_frag;
 }
 
-int GPU_max_batch_indices()
+int gpu_max_batch_indices()
 {
   return GCaps.max_batch_indices;
 }
 
-int GPU_max_batch_vertices()
+int gpu_max_batch_vertices()
 {
   return GCaps.max_batch_vertices;
 }
 
-int GPU_max_vertex_attribs()
+int gpu_max_vertex_attribs()
 {
   return GCaps.max_vertex_attribs;
 }
 
-int GPU_max_varying_floats()
+int gpu_max_varying_floats()
 {
   return GCaps.max_varying_floats;
 }
 
-int GPU_extensions_len()
+int gpu_extensions_len()
 {
   return GCaps.extensions_len;
 }
 
-const char *GPU_extension_get(int i)
+const char *gpu_extension_get(int i)
 {
   return GCaps.extension_get ? GCaps.extension_get(i) : "\0";
 }
 
-bool GPU_mip_render_workaround()
+bool gpu_mip_render_workaround()
 {
   return GCaps.mip_render_workaround;
 }
 
-bool GPU_depth_blitting_workaround()
+bool gpu_depth_blitting_workaround()
 {
   return GCaps.depth_blitting_workaround;
 }
 
-bool GPU_use_main_context_workaround()
+bool gpu_use_main_context_workaround()
 {
   return GCaps.use_main_context_workaround;
 }
 
-bool GPU_crappy_amd_driver()
+bool gpu_crappy_amd_driver()
 {
   /* Currently are the same drivers with the `unused_fb_slot` problem. */
   return GCaps.broken_amd_driver;
 }
 
-bool GPU_use_hq_normals_workaround()
+bool gpu_use_hq_normals_workaround()
 {
   return GCaps.use_hq_normals_workaround;
 }
 
-bool GPU_compute_shader_support()
+bool gpu_compute_shader_support()
 {
   return GCaps.compute_shader_support;
 }
 
-bool GPU_shader_storage_buffer_objects_support()
+bool gpu_shader_storage_buffer_objects_support()
 {
   return GCaps.shader_storage_buffer_objects_support;
 }
 
-bool GPU_shader_image_load_store_support()
+bool gpu_shader_image_load_store_support()
 {
   return GCaps.shader_image_load_store_support;
 }
 
-int GPU_max_shader_storage_buffer_bindings()
+int gpu_max_shader_storage_buffer_bindings()
 {
   return GCaps.max_shader_storage_buffer_bindings;
 }
 
-/** \} */
-
 /* -------------------------------------------------------------------- */
-/** \name Memory statistics
- * \{ */
+/** Memory statistics **/
 
-bool GPU_mem_stats_supported()
+bool gpu_mem_stats_supported()
 {
   return GCaps.mem_stats_support;
 }
 
-void GPU_mem_stats_get(int *totalmem, int *freemem)
+void gpu_mem_stats_get(int *totalmem, int *freemem)
 {
   Context::get()->memory_statistics_get(totalmem, freemem);
 }
