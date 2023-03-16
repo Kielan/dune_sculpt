190965dcb438a8d867f67b162cacb9754bf548a6
Kielan <ki.lemons@gmail.com>
2023-03-16 07:57:46 -0700

    Update gpu_viewport.c

--- source/dune/gpu/intern/gpu_viewport.c
+++ source/dune/gpu/intern/gpu_viewport.c
@@ -117,26 +117,26 @@ static void gpu_viewport_textures_create(GPUViewport *viewport)
   float empty_pixel[4] = {0.0f, 0.0f, 0.0f, 0.0f};
 
   if (viewport->color_render_tx[0] == NULL) {
-    viewport->color_render_tx[0] = GPU_texture_create_2d(
+    viewport->color_render_tx[0] = gpu_texture_create_2d(
         "dtxl_color", UNPACK2(size), 1, GPU_RGBA16F, NULL);
     gpu_texture_clear(viewport->color_render_tx[0], GPU_DATA_FLOAT, empty_pixel);
-    viewport->color_overlay_tx[0] = GPU_texture_create_2d(
+    viewport->color_overlay_tx[0] = gpu_texture_create_2d(
         "dtxl_color_overlay", UNPACK2(size), 1, GPU_SRGB8_A8, NULL);
     gpu_texture_clear(viewport->color_overlay_tx[0], GPU_DATA_FLOAT, empty_pixel);
   }
 
   if ((viewport->flag & GPU_VIEWPORT_STEREO) != 0 && viewport->color_render_tx[1] == NULL) {
-    viewport->color_render_tx[1] = GPU_texture_create_2d(
+    viewport->color_render_tx[1] = gpu_texture_create_2d(
         "dtxl_color_stereo", UNPACK2(size), 1, GPU_RGBA16F, NULL);
     gpu_texture_clear(viewport->color_render_tx[1], GPU_DATA_FLOAT, empty_pixel);
-    viewport->color_overlay_tx[1] = GPU_texture_create_2d(
+    viewport->color_overlay_tx[1] = gpu_texture_create_2d(
         "dtxl_color_overlay_stereo", UNPACK2(size), 1, GPU_SRGB8_A8, NULL);
     gpu_texture_clear(viewport->color_overlay_tx[1], GPU_DATA_FLOAT, empty_pixel);
   }
 
   /* Can be shared with GPUOffscreen. */
   if (viewport->depth_tx == NULL) {
-    viewport->depth_tx = GPU_texture_create_2d(
+    viewport->depth_tx = gpu_texture_create_2d(
         "dtxl_depth", UNPACK2(size), 1, GPU_DEPTH24_STENCIL8, NULL);
   }
 
@@ -340,7 +340,7 @@ static GPUBatch *gpu_viewport_batch_create(const rctf *rect_pos, const rctf *rec
   GPU_vertbuf_attr_get_raw_data(vbo, g_viewport.attr_id.pos, &pos_step);
   GPU_vertbuf_attr_get_raw_data(vbo, g_viewport.attr_id.tex_coord, &tex_coord_step);
 
-  copy_v2_fl2(GPU_vertbuf_raw_step(&pos_step), rect_pos->xmin, rect_pos->ymin);
+  copy_v2_fl2(gpu_vertbuf_raw_step(&pos_step), rect_pos->xmin, rect_pos->ymin);
   copy_v2_fl2(GPU_vertbuf_raw_step(&tex_coord_step), rect_uv->xmin, rect_uv->ymin);
   copy_v2_fl2(GPU_vertbuf_raw_step(&pos_step), rect_pos->xmax, rect_pos->ymin);
   copy_v2_fl2(GPU_vertbuf_raw_step(&tex_coord_step), rect_uv->xmax, rect_uv->ymin);
