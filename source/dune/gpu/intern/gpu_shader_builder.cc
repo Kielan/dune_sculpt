48b0f2deb42ae4c4fe48343e39285820d3c48a30
Kielan <ki.lemons@gmail.com>
2023-03-18 09:14:02 -0700

    Update gpu_shader_builder.cc

--- source/dune/gpu/intern/gpu_shader_builder.cc
+++ source/dune/gpu/intern/gpu_shader_builder.cc
@@ -1,20 +1,16 @@
-/** \file
- * \ingroup gpu
- *
- * Compile time automation of shader compilation and validation.
- */
+/** Compile time automation of shader compilation and validation. **/
 
 #include <iostream>
 
 #include "GHOST_C-api.h"
 
-#include "GPU_context.h"
-#include "GPU_init_exit.h"
+#include "gpu_context.h"
+#include "gpu_init_exit.h"
 #include "gpu_shader_create_info_private.hh"
 
 #include "CLG_log.h"
 
-namespace blender::gpu::shader_builder {
+namespace dune::gpu::shader_builder {
 
 class ShaderBuilder {
  private:
@@ -43,15 +39,15 @@ void ShaderBuilder::init()
   GHOST_ActivateOpenGLContext(ghost_context_);
 
   gpu_context_ = GPU_context_create(nullptr);
-  GPU_init();
+  gpu_init();
 }
 
 void ShaderBuilder::exit()
 {
-  GPU_backend_exit();
-  GPU_exit();
+  gpu_backend_exit();
+  gpu_exit();
 
-  GPU_context_discard(gpu_context_);
+  gpu_context_discard(gpu_context_);
 
   GHOST_DisposeOpenGLContext(ghost_system_, ghost_context_);
   GHOST_DisposeSystem(ghost_system_);
@@ -59,9 +55,9 @@ void ShaderBuilder::exit()
   CLG_exit();
 }
 
-}  // namespace blender::gpu::shader_builder
+}  // namespace dune::gpu::shader_builder
 
-/** \brief Entry point for the shader_builder. */
+/** brief Entry point for the shader_builder. */
 int main(int argc, const char *argv[])
 {
   if (argc < 2) {
@@ -71,7 +67,7 @@ int main(int argc, const char *argv[])
 
   int exit_code = 0;
 
-  blender::gpu::shader_builder::ShaderBuilder builder;
+  dune::gpu::shader_builder::ShaderBuilder builder;
   builder.init();
   if (!builder.bake_create_infos()) {
     exit_code = 1;
