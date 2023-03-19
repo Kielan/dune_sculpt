0fc2b821af481e3920ac5d28c9708bfe34733284
Kielan <ki.lemons@gmail.com>
2023-03-19 10:41:04 -0700

    Create external_engine.h

---
+++ source/dune/draw/engines/external/external_engine.h
@@ -0,0 +1,11 @@
+#pragma once
+
+extern DrawEngineType draw_engine_external_type;
+extern RenderEngineType draw_engine_viewport_external_type;
+
+/* Check whether an external engine is to be used to draw content of an image editor.
+ * If the drawing is possible, the render engine is "acquired" so that it is not freed by the
+ * render engine for until drawing is finished.
+ *
+ * NOTE: Released by the draw engine when it is done drawing. */
+bool draw_engine_external_acquire_for_image_editor(void);
