deb87a26a40b22edb51ee6247e2b4bfc14c86724
Kielan <ki.lemons@gmail.com>
2023-03-23 15:28:25 -0700

    Update overlay_lattice.c

--- source/dune/draw/engines/overlay/overlay_lattice.c
+++ source/dune/draw/engines/overlay/overlay_lattice.c
@@ -1,61 +1,61 @@
-#include "DRW_render.h"
+#include "draw_render.h"
 
 #include "overlay_private.h"
 
-void OVERLAY_edit_lattice_cache_init(OVERLAY_Data *vedata)
+void overlay_edit_lattice_cache_init(OVERLAY_Data *vedata)
 {
-  OVERLAY_PassList *psl = vedata->psl;
-  OVERLAY_PrivateData *pd = vedata->stl->pd;
+  OverlayPassList *psl = vedata->psl;
+  OverlayPrivateData *pd = vedata->stl->pd;
   struct GPUShader *sh;
-  DRWShadingGroup *grp;
+  DrawShadingGroup *grp;
 
   {
-    DRWState state = DRW_STATE_WRITE_COLOR | DRW_STATE_WRITE_DEPTH | DRW_STATE_DEPTH_LESS_EQUAL;
-    DRW_PASS_CREATE(psl->edit_lattice_ps, state | pd->clipping_state);
+    DrawState state = DRAW_STATE_WRITE_COLOR | DRAW_STATE_WRITE_DEPTH | DRW_STATE_DEPTH_LESS_EQUAL;
+    DRAW_PASS_CREATE(psl->edit_lattice_ps, state | pd->clipping_state);
 
-    sh = OVERLAY_shader_edit_lattice_wire();
-    pd->edit_lattice_wires_grp = grp = DRW_shgroup_create(sh, psl->edit_lattice_ps);
-    DRW_shgroup_uniform_block(grp, "globalsBlock", G_draw.block_ubo);
-    DRW_shgroup_uniform_texture(grp, "weightTex", G_draw.weight_ramp);
+    sh = overlay_shader_edit_lattice_wire();
+    pd->edit_lattice_wires_grp = grp = draw_shgroup_create(sh, psl->edit_lattice_ps);
+    draw_shgroup_uniform_block(grp, "globalsBlock", G_draw.block_ubo);
+    draw_shgroup_uniform_texture(grp, "weightTex", G_draw.weight_ramp);
 
-    sh = OVERLAY_shader_edit_lattice_point();
-    pd->edit_lattice_points_grp = grp = DRW_shgroup_create(sh, psl->edit_lattice_ps);
-    DRW_shgroup_uniform_block(grp, "globalsBlock", G_draw.block_ubo);
+    sh = overlay_shader_edit_lattice_point();
+    pd->edit_lattice_points_grp = grp = draw_shgroup_create(sh, psl->edit_lattice_ps);
+    draw_shgroup_uniform_block(grp, "globalsBlock", G_draw.block_ubo);
   }
 }
 
-void OVERLAY_edit_lattice_cache_populate(OVERLAY_Data *vedata, Object *ob)
+void overlay_edit_lattice_cache_populate(OverlayData *vedata, Object *ob)
 {
-  OVERLAY_PrivateData *pd = vedata->stl->pd;
+  OverlayPrivateData *pd = vedata->stl->pd;
   struct GPUBatch *geom;
 
-  geom = DRW_cache_lattice_wire_get(ob, true);
-  DRW_shgroup_call(pd->edit_lattice_wires_grp, geom, ob);
+  geom = draw_cache_lattice_wire_get(ob, true);
+  draw_shgroup_call(pd->edit_lattice_wires_grp, geom, ob);
 
-  geom = DRW_cache_lattice_vert_overlay_get(ob);
-  DRW_shgroup_call(pd->edit_lattice_points_grp, geom, ob);
+  geom = draw_cache_lattice_vert_overlay_get(ob);
+  draw_shgroup_call(pd->edit_lattice_points_grp, geom, ob);
 }
 
-void OVERLAY_lattice_cache_populate(OVERLAY_Data *vedata, Object *ob)
+void overlay_lattice_cache_populate(OverlayData *vedata, Object *ob)
 {
-  OVERLAY_ExtraCallBuffers *cb = OVERLAY_extra_call_buffer_get(vedata, ob);
-  const DRWContextState *draw_ctx = DRW_context_state_get();
+  OverlayExtraCallBuffers *cb = overlay_extra_call_buffer_get(vedata, ob);
+  const DrawCtxState *draw_ctx = draw_ctx_state_get();
 
   float *color;
-  DRW_object_wire_theme_get(ob, draw_ctx->view_layer, &color);
+  draw_object_wire_theme_get(ob, draw_ctx->view_layer, &color);
 
-  struct GPUBatch *geom = DRW_cache_lattice_wire_get(ob, false);
-  OVERLAY_extra_wire(cb, geom, ob->obmat, color);
+  struct GPUBatch *geom = draw_cache_lattice_wire_get(ob, false);
+  overlay_extra_wire(cb, geom, ob->obmat, color);
 }
 
-void OVERLAY_edit_lattice_draw(OVERLAY_Data *vedata)
+void overlay_edit_lattice_draw(OverlayData *vedata)
 {
-  OVERLAY_PassList *psl = vedata->psl;
-  OVERLAY_FramebufferList *fbl = vedata->fbl;
+  OverlayPassList *psl = vedata->psl;
+  OverlayFramebufferList *fbl = vedata->fbl;
 
-  if (DRW_state_is_fbo()) {
-    GPU_framebuffer_bind(fbl->overlay_default_fb);
+  if (draw_state_is_fbo()) {
+    gpu_framebuffer_bind(fbl->overlay_default_fb);
   }
 
-  DRW_draw_pass(psl->edit_lattice_ps);
+  draw_draw_pass(psl->edit_lattice_ps);
 }
