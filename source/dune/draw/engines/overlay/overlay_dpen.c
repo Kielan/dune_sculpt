3d4e5ebfd1d8ecebca6545c0a244ac5e6c4ebdcc
Kielan <ki.lemons@gmail.com>
2023-03-23 07:57:36 -0700

    Update overlay_dpen.c

--- source/dune/draw/engines/overlay/overlay_dpen.c
+++ source/dune/draw/engines/overlay/overlay_dpen.c
@@ -149,9 +149,9 @@ void overlay_edit_dpen_cache_init(OverlayData *vedata)
   }
 
   /* control points for primitives and speed guide */
-  const bool is_cppoint = (gpd->runtime.tot_cp_points > 0);
-  const bool is_speed_guide = (ts->gp_sculpt.guide.use_guide &&
-                               (draw_ctx->object_mode == OB_MODE_PAINT_GPENCIL));
+  const bool is_cppoint = (dpd->runtime.tot_cp_points > 0);
+  const bool is_speed_guide = (ts->dp_sculpt.guide.use_guide &&
+                               (draw_ctx->object_mode == OB_MODE_PAINT_DPEN));
   const bool is_show_gizmo = (((v3d->gizmo_flag & V3D_GIZMO_HIDE) == 0) &&
                               ((v3d->gizmo_flag & V3D_GIZMO_HIDE_TOOL) == 0));
 
@@ -195,20 +195,20 @@ void overlay_edit_dpen_cache_init(OverlayData *vedata)
   }
 }
 
-void OVERLAY_gpencil_cache_init(OVERLAY_Data *vedata)
+void overlay_dpen_cache_init(OverlayData *vedata)
 {
-  OVERLAY_PassList *psl = vedata->psl;
-  OVERLAY_PrivateData *pd = vedata->stl->pd;
+  OverlayPassList *psl = vedata->psl;
+  OverlayPrivateData *pd = vedata->stl->pd;
   struct GPUShader *sh;
-  DRWShadingGroup *grp;
+  DrawShadingGroup *dsg;
 
   /* Default: Display nothing. */
-  psl->gpencil_canvas_ps = NULL;
+  psl->dpen_canvas_ps = NULL;
 
-  const DRWContextState *draw_ctx = DRW_context_state_get();
+  const DrawCtxState *draw_ctx = draw_ctx_state_get();
   View3D *v3d = draw_ctx->v3d;
   Object *ob = draw_ctx->obact;
-  bGPdata *gpd = ob ? (bGPdata *)ob->data : NULL;
+  DPenData *gpd = ob ? (DPenData *)ob->data : NULL;
   Scene *scene = draw_ctx->scene;
   ToolSettings *ts = scene->toolsettings;
   const View3DCursor *cursor = &scene->cursor;
@@ -216,14 +216,14 @@ void OVERLAY_gpencil_cache_init(OVERLAY_Data *vedata)
   pd->edit_curve.show_handles = v3d->overlay.handle_display != CURVE_HANDLE_NONE;
   pd->edit_curve.handle_display = v3d->overlay.handle_display;
 
-  if (gpd == NULL || ob->type != OB_GPENCIL) {
+  if (dpd == NULL || ob->type != OB_DPEN) {
     return;
   }
 
   const bool show_overlays = (v3d->flag2 & V3D_HIDE_OVERLAYS) == 0;
-  const bool show_grid = (v3d->gp_flag & V3D_GP_SHOW_GRID) != 0 &&
-                         ((ts->gpencil_v3d_align &
-                           (GP_PROJECT_DEPTH_VIEW | GP_PROJECT_DEPTH_STROKE)) == 0);
+  const bool show_grid = (v3d->dp_flag & V3D_GP_SHOW_GRID) != 0 &&
+                         ((ts->dpen_v3d_align &
+                           (DP_PROJECT_DEPTH_VIEW | GP_PROJECT_DEPTH_STROKE)) == 0);
   const bool grid_xray = (v3d->gp_flag & V3D_GP_SHOW_GRID_XRAY);
 
   if (show_grid && show_overlays) {
@@ -233,39 +233,39 @@ void OVERLAY_gpencil_cache_init(OVERLAY_Data *vedata)
     float size[2];
 
     /* set color */
-    copy_v3_v3(col_grid, gpd->grid.color);
-    col_grid[3] = max_ff(v3d->overlay.gpencil_grid_opacity, 0.01f);
+    copy_v3_v3(col_grid, dpd->grid.color);
+    col_grid[3] = max_ff(v3d->overlay.dpen_grid_opacity, 0.01f);
 
     copy_m4_m4(mat, ob->obmat);
 
     /* Rotate and scale except align to cursor. */
-    bGPDlayer *gpl = BKE_gpencil_layer_active_get(gpd);
-    if (gpl != NULL) {
-      if (ts->gp_sculpt.lock_axis != GP_LOCKAXIS_CURSOR) {
+    DPenDataLayer *dpl = dune_dpen_layer_active_get(dpd);
+    if (dpl != NULL) {
+      if (ts->dp_sculpt.lock_axis != DP_LOCKAXIS_CURSOR) {
         float matrot[3][3];
-        copy_m3_m4(matrot, gpl->layer_mat);
+        copy_m3_m4(matrot, dpl->layer_mat);
         mul_m4_m4m3(mat, mat, matrot);
       }
     }
 
     float viewinv[4][4];
     /* Set the grid in the selected axis */
-    switch (ts->gp_sculpt.lock_axis) {
-      case GP_LOCKAXIS_X:
+    switch (ts->dp_sculpt.lock_axis) {
+      case DP_LOCKAXIS_X:
         swap_v4_v4(mat[0], mat[2]);
         break;
-      case GP_LOCKAXIS_Y:
+      case DP_LOCKAXIS_Y:
         swap_v4_v4(mat[1], mat[2]);
         break;
-      case GP_LOCKAXIS_Z:
+      case DP_LOCKAXIS_Z:
         /* Default. */
         break;
-      case GP_LOCKAXIS_CURSOR:
+      case DP_LOCKAXIS_CURSOR:
         loc_eul_size_to_mat4(mat, cursor->location, cursor->rotation_euler, (float[3]){1, 1, 1});
         break;
-      case GP_LOCKAXIS_VIEW:
+      case DP_LOCKAXIS_VIEW:
         /* view aligned */
-        DRW_view_viewmat_get(NULL, viewinv, true);
+        draw_view_viewmat_get(NULL, viewinv, true);
         copy_v3_v3(mat[0], viewinv[0]);
         copy_v3_v3(mat[1], viewinv[1]);
         break;
@@ -273,47 +273,47 @@ void OVERLAY_gpencil_cache_init(OVERLAY_Data *vedata)
 
     /* Move the grid to the right location depending of the align type.
      * This is required only for 3D Cursor or Origin. */
-    if (ts->gpencil_v3d_align & GP_PROJECT_CURSOR) {
+    if (ts->dpen_v3d_align & DP_PROJECT_CURSOR) {
       copy_v3_v3(mat[3], cursor->location);
     }
-    else if (ts->gpencil_v3d_align & GP_PROJECT_VIEWSPACE) {
+    else if (ts->dpen_v3d_align & DP_PROJECT_VIEWSPACE) {
       copy_v3_v3(mat[3], ob->obmat[3]);
     }
 
-    translate_m4(mat, gpd->grid.offset[0], gpd->grid.offset[1], 0.0f);
-    mul_v2_v2fl(size, gpd->grid.scale, 2.0f * ED_scene_grid_scale(scene, &grid_unit));
+    translate_m4(mat, dpd->grid.offset[0], dpd->grid.offset[1], 0.0f);
+    mul_v2_v2fl(size, dpd->grid.scale, 2.0f * ed_scene_grid_scale(scene, &grid_unit));
     rescale_m4(mat, (float[3]){size[0], size[1], 0.0f});
 
     /* Apply layer loc transform, except cursor mode. */
-    if ((gpl != NULL) && (ts->gpencil_v3d_align & GP_PROJECT_CURSOR) == 0) {
-      add_v3_v3(mat[3], gpl->layer_mat[3]);
+    if ((dpl != NULL) && (ts->dpen_v3d_align & DP_PROJECT_CURSOR) == 0) {
+      add_v3_v3(mat[3], dpl->layer_mat[3]);
     }
 
-    const int gridlines = (gpd->grid.lines <= 0) ? 1 : gpd->grid.lines;
+    const int gridlines = (dpd->grid.lines <= 0) ? 1 : gpd->grid.lines;
     int line_ct = gridlines * 4 + 2;
 
-    DRWState state = DRW_STATE_WRITE_COLOR | DRW_STATE_BLEND_ALPHA;
-    state |= (grid_xray) ? DRW_STATE_DEPTH_ALWAYS : DRW_STATE_DEPTH_LESS_EQUAL;
+    DrawState state = DRAW_STATE_WRITE_COLOR | DRW_STATE_BLEND_ALPHA;
+    state |= (grid_xray) ? DRAW_STATE_DEPTH_ALWAYS : DRAW_STATE_DEPTH_LESS_EQUAL;
 
-    DRW_PASS_CREATE(psl->gpencil_canvas_ps, state);
+    DRAW_PASS_CREATE(psl->dpen_canvas_ps, state);
 
-    sh = OVERLAY_shader_gpencil_canvas();
-    grp = DRW_shgroup_create(sh, psl->gpencil_canvas_ps);
-    DRW_shgroup_uniform_block(grp, "globalsBlock", G_draw.block_ubo);
-    DRW_shgroup_uniform_vec4_copy(grp, "color", col_grid);
-    DRW_shgroup_uniform_vec3_copy(grp, "xAxis", mat[0]);
-    DRW_shgroup_uniform_vec3_copy(grp, "yAxis", mat[1]);
-    DRW_shgroup_uniform_vec3_copy(grp, "origin", mat[3]);
-    DRW_shgroup_uniform_int_copy(grp, "halfLineCount", line_ct / 2);
-    DRW_shgroup_call_procedural_lines(grp, NULL, line_ct);
+    sh = overlay_shader_dpen_canvas();
+    grp = draw_shgroup_create(sh, psl->dpen_canvas_ps);
+    draw_shgroup_uniform_block(grp, "globalsBlock", G_draw.block_ubo);
+    draw_shgroup_uniform_vec4_copy(grp, "color", col_grid);
+    draw_shgroup_uniform_vec3_copy(grp, "xAxis", mat[0]);
+    draw_shgroup_uniform_vec3_copy(grp, "yAxis", mat[1]);
+    draw_shgroup_uniform_vec3_copy(grp, "origin", mat[3]);
+    draw_shgroup_uniform_int_copy(grp, "halfLineCount", line_ct / 2);
+    draw_shgroup_call_procedural_lines(grp, NULL, line_ct);
   }
 }
 
-static void OVERLAY_edit_gpencil_cache_populate(OVERLAY_Data *vedata, Object *ob)
+static void overlay_edit_dpen_cache_populate(OverlayData *vedata, Object *ob)
 {
-  OVERLAY_PrivateData *pd = vedata->stl->pd;
-  bGPdata *gpd = (bGPdata *)ob->data;
-  const DRWContextState *draw_ctx = DRW_context_state_get();
+  OverlayPrivateData *pd = vedata->stl->pd;
+  DPenData *dpd = (DPenData *)ob->data;
+  const DrawCtxState *draw_ctx = draw_ctx_state_get();
   View3D *v3d = draw_ctx->v3d;
 
   /* Overlay is only for active object. */
@@ -321,82 +321,82 @@ static void OVERLAY_edit_gpencil_cache_populate(OVERLAY_Data *vedata, Object *ob
     return;
   }
 
-  if (pd->edit_gpencil_wires_grp) {
-    DRWShadingGroup *grp = DRW_shgroup_create_sub(pd->edit_gpencil_wires_grp);
-    DRW_shgroup_uniform_vec4_copy(grp, "gpEditColor", gpd->line_color);
+  if (pd->edit_dpen_wires_grp) {
+    DrawShadingGroup *grp = draw_shgroup_create_sub(pd->edit_dpen_wires_grp);
+    draw_shgroup_uniform_vec4_copy(grp, "dpEditColor", dpd->line_color);
 
-    struct GPUBatch *geom = DRW_cache_gpencil_edit_lines_get(ob, pd->cfra);
-    DRW_shgroup_call_no_cull(pd->edit_gpencil_wires_grp, geom, ob);
+    struct GPUBatch *geom = draw_cache_dpen_edit_lines_get(ob, pd->cfra);
+    draw_shgroup_call_no_cull(pd->edit_dpen_wires_grp, geom, ob);
   }
 
-  if (pd->edit_gpencil_points_grp) {
+  if (pd->edit_dpen_points_grp) {
     const bool show_direction = (v3d->gp_flag & V3D_GP_SHOW_STROKE_DIRECTION) != 0;
 
-    DRWShadingGroup *grp = DRW_shgroup_create_sub(pd->edit_gpencil_points_grp);
-    DRW_shgroup_uniform_float_copy(grp, "doStrokeEndpoints", show_direction);
+    DrawShadingGroup *grp = draw_shgroup_create_sub(pd->edit_dpen_points_grp);
+    draw_shgroup_uniform_float_copy(grp, "doStrokeEndpoints", show_direction);
 
-    struct GPUBatch *geom = DRW_cache_gpencil_edit_points_get(ob, pd->cfra);
-    DRW_shgroup_call_no_cull(grp, geom, ob);
+    struct GPUBatch *geom = draw_cache_dpen_edit_points_get(ob, pd->cfra);
+    draw_shgroup_call_no_cull(grp, geom, ob);
   }
 
-  if (pd->edit_gpencil_curve_handle_grp) {
-    struct GPUBatch *geom = DRW_cache_gpencil_edit_curve_handles_get(ob, pd->cfra);
+  if (pd->edit_dpen_curve_handle_grp) {
+    struct GPUBatch *geom = draw_cache_dpen_edit_curve_handles_get(ob, pd->cfra);
     if (geom) {
-      DRW_shgroup_call_no_cull(pd->edit_gpencil_curve_handle_grp, geom, ob);
+      draw_shgroup_call_no_cull(pd->edit_dpen_curve_handle_grp, geom, ob);
     }
   }
 
   if (pd->edit_gpencil_curve_points_grp) {
-    struct GPUBatch *geom = DRW_cache_gpencil_edit_curve_points_get(ob, pd->cfra);
+    struct GPUBatch *geom = draw_cache_dpen_edit_curve_points_get(ob, pd->cfra);
     if (geom) {
-      DRW_shgroup_call_no_cull(pd->edit_gpencil_curve_points_grp, geom, ob);
+      draw_shgroup_call_no_cull(pd->edit_dpen_curve_points_grp, geom, ob);
     }
   }
 }
 
-static void overlay_gpencil_draw_stroke_color_name(bGPDlayer *UNUSED(gpl),
-                                                   bGPDframe *UNUSED(gpf),
-                                                   bGPDstroke *gps,
+static void overlay_dpen_draw_stroke_color_name(DPenDataLayer *UNUSED(dpl),
+                                                   DPenDataFrame *UNUSED(dpf),
+                                                   DPenDataStroke *dps,
                                                    void *thunk)
 {
   Object *ob = (Object *)thunk;
-  Material *ma = BKE_object_material_get_eval(ob, gps->mat_nr + 1);
+  Material *ma = dune_object_material_get_eval(ob, dps->mat_nr + 1);
   if (ma == NULL) {
     return;
   }
-  MaterialGPencilStyle *gp_style = ma->gp_style;
+  MaterialDPenStyle *dp_style = ma->dp_style;
   /* skip stroke if it doesn't have any valid data */
-  if ((gps->points == NULL) || (gps->totpoints < 1) || (gp_style == NULL)) {
+  if ((dps->points == NULL) || (dps->totpoints < 1) || (gp_style == NULL)) {
     return;
   }
   /* check if the color is visible */
-  if (gp_style->flag & GP_MATERIAL_HIDE) {
+  if (dp_style->flag & DP_MATERIAL_HIDE) {
     return;
   }
   /* only if selected */
-  if (gps->flag & GP_STROKE_SELECT) {
-    for (int i = 0; i < gps->totpoints; i++) {
-      bGPDspoint *pt = &gps->points[i];
+  if (dps->flag & DPEN_STROKE_SELECT) {
+    for (int i = 0; i < dps->totpoints; i++) {
+      DPenStrokePoint *pt = &dps->points[i];
       /* Draw name at the first selected point. */
       if (pt->flag & GP_SPOINT_SELECT) {
-        const DRWContextState *draw_ctx = DRW_context_state_get();
+        const DrawCtxState *draw_ctx = draw_ctx_state_get();
         ViewLayer *view_layer = draw_ctx->view_layer;
 
-        int theme_id = DRW_object_wire_theme_get(ob, view_layer, NULL);
+        int theme_id = draw_object_wire_theme_get(ob, view_layer, NULL);
         uchar color[4];
-        UI_GetThemeColor4ubv(theme_id, color);
+        ui_GetThemeColor4ubv(theme_id, color);
 
         float fpt[3];
         mul_v3_m4v3(fpt, ob->obmat, &pt->x);
 
-        struct DRWTextStore *dt = DRW_text_cache_ensure();
-        DRW_text_cache_add(dt,
+        struct DrawTextStore *dt = draw_text_cache_ensure();
+        draw_text_cache_add(dt,
                            fpt,
                            ma->id.name + 2,
                            strlen(ma->id.name + 2),
                            10,
                            0,
-                           DRW_TEXT_CACHE_GLOBALSPACE | DRW_TEXT_CACHE_STRING_PTR,
+                           DRAW_TEXT_CACHE_GLOBALSPACE | DRW_TEXT_CACHE_STRING_PTR,
                            color);
         break;
       }
@@ -404,50 +404,50 @@ static void overlay_gpencil_draw_stroke_color_name(bGPDlayer *UNUSED(gpl),
   }
 }
 
-static void OVERLAY_gpencil_color_names(Object *ob)
+static void overlay_dpen_color_names(Object *ob)
 {
-  const DRWContextState *draw_ctx = DRW_context_state_get();
-  int cfra = DEG_get_ctime(draw_ctx->depsgraph);
+  const DrawCtxState *draw_ctx = draw_ctx_state_get();
+  int cfra = dgraph_get_ctime(draw_ctx->dgraph);
 
-  BKE_gpencil_visible_stroke_advanced_iter(
-      NULL, ob, NULL, overlay_gpencil_draw_stroke_color_name, ob, false, cfra);
+  dune_dpen_visible_stroke_advanced_iter(
+      NULL, ob, NULL, overlay_dpen_draw_stroke_color_name, ob, false, cfra);
 }
 
-void OVERLAY_gpencil_cache_populate(OVERLAY_Data *vedata, Object *ob)
+void overlay_dpen_cache_populate(OverlayData *vedata, Object *ob)
 {
-  const DRWContextState *draw_ctx = DRW_context_state_get();
+  const DrawCtxState *draw_ctx = draw_ctx_state_get();
   View3D *v3d = draw_ctx->v3d;
 
-  bGPdata *gpd = (bGPdata *)ob->data;
-  if (gpd == NULL) {
+  DPenData *dpd = (DPenData *)ob->data;
+  if (dpd == NULL) {
     return;
   }
 
-  if (GPENCIL_ANY_MODE(gpd)) {
-    OVERLAY_edit_gpencil_cache_populate(vedata, ob);
+  if (DPEN_ANY_MODE(dpd)) {
+    overlay_edit_dpen_cache_populate(vedata, ob);
   }
 
   /* don't show object extras in set's */
   if ((ob->base_flag & (BASE_FROM_SET | BASE_FROM_DUPLI)) == 0) {
-    if ((v3d->gp_flag & V3D_GP_SHOW_MATERIAL_NAME) && (ob->mode == OB_MODE_EDIT_GPENCIL) &&
-        DRW_state_show_text()) {
-      OVERLAY_gpencil_color_names(ob);
+    if ((v3d->gp_flag & V3D_GP_SHOW_MATERIAL_NAME) && (ob->mode == OB_MODE_EDIT_DPEN) &&
+        draw_state_show_text()) {
+      overlay_dpen_color_names(ob);
     }
   }
 }
 
-void OVERLAY_gpencil_draw(OVERLAY_Data *vedata)
+void overlay_dpen_draw(OverlayData *vedata)
 {
-  OVERLAY_PassList *psl = vedata->psl;
+  OverlayPassList *psl = vedata->psl;
 
-  if (psl->gpencil_canvas_ps) {
-    DRW_draw_pass(psl->gpencil_canvas_ps);
+  if (psl->dpen_canvas_ps) {
+    draw_draw_pass(psl->dpen_canvas_ps);
   }
 }
 
-void overlay_edit_dpen_draw(OVERLAY_Data *vedata)
+void overlay_edit_dpen_draw(OverlayData *vedata)
 {
-  overlay_PassList *psl = vedata->psl;
+  OverlayPassList *psl = vedata->psl;
 
   if (psl->edit_dpen_gizmos_ps) {
     draw_draw_pass(psl->edit_dpen_gizmos_ps);
