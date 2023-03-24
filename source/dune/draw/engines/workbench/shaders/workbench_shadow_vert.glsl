e0977624ff37c6060e46c729602b5a71c423ff14
Kielan <ki.lemons@gmail.com>
2023-03-24 06:53:15 -0700

    Create workbench_shadow_vert.glsl

---
+++ source/dune/draw/engines/workbench/shaders/workbench_shadow_vert.glsl
@@ -0,0 +1,8 @@
+#pragma BLENDER_REQUIRE(common_view_lib.glsl)
+
+void main()
+{
+  vData.pos = pos;
+  vData.frontPosition = point_object_to_ndc(pos);
+  vData.backPosition = point_object_to_ndc(pos + lightDirection * lightDistance);
+}
