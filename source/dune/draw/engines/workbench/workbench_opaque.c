aa46db31c7a45dae24ea79c2567b7838095fc532
Kielan <ki.lemons@gmail.com>
2023-03-25 05:34:18 -0700

    Update workbench_opaque.c

--- source/dune/draw/engines/workbench/workbench_opaque.c
+++ source/dune/draw/engines/workbench/workbench_opaque.c
@@ -16,21 +16,21 @@
 #include "workbench_engine.h"
 #include "workbench_private.h"
 
-void workbench_opaque_engine_init(WORKBENCH_Data *data)
+void workbench_opaque_engine_init(DBenchData *data)
 {
   DBenchFramebufferList *fbl = data->fbl;
   WORKBENCH_PrivateData *wpd = data->stl->wpd;
-  DefaultTextureList *dtxl = DRW_viewport_texture_list_get();
+  DefaultTextureList *dtxl = draw_viewport_texture_list_get();
   DrawEngineType *owner = (DrawEngineType *)&workbench_opaque_engine_init;
 
   /* Reused the same textures format for transparent pipeline to share the textures. */
   const eGPUTextureFormat col_tex_format = GPU_RGBA16F;
   const eGPUTextureFormat nor_tex_format = NORMAL_ENCODING_ENABLED() ? GPU_RG16F : GPU_RGBA16F;
 
-  wpd->material_buffer_tx = DRW_texture_pool_query_fullscreen(col_tex_format, owner);
-  wpd->normal_buffer_tx = DRW_texture_pool_query_fullscreen(nor_tex_format, owner);
+  wpd->material_buffer_tx = draw_texture_pool_query_fullscreen(col_tex_format, owner);
+  wpd->normal_buffer_tx = draw_texture_pool_query_fullscreen(nor_tex_format, owner);
 
-  GPU_framebuffer_ensure_config(&fbl->opaque_fb,
+  gpu_framebuffer_ensure_config(&fbl->opaque_fb,
                                 {
                                     GPU_ATTACHMENT_TEXTURE(dtxl->depth),
                                     GPU_ATTACHMENT_TEXTURE(wpd->material_buffer_tx),
@@ -84,50 +84,50 @@ void workbench_opaque_cache_init(WORKBENCH_Data *vedata)
         sh = workbench_shader_opaque_image_get(wpd, data, false);
 
         wpd->prepass[opaque][infront][data].image_shgrp = grp = DRW_shgroup_create(sh, pass);
-        DRW_shgroup_uniform_block(grp, "world_data", wpd->world_ubo);
-        DRW_shgroup_uniform_block(grp, "materials_data", wpd->material_ubo_curr);
-        DRW_shgroup_uniform_int_copy(grp, "materialIndex", 0); /* Default material. */
-        DRW_shgroup_uniform_bool_copy(grp, "useMatcap", use_matcap);
+        draw_shgroup_uniform_block(grp, "world_data", wpd->world_ubo);
+        draw_shgroup_uniform_block(grp, "materials_data", wpd->material_ubo_curr);
+        draw_shgroup_uniform_int_copy(grp, "materialIndex", 0); /* Default material. */
+        draw_shgroup_uniform_bool_copy(grp, "useMatcap", use_matcap);
 
         sh = workbench_shader_opaque_image_get(wpd, data, true);
 
         wpd->prepass[opaque][infront][data].image_tiled_shgrp = grp = DRW_shgroup_create(sh, pass);
-        DRW_shgroup_uniform_block(grp, "world_data", wpd->world_ubo);
-        DRW_shgroup_uniform_block(grp, "materials_data", wpd->material_ubo_curr);
-        DRW_shgroup_uniform_int_copy(grp, "materialIndex", 0); /* Default material. */
-        DRW_shgroup_uniform_bool_copy(grp, "useMatcap", use_matcap);
+        draw_shgroup_uniform_block(grp, "world_data", wpd->world_ubo);
+        draw_shgroup_uniform_block(grp, "materials_data", wpd->material_ubo_curr);
+        draw_shgroup_uniform_int_copy(grp, "materialIndex", 0); /* Default material. */
+        draw_shgroup_uniform_bool_copy(grp, "useMatcap", use_matcap);
       }
     }
   }
   {
-    DRWState state = DRW_STATE_WRITE_COLOR | DRW_STATE_DEPTH_GREATER | DRW_STATE_STENCIL_EQUAL;
+    DrawState state = DRAW_STATE_WRITE_COLOR | DRW_STATE_DEPTH_GREATER | DRW_STATE_STENCIL_EQUAL;
 
-    DRW_PASS_CREATE(psl->composite_ps, state);
+    DRAW_PASS_CREATE(psl->composite_ps, state);
 
     sh = workbench_shader_composite_get(wpd);
 
-    grp = DRW_shgroup_create(sh, psl->composite_ps);
-    DRW_shgroup_uniform_block(grp, "world_data", wpd->world_ubo);
-    DRW_shgroup_uniform_texture(grp, "materialBuffer", wpd->material_buffer_tx);
-    DRW_shgroup_uniform_texture(grp, "normalBuffer", wpd->normal_buffer_tx);
-    DRW_shgroup_uniform_bool_copy(grp, "forceShadowing", false);
-    DRW_shgroup_stencil_mask(grp, 0x00);
+    grp = draw_shgroup_create(sh, psl->composite_ps);
+    draw_shgroup_uniform_block(grp, "world_data", wpd->world_ubo);
+    draw_shgroup_uniform_texture(grp, "materialBuffer", wpd->material_buffer_tx);
+    draw_shgroup_uniform_texture(grp, "normalBuffer", wpd->normal_buffer_tx);
+    draw_shgroup_uniform_bool_copy(grp, "forceShadowing", false);
+    draw_shgroup_stencil_mask(grp, 0x00);
 
     if (STUDIOLIGHT_TYPE_MATCAP_ENABLED(wpd)) {
-      BKE_studiolight_ensure_flag(wpd->studio_light,
+      dune_studiolight_ensure_flag(wpd->studio_light,
                                   STUDIOLIGHT_MATCAP_DIFFUSE_GPUTEXTURE |
                                       STUDIOLIGHT_MATCAP_SPECULAR_GPUTEXTURE);
       struct GPUTexture *diff_tx = wpd->studio_light->matcap_diffuse.gputexture;
       struct GPUTexture *spec_tx = wpd->studio_light->matcap_specular.gputexture;
       const bool use_spec = workbench_is_specular_highlight_enabled(wpd);
       spec_tx = (use_spec && spec_tx) ? spec_tx : diff_tx;
-      DRW_shgroup_uniform_texture(grp, "matcap_diffuse_tx", diff_tx);
-      DRW_shgroup_uniform_texture(grp, "matcap_specular_tx", spec_tx);
+      draw_shgroup_uniform_texture(grp, "matcap_diffuse_tx", diff_tx);
+      draw_shgroup_uniform_texture(grp, "matcap_specular_tx", spec_tx);
     }
-    DRW_shgroup_call_procedural_triangles(grp, NULL, 1);
+    draw_shgroup_call_procedural_triangles(grp, NULL, 1);
 
     if (SHADOW_ENABLED(wpd)) {
-      grp = DRW_shgroup_create_sub(grp);
+      grp = draw_shgroup_create_sub(grp);
       DRW_shgroup_uniform_bool_copy(grp, "forceShadowing", true);
       DRW_shgroup_state_disable(grp, DRW_STATE_STENCIL_EQUAL);
       DRW_shgroup_state_enable(grp, DRW_STATE_STENCIL_NEQUAL);
