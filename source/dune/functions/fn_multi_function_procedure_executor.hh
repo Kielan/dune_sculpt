cb50c8bf654fcdba959a320c651853d8d7f8a9cd
Kielan <ki.lemons@gmail.com>
2023-03-28 07:16:06 -0700

    Create fn_multi_function_procedure_executor.hh

---
+++ source/dune/functions/fn_multi_function_procedure_executor.hh
@@ -0,0 +1,26 @@
+#pragma once
+
+/** \file
+ * \ingroup fn
+ */
+
+#include "FN_multi_function_procedure.hh"
+
+namespace blender::fn {
+
+/** A multi-function that executes a procedure internally. */
+class MFProcedureExecutor : public MultiFunction {
+ private:
+  MFSignature signature_;
+  const MFProcedure &procedure_;
+
+ public:
+  MFProcedureExecutor(const MFProcedure &procedure);
+
+  void call(IndexMask mask, MFParams params, MFContext context) const override;
+
+ private:
+  ExecutionHints get_execution_hints() const override;
+};
+
+}  // namespace dune::fn
