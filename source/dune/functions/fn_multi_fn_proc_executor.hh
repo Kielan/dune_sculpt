442b5b6a3f288785f5b16d14c82aca0323077c6e
Kielan <ki.lemons@gmail.com>
2023-03-28 08:11:18 -0700

    Update and rename fn_multi_fn_procedure_executor.hh to fn_multi_fn_proc_executor.hh

--- source/dune/functions/fn_multi_fn_proc_executor.hh
+++ source/dune/functions/fn_multi_fn_proc_executor.hh
@@ -1,19 +1,19 @@
 #pragma once
 
-#include "fn_multi_fn_procedure.hh"
+#include "fn_multi_fn_proc.hh"
 
 namespace dune::fn {
 
 /** A multi-function that executes a procedure internally. */
-class MFProcedureExecutor : public MultiFn {
+class MFProcExecutor : public MultiFn {
  private:
   MFSignature signature_;
   const MFProcedure &procedure_;
 
  public:
-  MFProcedureExecutor(const MFProcedure &procedure);
+  MFProcedureExecutor(const MFProc &proc);
 
-  void call(IndexMask mask, MFParams params, MFContext context) const override;
+  void call(IndexMask mask, MFParams params, MFCtx ctx) const override;
 
  private:
   ExecutionHints get_execution_hints() const override;
