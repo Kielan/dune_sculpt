28d14c6959b07122f04b450d5bfe6538cb12fd59
Kielan <ki.lemons@gmail.com>
2023-03-30 10:25:46 -0700

    Update graph_node_factory.cc

--- source/dune/graph/intern/node/graph_node_factory.cc
+++ source/dune/graph/intern/node/graph_node_factory.cc
@@ -1,22 +1,22 @@
-#include "intern/node/dgraph_node_factory.h"
+#include "intern/node/graph_node_factory.h"
 
-namespace dune::dgraph {
+namespace dune::graph {
 
 /* Global type registry */
-static DGraphNodeFactory *node_typeinfo_registry[static_cast<int>(NodeType::NUM_TYPES)] = {nullptr};
+static GraphNodeFactory *node_typeinfo_registry[static_cast<int>(NodeType::NUM_TYPES)] = {nullptr};
 
-void register_node_typeinfo(DGraphNodeFactory *factory)
+void register_node_typeinfo(GraphNodeFactory *factory)
 {
   lib_assert(factory != nullptr);
   const int type_as_int = static_cast<int>(factory->type());
   node_typeinfo_registry[type_as_int] = factory;
 }
 
-DGraphNodeFactory *type_get_factory(const NodeType type)
+GraphNodeFactory *type_get_factory(const NodeType type)
 {
   /* Look up type - at worst, it doesn't exist in table yet, and we fail. */
   const int type_as_int = static_cast<int>(type);
   return node_typeinfo_registry[type_as_int];
 }
 
-}  // namespace dune::dgraph
+}  // namespace dune::graph
