9f0aeafeaa3296cfce5ba82ac80d0a4ed6a5ee0e
Kielan <ki.lemons@gmail.com>
2023-03-29 17:14:49 -0700

    Update graph_phys.h

--- source/dune/graph/intern/graph_phys.h
+++ source/dune/graph/intern/graph_phys.h
@@ -4,12 +4,12 @@ struct Collection;
 struct ListBase;
 
 namespace dune {
-namespace dgraph {
+namespace graph {
 
-struct DGraph;
+struct Graph;
 
-ListBase *build_effector_relations(DGraph *graph, Collection *collection);
-ListBase *build_collision_relations(DGraph *graph,
+ListBase *build_effector_relations(Graph *graph, Collection *collection);
+ListBase *build_collision_relations(Graph *graph,
                                     Collection *collection,
                                     unsigned int modifier_type);
 void clear_phys_relations(DGraph *graph);
