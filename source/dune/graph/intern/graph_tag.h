c882a7dbf81642cee486ab92276191e698c17495
Kielan <ki.lemons@gmail.com>
2023-03-29 17:59:17 -0700

    Update graph_tag.h

--- source/dune/graph/intern/graph_tag.h
+++ source/dune/graph/intern/graph_tag.h
@@ -6,7 +6,7 @@ struct Main;
 namespace dune {
 namespace graph {
 
-struct DGraph;
+struct Graph;
 
 /* Get type of a node which corresponds to a ID_RECALC_GEOMETRY tag. */
 NodeType geometry_tag_to_component(const Id *id);
@@ -16,11 +16,11 @@ void id_tag_update(Main *dmain, Id *id, int flag, eUpdateSource update_source);
 
 /* Tag given ID for an update with in a given dependency graph. */
 void graph_id_tag_update(
-    Main *dmain, DGraph *graph, Id *id, int flag, eUpdateSource update_source);
+    Main *dmain, Graph *graph, Id *id, int flag, eUpdateSource update_source);
 
 /* Tag IDs of the graph for the visibility update tags.
  * Will do nothing if the graph is not tagged for visibility update. */
-void graph_tag_ids_for_visible_update(DGraph *graph);
+void graph_tag_ids_for_visible_update(Graph *graph);
 
-}  // namespace dgraph
+}  // namespace graph
 }  // namespace dune
