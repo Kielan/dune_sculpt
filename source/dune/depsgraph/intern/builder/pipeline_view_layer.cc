2d673bcbfe0cc8493b2d6783388f46b684c1ae8b
Kielan <ki.lemons@gmail.com>
2023-03-29 23:07:54 -0700

    Update pipeline_view_layer.cc

--- source/dune/depsgraph/intern/builder/pipeline_view_layer.cc
+++ source/dune/depsgraph/intern/builder/pipeline_view_layer.cc
@@ -1,24 +1,24 @@
 #include "pipeline_view_layer.h"
 
-#include "intern/builder/dgraph_builder_nodes.h"
-#include "intern/builder/dgraph_builder_relations.h"
-#include "intern/dgraph.h"
+#include "intern/builder/graph_builder_nodes.h"
+#include "intern/builder/graph_builder_relations.h"
+#include "intern/graph.h"
 
-namespace dune::dgraph {
+namespace dune::graph {
 
 ViewLayerBuilderPipeline::ViewLayerBuilderPipeline(::DGraph *graph)
     : AbstractBuilderPipeline(graph)
 {
 }
 
-void ViewLayerBuilderPipeline::build_nodes(DGraphNodeBuilder &node_builder)
+void ViewLayerBuilderPipeline::build_nodes(GraphNodeBuilder &node_builder)
 {
-  node_builder.build_view_layer(scene_, view_layer_, DGRAPH_ID_LINKED_DIRECTLY);
+  node_builder.build_view_layer(scene_, view_layer_, GRAPH_ID_LINKED_DIRECTLY);
 }
 
-void ViewLayerBuilderPipeline::build_relations(DGraphRelationBuilder &relation_builder)
+void ViewLayerBuilderPipeline::build_relations(GraphRelationBuilder &relation_builder)
 {
-  relation_builder.build_view_layer(scene_, view_layer_, DGRAPH_ID_LINKED_DIRECTLY);
+  relation_builder.build_view_layer(scene_, view_layer_, GRAPH_ID_LINKED_DIRECTLY);
 }
 
-}  // namespace dune::dgraph
+}  // namespace dune::graph
