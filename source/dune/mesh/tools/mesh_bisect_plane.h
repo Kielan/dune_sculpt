9870f78054fdfaff7e6492e997a5d04ecfbc06d1
Kielan <ki.lemons@gmail.com>
2023-04-02 06:45:00 -0700

    Update mesh_bisect_plane.h

--- source/dune/mesh/tools/mesh_bisect_plane.h
+++ source/dune/mesh/tools/mesh_bisect_plane.h
@@ -1,18 +1,14 @@
 #pragma once
 
-/** \file
- * \ingroup bmesh
- */
-
 /**
- * \param use_snap_center: Snap verts onto the plane.
- * \param use_tag: Only bisect tagged edges and faces.
- * \param oflag_center: Operator flag, enabled for geometry on the axis (existing and created)
+ * param use_snap_center: Snap verts onto the plane.
+ * param use_tag: Only bisect tagged edges and faces.
+ * param oflag_center: Operator flag, enabled for geometry on the axis (existing and created)
  */
-void BM_mesh_bisect_plane(BMesh *bm,
-                          const float plane[4],
-                          bool use_snap_center,
-                          bool use_tag,
-                          short oflag_center,
-                          short oflag_new,
-                          float eps);
+void mesh_bisect_plane(Mesh *bm,
+                       const float plane[4],
+                       bool use_snap_center,
+                       bool use_tag,
+                       short oflag_center,
+                       short oflag_new,
+                       float eps);
