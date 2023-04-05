d55180a5e8711219bd18d479575199d374b253d9
Kielan <ki.lemons@gmail.com>
2023-04-05 09:38:38 -0700

    Update mesh_tessellate.h

--- source/dune/mesh/tools/intern/mesh_tessellate.h
+++ source/dune/mesh/tools/intern/mesh_tessellate.h
@@ -2,10 +2,10 @@
 
 struct MeshPartialUpdate;
 
-struct BeshCalcTessellation_Params {
+struct MeshCalcTessellation_Params {
   /**
    * When calculating normals as well as tessellation, calculate normals after tessellation
-   * for improved performance. See MeshCalcTessellation_Params
+   * for improved performance. See MeshCalcTessellationParams
    */
   bool face_normals;
 };
@@ -15,15 +15,13 @@ void mesh_calc_tessellation_ex(Mesh *mesh,
                                const struct MeshCalcTessellationParams *params);
 void mesh_calc_tessellation(Mesh *m, MeshLoop *(*looptris)[3]);
 
-/**
- * A version of #BM_mesh_calc_tessellation that avoids degenerate triangles.
- */
-void BM_mesh_calc_tessellation_beauty(BMesh *bm, BMLoop *(*looptris)[3]);
+/** A version of mesh_calc_tessellation that avoids degenerate triangles. **/
+void mesh_calc_tessellation_beauty(Mesh *m, MeshLoop *(*looptris)[3]);
 
-void BM_mesh_calc_tessellation_with_partial_ex(BMesh *bm,
-                                               BMLoop *(*looptris)[3],
-                                               const struct BMPartialUpdate *bmpinfo,
-                                               const struct BMeshCalcTessellation_Params *params);
-void BM_mesh_calc_tessellation_with_partial(BMesh *bm,
-                                            BMLoop *(*looptris)[3],
-                                            const struct BMPartialUpdate *bmpinfo);
+void mesh_calc_tessellation_with_partial_ex(Mesh *m,
+                                            MeshLoop *(*looptris)[3],
+                                            const struct MeshPartialUpdate *mpinfo,
+                                            const struct MeshCalcTessellationParams *params);
+void mesh_calc_tessellation_with_partial(Mesh *m,
+                                         MeshLoop *(*looptris)[3],
+                                         const struct MeshPartialUpdate *mpinfo);
