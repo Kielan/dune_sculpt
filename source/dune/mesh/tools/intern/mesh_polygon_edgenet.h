4ab7744e19c49e16cf0166279a8350b54fd4ceae
Kielan <ki.lemons@gmail.com>
2023-04-08 17:39:29 -0700

    Update mesh_polygon_edgenet.h

--- source/dune/mesh/tools/intern/mesh_polygon_edgenet.h
+++ source/dune/mesh/tools/intern/mesh_polygon_edgenet.h
@@ -1,9 +1,5 @@
 #pragma once
 
-/** \file
- * \ingroup bmesh
- */
-
 /**
  * Splits a face into many smaller faces defined by an edge-net.
  * handle customdata and degenerate cases.
@@ -12,27 +8,27 @@
  * - Customdata calculations aren't efficient
  *   (need to calculate weights for each vert).
  */
-bool BM_face_split_edgenet(BMesh *bm,
-                           BMFace *f,
-                           BMEdge **edge_net,
-                           int edge_net_len,
-                           BMFace ***r_face_arr,
-                           int *r_face_arr_len);
+bool mesh_face_split_edgenet(Mesh *mesh,
+                             MeshFace *f,
+                             MeshEdge **edge_net,
+                             int edge_net_len,
+                             MeshFace ***r_face_arr,
+                             int *r_face_arr_len);
 
 /**
  * For when the edge-net has holes in it-this connects them.
  *
- * \param use_partial_connect: Support for handling islands connected by only a single edge,
- * \note that this is quite slow so avoid using where possible.
- * \param mem_arena: Avoids many small allocs & should be cleared after each use.
- * take care since \a edge_net_new is stored in \a r_edge_net_new.
+ * param use_partial_connect: Support for handling islands connected by only a single edge,
+ * note: is quite slow so avoid using where possible.
+ * param mem_arena: Avoids many small allocs & should be cleared after each use.
+ * take care since edge_net_new is stored in \a r_edge_net_new.
  */
-bool BM_face_split_edgenet_connect_islands(BMesh *bm,
-                                           BMFace *f,
-                                           BMEdge **edge_net_init,
-                                           uint edge_net_init_len,
-                                           bool use_partial_connect,
-                                           struct MemArena *mem_arena,
-                                           BMEdge ***r_edge_net_new,
-                                           uint *r_edge_net_new_len) ATTR_WARN_UNUSED_RESULT
+bool mesh_face_split_edgenet_connect_islands(Mesh *mesh,
+                                             MeshFace *f,
+                                             MeshEdge **edge_net_init,
+                                             uint edge_net_init_len,
+                                             bool use_partial_connect,
+                                             struct MemArena *mem_arena,
+                                             MeshEdge ***r_edge_net_new,
+                                             uint *r_edge_net_new_len) ATTR_WARN_UNUSED_RESULT
     ATTR_NONNULL(1, 2, 3, 6, 7, 8);
