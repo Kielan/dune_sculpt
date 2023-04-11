408f0c52dc45fe893f18038a0481a60f4ae9c223
Kielan <ki.lemons@gmail.com>
2023-04-11 06:22:48 -0700

    Update mesh_duplicate.h

--- source/dune/mesh/tools/intern/mesh_duplicate.h
+++ source/dune/mesh/tools/intern/mesh_duplicate.h
@@ -2,11 +2,11 @@
 
 /**
  * Geometry must be completely isolated. */
-void mesh_copy_arrays(Mesh *bm_src,
-                         Mesh *bm_dst,
-                         MVert **verts_src,
-                         uint verts_src_len,
-                         MEdge **edges_src,
-                         uint edges_src_len,
-                         MFace **faces_src,
-                         uint faces_src_len);
+void mesh_copy_arrays(Mesh *mesh_src,
+                      Mesh *mesh_dst,
+                      MeshVert **verts_src,
+                      uint verts_src_len,
+                      MeshEdge **edges_src,
+                      uint edges_src_len,
+                      MeshFace **faces_src,
+                      uint faces_src_len);
