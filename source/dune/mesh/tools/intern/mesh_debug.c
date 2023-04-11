3eda9edfb146d580954e644c62085535d83dd297
Kielan <ki.lemons@gmail.com>
2023-04-10 23:06:14 -0700

    Update mesh_debug.c

--- source/dune/mesh/tools/intern/mesh_debug.c
+++ source/dune/mesh/tools/intern/mesh_debug.c
@@ -1,70 +1,67 @@
-/** \file
- * \ingroup bke
- *
- * Evaluated mesh info printing function, to help track down differences output.
+/* Evaluated mesh info printing function, to help track down differences output.
  *
  * Output from these functions can be evaluated as Python literals.
- * See `mesh_debug.cc` for the equivalent #Mesh functionality.
+ * See `mesh_debug.cc` for the equivalent Mesh functionality.
  */
 
 #ifndef NDEBUG
 
 #  include <stdio.h>
 
-#  include "MEM_guardedalloc.h"
+#  include "mem_guardedalloc.h"
 
-#  include "BLI_utildefines.h"
+#  include "lib_utildefines.h"
 
-#  include "BKE_customdata.h"
+#  include "dune_customdata.h"
 
-#  include "bmesh.h"
+#  include "mesh.h"
 
-#  include "bmesh_mesh_debug.h"
+#  include "mesh_mesh_debug.h"
 
-#  include "BLI_dynstr.h"
+#  include "lib_dynstr.h"
 
-char *BM_mesh_debug_info(BMesh *bm)
+char *mesh_debug_info(Mesh *mesh)
 {
-  DynStr *dynstr = BLI_dynstr_new();
+  DynStr *dynstr = lib_dynstr_new();
   char *ret;
 
   const char *indent8 = "        ";
 
-  BLI_dynstr_append(dynstr, "{\n");
-  BLI_dynstr_appendf(dynstr, "    'ptr': '%p',\n", (void *)bm);
-  BLI_dynstr_appendf(dynstr, "    'totvert': %d,\n", bm->totvert);
-  BLI_dynstr_appendf(dynstr, "    'totedge': %d,\n", bm->totedge);
-  BLI_dynstr_appendf(dynstr, "    'totface': %d,\n", bm->totface);
+  lib_dynstr_append(dynstr, "{\n");
+  lib_dynstr_appendf(dynstr, "    'ptr': '%p',\n", (void *)meeh);
+  lib_dynstr_appendf(dynstr, "    'totvert': %d,\n", mesh->totvert);
+  lib_dynstr_appendf(dynstr, "    'totedge': %d,\n", mesh->totedge);
+  lib_dynstr_appendf(dynstr, "    'totface': %d,\n", mesh->totface);
 
-  BLI_dynstr_append(dynstr, "    'vert_layers': (\n");
-  CustomData_debug_info_from_layers(&bm->vdata, indent8, dynstr);
-  BLI_dynstr_append(dynstr, "    ),\n");
+  lib_dynstr_append(dynstr, "    'vert_layers': (\n");
+  CustomData_debug_info_from_layers(&mesh->vdata, indent8, dynstr);
+  lib_dynstr_append(dynstr, "    ),\n");
 
-  BLI_dynstr_append(dynstr, "    'edge_layers': (\n");
-  CustomData_debug_info_from_layers(&bm->edata, indent8, dynstr);
-  BLI_dynstr_append(dynstr, "    ),\n");
+  lib_dynstr_append(dynstr, "    'edge_layers': (\n");
+  CustomData_debug_info_from_layers(&mesh->edata, indent8, dynstr);
+  lib_dynstr_append(dynstr, "    ),\n");
 
-  BLI_dynstr_append(dynstr, "    'loop_layers': (\n");
-  CustomData_debug_info_from_layers(&bm->ldata, indent8, dynstr);
-  BLI_dynstr_append(dynstr, "    ),\n");
+  lib_dynstr_append(dynstr, "    'loop_layers': (\n");
+  CustomData_debug_info_from_layers(&mesh->ldata, indent8, dynstr);
+  lib_dynstr_append(dynstr, "    ),\n");
 
-  BLI_dynstr_append(dynstr, "    'poly_layers': (\n");
+  lib_dynstr_append(dynstr, "    'poly_layers': (\n");
   CustomData_debug_info_from_layers(&bm->pdata, indent8, dynstr);
-  BLI_dynstr_append(dynstr, "    ),\n");
+  lib_dynstr_append(dynstr, "    ),\n");
 
-  BLI_dynstr_append(dynstr, "}\n");
+  lib_dynstr_append(dynstr, "}\n");
 
-  ret = BLI_dynstr_get_cstring(dynstr);
-  BLI_dynstr_free(dynstr);
+  ret = lib_dynstr_get_cstring(dynstr);
+  lib_dynstr_free(dynstr);
   return ret;
 }
 
-void BM_mesh_debug_print(BMesh *bm)
+void mesh_debug_print(Mesh *mesh)
 {
-  char *str = BM_mesh_debug_info(bm);
+  char *str = mesh_debug_info(bm);
   puts(str);
   fflush(stdout);
-  MEM_freeN(str);
+  mem_freen(str);
 }
 
 #endif /* NDEBUG */
