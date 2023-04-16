621c2e25664146054a01e1801cd6525269f14ebf
Kielan <ki.lemons@gmail.com>
2023-04-16 11:37:39 -0700

    Update mesh_beautify.h

--- source/dune/mesh/tools/mesh_beautify.h
+++ source/dune/mesh/tools/mesh_beautify.h
@@ -1,9 +1,5 @@
 #pragma once
 
-/** \file
- * \ingroup bmesh
- */
-
 enum {
   /** Vertices tags must match (special case). */
   VERT_RESTRICT_TAG = (1 << 0),
@@ -11,26 +7,24 @@ enum {
   EDGE_RESTRICT_DEGENERATE = (1 << 1),
 };
 
-/**
- * \note This function sets the edge indices to invalid values.
- */
-void BM_mesh_beautify_fill(BMesh *bm,
-                           BMEdge **edge_array,
-                           int edge_array_len,
-                           short flag,
-                           short method,
-                           short oflag_edge,
-                           short oflag_face);
+/** This function sets the edge indices to invalid values. **/
+void mesh_beautify_fill(Mesh *mesh,
+                        MeshEdge **edge_array,
+                        int edge_array_len,
+                        short flag,
+                        short method,
+                        short oflag_edge,
+                        short oflag_face);
 
 /**
  * Assuming we have 2 triangles sharing an edge (2 - 4),
  * check if the edge running from (1 - 3) gives better results.
  *
- * \return (negative number means the edge can be rotated, lager == better).
+ * return (negative number means the edge can be rotated, lager == better).
  */
-float BM_verts_calc_rotate_beauty(const BMVert *v1,
-                                  const BMVert *v2,
-                                  const BMVert *v3,
-                                  const BMVert *v4,
+float mesh_verts_calc_rotate_beauty(const MeshVert *v1,
+                                  const MeshVert *v2,
+                                  const MeshVert *v3,
+                                  const MeshVert *v4,
                                   short flag,
                                   short method);
