b26608b59afb848cbcd7d146d75c09151b115a3e
Kielan <ki.lemons@gmail.com>
2023-04-21 07:22:40 -0700

    Update mesh_tools.h

--- source/dune/mesh/mesh_tools.h
+++ source/dune/mesh/mesh_tools.h
@@ -1,8 +1,7 @@
 #pragma once
 
-/** mesh
- *
- * Utility functions that operate directly on the Mesh,
+/**
+ * Util functions that operate directly on the Mesh,
  * These can be used by both Modifiers and Mesh-Operators.
  */
 
@@ -10,20 +9,20 @@
 extern "C" {
 #endif
 
-#include "tools/bmesh_beautify.h"
-#include "tools/bmesh_bevel.h"
-#include "tools/bmesh_bisect_plane.h"
-#include "tools/bmesh_boolean.h"
-#include "tools/bmesh_decimate.h"
-#include "tools/bmesh_edgenet.h"
-#include "tools/bmesh_edgesplit.h"
-#include "tools/bmesh_path.h"
-#include "tools/bmesh_path_region.h"
-#include "tools/bmesh_path_region_uv.h"
-#include "tools/bmesh_path_uv.h"
-#include "tools/bmesh_region_match.h"
-#include "tools/bmesh_separate.h"
-#include "tools/bmesh_triangulate.h"
+#include "tools/mesh_beautify.h"
+#include "tools/mesh_bevel.h"
+#include "tools/mesh_bisect_plane.h"
+#include "tools/mesh_boolean.h"
+#include "tools/mesh_decimate.h"
+#include "tools/mesh_edgenet.h"
+#include "tools/mesh_edgesplit.h"
+#include "tools/mesh_path.h"
+#include "tools/mesh_path_region.h"
+#include "tools/mesh_path_region_uv.h"
+#include "tools/mesh_path_uv.h"
+#include "tools/mesh_region_match.h"
+#include "tools/mesh_separate.h"
+#include "tools/mesh_triangulate.h"
 
 #ifdef __cplusplus
 }
