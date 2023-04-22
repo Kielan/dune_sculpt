2fec6b67a83e18de34fb3bc4c3a7a09f72d3e351
Kielan <ki.lemons@gmail.com>
2023-04-22 09:58:03 -0700

    Update mesh_delete.h

--- source/dune/mesh/tools/intern/mesh_delete.h
+++ source/dune/mesh/tools/intern/mesh_delete.h
@@ -7,9 +7,9 @@ void mesh_delete_hflag_tagged(Mesh *mesh, char hflag, char htype);
  * warning opflag applies to different types in some contexts,
  * not just the type being removed.
  */
-void mesh_op_mesh_delete_oflag_ctx(Mesh *mesh, short opflag, int type);
+void mesh_op_mesh_delete_opflag_ctx(Mesh *mesh, short opflag, int type);
 /**
- * \warning oflag applies to different types in some contexts,
+ * warning oflag applies to different types in some contexts,
  * not just the type being removed.
  */
-void BM_mesh_delete_hflag_context(BMesh *bm, char hflag, int type);
+void mesh_delete_hflag_ctx(Mesh *mesh, char hflag, int type);
