fc2b70972666afdc7424c614ab948aad2ad3053e
Kielan <ki.lemons@gmail.com>
2023-04-28 06:26:08 -0700

    Update render_texture_margin.h

--- source/dune/render/render_texture_margin.h
+++ source/dune/render/render_texture_margin.h
@@ -7,17 +7,17 @@ extern "C" {
 struct DerivedMesh;
 struct IMBuf;
 struct ImBuf;
-struct MLoopUV;
+struct MeshLoopUV;
 struct Mesh;
 
 /**
  * Generate a margin around the textures uv islands by copying pixels from the adjacent polygon.
  *
- * \param ibuf: the texture image.
- * \param mask: pixels with a mask value of 1 are not written to.
- * \param margin: the size of the margin in pixels.
- * \param me: the mesh to use the polygons of.
- * \param mloopuv: the uv data to use.
+ * param ibuf: the texture image.
+ * param mask: pixels with a mask value of 1 are not written to.
+ * param margin: the size of the margin in pixels.
+ * param me: the mesh to use the polygons of.
+ * param mloopuv: the uv data to use.
  */
 void RE_generate_texturemargin_adjacentfaces(
     struct ImBuf *ibuf, char *mask, const int margin, struct Mesh const *me, char const *uv_layer);
