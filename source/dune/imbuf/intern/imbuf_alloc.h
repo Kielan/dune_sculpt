d84065a30d6f11520a9f1d555d8f76fe39ec106f
Kielan <ki.lemons@gmail.com>
2023-05-05 16:48:47 -0700

    Create imbuf_alloc.h

---
+++ source/dune/imbuf/intern/imbuf_alloc.h
@@ -0,0 +1,33 @@
+/** \file
+ * \ingroup imbuf
+ * \brief Header file for allocimbuf.c
+ */
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct ImBuf;
+
+void imb_refcounter_lock_init(void);
+void imb_refcounter_lock_exit(void);
+
+#ifndef WIN32
+void imb_mmap_lock_init(void);
+void imb_mmap_lock_exit(void);
+void imb_mmap_lock(void);
+void imb_mmap_unlock(void);
+#else
+#  define imb_mmap_lock_init()
+#  define imb_mmap_lock_exit()
+#  define imb_mmap_lock()
+#  define imb_mmap_unlock()
+#endif
+
+bool imb_addencodedbufferImBuf(struct ImBuf *ibuf);
+bool imb_enlargeencodedbufferImBuf(struct ImBuf *ibuf);
+
+#ifdef __cplusplus
+}
+#endif
