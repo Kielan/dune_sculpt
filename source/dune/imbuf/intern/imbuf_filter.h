3ddff9a38a61289cb9f4912d3f33cf717ea6ddf2
Kielan <ki.lemons@gmail.com>
2023-05-06 13:56:27 -0700

    Update imbuf_filter.h

--- source/dune/imbuf/intern/imbuf_filter.h
+++ source/dune/imbuf/intern/imbuf_filter.h
@@ -4,7 +4,7 @@
 
 struct ImBuf;
 
-void imb_filterx(struct ImBuf *ibuf);
+void imbuf_filterx(struct ImBuf *ibuf);
 
 void imbuf_premultiply_rect(unsigned int *rect, char planes, int w, int h);
 void imbuf_premultiply_rect_float(float *rect_float, int channels, int w, int h);
@@ -12,7 +12,5 @@ void imbuf_premultiply_rect_float(float *rect_float, int channels, int w, int h)
 void imbuf_unpremultiply_rect(unsigned int *rect, char planes, int w, int h);
 void imbuf_unpremultiply_rect_float(float *rect_float, int channels, int w, int h);
 
-/**
- * Result in ibuf2, scaling should be done correctly.
- */
+/** Result in ibuf2, scaling should be done correctly. */
 void imbuf_onehalf_no_alloc(struct ImBuf *ibuf2, struct ImBuf *ibuf1);
