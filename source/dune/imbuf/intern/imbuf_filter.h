09b28baf6ad1afe12aeffab89abafe702a94d365
Kielan <ki.lemons@gmail.com>
2023-05-06 13:31:32 -0700

    Update imbuf_filter.h

--- source/dune/imbuf/intern/imbuf_filter.h
+++ source/dune/imbuf/intern/imbuf_filter.h
@@ -1,7 +1,4 @@
-/** \file
- * \ingroup imbuf
- * \brief Function declarations for filter.c
- */
+/** Function declarations for filter.c **/
 
 #pragma once
 
@@ -9,13 +6,13 @@ struct ImBuf;
 
 void imb_filterx(struct ImBuf *ibuf);
 
-void IMB_premultiply_rect(unsigned int *rect, char planes, int w, int h);
-void IMB_premultiply_rect_float(float *rect_float, int channels, int w, int h);
+void imbuf_premultiply_rect(unsigned int *rect, char planes, int w, int h);
+void imbuf_premultiply_rect_float(float *rect_float, int channels, int w, int h);
 
-void IMB_unpremultiply_rect(unsigned int *rect, char planes, int w, int h);
-void IMB_unpremultiply_rect_float(float *rect_float, int channels, int w, int h);
+void imbuf_unpremultiply_rect(unsigned int *rect, char planes, int w, int h);
+void imbuf_unpremultiply_rect_float(float *rect_float, int channels, int w, int h);
 
 /**
  * Result in ibuf2, scaling should be done correctly.
  */
-void imb_onehalf_no_alloc(struct ImBuf *ibuf2, struct ImBuf *ibuf1);
+void imbuf_onehalf_no_alloc(struct ImBuf *ibuf2, struct ImBuf *ibuf1);
