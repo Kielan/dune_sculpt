f06368dd27ba440ec72c29ae4cdc4f27f56fea75
Kielan <ki.lemons@gmail.com>
2023-05-05 19:38:36 -0700

    Update colormanagement.cc

--- source/dune/imbuf/intern/colormanagement.cc
+++ source/dune/imbuf/intern/colormanagement.cc
@@ -36,7 +36,7 @@
 
 #include "api_define.h"
 
-#include "SEQ_iterator.h"
+#include "seq_iterator.h"
 
 #include <ocio_capi.h>
 
@@ -319,7 +319,7 @@ static ImBuf *colormanage_cache_get_ibuf(ImBuf *ibuf,
 
   *cache_handle = nullptr;
 
-  cache_ibuf = IMB_moviecache_get(moviecache, key, nullptr);
+  cache_ibuf = imbuf_moviecache_get(moviecache, key, nullptr);
 
   *cache_handle = cache_ibuf;
 
@@ -349,7 +349,7 @@ static uchar *colormanage_cache_get(ImBuf *ibuf,
   if (cache_ibuf) {
     ColormanageCacheData *cache_data;
 
-    BLI_assert(cache_ibuf->x == ibuf->x && cache_ibuf->y == ibuf->y);
+    lib_assert(cache_ibuf->x == ibuf->x && cache_ibuf->y == ibuf->y);
 
     /* only buffers with different color space conversions are being stored
      * in cache separately. buffer which were used only different exposure/gamma
@@ -368,7 +368,7 @@ static uchar *colormanage_cache_get(ImBuf *ibuf,
     {
       *cache_handle = nullptr;
 
-      IMB_freeImBuf(cache_ibuf);
+      lib_freeImBuf(cache_ibuf);
 
       return nullptr;
     }
@@ -377,245 +377,6 @@ static uchar *colormanage_cache_get(ImBuf *ibuf,
   }
 
   return nullptr;
-}
-
-/* SPDX-License-Identifier: GPL-2.0-or-later
- * Copyright 2012 Blender Foundation */
-
-/** \file
- * \ingroup imbuf
- */
-
-#include "IMB_colormanagement.h"
-#include "IMB_colormanagement_intern.h"
-
-#include <math.h>
-#include <string.h>
-
-#include "DNA_color_types.h"
-#include "DNA_image_types.h"
-#include "DNA_movieclip_types.h"
-#include "DNA_scene_types.h"
-#include "DNA_space_types.h"
-
-#include "IMB_filetype.h"
-#include "IMB_filter.h"
-#include "IMB_imbuf.h"
-#include "IMB_imbuf_types.h"
-#include "IMB_metadata.h"
-#include "IMB_moviecache.h"
-
-#include "MEM_guardedalloc.h"
-
-#include "BLI_blenlib.h"
-#include "BLI_math.h"
-#include "BLI_math_color.h"
-#include "BLI_rect.h"
-#include "BLI_string.h"
-#include "BLI_task.h"
-#include "BLI_threads.h"
-
-#include "BKE_appdir.h"
-#include "BKE_colortools.h"
-#include "BKE_context.h"
-#include "BKE_image.h"
-#include "BKE_image_format.h"
-#include "BKE_main.h"
-
-#include "RNA_define.h"
-
-#include "SEQ_iterator.h"
-
-#include <ocio_capi.h>
-
-/* -------------------------------------------------------------------- */
-/** \name Global declarations
- * \{ */
-
-#define DISPLAY_BUFFER_CHANNELS 4
-
-/* ** list of all supported color spaces, displays and views */
-static char global_role_data[MAX_COLORSPACE_NAME];
-static char global_role_scene_linear[MAX_COLORSPACE_NAME];
-static char global_role_color_picking[MAX_COLORSPACE_NAME];
-static char global_role_texture_painting[MAX_COLORSPACE_NAME];
-static char global_role_default_byte[MAX_COLORSPACE_NAME];
-static char global_role_default_float[MAX_COLORSPACE_NAME];
-static char global_role_default_sequencer[MAX_COLORSPACE_NAME];
-
-static ListBase global_colorspaces = {nullptr, nullptr};
-static ListBase global_displays = {nullptr, nullptr};
-static ListBase global_views = {nullptr, nullptr};
-static ListBase global_looks = {nullptr, nullptr};
-
-static int global_tot_colorspace = 0;
-static int global_tot_display = 0;
-static int global_tot_view = 0;
-static int global_tot_looks = 0;
-
-/* Luma coefficients and XYZ to RGB to be initialized by OCIO. */
-
-float imbuf_luma_coefficients[3] = {0.0f};
-float imbuf_scene_linear_to_xyz[3][3] = {{0.0f}};
-float imbuf_xyz_to_scene_linear[3][3] = {{0.0f}};
-float imbuf_scene_linear_to_rec709[3][3] = {{0.0f}};
-float imbuf_rec709_to_scene_linear[3][3] = {{0.0f}};
-float imbuf_scene_linear_to_aces[3][3] = {{0.0f}};
-float imbuf_aces_to_scene_linear[3][3] = {{0.0f}};
-
-/* lock used by pre-cached processors getters, so processor wouldn't
- * be created several times
- * LOCK_COLORMANAGE can not be used since this mutex could be needed to
- * be locked before pre-cached processor are creating
- */
-static pthread_mutex_t processor_lock = BLI_MUTEX_INITIALIZER;
-
-typedef struct ColormanageProcessor {
-  OCIO_ConstCPUProcessorRcPtr *cpu_processor;
-  CurveMapping *curve_mapping;
-  bool is_data_result;
-} ColormanageProcessor;
-
-static struct global_gpu_state {
-  /* GPU shader currently bound. */
-  bool gpu_shader_bound;
-
-  /* Curve mapping. */
-  CurveMapping *curve_mapping, *orig_curve_mapping;
-  bool use_curve_mapping;
-  int curve_mapping_timestamp;
-  OCIO_CurveMappingSettings curve_mapping_settings;
-} global_gpu_state = {false};
-
-static struct global_color_picking_state {
-  /* Cached processor for color picking conversion. */
-  OCIO_ConstCPUProcessorRcPtr *cpu_processor_to;
-  OCIO_ConstCPUProcessorRcPtr *cpu_processor_from;
-  bool failed;
-} global_color_picking_state = {nullptr};
-
-/** \} */
-
-/* -------------------------------------------------------------------- */
-/** \name Color Managed Cache
- * \{ */
-
-/**
- * Cache Implementation Notes
- * ==========================
- *
- * All color management cache stuff is stored in two properties of
- * image buffers:
- *
- *   1. display_buffer_flags
- *
- *      This is a bit field which used to mark calculated transformations
- *      for particular image buffer. Index inside of this array means index
- *      of a color managed display. Element with given index matches view
- *      transformations applied for a given display. So if bit B of array
- *      element B is set to 1, this means display buffer with display index
- *      of A and view transform of B was ever calculated for this imbuf.
- *
- *      In contrast with indices in global lists of displays and views this
- *      indices are 0-based, not 1-based. This is needed to save some bytes
- *      of memory.
- *
- *   2. colormanage_cache
- *
- *      This is a pointer to a structure which holds all data which is
- *      needed for color management cache to work.
- *
- *      It contains two parts:
- *        - data
- *        - moviecache
- *
- *      Data field is used to store additional information about cached
- *      buffers which affects on whether cached buffer could be used.
- *      This data can't go to cache key because changes in this data
- *      shouldn't lead extra buffers adding to cache, it shall
- *      invalidate cached images.
- *
- *      Currently such a data contains only exposure and gamma, but
- *      would likely extended further.
- *
- *      data field is not null only for elements of cache, not used for
- *      original image buffers.
- *
- *      Color management cache is using generic MovieCache implementation
- *      to make it easier to deal with memory limitation.
- *
- *      Currently color management is using the same memory limitation
- *      pool as sequencer and clip editor are using which means color
- *      managed buffers would be removed from the cache as soon as new
- *      frames are loading for the movie clip and there's no space in
- *      cache.
- *
- *      Every image buffer has got own movie cache instance, which
- *      means keys for color managed buffers could be really simple
- *      and look up in this cache would be fast and independent from
- *      overall amount of color managed images.
- */
-
-/* NOTE: ColormanageCacheViewSettings and ColormanageCacheDisplaySettings are
- *       quite the same as ColorManagedViewSettings and ColorManageDisplaySettings
- *       but they holds indexes of all transformations and color spaces, not
- *       their names.
- *
- *       This helps avoid extra colorspace / display / view lookup without
- *       requiring to pass all variables which affects on display buffer
- *       to color management cache system and keeps calls small and nice.
- */
-typedef struct ColormanageCacheViewSettings {
-  int flag;
-  int look;
-  int view;
-  float exposure;
-  float gamma;
-  float dither;
-  CurveMapping *curve_mapping;
-} ColormanageCacheViewSettings;
-
-typedef struct ColormanageCacheDisplaySettings {
-  int display;
-} ColormanageCacheDisplaySettings;
-
-typedef struct ColormanageCacheKey {
-  int view;    /* view transformation used for display buffer */
-  int display; /* display device name */
-} ColormanageCacheKey;
-
-typedef struct ColormanageCacheData {
-  int flag;                    /* view flags of cached buffer */
-  int look;                    /* Additional artistic transform. */
-  float exposure;              /* exposure value cached buffer is calculated with */
-  float gamma;                 /* gamma value cached buffer is calculated with */
-  float dither;                /* dither value cached buffer is calculated with */
-  CurveMapping *curve_mapping; /* curve mapping used for cached buffer */
-  int curve_mapping_timestamp; /* time stamp of curve mapping used for cached buffer */
-} ColormanageCacheData;
-
-typedef struct ColormanageCache {
-  struct MovieCache *moviecache;
-
-  ColormanageCacheData *data;
-} ColormanageCache;
-
-static struct MovieCache *colormanage_moviecache_get(const ImBuf *ibuf)
-{
-  if (!ibuf->colormanage_cache) {
-    return nullptr;
-  }
-
-  return ibuf->colormanage_cache->moviecache;
-}
-
-static ColormanageCacheData *colormanage_cachedata_get(const ImBuf *ibuf)
-{
-  if (!ibuf->colormanage_cache) {
-    return nullptr;
-  }
-
-  return ibuf->colormanage_cache->data;
 }
 
 static uint colormanage_hashhash(const void *key_v)
@@ -644,7 +405,7 @@ static struct MovieCache *colormanage_moviecache_ensure(ImBuf *ibuf)
   if (!ibuf->colormanage_cache->moviecache) {
     struct MovieCache *moviecache;
 
-    moviecache = IMB_moviecache_create("colormanage cache",
+    moviecache = imbuf_moviecache_create("colormanage cache",
                                        sizeof(ColormanageCacheKey),
                                        colormanage_hashhash,
                                        colormanage_hashcmp);
@@ -658,118 +419,10 @@ static struct MovieCache *colormanage_moviecache_ensure(ImBuf *ibuf)
 static void colormanage_cachedata_set(ImBuf *ibuf, ColormanageCacheData *data)
 {
   if (!ibuf->colormanage_cache) {
-    ibuf->colormanage_cache = MEM_cnew<ColormanageCache>("imbuf colormanage cache");
+    ibuf->colormanage_cache = mem_cnew<ColormanageCache>("imbuf colormanage cache");
   }
 
   ibuf->colormanage_cache->data = data;
-}
-
-static void colormanage_view_settings_to_cache(ImBuf *ibuf,
-                                               ColormanageCacheViewSettings *cache_view_settings,
-                                               const ColorManagedViewSettings *view_settings)
-{
-  int look = IMB_colormanagement_look_get_named_index(view_settings->look);
-  int view = IMB_colormanagement_view_get_named_index(view_settings->view_transform);
-
-  cache_view_settings->look = look;
-  cache_view_settings->view = view;
-  cache_view_settings->exposure = view_settings->exposure;
-  cache_view_settings->gamma = view_settings->gamma;
-  cache_view_settings->dither = ibuf->dither;
-  cache_view_settings->flag = view_settings->flag;
-  cache_view_settings->curve_mapping = view_settings->curve_mapping;
-}
-
-static void colormanage_display_settings_to_cache(
-    ColormanageCacheDisplaySettings *cache_display_settings,
-    const ColorManagedDisplaySettings *display_settings)
-{
-  int display = IMB_colormanagement_display_get_named_index(display_settings->display_device);
-
-  cache_display_settings->display = display;
-}
-
-static void colormanage_settings_to_key(ColormanageCacheKey *key,
-                                        const ColormanageCacheViewSettings *view_settings,
-                                        const ColormanageCacheDisplaySettings *display_settings)
-{
-  key->view = view_settings->view;
-  key->display = display_settings->display;
-}
-
-static ImBuf *colormanage_cache_get_ibuf(ImBuf *ibuf,
-                                         ColormanageCacheKey *key,
-                                         void **cache_handle)
-{
-  ImBuf *cache_ibuf;
-  struct MovieCache *moviecache = colormanage_moviecache_get(ibuf);
-
-  if (!moviecache) {
-    /* If there's no moviecache it means no color management was applied
-     * on given image buffer before. */
-    return nullptr;
-  }
-
-  *cache_handle = nullptr;
-
-  cache_ibuf = IMB_moviecache_get(moviecache, key, nullptr);
-
-  *cache_handle = cache_ibuf;
-
-  return cache_ibuf;
-}
-
-static uchar *colormanage_cache_get(ImBuf *ibuf,
-                                    const ColormanageCacheViewSettings *view_settings,
-                                    const ColormanageCacheDisplaySettings *display_settings,
-                                    void **cache_handle)
-{
-  ColormanageCacheKey key;
-  ImBuf *cache_ibuf;
-  int view_flag = 1 << (view_settings->view - 1);
-  CurveMapping *curve_mapping = view_settings->curve_mapping;
-  int curve_mapping_timestamp = curve_mapping ? curve_mapping->changed_timestamp : 0;
-
-  colormanage_settings_to_key(&key, view_settings, display_settings);
-
-  /* check whether image was marked as dirty for requested transform */
-  if ((ibuf->display_buffer_flags[display_settings->display - 1] & view_flag) == 0) {
-    return nullptr;
-  }
-
-  cache_ibuf = colormanage_cache_get_ibuf(ibuf, &key, cache_handle);
-
-  if (cache_ibuf) {
-    ColormanageCacheData *cache_data;
-
-    BLI_assert(cache_ibuf->x == ibuf->x && cache_ibuf->y == ibuf->y);
-
-    /* only buffers with different color space conversions are being stored
-     * in cache separately. buffer which were used only different exposure/gamma
-     * are re-suing the same cached buffer
-     *
-     * check here which exposure/gamma/curve was used for cached buffer and if they're
-     * different from requested buffer should be re-generated
-     */
-    cache_data = colormanage_cachedata_get(cache_ibuf);
-
-    if (cache_data->look != view_settings->look ||
-        cache_data->exposure != view_settings->exposure ||
-        cache_data->gamma != view_settings->gamma || cache_data->dither != view_settings->dither ||
-        cache_data->flag != view_settings->flag || cache_data->curve_mapping != curve_mapping ||
-        cache_data->curve_mapping_timestamp != curve_mapping_timestamp)
-    {
-      *cache_handle = nullptr;
-
-      IMB_freeImBuf(cache_ibuf);
-
-      return nullptr;
-    }
-
-    return (uchar *)cache_ibuf->rect;
-  }
-
-  return nullptr;
 }
 
 static void colormanage_cache_put(ImBuf *ibuf,
@@ -792,7 +445,7 @@ static void colormanage_cache_put(ImBuf *ibuf,
   ibuf->display_buffer_flags[display_settings->display - 1] |= view_flag;
 
   /* buffer itself */
-  cache_ibuf = IMB_allocImBuf(ibuf->x, ibuf->y, ibuf->planes, 0);
+  cache_ibuf = imbuf_allocImBuf(ibuf->x, ibuf->y, ibuf->planes, 0);
   cache_ibuf->rect = (uint *)display_buffer;
 
   cache_ibuf->mall |= IB_rect;
@@ -800,7 +453,7 @@ static void colormanage_cache_put(ImBuf *ibuf,
 
   /* Store data which is needed to check whether cached buffer
    * could be used for color managed display settings. */
-  cache_data = MEM_cnew<ColormanageCacheData>("color manage cache imbuf data");
+  cache_data = mem_cnew<ColormanageCacheData>("color manage cache imbuf data");
   cache_data->look = view_settings->look;
   cache_data->exposure = view_settings->exposure;
   cache_data->gamma = view_settings->gamma;
@@ -820,14 +473,11 @@ static void colormanage_cache_handle_release(void *cache_handle)
 {
   ImBuf *cache_ibuf = static_cast<ImBuf *>(cache_handle);
 
-  IMB_freeImBuf(cache_ibuf);
+  imbuf_freeImBuf(cache_ibuf);
 }
 
-/** \} */
-
 /* -------------------------------------------------------------------- */
-/** \name Initialization / De-initialization
- * \{ */
+/** Initialization / De-initialization **/
 
 static void colormanage_role_color_space_name_get(OCIO_ConstConfigRcPtr *config,
                                                   char *colorspace_name,
@@ -846,7 +496,7 @@ static void colormanage_role_color_space_name_get(OCIO_ConstConfigRcPtr *config,
     const char *name = OCIO_colorSpaceGetName(ociocs);
 
     /* assume function was called with buffer properly allocated to MAX_COLORSPACE_NAME chars */
-    BLI_strncpy(colorspace_name, name, MAX_COLORSPACE_NAME);
+    lib_strncpy(colorspace_name, name, MAX_COLORSPACE_NAME);
     OCIO_colorSpaceRelease(ociocs);
   }
   else {
@@ -896,7 +546,7 @@ static void colormanage_load_config(OCIO_ConstConfigRcPtr *config)
       colorspace->aliases = static_cast<char(*)[MAX_COLORSPACE_NAME]>(MEM_callocN(
           sizeof(*colorspace->aliases) * colorspace->num_aliases, "ColorSpace aliases"));
       for (int i = 0; i < colorspace->num_aliases; i++) {
-        BLI_strncpy(colorspace->aliases[i],
+        lib_strncpy(colorspace->aliases[i],
                     OCIO_colorSpaceGetAlias(ocio_colorspace, i),
                     MAX_COLORSPACE_NAME);
       }
@@ -933,9 +583,9 @@ static void colormanage_load_config(OCIO_ConstConfigRcPtr *config)
         view = colormanage_view_add(viewname);
       }
 
-      display_view = BLI_genericNodeN(view);
+      display_view = lib_genericNodeN(view);
 
-      BLI_addtail(&display->views, display_view);
+      lib_addtail(&display->views, display_view);
     }
   }
 
@@ -1011,20 +661,20 @@ static void colormanage_free_config(void)
     }
 
     /* free list of views */
-    BLI_freelistN(&display->views);
+    lib_freelistn(&display->views);
 
-    MEM_freeN(display);
+    mem_freen(display);
     display = display_next;
   }
-  BLI_listbase_clear(&global_displays);
+  lib_listbase_clear(&global_displays);
   global_tot_display = 0;
 
   /* free views */
-  BLI_freelistN(&global_views);
+  lib_freelistn(&global_views);
   global_tot_view = 0;
 
   /* free looks */
-  BLI_freelistN(&global_looks);
+  lib_freelistn(&global_looks);
   global_tot_looks = 0;
 
   OCIO_exit();
@@ -1039,7 +689,7 @@ void colormanagement_init(void)
 
   OCIO_init();
 
-  ocio_env = BLI_getenv("OCIO");
+  ocio_env = lib_getenv("OCIO");
 
   if (ocio_env && ocio_env[0] != '\0') {
     config = OCIO_configCreateFromEnv();
