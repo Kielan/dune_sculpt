24ae3b99cc3f62efd4fd8b5dfb0645ee6050eaeb
Kielan <ki.lemons@gmail.com>
2023-05-06 18:21:46 -0700

    Update and rename LIB_ghash.h to lib_ghash.h

--- source/dune/lib/lib_ghash.h
+++ source/dune/lib/lib_ghash.h
@@ -1,15 +1,11 @@
 #pragma once
 
-/**
- *
- * GHash is a hash-map implementation (unordered key, value pairs).
- *
- * This is also used to implement a 'set' (see #GSet below).
- */
+/** GHash is a hash-map implementation (unordered key, value pairs).
+ * This is also used to implement a 'set' (see GSet below). */
 
-#include "LIB_compiler_attrs.h"
-#include "LIB_compiler_compat.h"
-#include "LIB_sys_types.h" /* for bool */
+#include "lib_compiler_attrs.h"
+#include "lib_compiler_compat.h"
+#include "lib_sys_types.h" /* for bool */
 
 #define _GHASH_INTERNAL_ATTR
 #ifndef GHASH_INTERNAL_API
@@ -59,21 +55,17 @@ enum {
  * Use this to avoid resizing buckets if the size is known or can be closely approximated.
  * return  An empty GHash.
  */
-GHash *LIB_ghash_new_ex(GHashHashFP hashfp,
+GHash *lib_ghash_new_ex(GHashHashFP hashfp,
                         GHashCmpFP cmpfp,
                         const char *info,
                         unsigned int nentries_reserve) ATTR_MALLOC ATTR_WARN_UNUSED_RESULT;
-/**
- * Wraps #LIB_ghash_new_ex with zero entries reserved.
- */
-GHash *LIB_ghash_new(GHashHashFP hashfp,
+/** Wraps Lib_ghash_new_ex with zero entries reserved. */
+GHash *lib_ghash_new(GHashHashFP hashfp,
                      GHashCmpFP cmpfp,
                      const char *info) ATTR_MALLOC ATTR_WARN_UNUSED_RESULT;
-/**
- * Copy given GHash. Keys and values are also copied if relevant callback is provided,
- * else pointers remain the same.
- */
-GHash *LIB_ghash_copy(const GHash *gh,
+/** Copy given GHash. Keys and values are also copied if relevant callback is provided,
+ * else pointers remain the same. */
+GHash *lib_ghash_copy(const GHash *gh,
                       GHashKeyCopyFP keycopyfp,
                       GHashValCopyFP valcopyfp) ATTR_MALLOC ATTR_WARN_UNUSED_RESULT;
 /**
@@ -83,11 +75,9 @@ GHash *LIB_ghash_copy(const GHash *gh,
  * param keyfreefp: Optional callback to free the key.
  * param valfreefp: Optional callback to free the value.
  */
-void LIB_ghash_free(GHash *gh, GHashKeyFreeFP keyfreefp, GHashValFreeFP valfreefp);
-/**
- * Reserve given amount of entries (resize \a gh accordingly if needed).
- */
-void LIB_ghash_reserve(GHash *gh, unsigned int nentries_reserve);
+void lib_ghash_free(GHash *gh, GHashKeyFreeFP keyfreefp, GHashValFreeFP valfreefp);
+/** Reserve given amount of entries (resize gh accordingly if needed). */
+void lib_ghash_reserve(GHash *gh, unsigned int nentries_reserve);
 /**
  * Insert a key/value pair into the \a gh.
  *
@@ -95,26 +85,26 @@ void LIB_ghash_reserve(GHash *gh, unsigned int nentries_reserve);
  * the caller is expected to ensure elements are unique unless
  * GHASH_FLAG_ALLOW_DUPES flag is set.
  */
-void LIB_ghash_insert(GHash *gh, void *key, void *val);
+void lib_ghash_insert(GHash *gh, void *key, void *val);
 /**
  * Inserts a new value to a key that may already be in ghash.
  *
- * Avoids #LIB_ghash_remove, #LIB_ghash_insert calls (double lookups)
+ * Avoids lib_ghash_remove, lib_ghash_insert calls (double lookups)
  *
  * returns true if a new key has been added.
  */
-bool LIB_ghash_reinsert(
+bool lib_ghash_reinsert(
     GHash *gh, void *key, void *val, GHashKeyFreeFP keyfreefp, GHashValFreeFP valfreefp);
 /**
- * Replaces the key of an item in the \a gh.
+ * Replaces the key of an item in the gh.
  *
  * Use when a key is re-allocated or its memory location is changed.
  *
  * returns The previous key or NULL if not found, the caller may free if it's needed.
  */
-void *LIB_ghash_replace_key(GHash *gh, void *key);
+void *lib_ghash_replace_key(GHash *gh, void *key);
 /**
- * Lookup the value of \a key in \a gh.
+ * Lookup the value of key in gh.
  *
  * param key: The key to lookup.
  * returns the value for \a key or NULL.
@@ -122,26 +112,24 @@ void *LIB_ghash_replace_key(GHash *gh, void *key);
  * note When NULL is a valid value, use #LIB_ghash_lookup_p to differentiate a missing key
  * from a key with a NULL value. (Avoids calling #LIB_ghash_haskey before #LIB_ghash_lookup)
  */
-void *LIB_ghash_lookup(const GHash *gh, const void *key) ATTR_WARN_UNUSED_RESULT;
-/**
- * A version of #BLI_ghash_lookup which accepts a fallback argument.
- */
-void *LIB_ghash_lookup_default(const GHash *gh,
+void *lib_ghash_lookup(const GHash *gh, const void *key) ATTR_WARN_UNUSED_RESULT;
+/** A version of lib_ghash_lookup which accepts a fallback argument. */
+void *lib_ghash_lookup_default(const GHash *gh,
                                const void *key,
                                void *val_default) ATTR_WARN_UNUSED_RESULT;
 /**
- * Lookup a pointer to the value of \a key in \a gh.
+ * Lookup a pointer to the value of key in gh.
  *
  * param key: The key to lookup.
  * returns the pointer to value for \a key or NULL.
  *
- * note This has 2 main benefits over #LIB_ghash_lookup.
- * - A NULL return always means that \a key isn't in \a gh.
+ * note This has 2 main benefits over lib_ghash_lookup.
+ * - A NULL return always means that key isn't in gh.
  * - The value can be modified in-place without further function calls (faster).
  */
-void **LIB_ghash_lookup_p(GHash *gh, const void *key) ATTR_WARN_UNUSED_RESULT;
+void **lib_ghash_lookup_p(GHash *gh, const void *key) ATTR_WARN_UNUSED_RESULT;
 /**
- * Ensure \a key is exists in \a gh.
+ * Ensure key is exists in gh.
  *
  * This handles the common situation where the caller needs ensure a key is added to \a gh,
  * constructing a new value in the case the key isn't found.
@@ -154,56 +142,54 @@ void **LIB_ghash_lookup_p(GHash *gh, const void *key) ATTR_WARN_UNUSED_RESULT;
  * returns true when the value didn't need to be added.
  * (when false, the caller _must_ initialize the value).
  */
-bool LIB_ghash_ensure_p(GHash *gh, void *key, void ***r_val) ATTR_WARN_UNUSED_RESULT;
+bool lib_ghash_ensure_p(GHash *gh, void *key, void ***r_val) ATTR_WARN_UNUSED_RESULT;
 /**
- * A version of #LIB_ghash_ensure_p that allows caller to re-assign the key.
+ * A version of lib_ghash_ensure_p that allows caller to re-assign the key.
  * Typically used when the key is to be duplicated.
  *
  * warning Caller _must_ write to \a r_key when returning false.
  */
-bool LIB_ghash_ensure_p_ex(GHash *gh, const void *key, void ***r_key, void ***r_val)
+bool lib_ghash_ensure_p_ex(GHash *gh, const void *key, void ***r_key, void ***r_val)
     ATTR_WARN_UNUSED_RESULT;
 /**
- * Remove \a key from \a gh, or return false if the key wasn't found.
+ * Remove key from gh, or return false if the key wasn't found.
  *
  * param key: The key to remove.
  * param keyfreefp: Optional callback to free the key.
  * param valfreefp: Optional callback to free the value.
  * return true if \a key was removed from \a gh.
  */
-bool LIB_ghash_remove(GHash *gh,
+bool lib_ghash_remove(GHash *gh,
                       const void *key,
                       GHashKeyFreeFP keyfreefp,
                       GHashValFreeFP valfreefp);
+/** Wraps lib_ghash_clear_ex with zero entries reserved. */
+void lib_ghash_clear(GHash *gh, GHashKeyFreeFP keyfreefp, GHashValFreeFP valfreefp);
 /**
- * Wraps #LIB_ghash_clear_ex with zero entries reserved.
- */
-void LIB_ghash_clear(GHash *gh, GHashKeyFreeFP keyfreefp, GHashValFreeFP valfreefp);
-/**
- * Reset \a gh clearing all entries.
+ * Reset gh clearing all entries.
  *
  * param keyfreefp: Optional callback to free the key.
  * param valfreefp: Optional callback to free the value.
  * param nentries_reserve: Optionally reserve the number of members that the hash will hold.
  */
-void LIB_ghash_clear_ex(GHash *gh,
+void lib_ghash_clear_ex(GHash *gh,
                         GHashKeyFreeFP keyfreefp,
                         GHashValFreeFP valfreefp,
                         unsigned int nentries_reserve);
 /**
- * Remove \a key from \a gh, returning the value or NULL if the key wasn't found.
+ * Remove key from gh, returning the value or NULL if the key wasn't found.
  *
  * param key: The key to remove.
  * param keyfreefp: Optional callback to free the key.
- * return the value of \a key int \a gh or NULL.
+ * return the value of key int gh or NULL.
  */
-void *LIB_ghash_popkey(GHash *gh,
+void *lib_ghash_popkey(GHash *gh,
                        const void *key,
                        GHashKeyFreeFP keyfreefp) ATTR_WARN_UNUSED_RESULT;
 /**
- * return true if the \a key is in \a gh.
+ * return true if the key is in gh.
  */
-bool LIB_ghash_haskey(const GHash *gh, const void *key) ATTR_WARN_UNUSED_RESULT;
+bool lib_ghash_haskey(const GHash *gh, const void *key) ATTR_WARN_UNUSED_RESULT;
 /**
  * Remove a random entry from \a gh, returning true
  * if a key/value pair could be removed, false otherwise.
@@ -213,16 +199,12 @@ bool LIB_ghash_haskey(const GHash *gh, const void *key) ATTR_WARN_UNUSED_RESULT;
  * param state: Used for efficient removal.
  * return true if there was something to pop, false if ghash was already empty.
  */
-bool LIB_ghash_pop(GHash *gh, GHashIterState *state, void **r_key, void **r_val)
+bool lib_ghash_pop(GHash *gh, GHashIterState *state, void **r_key, void **r_val)
     ATTR_WARN_UNUSED_RESULT ATTR_NONNULL();
-/**
- * return size of the GHash.
- */
-unsigned int LIB_ghash_len(const GHash *gh) ATTR_WARN_UNUSED_RESULT;
-/**
- * Sets a GHash flag.
- */
-void LIB_ghash_flag_set(GHash *gh, unsigned int flag);
+/** return size of the GHash. */
+unsigned int lib_ghash_len(const GHash *gh) ATTR_WARN_UNUSED_RESULT;
+/** Sets a GHash flag. */
+void lib_ghash_flag_set(GHash *gh, unsigned int flag);
 /**
  * Clear a GHash flag.
  */
