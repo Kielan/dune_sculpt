c13d4acc4b675cebbea50af15fef00969e91385b
Kielan <ki.lemons@gmail.com>
2023-05-10 18:55:49 -0700

    Create api_material_api.c

---
+++ source/dune/api/intern/api_material_api.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lib_utildefines.h"
+
+#include "api_define.h"
+
+#include "types_material_types.h"
+
+#include "api_internal.h" /* own include */
+
+#ifdef API_RUNTIME
+
+#else
+
+void api_material(ApiStruct *UNUSED(srna))
+{
+  // ApiFn *fn;
+  // ApiProp *parm;
+}
+
+#endif
