c440441ce42cbf541137a1a290aafd66a60608ca
Kielan <ki.lemons@gmail.com>
2023-05-22 17:44:54 -0700

    Update api_shader.c

--- source/dune/api/intern/api_shader.c
+++ source/dune/api/intern/api_shader.c
@@ -1,36 +1,36 @@
 #include <stdlib.h>
 
-#include "RNA_define.h"
-#include "RNA_enum_types.h"
+#include "api_define.h"
+#include "api_enum_types.h"
 
-#include "DNA_simulation_types.h"
+#include "types_simulation_types.h"
 
-#include "rna_internal.h"
+#include "api_internal.h"
 
-#ifdef RNA_RUNTIME
+#ifdef API_RUNTIME
 
 #else
 
-static void rna_def_simulation(BlenderRNA *brna)
+static void api_def_simulation(DuneApi *dapi)
 {
-  StructRNA *srna;
-  PropertyRNA *prop;
+  ApiStruct *sapi;
+  ApiProp *prop;
 
-  srna = RNA_def_struct(brna, "Simulation", "ID");
-  RNA_def_struct_ui_text(srna, "Simulation", "Simulation data-block");
-  RNA_def_struct_ui_icon(srna, ICON_PHYSICS); /* TODO: Use correct icon. */
+  sapi = api_def_struct(dapi, "Simulation", "ID");
+  api_def_struct_ui_text(sapi, "Simulation", "Simulation data-block");
+  api_def_struct_ui_icon(sapi, ICON_PHYSICS); /* TODO: Use correct icon. */
 
-  prop = RNA_def_property(srna, "node_tree", PROP_POINTER, PROP_NONE);
-  RNA_def_property_pointer_sdna(prop, NULL, "nodetree");
-  RNA_def_property_clear_flag(prop, PROP_PTR_NO_OWNERSHIP);
-  RNA_def_property_override_flag(prop, PROPOVERRIDE_OVERRIDABLE_LIBRARY);
-  RNA_def_property_ui_text(prop, "Node Tree", "Node tree defining the simulation");
+  prop = api_def_prop(sapi, "node_tree", PROP_PTR, PROP_NONE);
+  api_def_prop_ptr_stype(prop, NULL, "nodetree");
+  api_def_prop_clear_flag(prop, PROP_PTR_NO_OWNERSHIP);
+  api_def_prop_override_flag(prop, PROPOVERRIDE_OVERRIDABLE_LIBRARY);
+  api_def_prop_ui_text(prop, "Node Tree", "Node tree defining the simulation");
 
   /* common */
-  rna_def_animdata_common(srna);
+  rna_def_animdata_common(sapi);
 }
 
-void RNA_def_simulation(BlenderRNA *brna)
+void RNA_def_simulation(DuneApi *dapi)
 {
   rna_def_simulation(brna);
 }
