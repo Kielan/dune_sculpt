e3da94e9eda78df0144b89668c0f9333a71158fb
Kielan <ki.lemons@gmail.com>
2023-05-23 16:37:29 -0700

    Update api_sequencer_api.c

--- source/dune/api/intern/api_sequencer_api.c
+++ source/dune/api/intern/api_sequencer_api.c
@@ -912,17 +912,17 @@ void api_sequences(DuneApi *dapi, PropertyRNA *cprop, const bool metastrip)
   parm = api_def_ptr(fn, "sequence", "Sequence", "", "New Sequence");
   api_def_fn_return(fn, parm);
 
-  fn = api_def_fn(srna, "new_scene", new_scene_func_name);
-  api_def_fn_flag(func, FUNC_USE_SELF_ID | FUNC_USE_MAIN);
-  api_def_fn_ui_description(func, "Add a new scene sequence");
-  parm = api_def_string(func, "name", "Name", 0, "", "Name for the new sequence");
-  RNA_def_parameter_flags(parm, 0, PARM_REQUIRED);
-  parm = RNA_def_pointer(func, "scene", "Scene", "", "Scene to add");
-  RNA_def_parameter_flags(parm, PROP_NEVER_NULL, PARM_REQUIRED);
-  parm = RNA_def_int(
+  fn = api_def_fn(sapi, "new_scene", new_scene_func_name);
+  api_def_fn_flag(fn, FN_USE_SELF_ID | FUNC_USE_MAIN);
+  api_def_fn_ui_description(fn, "Add a new scene sequence");
+  parm = api_def_string(fn, "name", "Name", 0, "", "Name for the new sequence");
+  api_def_param_flags(parm, 0, PARM_REQUIRED);
+  parm = api_def_ptr(fn, "scene", "Scene", "", "Scene to add
+  api_def_param_flags(parm, PROP_NEVER_NULL, PARM_REQUIRED);
+  parm = api_def_int(
       func, "channel", 0, 1, MAXSEQ, "Channel", "The channel for the new sequence", 1, MAXSEQ);
-  RNA_def_parameter_flags(parm, 0, PARM_REQUIRED);
-  parm = RNA_def_int(func,
+  RNA_def_param_flags(parm, 0, PARM_REQUIRED);
+  parm = RNA_def_int(fn,
                      "frame_start",
                      0,
                      -MAXFRAME,
