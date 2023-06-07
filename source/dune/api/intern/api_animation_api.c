8bcdc9eb473c0d467ee5bcc53439dfd36d9fab4d
Kielan <ki.lemons@gmail.com>
2023-06-07 01:03:03 -0700

    Update api_animation_api.c

--- source/dune/api/intern/api_animation_api.c
+++ source/dune/api/intern/api_animation_api.c
@@ -1,82 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "RNA_define.h"
-#include "RNA_enum_types.h"
+#include "api_define.h"
+#include "api_enum_types.h"
 
-#include "DNA_anim_types.h"
-#include "DNA_object_types.h"
-#include "DNA_scene_types.h"
+#include "types_anim.h"
+#include "types_object.h"
+#include "types_scene.h"
 
-#include "rna_internal.h" /* own include */
+#include "api_internal.h" /* own include */
 
-#ifdef RNA_RUNTIME
+#ifdef API_RUNTIME
 
-#  include "BKE_context.h"
-#  include "BKE_nla.h"
-#  include "BKE_report.h"
+#  include "dune_context.h"
+#  include "dune_nla.h"
+#  include "dune_report.h"
 
-#  include "ED_keyframing.h"
+#  include "ed_keyframing.h"
 
-static void rna_KeyingSet_context_refresh(KeyingSet *ks, bContext *C, ReportList *reports)
+static void api_KeyingSet_cxt_refresh(KeyingSet *ks, Cxt *C, ReportList *reports)
 {
   /* TODO: enable access to providing a list of overrides (dsources)? */
   const eModifyKey_Returns error = ANIM_validate_keyingset(C, NULL, ks);
 
   if (error != 0) {
     switch (error) {
-      case MODIFYKEY_INVALID_CONTEXT:
-        BKE_report(reports, RPT_ERROR, "Invalid context for keying set");
+      case MODIFYKEY_INVALID_CXT:
+        dune_report(reports, RPT_ERROR, "Invalid context for keying set");
         break;
 
       case MODIFYKEY_MISSING_TYPEINFO:
-        BKE_report(
+        dune_report(
             reports, RPT_ERROR, "Incomplete built-in keying set, appears to be missing type info");
         break;
     }
   }
 }
 
-static float rna_AnimData_nla_tweak_strip_time_to_scene(AnimData *adt, float frame, bool invert)
+static float api_AnimData_nla_tweak_strip_time_to_scene(AnimData *adt, float frame, bool invert)
 {
-  return BKE_nla_tweakedit_remap(adt, frame, invert ? NLATIME_CONVERT_UNMAP : NLATIME_CONVERT_MAP);
+  return dune_nla_tweakedit_remap(adt, frame, invert ? NLATIME_CONVERT_UNMAP : NLATIME_CONVERT_MAP);
 }
 
 #else
 
-void RNA_api_keyingset(StructRNA *srna)
+void api_keyingset(ApiStruct *sapi)
 {
-  FunctionRNA *func;
-  // PropertyRNA *parm;
+  ApiFn *fn;
+  // ApiProp *parm;
 
   /* validate relative Keying Set (used to ensure paths are ok for context) */
-  func = RNA_def_function(srna, "refresh", "rna_KeyingSet_context_refresh");
-  RNA_def_function_ui_description(
-      func,
+  fn = api_def_fn(sapi, "refresh", "api_KeyingSet_cxt_refresh");
+  api_def_fn_ui_description(
+      fn,
       "Refresh Keying Set to ensure that it is valid for the current context "
       "(call before each use of one)");
-  RNA_def_function_flag(func, FUNC_USE_CONTEXT | FUNC_USE_REPORTS);
+  api_def_fn_flag(fn, FN_USE_CXT | FN_USE_REPORTS);
 }
 
-void RNA_api_animdata(StructRNA *srna)
+void api_animdata(ApiStruct *sapi)
 {
-  FunctionRNA *func;
-  PropertyRNA *parm;
+  ApiFn *fn;
+  ApiProp *parm;
 
   /* Convert between action time and scene time when tweaking a NLA strip. */
-  func = RNA_def_function(
-      srna, "nla_tweak_strip_time_to_scene", "rna_AnimData_nla_tweak_strip_time_to_scene");
-  RNA_def_function_ui_description(func,
-                                  "Convert a time value from the local time of the tweaked strip "
-                                  "to scene time, exactly as done by built-in key editing tools. "
-                                  "Returns the input time unchanged if not tweaking.");
-  parm = RNA_def_float(
-      func, "frame", 0.0, MINAFRAME, MAXFRAME, "", "Input time", MINAFRAME, MAXFRAME);
-  RNA_def_parameter_flags(parm, 0, PARM_REQUIRED);
-  RNA_def_boolean(func, "invert", false, "Invert", "Convert scene time to action time");
-  parm = RNA_def_float(
-      func, "result", 0.0, MINAFRAME, MAXFRAME, "", "Converted time", MINAFRAME, MAXFRAME);
-  RNA_def_function_return(func, parm);
+  fn = api_def_fn(
+      sapi, "nla_tweak_strip_time_to_scene", "rna_AnimData_nla_tweak_strip_time_to_scene");
+  api_def_fn_ui_description(fn,
+                            "Convert a time value from the local time of the tweaked strip "
+                            "to scene time, exactly as done by built-in key editing tools. "
+                            "Returns the input time unchanged if not tweaking.");
+  parm = api_def_float(
+      fn, "frame", 0.0, MINAFRAME, MAXFRAME, "", "Input time", MINAFRAME, MAXFRAME);
+  api_def_param_flags(parm, 0, PARM_REQUIRED);
+  api_def_bool(fn, "invert", false, "Invert", "Convert scene time to action time");
+  parm = api_def_float(
+      fn, "result", 0.0, MINAFRAME, MAXFRAME, "", "Converted time", MINAFRAME, MAXFRAME);
+  api_def_fn_return(fn, parm);
 }
 
 #endif
