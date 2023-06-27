3246aeadd452828ffbff2e2ef255bcdd6c2096cb
Kielan <ki.lemons@gmail.com>
2023-06-26 17:26:59 -0700

    Update api_lattice_api.c

--- source/dune/api/intern/api_lattice_api.c
+++ source/dune/api/intern/api_lattice_api.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "RNA_define.h"
+#include "api_define.h"
 
-#include "BLI_sys_types.h"
+#include "lib_sys_types.h"
 
-#include "BLI_utildefines.h"
+#include "lib_utildefines.h"
 
-#include "rna_internal.h" /* own include */
+#include "api_internal.h" /* own include */
 
-#ifdef RNA_RUNTIME
-static void rna_Lattice_transform(Lattice *lt, float mat[16], bool shape_keys)
+#ifdef API_RUNTIME
+static void api_Lattice_transform(Lattice *lt, float mat[16], bool shape_keys)
 {
-  BKE_lattice_transform(lt, (float(*)[4])mat, shape_keys);
+  dune_lattice_transform(lt, (float(*)[4])mat, shape_keys);
 
-  DEG_id_tag_update(&lt->id, 0);
+  dune_id_tag_update(&lt->id, 0);
 }
 
-static void rna_Lattice_update_gpu_tag(Lattice *lt)
+static void api_Lattice_update_gpu_tag(Lattice *lt)
 {
-  BKE_lattice_batch_cache_dirty_tag(lt, BKE_LATTICE_BATCH_DIRTY_ALL);
+  dune_lattice_batch_cache_dirty_tag(lt, DUNE_LATTICE_BATCH_DIRTY_ALL);
 }
 
 #else
 
-void RNA_api_lattice(StructRNA *srna)
+void api_api_lattice(ApiStruct *sapi)
 {
-  FunctionRNA *func;
-  PropertyRNA *parm;
+  Fn *fn;
+  ApiProp *parm;
 
-  func = RNA_def_function(srna, "transform", "rna_Lattice_transform");
-  RNA_def_function_ui_description(func, "Transform lattice by a matrix");
+  fn = api_def_fn(sapi, "transform", "rna_Lattice_transform");
+  RNA_def_function_ui_description(fn, "Transform lattice by a matrix");
   parm = RNA_def_float_matrix(func, "matrix", 4, 4, NULL, 0.0f, 0.0f, "", "Matrix", 0.0f, 0.0f);
   RNA_def_parameter_flags(parm, 0, PARM_REQUIRED);
   RNA_def_boolean(func, "shape_keys", 0, "", "Transform Shape Keys");
