4a8ea5a525a989a4b987f459d03f0943ff41354b
Kielan <ki.lemons@gmail.com>
2023-06-27 14:03:39 -0700

    Update api_lightprobe.c

--- source/dune/api/intern/api_lightprobe.c
+++ source/dune/api/intern/api_lightprobe.c
@@ -65,31 +65,31 @@ static void api_def_lightprobe(DuneApi *dapi)
   api_def_prop_clear_flag(prop, PROP_EDITABLE);
 
   prop = api_def_prop(sapi, "clip_start", PROP_FLOAT, PROP_DISTANCE);
-  api_def_prop_float_styoe(prop, NULL, "clipsta");
+  api_def_prop_float_stype(prop, NULL, "clipsta");
   api_def_prop_range(prop, 1e-6f, FLT_MAX);
   api_def_prop_ui_range(prop, 0.001f, FLT_MAX, 10, 3);
   api_def_prop_ui_text(
       prop, "Clip Start", "Probe clip start, below which objects will not appear in reflections");
   api_def_prop_update(prop, NC_MATERIAL | ND_SHADING, "rna_LightProbe_recalc");
 
-  prop = RNA_def_property(srna, "clip_end", PROP_FLOAT, PROP_DISTANCE);
-  RNA_def_property_float_sdna(prop, NULL, "clipend");
-  RNA_def_property_range(prop, 1e-6f, FLT_MAX);
-  RNA_def_property_ui_range(prop, 0.001f, FLT_MAX, 10, 3);
+  prop = api_def_prop(sapi, "clip_end", PROP_FLOAT, PROP_DISTANCE);
+  api_def_prop_float_stype(prop, NULL, "clipend");
+  api_def_prop_range(prop, 1e-6f, FLT_MAX);
+  api_def_prop_ui_range(prop, 0.001f, FLT_MAX, 10, 3);
   RNA_def_property_ui_text(
       prop, "Clip End", "Probe clip end, beyond which objects will not appear in reflections");
   RNA_def_property_update(prop, NC_MATERIAL | ND_SHADING, "rna_LightProbe_recalc");
 
-  prop = RNA_def_property(srna, "show_clip", PROP_BOOLEAN, PROP_NONE);
-  RNA_def_property_boolean_sdna(prop, NULL, "flag", LIGHTPROBE_FLAG_SHOW_CLIP_DIST);
-  RNA_def_property_ui_text(prop, "Clipping", "Show the clipping distances in the 3D view");
-  RNA_def_property_update(prop, NC_MATERIAL | ND_SHADING, NULL);
+  prop = RNA_def_prop(sapi, "show_clip", PROP_BOOLEAN, PROP_NONE);
+  RNA_def_prop_bool_stype(prop, NULL, "flag", LIGHTPROBE_FLAG_SHOW_CLIP_DIST);
+  RNA_def_prop_ui_text(prop, "Clipping", "Show the clipping distances in the 3D view");
+  RNA_def_prop_update(prop, NC_MATERIAL | ND_SHADING, NULL);
 
-  prop = RNA_def_property(srna, "influence_type", PROP_ENUM, PROP_NONE);
-  RNA_def_property_enum_sdna(prop, NULL, "attenuation_type");
-  RNA_def_property_enum_items(prop, parallax_type_items);
-  RNA_def_property_ui_text(prop, "Type", "Type of influence volume");
-  RNA_def_property_update(prop, NC_MATERIAL | ND_SHADING, NULL);
+  prop = api_def_prop(sapi, "influence_type", PROP_ENUM, PROP_NONE);
+  RNA_def_prop_enum_stype(prop, NULL, "attenuation_type");
+  RNA_def_prop_enum_items(prop, parallax_type_items);
+  RNA_def_prop_ui_text(prop, "Type", "Type of influence volume");
+  RNA_def_prop_update(prop, NC_MATERIAL | ND_SHADING, NULL);
 
   prop = RNA_def_property(srna, "show_influence", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, NULL, "flag", LIGHTPROBE_FLAG_SHOW_INFLUENCE);
