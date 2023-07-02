89d37b4048a542ac1749d088cc8283cc5ec03c40
Kielan <ki.lemons@gmail.com>
2023-07-01 22:01:51 -0700

    Update api_mesh.c

--- source/dune/api/intern/api_mesh.c
+++ source/dune/api/intern/api_mesh.c
@@ -1,16 +1,10 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-/* NOTE: the original vertex color stuff is now just used for
+/* NOTE: The original vertex color stuff is now just used for
  * getting info on the layers themselves, accessing the data is
  * done through the (not yet written) mpoly interfaces. */
 
-/** \file
- * \ingroup RNA
- */
-
 #include <stdlib.h>
 
-#include "MEM_guardedalloc.h"
+#include "mem_guardedalloc.h"
 
 #include "DNA_material_types.h"
 #include "DNA_mesh_types.h"
