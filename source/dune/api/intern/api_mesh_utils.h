7731a60c07667af458fda6676eb1a8e4b24e0931
Kielan <ki.lemons@gmail.com>
2023-07-04 07:39:06 -0700

    Update api_mesh_utils.h

--- source/dune/api/intern/api_mesh_utils.h
+++ source/dune/api/intern/api_mesh_utils.h
@@ -1,43 +1,42 @@
 #pragma once
 
 /* Macros to help reduce code clutter in rna_mesh.c */
-
 /* Define the accessors for a basic CustomDataLayer collection */
 #define DEFINE_CUSTOMDATA_LAYER_COLLECTION(collection_name, customdata_type, layer_type) \
   /* check */ \
-  static int rna_##collection_name##_check(CollectionPropertyIterator *UNUSED(iter), void *data) \
+  static int api_##collection_name##_check(CollectionPropIter *UNUSED(iter), void *data) \
   { \
     CustomDataLayer *layer = (CustomDataLayer *)data; \
     return (layer->type != layer_type); \
   } \
   /* begin */ \
-  static void rna_Mesh_##collection_name##s_begin(CollectionPropertyIterator *iter, \
-                                                  PointerRNA *ptr) \
+  static void api_Mesh_##collection_name##s_begin(CollectionPropIter iter, \
+                                                  ApiPtr *ptr) \
   { \
-    CustomData *data = rna_mesh_##customdata_type(ptr); \
+    CustomData *data = api_mesh_##customdata_type(ptr); \
     if (data) { \
-      rna_iterator_array_begin(iter, \
+       api_iter_array_begin(iter, \
                                (void *)data->layers, \
                                sizeof(CustomDataLayer), \
                                data->totlayer, \
                                0, \
-                               rna_##collection_name##_check); \
+                               api_##collection_name##_check); \
     } \
     else { \
-      rna_iterator_array_begin(iter, NULL, 0, 0, 0, NULL); \
+      api_iter_array_begin(iter, NULL, 0, 0, 0, NULL); \
     } \
   } \
   /* length */ \
-  static int rna_Mesh_##collection_name##s_length(PointerRNA *ptr) \
+  static int api_Mesh_##collection_name##s_length(ApiPtr *ptr) \
   { \
-    CustomData *data = rna_mesh_##customdata_type(ptr); \
+    CustomData *data = api_mesh_##customdata_type(ptr); \
     return data ? CustomData_number_of_layers(data, layer_type) : 0; \
   } \
   /* index range */ \
-  static void rna_Mesh_##collection_name##_index_range( \
-      PointerRNA *ptr, int *min, int *max, int *UNUSED(softmin), int *UNUSED(softmax)) \
+  static void api_Mesh_##collection_name##_index_range( \
+      ApiPtr *ptr, int *min, int *max, int *UNUSED(softmin), int *UNUSED(softmax)) \
   { \
-    CustomData *data = rna_mesh_##customdata_type(ptr); \
+    CustomData *data = api_mesh_##customdata_type(ptr); \
     *min = 0; \
     *max = data ? CustomData_number_of_layers(data, layer_type) - 1 : 0; \
     *max = MAX2(0, *max); \
@@ -46,11 +45,11 @@
 /* Define the accessors for special CustomDataLayers in the collection
  * (active, render, clone, stencil, etc) */
 #define DEFINE_CUSTOMDATA_LAYER_COLLECTION_ACTIVEITEM( \
-    collection_name, customdata_type, layer_type, active_type, layer_rna_type) \
+    collection_name, customdata_type, layer_type, active_type, layer_api_type) \
 \
-  static PointerRNA rna_Mesh_##collection_name##_##active_type##_get(PointerRNA *ptr) \
+  static ApiPtr api_Mesh_##collection_name##_##active_type##_get(ApiPtr *ptr) \
   { \
-    CustomData *data = rna_mesh_##customdata_type(ptr); \
+    CustomData *data = api_mesh_##customdata_type(ptr); \
     CustomDataLayer *layer; \
     if (data) { \
       int index = CustomData_get_##active_type##_layer_index(data, layer_type); \
@@ -59,14 +58,14 @@
     else { \
       layer = NULL; \
     } \
-    return rna_pointer_inherit_refine(ptr, &RNA_##layer_rna_type, layer); \
+    return api_ptr_inherit_refine(ptr, &api_##layer_api_type, layer); \
   } \
 \
-  static void rna_Mesh_##collection_name##_##active_type##_set( \
-      PointerRNA *ptr, PointerRNA value, struct ReportList *UNUSED(reports)) \
+  static void api_Mesh_##collection_name##_##active_type##_set( \
+      ApiPtr *ptr, ApiPtr value, struct ReportList *UNUSED(reports)) \
   { \
-    Mesh *me = rna_mesh(ptr); \
-    CustomData *data = rna_mesh_##customdata_type(ptr); \
+    Mesh *me = api_mesh(ptr); \
+    CustomData *data = api_mesh_##customdata_type(ptr); \
     int a; \
     if (data) { \
       CustomDataLayer *layer; \
@@ -75,16 +74,16 @@
            layer++, a++) { \
         if (value.data == layer) { \
           CustomData_set_layer_##active_type(data, layer_type, a); \
-          BKE_mesh_update_customdata_pointers(me, true); \
+          dune_mesh_update_customdata_pointers(me, true); \
           return; \
         } \
       } \
     } \
   } \
 \
-  static int rna_Mesh_##collection_name##_##active_type##_index_get(PointerRNA *ptr) \
+  static int api_Mesh_##collection_name##_##active_type##_index_get(ApiPtr *ptr) \
   { \
-    CustomData *data = rna_mesh_##customdata_type(ptr); \
+    CustomData *data = api_mesh_##customdata_type(ptr); \
     if (data) { \
       return CustomData_get_##active_type##_layer(data, layer_type); \
     } \
@@ -93,12 +92,12 @@
     } \
   } \
 \
-  static void rna_Mesh_##collection_name##_##active_type##_index_set(PointerRNA *ptr, int value) \
+  static void api_Mesh_##collection_name##_##active_type##_index_set(ApiPtr *ptr, int value) \
   { \
-    Mesh *me = rna_mesh(ptr); \
-    CustomData *data = rna_mesh_##customdata_type(ptr); \
+    Mesh *me = api_mesh(ptr); \
+    CustomData *data = api_mesh_##customdata_type(ptr); \
     if (data) { \
       CustomData_set_layer_##active_type(data, layer_type, value); \
-      BKE_mesh_update_customdata_pointers(me, true); \
+      dune_mesh_update_customdata_ptrs(me, true); \
     } \
   }
