89b18c8af480002e92d29600b47e3d39eb0c29d9
Kielan <ki.lemons@gmail.com>
2023-08-09 06:34:29 -0700

    Update api_text_api.c

--- source/dune/api/intern/api_text_api.c
+++ source/dune/api/intern/api_text_api.c
@@ -58,46 +58,45 @@ void api_text(ApiStruct *sapi)
   ApiFn *fn;
   ApiProp *parm;
 
-  fn = api_def_fn(sapi, "clear", "rna_Text_clear");
+  fn = api_def_fn(sapi, "clear", "api_Text_clear");
   api_def_fn_ui_description(fn, "clear the text block");
 
-  fn = api_def_fn(sapi, "write", "rna_Text_write");
+  fn = api_def_fn(sapi, "write", "api_Text_write");
   api_def_fn_ui_description(
       fn, "write text at the cursor location and advance to the end of the text block");
   parm = api_def_string(fn, "text", "Text", 0, "", "New text for this data-block");
   api_def_param_flags(parm, 0, PARM_REQUIRED);
 
-  fn = api_def_fn(sapi, "from_string", "rna_Text_from_string");
+  fn = api_def_fn(sapi, "from_string", "api_Text_from_string");
   api_def_fn_ui_description(fn, "Replace text with this string.");
   parm = api_def_string(fn, "text", "Text", 0, "", "");
   api_def_param_flags(parm, 0, PARM_REQUIRED);
 
-  fn = api_def_fn(sapi, "as_string", "rna_Text_as_string");
+  fn = api_def_fn(sapi, "as_string", "api_Text_as_string");
   api_def_fn_ui_description(fn, "Return the text as a string");
   parm = api_def_string(fn, "text", "Text", 0, "", "");
   api_def_param_flags(parm, PROP_DYNAMIC, PARM_OUTPUT);
 
   fn = api_def_fn(
       sapi, "is_syntax_highlight_supported", "ed_text_is_syntax_highlight_supported");
-  api_def_fn_return(fn,
-                          api_def_bool(fn, "is_syntax_highlight_supported", false, "", ""));
+  api_def_fn_return(fn, api_def_bool(fn, "is_syntax_highlight_supported", false, "", ""));
   api_def_fn_ui_description(fn,
-                                  "Returns True if the editor supports syntax highlighting "
-                                  "for the current text datablock");
+                            "Returns True if the editor supports syntax highlighting "
+                            "for the current text datablock");
 
-  fn = api_def_fn(sapi, "select_set", "rna_Text_select_set");
+  fn = api_def_fn(sapi, "select_set", api_Text_select_set");
   api_def_fn_ui_description(fn, "Set selection range by line and character index");
   parm = api_def_int(fn, "line_start", 0, INT_MIN, INT_MAX, "Start Line", "", INT_MIN, INT_MAX);
   api_def_param_flags(parm, 0, PARM_REQUIRED);
   parm = api_def_int(
       fn, "char_start", 0, INT_MIN, INT_MAX, "Start Character", "", INT_MIN, INT_MAX);
   api_def_param_flags(parm, 0, PARM_REQUIRED);
-  parm = api_def_int(func, "line_end", 0, INT_MIN, INT_MAX, "End Line", "", INT_MIN, INT_MAX);
+  parm = api_def_int(fn, "line_end", 0, INT_MIN, INT_MAX, "End Line", "", INT_MIN, INT_MAX);
   api_def_param_flags(parm, 0, PARM_REQUIRED);
-  parm = api_def_int(func, "char_end", 0, INT_MIN, INT_MAX, "End Character", "", INT_MIN, INT_MAX);
+  parm = api_def_int(fn, "char_end", 0, INT_MIN, INT_MAX, "End Character", "", INT_MIN, INT_MAX);
   api_def_param_flags(parm, 0, PARM_REQUIRED);
 
-  fn = api_def_fn(sapi, "cursor_set", "rna_Text_cursor_set");
+  fn = api_def_fn(sapi, "cursor_set", "api_Text_cursor_set");
   api_def_fn_ui_description(fn, "Set cursor by line and (optionally) character index");
   parm = api_def_int(fn, "line", 0, 0, INT_MAX, "Line", "", 0, INT_MAX);
   api_def_param_flags(parm, 0, PARM_REQUIRED);
