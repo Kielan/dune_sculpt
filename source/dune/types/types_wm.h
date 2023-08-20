397b8871c0bf4ec74e23d7b05f1c0b44b80fd449
Kielan <ki.lemons@gmail.com>
2023-08-20 05:48:49 -0700

    Update types_wm.h

--- source/dune/types/types_wm.h
+++ source/dune/types/types_wm.h
@@ -1,30 +1,29 @@
 #pragma once
 
-#include "STRUCTS_listBase.h"
-#include "STRUCTS_screen_types.h" /* for #ScrAreaMap */
-#include "STRUCTS_xr_types.h"     /* for #XrSessionSettings */
+#include "types_list.h"
+#include "types_screen.h" /* for #ScrAreaMap */
+#include "types_xr.h"     /* for #XrSessionSettings */
 
-#include "STRUCTS_ID.h"
+#include "types_id.h"
 
-struct wmWindow;
-struct wmWindowManager;
+struct Window;
+struct WM;
 
 struct wmEvent;
 struct wmGesture;
 struct wmKeyConfig;
 struct wmKeyMap;
 struct wmMsgBus;
-struct wmOperator;
-struct wmOperatorType;
+struct wmOp;
+struct wmOpType;
 
 /* Forward declarations: */
-
-struct PointerAPI;
+struct PtrAPI;
 struct Report;
 struct ReportList;
 struct Stereo3dFormat;
-struct bContext;
-struct bScreen;
+struct Cxt;
+struct Screen;
 struct uiLayout;
 struct wmTimer;
 
@@ -35,8 +34,8 @@ struct wmTimer;
 typedef enum eReportType {
   RPT_DEBUG = (1 << 0),
   RPT_INFO = (1 << 1),
-  RPT_OPERATOR = (1 << 2),
-  RPT_PROPERTY = (1 << 3),
+  RPT_OP = (1 << 2),
+  RPT_PROP = (1 << 3),
   RPT_WARNING = (1 << 4),
   RPT_ERROR = (1 << 5),
   RPT_ERROR_INVALID_INPUT = (1 << 6),
@@ -46,8 +45,8 @@ typedef enum eReportType {
 
 #define RPT_DEBUG_ALL (RPT_DEBUG)
 #define RPT_INFO_ALL (RPT_INFO)
-#define RPT_OPERATOR_ALL (RPT_OPERATOR)
-#define RPT_PROPERTY_ALL (RPT_PROPERTY)
+#define RPT_OP_ALL (RPT_OP)
+#define RPT_PROP_ALL (RPT_PROP)
 #define RPT_WARNING_ALL (RPT_WARNING)
 #define RPT_ERROR_ALL \
   (RPT_ERROR | RPT_ERROR_INVALID_INPUT | RPT_ERROR_INVALID_CONTEXT | RPT_ERROR_OUT_OF_MEMORY)
@@ -59,7 +58,7 @@ enum ReportListFlags {
   RPT_OP_HOLD = (1 << 3), /* don't move them into the operator global list (caller will use) */
 };
 
-/* These two Lines with # tell makesdna this struct can be excluded. */
+/* These two Lines with # tell types this struct can be excluded. */
 #
 #
 typedef struct Report {
@@ -73,11 +72,9 @@ typedef struct Report {
   const char *message;
 } Report;
 
-/**
- * Saved in the wm, don't remove.
- */
+/** Saved in the wm, don't remove. */
 typedef struct ReportList {
-  ListBase list;
+  List list;
   /** eReportType. */
   int printlevel;
   /** eReportType. */
@@ -88,7 +85,7 @@ typedef struct ReportList {
 } ReportList;
 
 /* timer customdata to control reports display */
-/* These two Lines with # tell makesdna this struct can be excluded. */
+/* These two Lines with # tell types this struct can be excluded. */
 #
 #
 typedef struct ReportTimerInfo {
@@ -98,7 +95,7 @@ typedef struct ReportTimerInfo {
 
 //#ifdef WITH_XR_OPENXR
 typedef struct wmXrData {
-  /** Runtime information for managing Blender specific behaviors. */
+  /** Runtime information for managing Dune specific behaviors. */
   struct wmXrRuntimeData *runtime;
   /** Permanent session settings (draw mode, feature toggles, etc). Stored in files and accessible
    * even before the session runs. */
@@ -106,50 +103,48 @@ typedef struct wmXrData {
 } wmXrData;
 
 /** Window-manager is saved, tag WMAN. */
-typedef struct wmWindowManager {
-  ID id;
+typedef struct WM {
+  Id id;
 
   /** Separate active from drawable. */
-  struct wmWindow *windrawable;
-  /**
-   * `CTX_wm_window(C)` is usually preferred.
+  struct Window *windrawable;
+  /** `cxt_wm_window(C)` is usually preferred.
    * Avoid relying on this where possible as this may become NULL during when handling
    * events that close or replace windows (opening a file for e.g.).
-   * While this happens rarely in practice, it can cause difficult to reproduce bugs.
-   */
-  struct wmWindow *winactive;
-  ListBase windows;
+   * While this happens rarely in practice, it can cause difficult to reproduce bugs.  */
+  struct Window *winactive;
+  List windows;
 
   /** Set on file read. */
   short initialized;
   /** Indicator whether data was saved. */
   short file_saved;
-  /** Operator stack depth to avoid nested undo pushes. */
+  /** Op stack depth to avoid nested undo pushes. */
   short op_undo_depth;
 
   /** Set after selection to notify outliner to sync. Stores type of selection */
   short outliner_sync_select_dirty;
 
   /** Operator registry. */
-  ListBase operators;
+  List ops;
 
   /** Refresh/redraw #wmNotifier structs. */
-  ListBase notifier_queue;
+  List notifier_queue;
 
   /** Information and error reports. */
   struct ReportList reports;
 
   /** Threaded jobs manager. */
-  ListBase jobs;
+  List jobs;
 
   /** Extra overlay cursors to draw, like circles. */
-  ListBase paintcursors;
+  List paintcursors;
 
   /** Active dragged items. */
-  ListBase drags;
+  List drags;
 
   /** Known key configurations. */
-  ListBase keyconfigs;
+  List keyconfigs;
   /** Default configuration. */
   struct wmKeyConfig *defaultconf;
   /** Addon configuration. */
@@ -158,7 +153,7 @@ typedef struct wmWindowManager {
   struct wmKeyConfig *userconf;
 
   /** Active timers. */
-  ListBase timers;
+  List timers;
   /** Timer for auto save. */
   struct wmTimer *autosavetimer;
 
@@ -174,4 +169,4 @@ typedef struct wmWindowManager {
   //#ifdef WITH_XR_OPENXR
   wmXrData xr;
   //#endif
-} wmWindowManager;
+} WM;
