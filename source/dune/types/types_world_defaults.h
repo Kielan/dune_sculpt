c0fdfdef52af6330642ad0b4a501c07f9469d7f5
Kielan <ki.lemons@gmail.com>
2023-08-20 13:21:42 -0700

    Create types_world_defaults.h

---
+++ source/dune/types/types_world_defaults.h
@@ -0,0 +1,23 @@
+#pragma once
+
+/* Struct members on own line. */
+/* clang-format off */
+
+/* -------------------------------------------------------------------- */
+/** World Struct **/
+
+#define _TYPES_DEFAULT_World \
+  { \
+    .horr = 0.05f, \
+    .horg = 0.05f, \
+    .horb = 0.05f, \
+  \
+    .aodist = 10.0f, \
+    .aoenergy = 1.0f, \
+  \
+    .preview = NULL, \
+    .miststa = 5.0f, \
+    .mistdist = 25.0f, \
+  }
+
+/* clang-format on */
