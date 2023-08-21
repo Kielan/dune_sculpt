68eba5ba559b0fff0da94c14ef4654be01d48fa3
Kielan <ki.lemons@gmail.com>
2023-08-21 16:59:12 -0700

    Create types_pointcloud_defaults.h

---
+++ source/dune/types/types_pointcloud_defaults.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/* Struct members on own line. */
+/* clang-format off */
+
+/* -------------------------------------------------------------------- */
+/* PointCloud Struct */
+
+#define _TYPES_DEFAULT_PointCloud \
+  { \
+    .flag = 0, \
+  }
+
+/* clang-format on */
