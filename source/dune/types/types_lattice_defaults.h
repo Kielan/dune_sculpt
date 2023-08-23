8774a7c6a994523b4cdfb1c2504e39a819ea6232
Kielan <ki.lemons@gmail.com>
2023-08-23 10:38:40 -0700

    Create types_lattice_defaults.h

---
+++ source/dune/types/types_lattice_defaults.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/* Struct members on own line. */
+/* clang-format off */
+
+/* -------------------------------------------------------------------- */
+/** Lattice Struct **/
+
+#define _TYPES_DEFAULT_Lattice \
+  { \
+    .flag = LT_GRID, \
+    .typeu = KEY_BSPLINE, \
+    .typev = KEY_BSPLINE, \
+    .typew = KEY_BSPLINE, \
+    .actbp = LT_ACTBP_NONE, \
+  }
+
+/* clang-format on */
