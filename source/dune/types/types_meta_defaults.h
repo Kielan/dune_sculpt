ccd8e18b917eeb125fefd27e7e3fce87ca25a2cd
Kielan <ki.lemons@gmail.com>
2023-08-22 18:16:59 -0700

    Create types_meta_defaults.h

---
+++ source/dune/types/types_meta_defaults.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/* Struct members on own line. */
+/* clang-format off */
+
+/* -------------------------------------------------------------------- */
+/* MetaBall Struct */
+
+#define _TYPES_DEFAULT_MetaBall \
+  { \
+    .size = {1, 1, 1}, \
+    .texflag = MB_AUTOSPACE, \
+    .wiresize = 0.4f, \
+    .rendersize = 0.2f, \
+    .thresh = 0.6f, \
+  }
+
+/* clang-format on */
