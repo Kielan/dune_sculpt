5e4002a2ebb386a41916360f6a1c1956a57c92cb
Kielan <ki.lemons@gmail.com>
2023-08-24 09:24:45 -0700

    Create types_cachefile_defaults.h

---
+++ source/dune/types/types_cachefile_defaults.h
@@ -0,0 +1,26 @@
+#pragma once
+
+/* Struct members on own line. */
+/* clang-format off */
+
+/* -------------------------------------------------------------------- */
+/* CacheFile Struct */
+
+#define _TYPES_DEFAULT_CacheFile \
+  { \
+    .filepath[0] = '\0', \
+    .override_frame = false, \
+    .frame = 0.0f, \
+    .is_sequence = false, \
+    .scale = 1.0f, \
+    .object_paths ={NULL, NULL}, \
+ \
+    .type = 0, \
+    .handle = NULL, \
+    .handle_filepath[0] = '\0', \
+    .handle_readers = NULL, \
+    .use_prefetch = 1, \
+    .prefetch_cache_size = 4096, \
+  }
+
+/* clang-format on */
