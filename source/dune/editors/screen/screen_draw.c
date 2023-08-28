859e021eb4db529299fb6341a309be639bb53f6e
Kielan <ki.lemons@gmail.com>
2023-08-28 07:41:29 -0700

    Update screen_draw.c

--- source/dune/editors/screen/screen_draw.c
+++ source/dune/editors/screen/screen_draw.c
@@ -1,20 +1,20 @@
-#include "ED_screen.h"
+#include "ed_screen.h"
 
-#include "GPU_batch_presets.h"
-#include "GPU_framebuffer.h"
-#include "GPU_immediate.h"
-#include "GPU_matrix.h"
-#include "GPU_platform.h"
-#include "GPU_state.h"
+#include "gpu_batch_presets.h"
+#include "gpu_framebuffer.h"
+#include "gpu_immediate.h"
+#include "gpu_matrix.h"
+#include "gpu_platform.h"
+#include "gpu_state.h"
 
-#include "LIB_listbase.h"
-#include "LIB_math.h"
-#include "LIB_rect.h"
+#include "lib_list.h"
+#include "lib_math.h"
+#include "lib_rect.h"
 
-#include "WM_api.h"
+#include "wm_api.h"
 
-#include "UI_interface.h"
-#include "UI_resources.h"
+#include "ui_interface.h"
+#include "ui_resources.h"
 
 #include "screen_intern.h"
 
@@ -61,8 +61,8 @@ static void do_vert_pair(GPUVertBuf *vbo, uint pos, uint *vidx, int corner, int 
       break;
   }
 
-  GPU_vertbuf_attr_set(vbo, pos, (*vidx)++, inter);
-  GPU_vertbuf_attr_set(vbo, pos, (*vidx)++, exter);
+  gpu_vertbuf_attr_set(vbo, pos, (*vidx)++, inter);
+  gpu_vertbuf_attr_set(vbo, pos, (*vidx)++, exter);
 }
 
 static GPUBatch *batch_screen_edges_get(int *corner_len)
@@ -73,8 +73,8 @@ static GPUBatch *batch_screen_edges_get(int *corner_len)
     GPUVertFormat format = {0};
     uint pos = GPU_vertformat_attr_add(&format, "pos", GPU_COMP_F32, 2, GPU_FETCH_FLOAT);
 
-    GPUVertBuf *vbo = GPU_vertbuf_create_with_format(&format);
-    GPU_vertbuf_data_alloc(vbo, CORNER_RESOLUTION * 2 * 4 + 2);
+    GPUVertBuf *vbo = gpu_vertbuf_create_with_format(&format);
+    gpu_vertbuf_data_alloc(vbo, CORNER_RESOLUTION * 2 * 4 + 2);
 
     uint vidx = 0;
     for (int corner = 0; corner < 4; corner++) {
@@ -85,7 +85,7 @@ static GPUBatch *batch_screen_edges_get(int *corner_len)
     /* close the loop */
     do_vert_pair(vbo, pos, &vidx, 0, 0);
 
-    screen_edges_batch = GPU_batch_create_ex(GPU_PRIM_TRI_STRIP, vbo, NULL, GPU_BATCH_OWNS_VBO);
+    screen_edges_batch = gpu_batch_create_ex(GPU_PRIM_TRI_STRIP, vbo, NULL, GPU_BATCH_OWNS_VBO);
     gpu_batch_presets_register(screen_edges_batch);
   }
 
@@ -101,7 +101,7 @@ static void drawscredge_area_draw(
     int sizex, int sizey, short x1, short y1, short x2, short y2, float edge_thickness)
 {
   rctf rect;
-  LIB_rctf_init(&rect, (float)x1, (float)x2, (float)y1, (float)y2);
+  lib_rctf_init(&rect, (float)x1, (float)x2, (float)y1, (float)y2);
 
   /* right border area */
   if (x2 >= sizex - 1) {
@@ -124,9 +124,9 @@ static void drawscredge_area_draw(
   }
 
   GPUBatch *batch = batch_screen_edges_get(NULL);
-  GPU_batch_program_set_builtin(batch, GPU_SHADER_2D_AREA_BORDERS);
-  GPU_batch_uniform_4fv(batch, "rect", (float *)&rect);
-  GPU_batch_draw(batch);
+  gpu_batch_program_set_builtin(batch, GPU_SHADER_2D_AREA_BORDERS);
+  gpu_batch_uniform_4fv(batch, "rect", (float *)&rect);
+  gpu_batch_draw(batch);
 }
 
 /** Screen edges drawing **/
@@ -140,69 +140,69 @@ static void drawscredge_area(ScrArea *area, int sizex, int sizey, float edge_thi
   drawscredge_area_draw(sizex, sizey, x1, y1, x2, y2, edge_thickness);
 }
 
-void ED_screen_draw_edges(wmWindow *win)
+void ed_screen_draw_edges(Window *win)
 {
-  duneScreen *screen = WM_window_get_active_screen(win);
+  Screen *screen = wm_window_get_active_screen(win);
   screen->do_draw = false;
 
   if (screen->state == SCREENFULL) {
     return;
   }
 
-  if (screen->temp && LIB_listbase_is_single(&screen->areabase)) {
+  if (screen->temp && lib_list_is_single(&screen->areabase)) {
     return;
   }
 
-  const int winsize_x = WM_window_pixels_x(win);
-  const int winsize_y = WM_window_pixels_y(win);
+  const int winsize_x = wm_window_pixels_x(win);
+  const int winsize_y = wm_window_pixels_y(win);
   float col[4], corner_scale, edge_thickness;
   int verts_per_corner = 0;
 
   rcti scissor_rect;
-  LIB_rcti_init_minmax(&scissor_rect);
-  LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
-    LIB_rcti_do_minmax_v(&scissor_rect, (int[2]){area->v1->vec.x, area->v1->vec.y});
-    LIB_rcti_do_minmax_v(&scissor_rect, (int[2]){area->v3->vec.x, area->v3->vec.y});
+  lib_rcti_init_minmax(&scissor_rect);
+  LIST_FOREACH (ScrArea *, area, &screen->areabase) {
+    lib_rcti_do_minmax_v(&scissor_rect, (int[2]){area->v1->vec.x, area->v1->vec.y});
+    lib_rcti_do_minmax_v(&scissor_rect, (int[2]){area->v3->vec.x, area->v3->vec.y});
   }
 
-  if (GPU_type_matches(GPU_DEVICE_INTEL_UHD, GPU_OS_UNIX, GPU_DRIVER_ANY)) {
+  if (gpu_type_matches(GPU_DEVICE_INTEL_UHD, GPU_OS_UNIX, GPU_DRIVER_ANY)) {
     /* For some reason, on linux + Intel UHD Graphics 620 the driver
      * hangs if we don't flush before this. (See T57455) */
-    GPU_flush();
+    gpu_flush();
   }
 
-  GPU_scissor(scissor_rect.xmin,
+  gpu_scissor(scissor_rect.xmin,
               scissor_rect.ymin,
-              LIB_rcti_size_x(&scissor_rect) + 1,
-              LIB_rcti_size_y(&scissor_rect) + 1);
+              lib_rcti_size_x(&scissor_rect) + 1,
+              lib_rcti_size_y(&scissor_rect) + 1);
 
   /* It seems that all areas gets smaller when pixelsize is > 1.
    * So in order to avoid missing pixels we just disable de scissors. */
   if (U.pixelsize <= 1.0f) {
-    GPU_scissor_test(true);
+    gpu_scissor_test(true);
   }
 
-  UI_GetThemeColor4fv(TH_EDITOR_OUTLINE, col);
+  ui_GetThemeColor4fv(TH_EDITOR_OUTLINE, col);
   col[3] = 1.0f;
   corner_scale = U.pixelsize * 8.0f;
   edge_thickness = corner_scale * 0.21f;
 
-  GPU_blend(GPU_BLEND_ALPHA);
+  gpu_blend(GPU_BLEND_ALPHA);
 
   GPUBatch *batch = batch_screen_edges_get(&verts_per_corner);
-  GPU_batch_program_set_builtin(batch, GPU_SHADER_2D_AREA_BORDERS);
-  GPU_batch_uniform_1i(batch, "cornerLen", verts_per_corner);
-  GPU_batch_uniform_1f(batch, "scale", corner_scale);
-  GPU_batch_uniform_4fv(batch, "color", col);
+  gpu_batch_program_set_builtin(batch, GPU_SHADER_2D_AREA_BORDERS);
+  gpu_batch_uniform_1i(batch, "cornerLen", verts_per_corner);
+  gpu_batch_uniform_1f(batch, "scale", corner_scale);
+  gpu_batch_uniform_4fv(batch, "color", col);
 
-  LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
+  LIST_FOREACH (ScrArea *, area, &screen->areabase) {
     drawscredge_area(area, winsize_x, winsize_y, edge_thickness);
   }
 
-  GPU_blend(GPU_BLEND_NONE);
+  gpu_blend(GPU_BLEND_NONE);
 
   if (U.pixelsize <= 1.0f) {
-    GPU_scissor_test(false);
+    gpu_scissor_test(false);
   }
 }
 
@@ -226,10 +226,10 @@ void screen_draw_join_highlight(ScrArea *sa1, ScrArea *sa2)
                          MIN2(sa1->totrct.ymax, sa2->totrct.ymax),
   };
 
-  uint pos_id = GPU_vertformat_attr_add(
+  uint pos_id = gpu_vertformat_attr_add(
       immVertexFormat(), "pos", GPU_COMP_F32, 2, GPU_FETCH_FLOAT);
   immBindBuiltinProgram(GPU_SHADER_2D_UNIFORM_COLOR);
-  GPU_blend(GPU_BLEND_ALPHA);
+  gpu_blend(GPU_BLEND_ALPHA);
 
   /* Highlight source (sa1) within combined area. */
   immUniformColor4fv((const float[4]){1.0f, 1.0f, 1.0f, 0.10f});
@@ -284,20 +284,20 @@ void screen_draw_join_highlight(ScrArea *sa1, ScrArea *sa2)
   }
 
   immUnbindProgram();
-  GPU_blend(GPU_BLEND_NONE);
+  gpu_blend(GPU_BLEND_NONE);
 
   /* Outline the combined area. */
-  UI_draw_roundbox_corner_set(UI_CNR_ALL);
-  UI_draw_roundbox_4fv(&combined, false, 7 * U.pixelsize, (float[4]){1.0f, 1.0f, 1.0f, 0.8f});
+  ui_draw_roundbox_corner_set(UI_CNR_ALL);
+  ui_draw_roundbox_4fv(&combined, false, 7 * U.pixelsize, (float[4]){1.0f, 1.0f, 1.0f, 0.8f});
 }
 
 void screen_draw_split_preview(ScrArea *area, const eScreenAxis dir_axis, const float fac)
 {
-  uint pos = GPU_vertformat_attr_add(immVertexFormat(), "pos", GPU_COMP_F32, 2, GPU_FETCH_FLOAT);
+  uint pos = gpu_vertformat_attr_add(immVertexFormat(), "pos", GPU_COMP_F32, 2, GPU_FETCH_FLOAT);
   immBindBuiltinProgram(GPU_SHADER_2D_UNIFORM_COLOR);
 
   /* Split-point. */
-  GPU_blend(GPU_BLEND_ALPHA);
+  gpu_blend(GPU_BLEND_ALPHA);
 
   immUniformColor4ub(255, 255, 255, 100);
 
@@ -321,7 +321,7 @@ void screen_draw_split_preview(ScrArea *area, const eScreenAxis dir_axis, const 
     immEnd();
   }
   else {
-    LIB_assert(dir_axis == SCREEN_AXIS_V);
+    lib_assert(dir_axis == SCREEN_AXIS_V);
     const float x = (1 - fac) * area->totrct.xmin + fac * area->totrct.xmax;
 
     immVertex2f(pos, x, area->totrct.ymin);
@@ -339,24 +339,21 @@ void screen_draw_split_preview(ScrArea *area, const eScreenAxis dir_axis, const 
     immEnd();
   }
 
-  GPU_blend(GPU_BLEND_NONE);
+  gpu_blend(GPU_BLEND_NONE);
 
   immUnbindProgram();
 }
 
-/* -------------------------------------------------------------------- */
 /* Screen Thumbnail Preview */
 
-/**
- * Calculates a scale factor to squash the preview for \a screen into a rectangle
- * of given size and aspect.
- */
+/* Calculates a scale factor to squash the preview for \a screen into a rectangle
+ * of given size and aspect. */
 static void screen_preview_scale_get(
-    const bScreen *screen, float size_x, float size_y, const float asp[2], float r_scale[2])
+    const Screen *screen, float size_x, float size_y, const float asp[2], float r_scale[2])
 {
   float max_x = 0, max_y = 0;
 
-  LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
+  LIST_FOREACH (ScrArea *, area, &screen->areabase) {
     max_x = MAX2(max_x, area->totrct.xmax);
     max_y = MAX2(max_y, area->totrct.ymax);
   }
@@ -364,18 +361,18 @@ static void screen_preview_scale_get(
   r_scale[1] = (size_y * asp[1]) / max_y;
 }
 
-static void screen_preview_draw_areas(const bScreen *screen,
+static void screen_preview_draw_areas(const Screen *screen,
                                       const float scale[2],
                                       const float col[4],
                                       const float ofs_between_areas)
 {
   const float ofs_h = ofs_between_areas * 0.5f;
-  uint pos = GPU_vertformat_attr_add(immVertexFormat(), "pos", GPU_COMP_F32, 2, GPU_FETCH_FLOAT);
+  uint pos = gpu_vertformat_attr_add(immVertexFormat(), "pos", GPU_COMP_F32, 2, GPU_FETCH_FLOAT);
 
   immBindBuiltinProgram(GPU_SHADER_2D_UNIFORM_COLOR);
   immUniformColor4fv(col);
 
-  LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
+  LIST_FOREACH (ScrArea *, area, &screen->areabase) {
     rctf rect = {
         .xmin = area->totrct.xmin * scale[0] + ofs_h,
         .xmax = area->totrct.xmax * scale[0] - ofs_h,
@@ -394,7 +391,7 @@ static void screen_preview_draw_areas(const bScreen *screen,
   immUnbindProgram();
 }
 
-static void screen_preview_draw(const bScreen *screen, int size_x, int size_y)
+static void screen_preview_draw(const Screen *screen, int size_x, int size_y)
 {
   const float asp[2] = {1.0f, 0.8f}; /* square previews look a bit ugly */
   /* could use theme color (tui.wcol_menu_item.text),
@@ -404,29 +401,29 @@ static void screen_preview_draw(const bScreen *screen, int size_x, int size_y)
 
   wmOrtho2(0.0f, size_x, 0.0f, size_y);
   /* center */
-  GPU_matrix_push();
-  GPU_matrix_identity_set();
-  GPU_matrix_translate_2f(size_x * (1.0f - asp[0]) * 0.5f, size_y * (1.0f - asp[1]) * 0.5f);
+  gpu_matrix_push();
+  gpu_matrix_identity_set();
+  gpu_matrix_translate_2f(size_x * (1.0f - asp[0]) * 0.5f, size_y * (1.0f - asp[1]) * 0.5f);
 
   screen_preview_scale_get(screen, size_x, size_y, asp, scale);
   screen_preview_draw_areas(screen, scale, col, 1.5f);
 
-  GPU_matrix_pop();
+  gpu_matrix_pop();
 }
 
-void ED_screen_preview_render(const bScreen *screen, int size_x, int size_y, uint *r_rect)
+void ed_screen_preview_render(const Screen *screen, int size_x, int size_y, uint *r_rect)
 {
   char err_out[256] = "unknown";
-  GPUOffScreen *offscreen = GPU_offscreen_create(size_x, size_y, true, GPU_RGBA8, err_out);
+  GPUOffScreen *offscreen = gpu_offscreen_create(size_x, size_y, true, GPU_RGBA8, err_out);
 
-  GPU_offscreen_bind(offscreen, true);
-  GPU_clear_color(0.0f, 0.0f, 0.0f, 0.0f);
-  GPU_clear_depth(1.0f);
+  gpu_offscreen_bind(offscreen, true);
+  gpu_clear_color(0.0f, 0.0f, 0.0f, 0.0f);
+  gpu_clear_depth(1.0f);
 
   screen_preview_draw(screen, size_x, size_y);
 
-  GPU_offscreen_read_pixels(offscreen, GPU_DATA_UBYTE, r_rect);
-  GPU_offscreen_unbind(offscreen, true);
+  gpu_offscreen_read_pixels(offscreen, GPU_DATA_UBYTE, r_rect);
+  gpu_offscreen_unbind(offscreen, true);
 
-  GPU_offscreen_free(offscreen);
+  gpu_offscreen_free(offscreen);
 }
