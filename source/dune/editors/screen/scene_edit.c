b38e1419a99b2eab0cfa2dccafc01f7555214694
Kielan <ki.lemons@gmail.com>
2023-08-28 11:33:32 -0700

    Update scene_edit.c

--- source/dune/editors/screen/scene_edit.c
+++ source/dune/editors/screen/scene_edit.c
@@ -1,70 +1,67 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "BLI_compiler_attrs.h"
-#include "BLI_listbase.h"
-#include "BLI_string.h"
+#include "lib_compiler_attrs.h"
+#include "lib_list.h"
+#include "lib_string.h"
 
-#include "DNA_sequence_types.h"
+#include "types_seq.h"
 
-#include "BKE_context.h"
-#include "BKE_global.h"
-#include "BKE_layer.h"
-#include "BKE_lib_id.h"
-#include "BKE_main.h"
-#include "BKE_node.h"
-#include "BKE_report.h"
-#include "BKE_scene.h"
+#include "dune_cxt.h"
+#include "dune_global.h"
+#include "dune_layer.h"
+#include "dune_lib_id.h"
+#include "dune_main.h"
+#include "dune_node.h"
+#include "dune_report.h"
+#include "dune_scene.h"
 
-#include "DEG_depsgraph.h"
-#include "DEG_depsgraph_build.h"
+#include "graph.h"
+#include "graph_build.h"
 
-#include "BLT_translation.h"
+#include "lang.h"
 
-#include "ED_object.h"
-#include "ED_render.h"
-#include "ED_scene.h"
-#include "ED_screen.h"
-#include "ED_util.h"
+#include "ed_object.h"
+#include "ed_render.h"
+#include "ed_scene.h"
+#include "ed_screen.h"
+#include "ed_util.h"
 
-#include "SEQ_relations.h"
-#include "SEQ_select.h"
+#include "seq_relations.h"
+#include "seq_select.h"
 
-#include "RNA_access.h"
-#include "RNA_define.h"
-#include "RNA_enum_types.h"
+#include "api_access.h"
+#include "api_define.h"
+#include "api_enum_types.h"
 
-#include "WM_api.h"
-#include "WM_types.h"
+#include "wm_api.h"
+#include "wm_types.h"
 
-/* -------------------------------------------------------------------- */
-/** \name Scene Utilities
- * \{ */
-
-static Scene *scene_add(Main *bmain, Scene *scene_old, eSceneCopyMethod method)
+/* Scene Utilities */
+static Scene *scene_add(Main *main, Scene *scene_old, eSceneCopyMethod method)
 {
   Scene *scene_new = NULL;
   if (method == SCE_COPY_NEW) {
-    scene_new = BKE_scene_add(bmain, DATA_("Scene"));
+    scene_new = dune_scene_add(main, DATA_("Scene"));
   }
   else { /* different kinds of copying */
     /* We are going to deep-copy collections, objects and various object data, we need to have
      * up-to-date obdata for that. */
     if (method == SCE_COPY_FULL) {
-      ED_editors_flush_edits(bmain);
+      ed_editors_flush_edits(main);
     }
 
-    scene_new = BKE_scene_duplicate(bmain, scene_old, method);
+    scene_new = dune_scene_duplicate(main, scene_old, method);
   }
 
   return scene_new;
 }
 
-/** Add a new scene in the sequence editor. */
-static Scene *ED_scene_sequencer_add(Main *bmain, bContext *C, eSceneCopyMethod method)
+/* Add a new scene in the seq editor. */
+static Scene *ed_scene_seq_add(Main *main, Cxt *C, eSceneCopyMethod method)
 {
-  Sequence *seq = NULL;
-  Scene *scene_active = CTX_data_scene(C);
+  Seq *seq = NULL;
+  Scene *scene_active = cxt_data_scene(C);
   Scene *scene_strip = NULL;
   /* Sequencer need to use as base the scene defined in the strip, not the main scene. */
   Editing *ed = scene_active->ed;
@@ -80,44 +77,44 @@ static Scene *ED_scene_sequencer_add(Main *bmain, bContext *C, eSceneCopyMethod 
     method = SCE_COPY_NEW;
   }
 
-  Scene *scene_new = scene_add(bmain, scene_strip, method);
+  Scene *scene_new = scene_add(main, scene_strip, method);
 
-  /* As the scene is created in sequencer, do not set the new scene as active.
+  /* As the scene is created in seq, do not set the new scene as active.
    * This is useful for story-boarding where we want to keep actual scene active.
    * The new scene is linked to the active strip and the viewport updated. */
   if (scene_new && seq) {
     seq->scene = scene_new;
     /* Do a refresh of the sequencer data. */
-    SEQ_relations_invalidate_cache_raw(scene_active, seq);
-    DEG_id_tag_update(&scene_active->id, ID_RECALC_AUDIO | ID_RECALC_SEQUENCER_STRIPS);
-    DEG_relations_tag_update(bmain);
+    seq_relations_invalidate_cache_raw(scene_active, seq);
+    graph_id_tag_update(&scene_active->id, ID_RECALC_AUDIO | ID_RECALC_SEQUENCER_STRIPS);
+    graph_relations_tag_update(main);
   }
 
-  WM_event_add_notifier(C, NC_SCENE | ND_SEQUENCER, scene_active);
-  WM_event_add_notifier(C, NC_SCENE | ND_SCENEBROWSE, scene_active);
+  wm_event_add_notifier(C, NC_SCENE | ND_SE, scene_active);
+  wm_event_add_notifier(C, NC_SCENE | ND_SCENEBROWSE, scene_active);
 
   return scene_new;
 }
 
-Scene *ED_scene_add(Main *duneMain, duneContext *C, wmWindow *win, eSceneCopyMethod method)
+Scene *ed_scene_add(Main *duneMain, Cxt *C, Window *win, eSceneCopyMethod method)
 {
-  Scene *scene_old = WM_window_get_active_scene(win);
+  Scene *scene_old = wm_window_get_active_scene(win);
   Scene *scene_new = scene_add(duneMain, scene_old, method);
 
-  WM_window_set_active_scene(duneMain, C, win, scene_new);
+  wm_window_set_active_scene(duneMain, C, win, scene_new);
 
-  WM_event_add_notifier(C, NC_SCENE | ND_SCENEBROWSE, scene_new);
+  wm_event_add_notifier(C, NC_SCENE | ND_SCENEBROWSE, scene_new);
 
   return scene_new;
 }
 
-bool ED_scene_delete(duneContext *C, Main *duneMain, Scene *scene)
+bool ed_scene_delete(Cxt *C, Main *duneMain, Scene *scene)
 {
   Scene *scene_new;
 
   /* kill running jobs */
-  wmWindowManager *wm = duneMain->wm.first;
-  WM_jobs_kill_type(wm, scene, WM_JOB_TYPE_ANY);
+  WindowManager *wm = duneMain->wm.first;
+  wm_jobs_kill_type(wm, scene, WM_JOB_TYPE_ANY);
 
   if (scene->id.prev) {
     scene_new = scene->id.prev;
@@ -129,35 +126,35 @@ bool ED_scene_delete(duneContext *C, Main *duneMain, Scene *scene)
     return false;
   }
 
-  LISTBASE_FOREACH (wmWindow *, win, &wm->windows) {
+  LIST_FOREACH (Window *, win, &wm->windows) {
     if (win->parent != NULL) { /* We only care about main windows here... */
       continue;
     }
     if (win->scene == scene) {
-      WM_window_set_active_scene(duneMain, C, win, scene_new);
+      wm_window_set_active_scene(duneMain, C, win, scene_new);
     }
   }
 
-  DUNE_id_delete(duneMain, scene);
+  dune_id_delete(duneMain, scene);
 
   return true;
 }
 
-void ED_scene_change_update(Main *duneMain, Scene *scene, ViewLayer *layer)
+void ed_scene_change_update(Main *main, Scene *scene, ViewLayer *layer)
 {
-  Depsgraph *depsgraph = DUNE_scene_ensure_depsgraph(duneMain, scene, layer);
+  Graph *graph = dune_scene_ensure_graph(main, scene, layer);
 
-  DUNE_scene_set_background(duneMain, scene);
-  DEG_graph_relations_update(depsgraph);
-  DEG_tag_on_visible_update(bmain, false);
+  dune_scene_set_background(main, scene);
+  graph_relations_update(graph);
+  graph_tag_on_visible_update(main, false);
 
-  ED_render_engine_changed(bmain, false);
-  ED_update_for_newframe(bmain, depsgraph);
+  ed_render_engine_changed(main, false);
+  ed_update_for_newframe(main, graph);
 }
 
 static bool view_layer_remove_poll(const Scene *scene, const ViewLayer *layer)
 {
-  const int act = LIB_findindex(&scene->view_layers, layer);
+  const int act = lib_findindex(&scene->view_layers, layer);
 
   if (act == -1) {
     return false;
@@ -173,7 +170,7 @@ static bool view_layer_remove_poll(const Scene *scene, const ViewLayer *layer)
 
 static void view_layer_remove_unset_nodetrees(const Main *duneMain, Scene *scene, ViewLayer *layer)
 {
-  int act_layer_index = LIB_findindex(&scene->view_layers, layer);
+  int act_layer_index = lib_findindex(&scene->view_layers, layer);
 
   for (Scene *sce = duneMain->scenes.first; sce; sce = sce->id.next) {
     if (sce->nodetree) {
@@ -182,11 +179,11 @@ static void view_layer_remove_unset_nodetrees(const Main *duneMain, Scene *scene
   }
 }
 
-bool ED_scene_view_layer_delete(Main *duneMain, Scene *scene, ViewLayer *layer, ReportList *reports)
+bool ed_scene_view_layer_delete(Main *main, Scene *scene, ViewLayer *layer, ReportList *reports)
 {
   if (view_layer_remove_poll(scene, layer) == false) {
     if (reports) {
-      DUNE_reportf(reports,
+      dune_reportf(reports,
                   RPT_ERROR,
                   "View layer '%s' could not be removed from scene '%s'",
                   layer->name,
@@ -197,46 +194,44 @@ bool ED_scene_view_layer_delete(Main *duneMain, Scene *scene, ViewLayer *layer, 
   }
 
   /* We need to unset nodetrees before removing the layer, otherwise its index will be -1. */
-  view_layer_remove_unset_nodetrees(duneMain, scene, layer);
+  view_layer_remove_unset_nodetrees(main, scene, layer);
 
-  LIB_remlink(&scene->view_layers, layer);
-  LIB_assert(LIB_listbase_is_empty(&scene->view_layers) == false);
+  lib_remlink(&scene->view_layers, layer);
+  lib_assert(lib_list_is_empty(&scene->view_layers) == false);
 
   /* Remove from windows. */
-  wmWindowManager *wm = duneMain->wm.first;
-  LISTBASE_FOREACH (wmWindow *, win, &wm->windows) {
+  WindowManager *wm = duneMain->wm.first;
+  LIST_FOREACH (Window *, win, &wm->windows) {
     if (win->scene == scene && STREQ(win->view_layer_name, layer->name)) {
-      ViewLayer *first_layer = DUNE_view_layer_default_view(scene);
+      ViewLayer *first_layer = dune_view_layer_default_view(scene);
       STRNCPY(win->view_layer_name, first_layer->name);
     }
   }
 
-  DUNE_scene_free_view_layer_depsgraph(scene, layer);
+  dune_scene_free_view_layer_graph(scene, layer);
 
-  DUNE_view_layer_free(layer);
+  dune_view_layer_free(layer);
 
-  DEG_id_tag_update(&scene->id, 0);
-  DEG_relations_tag_update(duneMain);
-  WM_main_add_notifier(NC_SCENE | ND_LAYER | NA_REMOVED, scene);
+  graph_id_tag_update(&scene->id, 0);
+  graph_relations_tag_update(duneMain);
+  wm_main_add_notifier(NC_SCENE | ND_LAYER | NA_REMOVED, scene);
 
   return true;
 }
 
-/* -------------------------------------------------------------------- */
-/** Scene New Operator **/
-
-static int scene_new_exec(duneContext *C, wmOperator *op)
+/* Scene New Op */
+static int scene_new_ex(Cxt *C, wmOp *op)
 {
-  Main *duneMain = CTX_data_main(C);
-  wmWindow *win = CTX_wm_window(C);
-  int type = API_enum_get(op->ptr, "type");
+  Main *duneMain = cxt_data_main(C);
+  Window *win = cxt_wm_window(C);
+  int type = api_enum_get(op->ptr, "type");
 
-  ED_scene_add(duneMain, C, win, type);
+  ed_scene_add(duneMain, C, win, type);
 
-  return OPERATOR_FINISHED;
+  return OP_FINISHED;
 }
 
-static EnumPropertyItem scene_new_items[] = {
+static EnumPropItem scene_new_items[] = {
     {SCE_COPY_NEW, "NEW", 0, "New", "Add a new, empty scene with default settings"},
     {SCE_COPY_EMPTY,
      "EMPTY",
@@ -252,7 +247,7 @@ static EnumPropertyItem scene_new_items[] = {
     {0, NULL, 0, NULL, NULL},
 };
 
-static void SCENE_OT_new(wmOperatorType *ot)
+static void SCENE_OT_new(wmOpType *ot)
 {
 
   /* identifiers */
@@ -261,126 +256,122 @@ static void SCENE_OT_new(wmOperatorType *ot)
   ot->idname = "SCENE_OT_new";
 
   /* api callbacks */
-  ot->exec = scene_new_exec;
-  ot->invoke = WM_menu_invoke;
+  ot->exec = scene_new_ex;
+  ot->invoke = wm_menu_invoke;
 
   /* flags */
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
 
-  /* properties */
-  ot->prop = API_def_enum(ot->srna, "type", scene_new_items, SCE_COPY_NEW, "Type", "");
+  /* props */
+  ot->prop = api_def_enum(ot->sapi, "type", scene_new_items, SCE_COPY_NEW, "Type", "");
 }
 
-/* -------------------------------------------------------------------- */
-/** Scene New Sequencer Operator **/
-
-static int scene_new_sequencer_exec(bContext *C, wmOperator *op)
+/** Scene New Seq Operator **/
+static int scene_new_seq_ex(Cxt *C, wmOp *op)
 {
-  Main *duneMain = CTX_data_main(C);
-  int type = API_enum_get(op->ptr, "type");
+  Main *duneMain = cxt_data_main(C);
+  int type = api_enum_get(op->ptr, "type");
 
-  if (ED_scene_sequencer_add(duneMain, C, type) == NULL) {
-    return OPERATOR_CANCELLED;
+  if (ed_scene_seq_add(duneMain, C, type) == NULL) {
+    return OP_CANCELLED;
   }
 
-  return OPERATOR_FINISHED;
+  return OP_FINISHED;
 }
 
-static bool scene_new_sequencer_poll(duneContext *C)
+static bool scene_new_seq_poll(Cxt *C)
 {
-  Scene *scene = CTX_data_scene(C);
-  const Sequence *seq = SEQ_select_active_get(scene);
+  Scene *scene = cxt_data_scene(C);
+  const Seq *seq = seq_select_active_get(scene);
   return (seq && (seq->type == SEQ_TYPE_SCENE));
 }
 
-static const EnumPropertyItem *scene_new_sequencer_enum_itemf(duneContext *C,
-                                                              PointerAPI *UNUSED(ptr),
-                                                              PropertyAPI *UNUSED(prop),
-                                                              bool *r_free)
+static const EnumPropItem *scene_new_seq_enum_itemf(Cxt *C,
+                                                    ApiPtr *UNUSED(ptr),
+                                                    ApiProp *UNUSED(prop),
+                                                    bool *r_free)
 {
-  EnumPropertyItem *item = NULL;
+  EnumPropItem *item = NULL;
   int totitem = 0;
   uint item_index;
 
-  item_index = API_enum_from_value(scene_new_items, SCE_COPY_NEW);
-  API_enum_item_add(&item, &totitem, &scene_new_items[item_index]);
+  item_index = api_enum_from_value(scene_new_items, SCE_COPY_NEW);
+  api_enum_item_add(&item, &totitem, &scene_new_items[item_index]);
 
-  bool has_scene_or_no_context = false;
+  bool has_scene_or_no_cxt = false;
   if (C == NULL) {
     /* For documentation generation. */
-    has_scene_or_no_context = true;
+    has_scene_or_no_cxt = true;
   }
   else {
-    Scene *scene = CTX_data_scene(C);
-    Sequence *seq = SEQ_select_active_get(scene);
+    Scene *scene = cxt_data_scene(C);
+    Seq *seq = seq_select_active_get(scene);
     if ((seq && (seq->type == SEQ_TYPE_SCENE) && (seq->scene != NULL))) {
-      has_scene_or_no_context = true;
+      has_scene_or_no_cxt = true;
     }
   }
 
-  if (has_scene_or_no_context) {
+  if (has_scene_or_no_cxt) {
     int values[] = {SCE_COPY_EMPTY, SCE_COPY_LINK_COLLECTION, SCE_COPY_FULL};
     for (int i = 0; i < ARRAY_SIZE(values); i++) {
-      item_index = API_enum_from_value(scene_new_items, values[i]);
-      API_enum_item_add(&item, &totitem, &scene_new_items[item_index]);
+      item_index = api_enum_from_value(scene_new_items, values[i]);
+      api_enum_item_add(&item, &totitem, &scene_new_items[item_index]);
     }
   }
 
-  API_enum_item_end(&item, &totitem);
+  api_enum_item_end(&item, &totitem);
   *r_free = true;
   return item;
 }
 
-static void SCENE_OT_new_sequencer(wmOperatorType *ot)
+static void SCENE_OT_new_seq(wmOpType *ot)
 {
 
   /* identifiers */
   ot->name = "New Scene";
   ot->description = "Add new scene by type in the sequence editor and assign to active strip";
-  ot->idname = "SCENE_OT_new_sequencer";
+  ot->idname = "SCENE_OT_new_seq";
 
   /* api callbacks */
-  ot->exec = scene_new_sequencer_exec;
-  ot->invoke = WM_menu_invoke;
-  ot->poll = scene_new_sequencer_poll;
+  ot->ex = scene_new_seq_ex;
+  ot->invoke = wm_menu_invoke;
+  ot->poll = scene_new_seq_poll;
 
   /* flags */
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
 
-  /* properties */
-  ot->prop = API_def_enum(ot->srna, "type", scene_new_items, SCE_COPY_NEW, "Type", "");
-  API_def_enum_funcs(ot->prop, scene_new_sequencer_enum_itemf);
-  API_def_property_flag(ot->prop, PROP_ENUM_NO_TRANSLATE);
+  /* props */
+  ot->prop = api_def_enum(ot->sapi, "type", scene_new_items, SCE_COPY_NEW, "Type", "");
+  api_def_enum_fns(ot->prop, scene_new_seq_enum_itemf);
+  api_def_prop_flag(ot->prop, PROP_ENUM_NO_TRANSLATE);
 }
 
-/* -------------------------------------------------------------------- */
-/** Scene Delete Operator **/
-
-static bool scene_delete_poll(duneContext *C)
+/** Scene Delete Op **/
+static bool scene_delete_poll(Cxt *C)
 {
-  Main *duneMain = CTX_data_main(C);
-  Scene *scene = CTX_data_scene(C);
-  return DUNE_scene_can_be_removed(bmain, scene);
+  Main *main = cxt_data_main(C);
+  Scene *scene = cxt_data_scene(C);
+  return dune_scene_can_be_removed(main, scene);
 }
 
-static int scene_delete_exec(bContext *C, wmOperator *UNUSED(op))
+static int scene_delete_exec(Cxt *C, wmOp *UNUSED(op))
 {
-  Scene *scene = CTX_data_scene(C);
+  Scene *scene = cxt_data_scene(C);
 
-  if (ED_scene_delete(C, CTX_data_main(C), scene) == false) {
-    return OPERATOR_CANCELLED;
+  if (ed_scene_delete(C, cxt_data_main(C), scene) == false) {
+    return OP_CANCELLED;
   }
 
   if (G.debug & G_DEBUG) {
     printf("scene delete %p\n", scene);
   }
 
-  WM_event_add_notifier(C, NC_SCENE | NA_REMOVED, scene);
+  wm_event_add_notifier(C, NC_SCENE | NA_REMOVED, scene);
 
-  return OPERATOR_FINISHED;
+  return OP_FINISHED;
 }
 
-static void SCENE_OT_delete(wmOperatorType *ot)
+static void SCENE_OT_delete(wmOpType *ot)
 {
   /* identifiers */
   ot->name = "Delete Scene";
@@ -388,7 +379,7 @@ static void SCENE_OT_delete(wmOperatorType *ot)
   ot->idname = "SCENE_OT_delete";
 
   /* api callbacks */
-  ot->exec = scene_delete_exec;
+  ot->ex = scene_delete_ex;
   ot->poll = scene_delete_poll;
 
   /* flags */
