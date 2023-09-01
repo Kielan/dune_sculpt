17edb8025fc92d2306a6b433f9eeae9a4636592d
Kielan <ki.lemons@gmail.com>
2023-09-01 16:35:50 -0700

    Create font_util.c

---
+++ source/dune/font/intern/font_util.c
@@ -0,0 +1,35 @@
+/* Font Internal util API */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "lib_utildefines.h"
+
+#include "font_internal.h"
+
+unsigned int font_next_p2(unsigned int x)
+{
+  x -= 1;
+  x |= (x >> 16);
+  x |= (x >> 8);
+  x |= (x >> 4);
+  x |= (x >> 2);
+  x |= (x >> 1);
+  x += 1;
+  return x;
+}
+
+unsigned int font_hash(unsigned int val)
+{
+  unsigned int key;
+
+  key = val;
+  key += ~(key << 16);
+  key ^= (key >> 5);
+  key += (key << 3);
+  key ^= (key >> 13);
+  key += ~(key << 9);
+  key ^= (key >> 17);
+  return key % 257;
+}
