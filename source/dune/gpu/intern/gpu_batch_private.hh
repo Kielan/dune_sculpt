622428445cedc469271a17d7f0cd40eef4ced527
Kielan <ki.lemons@gmail.com>
2023-09-04 09:46:27 -0700

    Update gpu_batch_private.hh

--- source/dune/gpu/intern/gpu_batch_private.hh
+++ source/dune/gpu/intern/gpu_batch_private.hh
@@ -1,11 +1,10 @@
-/** GPU geometry batch
- * Contains VAOs + VBOs + Shader representing a drawable entity.
- */
+/* GPU geometry batch
+ * Contains VAOs + VBOs + Shader representing a drawable entity. */
 
 #pragma once
 
 #include "gpu_batch.h"
-#include "gpu_context.h"
+#include "gpu_cxt.h"
 
 #include "gpu_index_buffer_private.hh"
 #include "gpu_vertex_buffer_private.hh"
@@ -13,11 +12,8 @@
 namespace dune {
 namespace gpu {
 
-/**
- * Base class which is then specialized for each implementation (GL, VK, ...).
- *
- * note Extends #GPUBatch as we still needs to expose some of the internals to the outside C code.
- */
+/* Base class which is then specialized for each implementation (GL, VK, ...).
+ * note Extends GPUBatch as we still needs to expose some of the internals to the outside C code. */
 class Batch : public GPUBatch {
  public:
   virtual ~Batch() = default;
