2f2afc607f52669204c7bffb8879753e72f4bda5
Kielan <ki.lemons@gmail.com>
2023-09-04 20:35:40 -0700

    Update gl_cxt.hh

--- source/dune/gpu/opengl/gl_context.hh
+++ source/dune/gpu/opengl/gl_context.hh
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "gpu_context_private.hh"
+#include "gpu_cxt_private.hh"
 
-#include "Gpu_framebuffer.h"
+#include "gpu_framebuffer.h"
 
 #include "lib_set.hh"
 #include "lib_vector.hh"
@@ -20,9 +20,9 @@ class GLVaoCache;
 
 class GLSharedOrphanLists {
  public:
-  /** Mutex for the below structures. */
+  /* Mutex for the below structures. */
   std::mutex lists_mutex;
-  /** Buffers and textures are shared across context. Any context can free them. */
+  /* Buffers and textures are shared across context. Any context can free them. */
   Vector<GLuint> textures;
   Vector<GLuint> buffers;
 
@@ -30,10 +30,9 @@ class GLSharedOrphanLists {
   void orphans_clear();
 };
 
-class GLContext : public Context {
+class GLCxt : public Cxt {
  public:
-  /** Capabilities. */
-
+  /* Capabilities. */
   static GLint max_cubemap_size;
   static GLint max_texture_3d_size;
   static GLint max_ubo_size;
@@ -41,8 +40,7 @@ class GLContext : public Context {
   static GLint max_ssbo_size;
   static GLint max_ssbo_binds;
 
-  /** Extensions. */
-
+  /* Extensions. */
   static bool base_instance_support;
   static bool clear_texture_support;
   static bool copy_image_support;
@@ -63,37 +61,34 @@ class GLContext : public Context {
   static bool texture_storage_support;
   static bool vertex_attrib_binding_support;
 
-  /** Workarounds. */
-
+  /* Workarounds. */
   static bool debug_layer_workaround;
   static bool unused_fb_slot_workaround;
   static bool generate_mipmap_workaround;
   static float derivative_signs[2];
 
-  /** VBO for missing vertex attrib binding. Avoid undefined behavior on some implementation. */
+  /* VBO for missing vertex attrib binding. Avoid undefined behavior on some implementation. */
   GLuint default_attr_vbo_;
 
-  /** Used for debugging purpose. Bitflags of all bound slots. */
+  /* Used for debugging purpose. Bitflags of all bound slots. */
   uint16_t bound_ubo_slots;
 
  private:
-  /**
-   * #GPUBatch & #GPUFramebuffer have references to the context they are from, in the case the
-   * context is destroyed, we need to remove any reference to it.
-   */
+  /* GPUBatch & GPUFramebuffer have refs to the cxt they are from, in the case the
+   * cxt is destroyed, we need to remove any ref to it */
   Set<GLVaoCache *> vao_caches_;
   Set<GPUFrameBuffer *> framebuffers_;
-  /** Mutex for the below structures. */
+  /* Mutex for the below structures. */
   std::mutex lists_mutex_;
-  /** VertexArrays and framebuffers are not shared across context. */
+  /* VertexArrays and framebuffers are not shared across cxt. */
   Vector<GLuint> orphaned_vertarrays_;
   Vector<GLuint> orphaned_framebuffers_;
-  /** #GLBackend owns this data. */
+  /* GLBackend owns this data. */
   GLSharedOrphanLists &shared_orphan_list_;
 
  public:
-  GLContext(void *ghost_window, GLSharedOrphanLists &shared_orphan_list);
-  ~GLContext();
+  GLCxt(void *ghost_window, GLSharedOrphanLists &shared_orphan_list);
+  ~GLCxt();
 
   static void check_error(const char *info);
 
@@ -105,21 +100,21 @@ class GLContext : public Context {
 
   void memory_statistics_get(int *total_mem, int *free_mem) override;
 
-  static GLContext *get()
+  static GLCxt *get()
   {
-    return static_cast<GLContext *>(Context::get());
+    return static_cast<GLCxt *>(Cxt::get());
   }
 
   static GLStateManager *state_manager_active_get()
   {
-    GLContext *ctx = GLContext::get();
-    return static_cast<GLStateManager *>(ctx->state_manager);
+    GLCxt *cxt = GLCxt::get();
+    return static_cast<GLStateManager *>(cxt->state_manager);
   };
 
   /* These need to be called with the context the id was created with. */
   void vao_free(GLuint vao_id);
   void fbo_free(GLuint fbo_id);
-  /* These can be called by any threads even without OpenGL ctx. Deletion will be delayed. */
+  /* These can be called by any threads even without OpenGL cxt. Deletion will be delayed. */
   static void buf_free(GLuint buf_id);
   static void tex_free(GLuint tex_id);
 
@@ -133,7 +128,7 @@ class GLContext : public Context {
   static void orphans_add(Vector<GLuint> &orphan_list, std::mutex &list_mutex, GLuint id);
   void orphans_clear();
 
-  MEM_CXX_CLASS_ALLOC_FUNCS("GLContext")
+  MEM_CXX_CLASS_ALLOC_FNS("GLCxt")
 };
 
 }  // namespace gpu
