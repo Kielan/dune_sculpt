b63c1f51a1d1ff4414a618f5c311810c564845c3
Kielan <ki.lemons@gmail.com>
2023-09-06 13:12:49 -0700

    Update gl_storage_buffer.cc

--- source/dune/gpu/opengl/gl_storage_buffer.cc
+++ source/dune/gpu/opengl/gl_storage_buffer.cc
@@ -1,43 +1,36 @@
-#include "BKE_global.h"
+#include "dune_global.h"
 
-#include "BLI_string.h"
+#include "lib_string.h"
 
 #include "gpu_backend.hh"
-#include "gpu_context_private.hh"
+#include "gpu_cxt_private.hh"
 
 #include "gl_backend.hh"
 #include "gl_debug.hh"
 #include "gl_storage_buffer.hh"
 #include "gl_vertex_buffer.hh"
 
-namespace blender::gpu {
+namespace dune::gpu {
 
-/* -------------------------------------------------------------------- */
-/** \name Creation & Deletion
- * \{ */
+/* Creation & Deletion */
 
 GLStorageBuf::GLStorageBuf(size_t size, GPUUsageType usage, const char *name)
     : StorageBuf(size, name)
 {
   usage_ = usage;
   /* Do not create ubo GL buffer here to allow allocation from any thread. */
-  BLI_assert(size <= GLContext::max_ssbo_size);
+  lib_assert(size <= GLCxt::max_ssbo_size);
 }
 
 GLStorageBuf::~GLStorageBuf()
 {
-  GLContext::buf_free(ssbo_id_);
+  GLCxt::buf_free(ssbo_id_);
 }
 
-/** \} */
-
-/* -------------------------------------------------------------------- */
-/** \name Data upload / update
- * \{ */
-
+/* Data upload / update */
 void GLStorageBuf::init()
 {
-  BLI_assert(GLContext::get());
+  lib_assert(GLCxt::get());
 
   glGenBuffers(1, &ssbo_id_);
   glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo_id_);
@@ -56,21 +49,16 @@ void GLStorageBuf::update(const void *data)
   glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 }
 
-/** \} */
-
-/* -------------------------------------------------------------------- */
-/** \name Usage
- * \{ */
-
+/* Usage */
 void GLStorageBuf::bind(int slot)
 {
-  if (slot >= GLContext::max_ssbo_binds) {
+  if (slot >= GLCxt::max_ssbo_binds) {
     fprintf(
         stderr,
         "Error: Trying to bind \"%s\" ssbo to slot %d which is above the reported limit of %d.",
         name_,
         slot,
-        GLContext::max_ssbo_binds);
+        GLCxt::max_ssbo_binds);
     return;
   }
 
@@ -87,15 +75,15 @@ void GLStorageBuf::bind(int slot)
   glBindBufferBase(GL_SHADER_STORAGE_BUFFER, slot_, ssbo_id_);
 
 #ifdef DEBUG
-  BLI_assert(slot < 16);
+  lib_assert(slot < 16);
   /* TODO */
-  // GLContext::get()->bound_ssbo_slots |= 1 << slot;
+  // GLCxt::get()->bound_ssbo_slots |= 1 << slot;
 #endif
 }
 
 void GLStorageBuf::bind_as(GLenum target)
 {
-  BLI_assert_msg(ssbo_id_ != 0,
+  lib_assert_msg(ssbo_id_ != 0,
                  "Trying to use storage buf as indirect buffer but buffer was never filled.");
   glBindBuffer(target, ssbo_id_);
 }
@@ -107,7 +95,7 @@ void GLStorageBuf::unbind()
   glBindBufferBase(GL_SHADER_STORAGE_BUFFER, slot_, 0);
   /* Hope that the context did not change. */
   /* TODO */
-  // GLContext::get()->bound_ssbo_slots &= ~(1 << slot_);
+  // GLCxt::get()->bound_ssbo_slots &= ~(1 << slot_);
 #endif
   slot_ = 0;
 }
@@ -118,7 +106,7 @@ void GLStorageBuf::clear(eGPUTextureFormat internal_format, eGPUDataFormat data_
     this->init();
   }
 
-  if (GLContext::direct_state_access_support) {
+  if (GLCxt::direct_state_access_support) {
     glClearNamedBufferData(ssbo_id_,
                            to_gl_internal_format(internal_format),
                            to_gl_data_format(internal_format),
@@ -126,7 +114,7 @@ void GLStorageBuf::clear(eGPUTextureFormat internal_format, eGPUDataFormat data_
                            data);
   }
   else {
-    /* WATCH(@fclem): This should be ok since we only use clear outside of drawing functions. */
+    /* WATCH: This should be ok since we only use clear outside of drawing functions. */
     glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssbo_id_);
     glClearBufferData(GL_SHADER_STORAGE_BUFFER,
                       to_gl_internal_format(internal_format),
