8c608c1c8c3e17065369ddb5c6fada697344d8a8
Kielan <ki.lemons@gmail.com>
2023-09-05 22:57:51 -0700

    Update gl_drawlist.hh

--- source/dune/gpu/opengl/gl_drawlist.hh
+++ source/dune/gpu/opengl/gl_drawlist.hh
@@ -1,7 +1,5 @@
-/**
- * Implementation of Multi Draw Indirect using OpenGL.
- * Fallback if the needed extensions are not supported.
- */
+/* Implementation of Multi Draw Indirect using OpenGL.
+ * Fallback if the needed extensions are not supported. */
 
 #pragma once
 
@@ -13,14 +11,12 @@
 
 #include "gpu_drawlist_private.hh"
 
-#include "gl_context.hh"
+#include "gl_cxt.hh"
 
 namespace dune {
 namespace gpu {
 
-/**
- * Implementation of Multi Draw Indirect using OpenGL.
- */
+/* Implementation of Multi Draw Indirect using OpenGL. */
 class GLDrawList : public DrawList {
  public:
   GLDrawList(int length);
@@ -32,30 +28,30 @@ class GLDrawList : public DrawList {
  private:
   void init();
 
-  /** Batch for which we are recording commands for. */
+  /* Batch for which we are recording commands for. */
   GLBatch *batch_;
-  /** Mapped memory bounds. */
+  /* Mapped memory bounds. */
   GLbyte *data_;
-  /** Length of the mapped buffer (in byte). */
+  /* Length of the mapped buffer (in byte). */
   GLsizeiptr data_size_;
-  /** Current offset inside the mapped buffer (in byte). */
+  /* Current offset inside the mapped buffer (in byte). */
   GLintptr command_offset_;
-  /** Current number of command recorded inside the mapped buffer. */
+  /* Current number of command recorded inside the mapped buffer. */
   uint command_len_;
-  /** Is UINT_MAX if not drawing indexed geom. Also Avoid dereferencing batch. */
+  /* Is UINT_MAX if not drawing indexed geom. Also Avoid dereferencing batch. */
   GLuint base_index_;
-  /** Also Avoid dereferencing batch. */
+  /* Also Avoid dereferencing batch. */
   GLuint v_first_, v_count_;
 
-  /** GL Indirect Buffer id. 0 means MultiDrawIndirect is not supported/enabled. */
+  /* GL Indirect Buffer id. 0 means MultiDrawIndirect is not supported/enabled. */
   GLuint buffer_id_;
-  /** Length of whole the buffer (in byte). */
+  /* Length of whole the buffer (in byte). */
   GLsizeiptr buffer_size_;
-  /** Offset of `data_` inside the whole buffer (in byte). */
+  /* Offset of `data_` inside the whole buffer (in byte). */
   GLintptr data_offset_;
 
-  /** To free the buffer_id_. */
-  GLContext *context_;
+  /* To free the buffer_id_. */
+  GLCxt *cxt_;
 
   MEM_CXX_CLASS_ALLOC_FNS("GLDrawList");
 };
