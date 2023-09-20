eb8757e6ca7aa9ef2394fa2d9cc538c143bb9a0e
Kielan <ki.lemons@gmail.com>
2023-09-20 07:32:17 -0700

    Update types_utils.h

--- source/dune/types/intern/types_utils.h
+++ source/dune/types/intern/types_utils.h
@@ -7,31 +7,23 @@ extern "C" {
 struct GHash;
 struct MemArena;
 
-/**
- * Parses the `[n1][n2]...` on the end of an array name
- * and returns the number of array elements `n1 * n2 ...`.
- */
-int DNA_elem_array_size(const char *str);
+/* Parses the `[n1][n2]...` on the end of an array name
+ * and returns the number of array elements `n1 * n2 ...`. */
+int types_elem_array_size(const char *str);
 
-uint DNA_elem_id_offset_start(const char *elem_full);
-uint DNA_elem_id_offset_end(const char *elem_full);
-/**
- * \a elem_dst must be at least the size of \a elem_src.
- */
-uint DNA_elem_id_strip_copy(char *elem_dst, const char *elem_src);
-uint DNA_elem_id_strip(char *elem);
-/**
- * Check if 'var' matches '*var[3]' for eg,
- * return true if it does, with start/end offsets.
- */
-bool DNA_elem_id_match(const char *elem_search,
+uint types_elem_id_offset_start(const char *elem_full);
+uint types_elem_id_offset_end(const char *elem_full);
+/* elem_dst must be at least the size of \a elem_src. */
+uint types_elem_id_strip_copy(char *elem_dst, const char *elem_src);
+uint types_elem_id_strip(char *elem);
+/* Check if 'var' matches '*var[3]' for eg,
+ * return true if it does, with start/end offsets */
+bool types_elem_id_match(const char *elem_search,
                        int elem_search_len,
                        const char *elem_full,
                        uint *r_elem_full_offset);
-/**
- * Return a renamed dna name, allocated from \a mem_arena.
- */
-char *DNA_elem_id_rename(struct MemArena *mem_arena,
+/* Return a renamed dna name, allocated from mem_arena. */
+char *types_elem_id_rename(struct MemArena *mem_arena,
                          const char *elem_src,
                          int elem_src_len,
                          const char *elem_dst,
@@ -40,22 +32,18 @@ char *DNA_elem_id_rename(struct MemArena *mem_arena,
                          int elem_src_full_len,
                          uint elem_src_full_offset_len);
 
-/**
- * When requesting version info, support both directions.
- */
-enum eDNA_RenameDir {
-  DNA_RENAME_STATIC_FROM_ALIAS = -1,
-  DNA_RENAME_ALIAS_FROM_STATIC = 1,
+/* When requesting version info, support both directions. */
+enum eTypes_RenameDir {
+  TYPES_RENAME_STATIC_FROM_ALIAS = -1,
+  TYPES_RENAME_ALIAS_FROM_STATIC = 1,
 };
-void DNA_alias_maps(enum eDNA_RenameDir version_dir,
-                    struct GHash **r_struct_map,
-                    struct GHash **r_elem_map);
+void types_alias_maps(enum eTypes_RenameDir version_dir,
+                      struct GHash **r_struct_map,
+                      struct GHash **r_elem_map);
 
-const char *DNA_struct_rename_legacy_hack_alias_from_static(const char *name);
-/**
- * DNA Compatibility Hack.
- */
-const char *DNA_struct_rename_legacy_hack_static_from_alias(const char *name);
+const char *types_struct_rename_legacy_hack_alias_from_static(const char *name);
+/* Types Compatibility Hack. */
+const char *types_struct_rename_legacy_hack_static_from_alias(const char *name);
 
 #ifdef __cplusplus
 }
