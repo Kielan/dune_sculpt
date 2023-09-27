f50c897b1eda8b2ea21b4ec88e24ea1975eae748
Kielan <ki.lemons@gmail.com>
2023-09-27 07:58:15 -0700

    Create dune_autoex.h

---
+++ source/dune/kernel/dune_autoex.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* param path: The path to check against.
+ * return Success */
+bool dune_autoex_match(const char *path);
+
+#ifdef __cplusplus
+}
+#endif
