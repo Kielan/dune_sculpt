11d736736453081418341b1b8ee260a2f35fe383
Kielan <ki.lemons@gmail.com>
2023-10-05 13:12:33 -0700

    Update dune.h

--- source/dune/kernel/dune.h
+++ source/dune/kernel/dune.h
@@ -1,9 +1,6 @@
 #pragma once
 
-/** \file
- * \ingroup bke
- * \brief Blender util stuff
- */
+/* Duner utils */
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,37 +8,31 @@ extern "C" {
 
 struct UserDef;
 
-/**
- * Only to be called on exit Blender.
- */
-void BKE_blender_free(void);
+/* Only to be called on exit Dune. */
+void dune_free(void);
 
-void BKE_blender_globals_init(void);
-void BKE_blender_globals_clear(void);
+void dune_globals_init(void);
+void dune_globals_clear(void);
 
-void BKE_blender_userdef_data_swap(struct UserDef *userdef_a, struct UserDef *userdef_b);
-void BKE_blender_userdef_data_set(struct UserDef *userdef);
-void BKE_blender_userdef_data_set_and_free(struct UserDef *userdef);
+void dune_userdef_data_swap(struct UserDef *userdef_a, struct UserDef *userdef_b);
+void dune_userdef_data_set(struct UserDef *userdef);
+void dune_userdef_data_set_and_free(struct UserDef *userdef);
 
-/**
- * Write U from userdef.
- * This function defines which settings a template will override for the user preferences.
- */
-void BKE_blender_userdef_app_template_data_swap(struct UserDef *userdef_a,
-                                                struct UserDef *userdef_b);
-void BKE_blender_userdef_app_template_data_set(struct UserDef *userdef);
-void BKE_blender_userdef_app_template_data_set_and_free(struct UserDef *userdef);
+/* Write U from userdef.
+ * This fn defines which settings a template will override for the user prefs. */
+void dune_userdef_app_template_data_swap(struct UserDef *userdef_a,
+                                         struct UserDef *userdef_b);
+void dune_userdef_app_template_data_set(struct UserDef *userdef);
+void dune_userdef_app_template_data_set_and_free(struct UserDef *userdef);
 
-/**
- * When loading a new userdef from file,
- * or when exiting Blender.
- */
-void BKE_blender_userdef_data_free(struct UserDef *userdef, bool clear_fonts);
+/* When loading a new userdef from file,
+ * or when exiting Dune. */
+void dune_userdef_data_free(struct UserDef *userdef, bool clear_fonts);
 
-/* Blenders' own atexit (avoids leaking) */
-void BKE_blender_atexit_register(void (*func)(void *user_data), void *user_data);
-void BKE_blender_atexit_unregister(void (*func)(void *user_data), const void *user_data);
-void BKE_blender_atexit(void);
+/* Dunes' own atexit (avoids leaking) */
+void dune_atexit_register(void (*fn)(void *user_data), void *user_data);
+void dune_atexit_unregister(void (*fn)(void *user_data), const void *user_data);
+void dune_atexit(void);
 
 #ifdef __cplusplus
 }
