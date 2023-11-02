17fa411cfda0fb1b413d4ba219e40b31722773dc
Kielan <ki.lemons@gmail.com>
2023-11-01 23:37:43 -0700

    Create ui_query.c

---
+++ source/dune/editors/interface/ui_query.c
@@ -0,0 +1,17 @@
+/* ui inspect the ui extra info */
+
+#include "lib_list"
+#include "lib_math"
+#include "lib_rect.h"
+#include "lib_string.h"
+#include "lib_utildefines"
+
+#include "types_screen.h"
+
+#include "win_api.h"
+#include "win_types.h"
+
+/* Btn State */
+bool btn_is_editable() {
+  return !ELEM()
+}
