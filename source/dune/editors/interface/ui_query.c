dcb494f024afcd8ed5c304e895785e9ba31eabf4
Kielan <ki.lemons@gmail.com>
2023-11-02 08:26:24 -0700

    Update ui_query.c

--- source/dune/editors/interface/ui_query.c
+++ source/dune/editors/interface/ui_query.c
@@ -425,14 +425,307 @@ static bool btn_is_active_treerow(const Btn *btn, const void *customdata)
   return ui_tree_view_item_is_active(treerow_btn->tree_item);
 }
 
-bool btn_has_array_value(const Btn *btn)
+Btn *ui_tree_row_find_active(const ARegion *region)
 {
-  return (ELEM(btn->type, UI_BTYPE_BTN, UI_BTYPE_DECORATOR) || btn_is_toggle(btn))}
+  return btn_find(region, btn_is_active_treerow, NULL);
 }
-#ifdef
 
-bool btn_has_array_value(const Btn *btn)
+/* Btn Relations */
+Btn *btn_prev(Btn *btn)
 {
-  return (btn->apiptr.data, && btn->apiprop &&
-    )
+  while (btn->prev) {
+    btn = btn->prev;
+    if (btn_is_editable(btn)) {
+      return btn;
+    }
+  }
+  return NULL;
+}
+
+Btn *btn_next(Btn *btn)
+{
+  while (btn->next) {
+    btn = btn->next;
+    if (btn_is_editable(btn)) {
+      return btn;
+    }
+  }
+  return NULL;
+}
+
+Btn *btn_first(uiBlock *block)
+{
+  LIST_FOREACH (Btn *, btn, &block->btns) {
+    if (btn_is_editable(btn)) {
+      return btn;
+    }
+  }
+  return NULL;
+}
+
+Btn *btn_last(uiBlock *block)
+{
+  Btn *btn;
+
+  btn = block->buttons.last;
+  while (btn) {
+    if (btn_is_editable(btn)) {
+      return btn;
+    }
+    btn = btn->prev;
+  }
+  return NULL;
+}
+
+bool ui_btn_is_cursor_warp(const Btn *btn)
+{
+  if (U.uiflag & USER_CONTINUOUS_MOUSE) {
+    if (ELEM(but->type,
+             UI_BTYPE_NUM,
+             UI_BTYPE_NUM_SLIDER,
+             UI_BTYPE_TRACK_PREVIEW,
+             UI_BTYPE_HSVCUBE,
+             UI_BTYPE_HSVCIRCLE,
+             UI_BTYPE_CURVE,
+             UI_BTYPE_CURVEPROFILE)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool btn_contains_password(const Btn *btn)
+{
+  return btn->apiprop && (api_prop_subtype(btn->apiprop) == PROP_PASSWORD);
+}
+
+/* Btn Text */
+size_t btn_drawstr_len_without_sep_char(const Btn *btn)
+{
+  if (btn->flag & BTN_HAS_SEP_CHAR) {
+    const char *str_sep = strrchr(btn->drawstr, UI_SEP_CHAR);
+    if (str_sep != NULL) {
+      return (str_sep - btn->drawstr);
+    }
+  }
+  return strlen(btn->drawstr);
+}
+
+size_t  btn_drawstr_without_sep_char(const Btn *btn, char *str, size_t str_maxlen)
+{
+  size_t str_len_clip = btn_drawstr_len_without_sep_char(btn);
+  return lib_strncpy_rlen(str, btn->drawstr, min_zz(str_len_clip + 1, str_maxlen));
+}
+
+size_t btn_tip_len_only_first_line(const Btn *btn)
+{
+  if (btn->tip == NULL) {
+    return 0;
+  }
+
+  const char *str_sep = strchr(btn->tip, '\n');
+  if (str_sep != NULL) {
+    return (str_sep - btn->tip);
+  }
+  return strlen(btn->tip);
+}
+
+/* Block uiBlock State */
+Btn *ui_block_active_btn_get(const uiBlock *block)
+{
+  LIST_FOREACH (Btn *, btn, &block->btns) {
+    if (btn->active) {
+      return btn;
+    }
+  }
+
+  return NULL;
+}
+
+bool ui_block_is_menu(const uiBlock *block)
+{
+  return (((block->flag & UI_BLOCK_LOOP) != 0) &&
+          /* non-menu popups use keep-open, so check this is off */
+          ((block->flag & UI_BLOCK_KEEP_OPEN) == 0));
+}
+
+bool ui_block_is_popover(const uiBlock *block)
+{
+  return (block->flag & UI_BLOCK_POPOVER) != 0;
+}
+
+bool ui_block_is_pie_menu(const uiBlock *block)
+{
+  return ((block->flag & UI_BLOCK_RADIAL) != 0);
+}
+
+bool ui_block_is_popup_any(const uiBlock *block)
+{
+  return (ui_block_is_menu(block) || ui_block_is_popover(block) || ui_block_is_pie_menu(block));
+}
+
+static const Btn *btn_next_non_separator(const Btn *btn)
+{
+  for (; btn; btn = btn->next) {
+    if (!ELEM(btn->type, UI_BTYPE_SEPR, UI_BTYPE_SEPR_LINE)) {
+      return btn;
+    }
+  }
+  return NULL;
+}
+
+bool ui_block_is_empty_ex(const uiBlock *block, const bool skip_title)
+{
+  const Btn *btn = block->btns.first;
+  if (skip_title) {
+    /* Skip the first label, since popups often have a title,
+     * we may want to consider the block empty in this case. */
+    btn = btn_next_non_separator(btn);
+    if (btn && btn->type == UI_BTYPE_LABEL) {
+      btn = btn->next;
+    }
+  }
+  return (btn_next_non_separator(btn) == NULL);
+}
+
+bool ui_block_is_empty(const uiBlock *block)
+{
+  return ui_block_is_empty_ex(block, false);
+}
+
+bool ui_block_can_add_separator(const uiBlock *block)
+{
+  if (ui_block_is_menu(block) && !ui_block_is_pie_menu(block)) {
+    const Btn *btn = block->buttons.last;
+    return (btn && !ELEM(btn->type, UI_BTYPE_SEPR_LINE, UI_BTYPE_SEPR));
+  }
+  return true;
+}
+
+/* uiBlock Spatial */
+uiBlock *ui_block_find_mouse_over_ex(const ARegion *region, const int xy[2], bool only_clip)
+{
+  if (!ui_region_contains_point_px(region, xy)) {
+    return NULL;
+  }
+  LIST_FOREACH (uiBlock *, block, &region->uiblocks) {
+    if (only_clip) {
+      if ((block->flag & UI_BLOCK_CLIP_EVENTS) == 0) {
+        continue;
+      }
+    }
+    float mx = xy[0], my = xy[1];
+    ui_win_to_block_fl(region, block, &mx, &my);
+    if (lib_rctf_isect_pt(&block->rect, mx, my)) {
+      return block;
+    }
+  }
+  return NULL;
+}
+
+uiBlock *ui_block_find_mouse_over(const ARegion *region, const wmEvent *event, bool only_clip)
+{
+  return ui_block_find_mouse_over_ex(region, event->xy, only_clip);
+}
+
+/* ARegion State */
+Btn *ui_region_find_active_btn(ARegion *region)
+{
+  LIST_FOREACH (uiBlock *, block, &region->uiblocks) {
+    Btn *btn = ui_block_active_btn_get(block);
+    if (btn) {
+      return btn;
+    }
+  }
+
+  return NULL;
+}
+
+Btn *ui_region_find_first_btn_test_flag(ARegion *region, int flag_include, int flag_exclude)
+{
+  LIST_FOREACH (uiBlock *, block, &region->uiblocks) {
+    LIST_FOREACH (Btn *, btn, &block->btns) {
+      if (((btn->flag & flag_include) == flag_include) && ((btn->flag & flag_exclude) == 0)) {
+        return btn;
+      }
+    }
+  }
+
+  return NULL;
+}
+
+/* ARegion Spatial */
+bool ui_region_contains_point_px(const ARegion *region, const int xy[2])
+{
+  rcti winrct;
+  ui_region_winrct_get_no_margin(region, &winrct);
+  if (!lib_rcti_isect_pt_v(&winrct, xy)) {
+    return false;
+  }
+
+  /* also, check that with view2d, that the mouse is not over the scroll-bars
+   * NOTE: care is needed here, since the mask rect may include the scroll-bars
+   * even when they are not visible, so we need to make a copy of the mask to
+   * use to check */
+  if (region->v2d.mask.xmin != region->v2d.mask.xmax) {
+    const View2D *v2d = &region->v2d;
+    int mx = xy[0], my = xy[1];
+
+    ui_win_to_region(region, &mx, &my);
+    if (!lib_rcti_isect_pt(&v2d->mask, mx, my) ||
+        ui_view2d_mouse_in_scrollers(region, &region->v2d, xy)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool ui_region_contains_rect_px(const ARegion *region, const rcti *rect_px)
+{
+  rcti winrct;
+  ui_region_winrct_get_no_margin(region, &winrct);
+  if (!lib_rcti_isect(&winrct, rect_px, NULL)) {
+    return false;
+  }
+
+  /* See comment in 'ui_region_contains_point_px' */
+  if (region->v2d.mask.xmin != region->v2d.mask.xmax) {
+    const View2D *v2d = &region->v2d;
+    rcti rect_region;
+    ui_win_to_region_rcti(region, &rect_region, rect_px);
+    if (!lib_rcti_isect(&v2d->mask, &rect_region, NULL) ||
+        ui_view2d_rect_in_scrollers(region, &region->v2d, rect_px)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/* Screen Spatial */
+ARegion *ui_screen_region_find_mouse_over_ex(Screen *screen, const int xy[2])
+{
+  LIST_FOREACH (ARegion *, region, &screen->regionbase) {
+    rcti winrct;
+
+    ui_region_winrct_get_no_margin(region, &winrct);
+
+    if (lib_rcti_isect_pt_v(&winrct, xy)) {
+      return region;
+    }
+  }
+  return NULL;
+}
+
+ARegion *ui_screen_region_find_mouse_over(Screen *screen, const WinEvent *event)
+{
+  return ui_screen_region_find_mouse_over_ex(screen, event->xy);
+}
+
+/* Manage Internal State */
+void ui_tag_script_reload_queries(void)
+{
+  g_ot_tool_set_by_id = NULL;
 }
