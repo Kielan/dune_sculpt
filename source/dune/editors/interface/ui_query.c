7865b6409291c92e29cd217eac2ad4bb8c5772df
Kielan <ki.lemons@gmail.com>
2023-11-02 07:58:29 -0700

    Update ui_query.c

--- source/dune/editors/interface/ui_query.c
+++ source/dune/editors/interface/ui_query.c
@@ -148,8 +148,282 @@ static bool btn_isect_pie_seg(const uiBlock *uiBlock, const Btn *btn)
   }
 
   btn_pie_dir(btn->pie_dir, vec);
-  
-} saacos(dot_v2v2())
+
+  if(saacos(dot_v2v2(vec2, block->pie_data.pie_dir)) < angle_range) {
+    return true;
+  }
+
+  return false;
+} 
+
+bool btn_contains_pt(const Btn *btn, float mx, float my)
+{
+  return lib_rctf_isect_pt(&btn->rect, mx, my);
+}
+
+bool btn_contains_rect(const Btn *btn, const rctf *rect)
+{
+  return lib_rctf_isect(&btn->rect, rect, NULL);
+}
+
+bool btn_contains_point_px(const Btn *btn, const ARegion *region, const int xy[2])
+{
+  uiBlock *block = btn->block;
+  if (!ui_region_contains_point_px(region, xy)) {
+    return false;
+  }
+
+  float mx = xy[0], my = xy[1];
+  ui_win_to_block_fl(region, block, &mx, &my);
+
+  if (btn->pie_dir != UI_RADIAL_NONE) {
+    if (!btn_isect_pie_seg(block, btn)) {
+      return false;
+    }
+  }
+  else if (!btn_contains_pt(btn, mx, my)) {
+    return false;
+  }
+
+  return true;
+}
+
+bool btn_contains_point_px_icon(const Btn *btn, ARegion *region, const WinEvent *event)
+{
+  rcti rect;
+  int x = event->xy[0], y = event->xy[1];
+
+  ui_win_to_block(region, btn->block, &x, &y);
+
+  lib_rcti_rctf_copy(&rect, &btn->rect);
+
+  if (btn->imb || btn->type == UI_BTYPE_COLOR) {
+    /* use button size itself */
+  }
+  else if (btn->drawflag & UI_BTN_ICON_LEFT) {
+    rect.xmax = rect.xmin + (lib_rcti_size_y(&rect));
+  }
+  else {
+    const int delta = lib_rcti_size_x(&rect) - lib_rcti_size_y(&rect);
+    rect.xmin += delta / 2;
+    rect.xmax -= delta / 2;
+  }
+
+  return lib_rcti_isect_pt(&rect, x, y);
+}
+
+static Btm *btn_find(const ARegion *region,
+                     const BtnFindPollFn find_poll,
+                     const void *find_custom_data)
+{
+  LIST_FOREACH (uiBlock *, block, &region->uiblocks) {
+    LIST_FOREACH_BACKWARD (Btn *, btn, &block->btns) {
+      if (find_poll && find_poll(btn, find_custom_data) == false) {
+        continue;
+      }
+      return btn;
+    }
+  }
+
+  return NULL;
+}
+
+Btn *btn_find_mouse_over_ex(const ARegion *region,
+                              const int xy[2],
+                              const bool labeledit,
+                              const BtnFindPollFn find_poll,
+                              const void *find_custom_data)
+{
+  Btn *btnover = NULL;
+
+  if (!ui_region_contains_point_px(region, xy)) {
+    return NULL;
+  }
+  LIST_FOREACH (uiBlock *, block, &region->uiblocks) {
+    float mx = xy[0], my = xy[1];
+    ui_win_to_block_fl(region, block, &mx, &my);
+
+    LIST_FOREACH_BACKWARD (Btn *, btn, &block->btns) {
+      if (find_poll && find_poll(btn, find_custom_data) == false) {
+        continue;
+      }
+      if (btn_is_interactive(btn, labeledit)) {
+        if (btn->pie_dir != UI_RADIAL_NONE) {
+          if (btn_isect_pie_seg(block, but)) {
+            btnover = but;
+            break;
+          }
+        }
+        else if (btn_contains_pt(btn, mx, my)) {
+          btnover = btn;
+          break;
+        }
+      }
+    }
+
+    /* CLIP_EVENTS prevents the event from reaching other blocks */
+    if (block->flag & UI_BLOCK_CLIP_EVENTS) {
+      /* check if mouse is inside block */
+      if (lib_rctf_isect_pt(&block->rect, mx, my)) {
+        break;
+      }
+    }
+  }
+
+  return btnover;
+}
+
+Btn *btn_find_mouse_over(const ARegion *region, const WinEvent *event)
+{
+  return btn_find_mouse_over_ex(region, event->xy, event->mod & KM_CTRL, NULL, NULL);
+}
+
+Btn *btn_find_rect_over(const struct ARegion *region, const rcti *rect_px)
+{
+  if (!ui_region_contains_rect_px(region, rect_px)) {
+    return NULL;
+  }
+
+  /* Currently no need to expose this at the moment. */
+  const bool labeledit = true;
+  rctf rect_px_fl;
+  lib_rctf_rcti_copy(&rect_px_fl, rect_px);
+  Btn *btnover = NULL;
+
+  LIST_FOREACH (uiBlock *, block, &region->uiblocks) {
+    rctf rect_block;
+    ui_win_to_block_rctf(region, block, &rect_block, &rect_px_fl);
+
+    LIST_FOREACH_BACKWARD (Btn *, btn, &block->btns) {
+      if (btn_is_interactive(btn, labeledit)) {
+        /* No pie menu support. */
+        lib_assert(btn->pie_dir == UI_RADIAL_NONE);
+        if (btn_contains_rect(btn, &rect_block)) {
+          btnover = btn;
+          break;
+        }
+      }
+    }
+
+    /* CLIP_EVENTS prevents the event from reaching other blocks */
+    if (block->flag & UI_BLOCK_CLIP_EVENTS) {
+      /* check if mouse is inside block */
+      if (lib_rctf_isect(&block->rect, &rect_block, NULL)) {
+        break;
+      }
+    }
+  }
+  return btnover;
+}
+
+uiBut *ui_list_find_mouse_over_ex(const ARegion *region, const int xy[2])
+{
+  if (!ui_region_contains_point_px(region, xy)) {
+    return NULL;
+  }
+  LIST_FOREACH (uiBlock *, block, &region->uiblocks) {
+    float mx = xy[0], my = xy[1];
+    ui_win_to_block_fl(region, block, &mx, &my);
+    LIST_FOREACH_BACKWARD (Btn *, btn, &block->btns) {
+      if (btn->type == UI_BTYPE_LISTBOX && ui_btn_contains_pt(btn, mx, my)) {
+        return btn;
+      }
+    }
+  }
+
+  return NULL;
+}
+
+Btn *ui_list_find_mouse_over(const ARegion *region, const WinEvent *event)
+{
+  if (event == NULL) {
+    /* If there is no info about the mouse, just act as if there is nothing underneath it. */
+    return NULL;
+  }
+  return ui_list_find_mouse_over_ex(region, event->xy);
+}
+
+uiList *ui_list_find_mouse_over(const ARegion *region, const WinEvent *event)
+{
+  Btn *list_btn = ui_list_find_mouse_over(region, event);
+  if (!list_btn) {
+    return NULL;
+  }
+
+  return list_btn->custom_data;
+}
+
+static bool ui_list_contains_row(const Btn *listbox_btn, const Btn *listrow_btn)
+{
+  lib_assert(listbox_btn->type == UI_BTYPE_LISTBOX);
+  lib_assert(listrow_brn->type == UI_BTYPE_LISTROW);
+  /* The list box and its rows have the same RNA data (active data ptr/prop). */
+  return btn_api_equals(listbox_btn, listrow_btn);
+}
+
+static bool btn_is_listbox_with_row(const Btn *btn, const void *customdata)
+{
+  const Btn *row_btn = customdata;
+  return (btn->type == UI_BTYPE_LISTBOX) && ui_list_contains_row(btn, row_btn);
+}
+
+Btn *ui_list_find_from_row(const ARegion *region, const Btn *row_btn)
+{
+  return btn_find(region, btn_is_listbox_with_row, row_btn);
+}
+
+static bool btn_is_listrow(const Btn *btn, const void *UNUSED(customdata))
+{
+  return btn->type == UI_BTYPE_LISTROW;
+}
+
+Btn *ui_list_row_find_mouse_over(const ARegion *region, const int xy[2])
+{
+  return btn_find_mouse_over_ex(region, xy, false, btn_is_listrow, NULL);
+}
+
+struct ListRowFindIndexData {
+  int index;
+  uiBut *listbox;
+};
+
+static bool btn_is_listrow_at_index(const Btn *btn, const void *customdata)
+{
+  const struct ListRowFindIndexData *find_data = customdata;
+
+  return btn_is_listrow(btn, NULL) && ui_list_contains_row(find_data->listbox, btn) &&
+         (btn->hardmax == find_data->index);
+}
+
+Btn *ui_list_row_find_from_index(const ARegion *region, const int index, Btn *listbox)
+{
+  lib_assert(listbox->type == UI_BTYPE_LISTBOX);
+  struct ListRowFindIndexData data = {
+      .index = index,
+      .listbox = listbox,
+  };
+  return btn_find(region, ui_btn_is_listrow_at_index, &data);
+}
+
+static bool btn_is_treerow(const Btn *btn, const void *UNUSED(customdata))
+{
+  return btn->type == UI_BTYPE_TREEROW;
+}
+
+Btn *ui_tree_row_find_mouse_over(const ARegion *region, const int xy[2])
+{
+  return btn_find_mouse_over_ex(region, xy, false, btn_is_treerow, NULL);
+}
+
+static bool btn_is_active_treerow(const Btn *btn, const void *customdata)
+{
+  if (!btn_is_treerow(btn, customdata)) {
+    return false;
+  }
+
+  const BtnTreeRow *treerow_btn = (const BtnTreeRow *)btn;
+  return ui_tree_view_item_is_active(treerow_btn->tree_item);
+}
 
 bool btn_has_array_value(const Btn *btn)
 {
