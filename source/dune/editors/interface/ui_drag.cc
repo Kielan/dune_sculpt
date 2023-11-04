1b06b2a94e1f12413d5ded238b9760849681b205
Kielan <ki.lemons@gmail.com>
2023-11-03 19:06:42 -0700

    Update ui_drag.cc

--- source/dune/editors/interface/ui_drag.cc
+++ source/dune/editors/interface/ui_drag.cc
@@ -39,3 +39,81 @@ void btn_drag_set_asset(Btn btn,
   btn_drag_attach_image(btn, imbuf, scale);
 }
 
+void btn_drag_set_api(Btn *btn, ApiPtr *ptr)
+{
+  btn->dragtype = WIN_DRAG_API;
+  if (btn->dragflag & UI_BTN_DRAGPOINT_FREE) {
+    win_drag_data_free(btn->dragtype, btn->dragpoint);
+    btn->dragflag &= ~UI_BTN_DRAGPOINT_FREE;
+  }
+  btn->dragpoint = (void *)ptr;
+}
+
+void btn_drag_set_path(Btn *btn, const char *path)
+{
+  btn->dragtype = WIN_DRAG_PATH;
+  if (btn->dragflag & UI_BTN_DRAGPOINT_FREE) {
+    win_drag_data_free(btn->dragtype, btn->dragpoint);
+  }
+  btn->dragpoint = win_drag_create_path_data(path);
+  btn->dragflag |= UI_BTN_DRAGPOINT_FREE;
+}
+
+void btn_drag_set_name(Btn *btn, const char *name)
+{
+  btn->dragtype = WIN_DRAG_NAME;
+  if (btn->dragflag & UI_BTN_DRAGPOINT_FREE) {
+    win_drag_data_free(btn->dragtype, btn->dragpoint);
+    btn->dragflag &= ~UI_BTN_DRAGPOINT_FREE;
+  }
+  btn->dragpoint = (void *)name;
+}
+
+void btn_drag_set_value(Btn *btn)
+{
+  btn->dragtype = WIN_DRAG_VALUE;
+}
+void btn_drag_set_image(Btn *btn, const char *path, int icon, const ImBuf *imb, float scale)
+{
+  ui_def_btn_icon(btn, icon, 0); /* no flag UI_HAS_ICON, so icon doesn't draw in btn */
+  btn_drag_set_path(btn, path);
+  btn_drag_attach_image(btn, imb, scale);
+}
+
+void btn_drag_free(Btn *btn)
+{
+  if (btn->dragpoint && (btn->dragflag & UI_BTN_DRAGPOINT_FREE)) {
+    win_drag_data_free(but->dragtype, btn->dragpoint);
+  }
+}
+
+bool but_drag_is_draggable(const Btn *btn)
+{
+  return btn->dragpoint != nullptr;
+}
+
+void btn_drag_start(Cxt *C, Btn *btn)
+{
+  WinDrag *drag = win_drag_data_create(C,
+                                     btn->icon,
+                                     btn->dragtype,
+                                     btn->dragpoint,
+                                     btn_value_get(btn),
+                                     (btn->dragflag & UI_BTN_DRAGPOINT_FREE) ? WIN_DRAG_FREE_DATA :
+                                                                              WIN_DRAG_NOP);
+  /* WinDrag has ownership over dragpoint now, stop messing with it. */
+  btn->dragpoint = nullptr;
+
+  if (btn->imb) {
+    win_event_drag_image(drag, btn->imb, btn->imb_scale);
+  }
+
+  win_event_start_prepared_drag(C, drag);
+
+  /* Special feature for assets: We add another drag item that supports multiple assets. It
+   * gets the assets from cxt. */
+  if (ELEM(btn->dragtype, WIN_DRAG_ASSET, WIN_DRAG_ID)) {
+    win_event_start_drag(C, ICON_NONE, WIN_DRAG_ASSET_LIST, nullptr, 0, WIN_DRAG_NOP);
+  }
+}
+
