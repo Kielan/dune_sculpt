da3b6c584758d9b73675c4c5c04c4b349b2d8650
Kielan <ki.lemons@gmail.com>
2023-11-13 15:40:22 -0800

    Update view3d_header.c

--- source/dune/editors/space_view3d/view3d_header.c
+++ source/dune/editors/space_view3d/view3d_header.c
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "DNA_gpencil_types.h"
-#include "DNA_object_types.h"
-#include "DNA_scene_types.h"
+#include "types_pen.h"
+#include "types_obj.h"
+#include "types_scene.h"
 
-#include "BLI_math_base.h"
-#include "BLI_utildefines.h"
+#include "lib_math_base.h"
+#include "lib_utildefines.h"
 
-#include "BLT_translation.h"
+#include "lang.h"
 
-#include "BKE_context.h"
-#include "BKE_editmesh.h"
+#include "dune_cxt.h"
+#include "dune_editmesh.h"
 
-#include "DEG_depsgraph.h"
+#include "graph.h"
 
-#include "RNA_access.h"
-#include "RNA_prototypes.h"
+#include "api_access.h"
+#include "api_prototypes.h"
 
-#include "WM_api.h"
-#include "WM_types.h"
+#include "win_api.h"
+#include "win_types.h"
 
-#include "ED_mesh.h"
-#include "ED_undo.h"
+#include "ed_mesh.h"
+#include "ed_undo.h"
 
-#include "UI_interface.h"
-#include "UI_resources.h"
+#include "ui.h"
+#include "ui_resources.h"
 
 #include "view3d_intern.h"
 
@@ -34,98 +34,91 @@
 #define B_SEL_EDGE 111
 #define B_SEL_FACE 112
 
-/* -------------------------------------------------------------------- */
-/** \name Toggle Matcap Flip Operator
- * \{ */
-
-static int toggle_matcap_flip(bContext *C, wmOperator *UNUSED(op))
+/* Toggle Matcap Flip Op */
+static int toggle_matcap_flip(Cxt *C, WinOp *UNUSED(op))
 {
-  View3D *v3d = CTX_wm_view3d(C);
+  View3D *v3d = cxt_win_view3d(C);
 
   if (v3d) {
     v3d->shading.flag ^= V3D_SHADING_MATCAP_FLIP_X;
-    ED_view3d_shade_update(CTX_data_main(C), v3d, CTX_wm_area(C));
-    WM_event_add_notifier(C, NC_SPACE | ND_SPACE_VIEW3D, v3d);
+    ed_view3d_shade_update(cxt_data_main(C), v3d, cxt_win_area(C));
+    win_ev_add_notifier(C, NC_SPACE | ND_SPACE_VIEW3D, v3d);
   }
   else {
-    Scene *scene = CTX_data_scene(C);
+    Scene *scene = cxt_data_scene(C);
     scene->display.shading.flag ^= V3D_SHADING_MATCAP_FLIP_X;
-    DEG_id_tag_update(&scene->id, ID_RECALC_COPY_ON_WRITE);
-    WM_event_add_notifier(C, NC_SCENE | ND_RENDER_OPTIONS, scene);
+    graph_id_tag_update(&scene->id, ID_RECALC_COPY_ON_WRITE);
+    win_ev_add_notifier(C, NC_SCENE | ND_RENDER_OPTIONS, scene);
   }
 
-  return OPERATOR_FINISHED;
+  return OP_FINISHED;
 }
 
-void VIEW3D_OT_toggle_matcap_flip(wmOperatorType *ot)
+void VIEW3D_OT_toggle_matcap_flip(WinOpType *ot)
 {
-  /* identifiers */
+  /* ids */
   ot->name = "Flip MatCap";
   ot->description = "Flip MatCap";
   ot->idname = "VIEW3D_OT_toggle_matcap_flip";
 
-  /* api callbacks */
-  ot->exec = toggle_matcap_flip;
+  /* api cbs */
+  ot->ex = toggle_matcap_flip;
 }
 
-/** \} */
+/* UI Templates */
 
-/* -------------------------------------------------------------------- */
-/** \name UI Templates
- * \{ */
-
-void uiTemplateEditModeSelection(uiLayout *layout, struct bContext *C)
+void uiTemplateEditModeSelection(uiLayout *layout, struct Cxt *C)
 {
-  Object *obedit = CTX_data_edit_object(C);
+  Object *obedit = cxt_data_edit_obj(C);
   if (!obedit || obedit->type != OB_MESH) {
     return;
   }
 
-  BMEditMesh *em = BKE_editmesh_from_object(obedit);
+  MEditMesh *em = dune_editmesh_from_obj(obedit);
   uiLayout *row = uiLayoutRow(layout, true);
 
-  PointerRNA op_ptr;
-  wmOperatorType *ot = WM_operatortype_find("MESH_OT_select_mode", true);
+  ApiPtr op_ptr;
+  WinOpType *ot = win_optype_find("MESH_OT_sel_mode", true);
   uiItemFullO_ptr(row,
                   ot,
                   "",
                   ICON_VERTEXSEL,
                   NULL,
-                  WM_OP_INVOKE_DEFAULT,
-                  (em->selectmode & SCE_SELECT_VERTEX) ? UI_ITEM_O_DEPRESS : 0,
+                  WIN_OP_INVOKE_DEFAULT,
+                  (em->selmode & SCE_SEL_VERTEX) ? UI_ITEM_O_DEPRESS : 0,
                   &op_ptr);
-  RNA_enum_set(&op_ptr, "type", SCE_SELECT_VERTEX);
+  api_enum_set(&op_ptr, "type", SCE_SEL_VERTEX);
   uiItemFullO_ptr(row,
                   ot,
                   "",
                   ICON_EDGESEL,
                   NULL,
-                  WM_OP_INVOKE_DEFAULT,
-                  (em->selectmode & SCE_SELECT_EDGE) ? UI_ITEM_O_DEPRESS : 0,
+                  WIN_OP_INVOKE_DEFAULT,
+                  (em->selmode & SCE_SEL_EDGE) ? UI_ITEM_O_DEPRESS : 0,
                   &op_ptr);
-  RNA_enum_set(&op_ptr, "type", SCE_SELECT_EDGE);
+  api_enum_set(&op_ptr, "type", SCE_SEL_EDGE);
   uiItemFullO_ptr(row,
                   ot,
                   "",
                   ICON_FACESEL,
                   NULL,
-                  WM_OP_INVOKE_DEFAULT,
-                  (em->selectmode & SCE_SELECT_FACE) ? UI_ITEM_O_DEPRESS : 0,
+                  WIN_OP_INVOKE_DEFAULT,
+                  (em->selmode & SCE_SEL_FACE) ? UI_ITEM_O_DEPRESS : 0,
                   &op_ptr);
-  RNA_enum_set(&op_ptr, "type", SCE_SELECT_FACE);
+  api_enum_set(&op_ptr, "type", SCE_SEL_FACE);
 }
 
-static void uiTemplatePaintModeSelection(uiLayout *layout, struct bContext *C)
+static void uiTemplatePaintModeSelection(uiLayout *layout, struct Cxt *C)
 {
-  ViewLayer *view_layer = CTX_data_view_layer(C);
-  Object *ob = OBACT(view_layer);
+  ViewLayer *view_layer = cxt_data_view_layer(C);
+  Obj *ob = OBACT(view_layer);
 
   /* Gizmos aren't used in paint modes */
   if (!ELEM(ob->mode, OB_MODE_SCULPT, OB_MODE_PARTICLE_EDIT)) {
     /* masks aren't used for sculpt and particle painting */
-    PointerRNA meshptr;
+    ApiPtr meshptr;
 
-    RNA_pointer_create(ob->data, &RNA_Mesh, ob->data, &meshptr);
+    api_ptr_create(ob->data, &ApiMesh, ob->data, &meshptr);
     if (ob->mode & OB_MODE_TEXTURE_PAINT) {
       uiItemR(layout, &meshptr, "use_paint_mask", UI_ITEM_R_ICON_ONLY, "", ICON_NONE);
     }
@@ -137,14 +130,14 @@ static void uiTemplatePaintModeSelection(uiLayout *layout, struct bContext *C)
   }
 }
 
-void uiTemplateHeader3D_mode(uiLayout *layout, struct bContext *C)
+void uiTemplateHeader3D_mode(uiLayout *layout, struct Cxt *C)
 {
-  ViewLayer *view_layer = CTX_data_view_layer(C);
-  Object *ob = OBACT(view_layer);
-  Object *obedit = CTX_data_edit_object(C);
-  bGPdata *gpd = CTX_data_gpencil_data(C);
+  ViewLayer *view_layer = cxt_data_view_layer(C);
+  Obj *ob = OBACT(view_layer);
+  Obj *obedit = cxt_data_edit_obj(C);
+  PenData *pdata = cxt_data_pen_data(C);
 
-  bool is_paint = (ob && !(gpd && (gpd->flag & GP_DATA_STROKE_EDITMODE)) &&
+  bool is_paint = (ob && !(pdata && (gpd->flag & PEN_DATA_STROKE_EDITMODE)) &&
                    ELEM(ob->mode,
                         OB_MODE_SCULPT,
                         OB_MODE_VERTEX_PAINT,
