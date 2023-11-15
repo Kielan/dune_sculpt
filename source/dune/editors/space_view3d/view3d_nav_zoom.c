1078a80c2d30677f772fb74bd5c1a6d1e14cc501
Kielan <ki.lemons@gmail.com>
2023-11-15 08:11:05 -0800

    Update view3d_nav_zoom.c

--- source/dune/editors/space_view3d/view3d_nav_zoom.c
+++ source/dune/editors/space_view3d/view3d_nav_zoom.c
@@ -6,24 +6,23 @@
 
 #include "graph_query.h"
 
-#include "WM_api.h"
+#include "win_api.h"
 
-#include "API_access.h"
+#include "api_access.h"
 
-#include "ED_screen.h"
+#include "ed_screen.h"
 
 #include "PIL_time.h"
 
 #include "view3d_intern.h"
-#include "view3d_navigate.h" /* own include */
+#include "view3d_nav.h" /* own include */
 
-/* -------------------------------------------------------------------- */
-/** View Zoom Operator **/
+/* View Zoom Op */
 
-/* #viewdolly_modal_keymap has an exact copy of this, apply fixes to both. */
-void viewzoom_modal_keymap(wmKeyConfig *keyconf)
+/* viewdolly_modal_keymap has an exact copy of this, apply fixes to both. */
+void viewzoom_modal_keymap(KeyConfig *keyconf)
 {
-  static const EnumPropertyItem modal_items[] = {
+  static const EnumPropItem modal_items[] = {
       {VIEW_MODAL_CONFIRM, "CONFIRM", 0, "Confirm", ""},
 
       {VIEWROT_MODAL_SWITCH_ROTATE, "SWITCH_TO_ROTATE", 0, "Switch to Rotate"},
@@ -32,42 +31,40 @@ void viewzoom_modal_keymap(wmKeyConfig *keyconf)
       {0, NULL, 0, NULL, NULL},
   };
 
-  wmKeyMap *keymap = WM_modalkeymap_find(keyconf, "View3D Zoom Modal");
+  WinKeyMap *keymap = win_modalkeymap_find(keyconf, "View3D Zoom Modal");
 
-  /* this function is called for each spacetype, only needs to add map once */
+  /* this fn is called for each spacetype, only needs to add map once */
   if (keymap && keymap->modal_items) {
     return;
   }
 
-  keymap = WM_modalkeymap_ensure(keyconf, "View3D Zoom Modal", modal_items);
+  keymap = win_modalkeymap_ensure(keyconf, "View3D Zoom Modal", modal_items);
 
   /* disabled mode switching for now, can re-implement better, later on */
 #if 0
-  WM_modalkeymap_add_item(keymap, LEFTMOUSE, KM_RELEASE, KM_ANY, 0, VIEWROT_MODAL_SWITCH_ROTATE);
-  WM_modalkeymap_add_item(keymap, LEFTCTRLKEY, KM_RELEASE, KM_ANY, 0, VIEWROT_MODAL_SWITCH_ROTATE);
-  WM_modalkeymap_add_item(keymap, LEFTSHIFTKEY, KM_PRESS, KM_ANY, 0, VIEWROT_MODAL_SWITCH_MOVE);
+  win_modalkeymap_add_item(keymap, LEFTMOUSE, KM_RELEASE, KM_ANY, 0, VIEWROT_MODAL_SWITCH_ROTATE);
+  win_modalkeymap_add_item(keymap, LEFTCTRLKEY, KM_RELEASE, KM_ANY, 0, VIEWROT_MODAL_SWITCH_ROTATE);
+  win_modalkeymap_add_item(keymap, LEFTSHIFTKEY, KM_PRESS, KM_ANY, 0, VIEWROT_MODAL_SWITCH_MOVE);
 #endif
 
-  /* assign map to operators */
-  WM_modalkeymap_assign(keymap, "VIEW3D_OT_zoom");
+  /* assign map to ops */
+  win_modalkeymap_assign(keymap, "VIEW3D_OT_zoom");
 }
 
-/**
- * param zoom_xy: Optionally zoom to window location
- * (coords compatible w wmEvent.xy). Use when not NULL.
- */
-static void view_zoom_to_window_xy_camera(Scene *scene,
-                                          Depsgraph *depsgraph,
-                                          View3D *v3d,
-                                          ARegion *region,
-                                          float dfac,
-                                          const int zoom_xy[2])
+/* param zoom_xy: Optionally zoom to window location
+ * (coords compatible w wmEvent.xy). Use when not NULL. */
+static void view_zoom_to_win_xy_camera(Scene *scene,
+                                       Graph *graph,
+                                       View3D *v3d,
+                                       ARgn *rgn,
+                                       float dfac,
+                                       const int zoom_xy[2])
 {
-  RegionView3D *rv3d = region->regiondata;
-  const float zoomfac = BKE_screen_view3d_zoom_to_fac(rv3d->camzoom);
+  RgnView3D *rv3d = rgn->rgndata;
+  const float zoomfac = dune_screen_view3d_zoom_to_fac(rv3d->camzoom);
   const float zoomfac_new = clamp_f(
       zoomfac * (1.0f / dfac), RV3D_CAMZOOM_MIN_FACTOR, RV3D_CAMZOOM_MAX_FACTOR);
-  const float camzoom_new = BKE_screen_view3d_zoom_from_fac(zoomfac_new);
+  const float camzoom_new = dune_screen_view3d_zoom_from_fac(zoomfac_new);
 
   if (zoom_xy != NULL) {
     float zoomfac_px;
@@ -78,24 +75,24 @@ static void view_zoom_to_window_xy_camera(Scene *scene,
     float pt_dst[2];
     float delta_px[2];
 
-    ED_view3d_calc_camera_border(scene, depsgraph, region, v3d, rv3d, &camera_frame_old, false);
-    LIB_rctf_translate(&camera_frame_old, region->winrct.xmin, region->winrct.ymin);
+    ed_view3d_calc_camera_border(scene, graph, rgn, v3d, rv3d, &camera_frame_old, false);
+    lib_rctf_translate(&camera_frame_old, rgn->winrct.xmin, rgn->winrct.ymin);
 
     rv3d->camzoom = camzoom_new;
     CLAMP(rv3d->camzoom, RV3D_CAMZOOM_MIN, RV3D_CAMZOOM_MAX);
 
-    ED_view3d_calc_camera_border(scene, depsgraph, region, v3d, rv3d, &camera_frame_new, false);
-    LIB_rctf_translate(&camera_frame_new, region->winrct.xmin, region->winrct.ymin);
+    ed_view3d_calc_camera_border(scene, graph, rgn, v3d, rv3d, &camera_frame_new, false);
+    lib_rctf_translate(&camera_frame_new, rgn->winrct.xmin, rgn->winrct.ymin);
 
-    LIB_rctf_transform_pt_v(&camera_frame_new, &camera_frame_old, pt_dst, pt_src);
+    lib_rctf_transform_pt_v(&camera_frame_new, &camera_frame_old, pt_dst, pt_src);
     sub_v2_v2v2(delta_px, pt_dst, pt_src);
 
     /* translate the camera offset using pixel space delta
      * mapped back to the camera (same logic as panning in camera view) */
-    zoomfac_px = BKE_screen_view3d_zoom_to_fac(rv3d->camzoom) * 2.0f;
+    zoomfac_px = dune_screen_view3d_zoom_to_fac(rv3d->camzoom) * 2.0f;
 
-    rv3d->camdx += delta_px[0] / (region->winx * zoomfac_px);
-    rv3d->camdy += delta_px[1] / (region->winy * zoomfac_px);
+    rv3d->camdx += delta_px[0] / (rgn->winx * zoomfac_px);
+    rv3d->camdy += delta_px[1] / (rgn->winy * zoomfac_px);
     CLAMP(rv3d->camdx, -1.0f, 1.0f);
     CLAMP(rv3d->camdy, -1.0f, 1.0f);
   }
@@ -105,13 +102,11 @@ static void view_zoom_to_window_xy_camera(Scene *scene,
   }
 }
 
-/**
- * param zoom_xy: Optionally zoom to window location
- * (coords compatible w wmEvent.xy). Use when not NULL.
- */
-static void view_zoom_to_window_xy_3d(ARegion *region, float dfac, const int zoom_xy[2])
+/* param zoom_xy: Optionally zoom to win location
+ * (coords compatible w WinEv.xy). Use when not NULL. */
+static void view_zoom_to_win_xy_3d(ARgn *rgn, float dfac, const int zoom_xy[2])
 {
-  RegionView3D *rv3d = region->regiondata;
+  RgnView3D *rv3d = rgn->rgndata;
   const float dist_new = rv3d->dist * dfac;
 
   if (zoom_xy != NULL) {
@@ -124,14 +119,14 @@ static void view_zoom_to_window_xy_3d(ARegion *region, float dfac, const int zoo
 
     negate_v3_v3(tpos, rv3d->ofs);
 
-    xy_delta[0] = (float)(((zoom_xy[0] - region->winrct.xmin) * 2) - region->winx) / 2.0f;
-    xy_delta[1] = (float)(((zoom_xy[1] - region->winrct.ymin) * 2) - region->winy) / 2.0f;
+    xy_delta[0] = (float)(((zoom_xy[0] - rgn->winrct.xmin) * 2) - rgn->winx) / 2.0f;
+    xy_delta[1] = (float)(((zoom_xy[1] - rgn->winrct.ymin) * 2) - rgn->winy) / 2.0f;
 
     /* Project cursor position into 3D space */
-    zfac = ED_view3d_calc_zfac(rv3d, tpos);
-    ED_view3d_win_to_delta(region, xy_delta, zfac, dvec);
+    zfac = ed_view3d_calc_zfac(rv3d, tpos);
+    ed_view3d_win_to_delta(rgn, xy_delta, zfac, dvec);
 
-    /* Calculate view target position for dolly */
+    /* Calc view target position for dolly */
     add_v3_v3v3(tvec, tpos, dvec);
     negate_v3(tvec);
 
@@ -139,7 +134,7 @@ static void view_zoom_to_window_xy_3d(ARegion *region, float dfac, const int zoo
     copy_v3_v3(rv3d->ofs, tvec);
     rv3d->dist = dist_new;
 
-    /* Calculate final offset */
+    /* Calc final offset */
     madd_v3_v3v3fl(rv3d->ofs, tvec, dvec, dfac);
   }
   else {
@@ -184,8 +179,8 @@ static float viewzoom_scale_value(const rcti *winrct,
     /* method which zooms based on how far you move the mouse */
 
     const int ctr[2] = {
-        LIB_rcti_cent_x(winrct),
-        LIB_rcti_cent_y(winrct),
+        lib_rcti_cent_x(winrct),
+        lib_rcti_cent_y(winrct),
     };
     float len_new = (5 * U.dpi_fac) + ((float)len_v2v2_int(ctr, xy_curr) / U.dpi_fac);
     float len_old = (5 * U.dpi_fac) + ((float)len_v2v2_int(ctr, xy_init) / U.dpi_fac);
@@ -239,15 +234,15 @@ static float viewzoom_scale_value_offset(const rcti *winrct,
       xy_init[0] + xy_offset[0],
       xy_init[1] + xy_offset[1],
   };
-  return viewzoom_scale_value(winrct,
-                              viewzoom,
-                              zoom_invert,
-                              zoom_invert_force,
-                              xy_curr_offset,
-                              xy_init_offset,
-                              val,
-                              val_orig,
-                              r_timer_lastdraw);
+  return viewzoom_scale_val(winrct,
+                            viewzoom,
+                            zoom_invert,
+                            zoom_invert_force,
+                            xy_curr_offset,
+                            xy_init_offset,
+                            val,
+                            val_orig,
+                            r_timer_lastdraw);
 }
 
 static void viewzoom_apply_camera(ViewOpsData *vod,
@@ -257,32 +252,32 @@ static void viewzoom_apply_camera(ViewOpsData *vod,
                                   const bool zoom_to_pos)
 {
   float zfac;
-  float zoomfac_prev = DUNE_screen_view3d_zoom_to_fac(vod->init.camzoom) * 2.0f;
-  float zoomfac = DUNE_screen_view3d_zoom_to_fac(vod->rv3d->camzoom) * 2.0f;
+  float zoomfac_prev = dune_screen_view3d_zoom_to_fac(vod->init.camzoom) * 2.0f;
+  float zoomfac = dune_screen_view3d_zoom_to_fac(vod->rv3d->camzoom) * 2.0f;
 
-  zfac = viewzoom_scale_value_offset(&vod->region->winrct,
-                                     viewzoom,
-                                     zoom_invert,
-                                     true,
-                                     xy,
-                                     vod->init.event_xy,
-                                     vod->init.event_xy_offset,
-                                     zoomfac,
-                                     zoomfac_prev,
-                                     &vod->prev.time);
+  zfac = viewzoom_scale_val_offset(&vod->rgn->winrct,
+                                   viewzoom,
+                                   zoom_invert,
+                                   true,
+                                   xy,
+                                   vod->init.ev_xy,
+                                   vod->init.ev_xy_offset,
+                                   zoomfac,
+                                   zoomfac_prev,
+                                   &vod->prev.time);
 
   if (!ELEM(zfac, 1.0f, 0.0f)) {
-    /* calculate inverted, then invert again (needed because of camera zoom scaling) */
+    /* calc inverted, then invert again (needed because of camera zoom scaling) */
     zfac = 1.0f / zfac;
-    view_zoom_to_window_xy_camera(vod->scene,
-                                  vod->depsgraph,
-                                  vod->v3d,
-                                  vod->region,
-                                  zfac,
-                                  zoom_to_pos ? vod->prev.event_xy : NULL);
+    view_zoom_to_win_xy_camera(vod->scene,
+                               vod->graph,
+                               vod->v3d,
+                               vod->rgn,
+                               zfac,
+                               zoom_to_pos ? vod->prev.ev_xy : NULL);
   }
 
-  ED_region_tag_redraw(vod->region);
+  ed_rgn_tag_redraw(vod->rgn);
 }
 
 static void viewzoom_apply_3d(ViewOpsData *vod,
@@ -294,15 +289,15 @@ static void viewzoom_apply_3d(ViewOpsData *vod,
   float zfac;
   float dist_range[2];
 
-  ED_view3d_dist_range_get(vod->v3d, dist_range);
+  ed_view3d_dist_range_get(vod->v3d, dist_range);
 
-  zfac = viewzoom_scale_value_offset(&vod->region->winrct,
+  zfac = viewzoom_scale_value_offset(&vod->rgn->winrct,
                                      viewzoom,
                                      zoom_invert,
                                      false,
                                      xy,
-                                     vod->init.event_xy,
-                                     vod->init.event_xy_offset,
+                                     vod->init.ev_xy,
+                                     vod->init.ev_xy_offset,
                                      vod->rv3d->dist,
                                      vod->init.dist,
                                      &vod->prev.time);
@@ -312,19 +307,19 @@ static void viewzoom_apply_3d(ViewOpsData *vod,
     const float zfac_max = dist_range[1] / vod->rv3d->dist;
     CLAMP(zfac, zfac_min, zfac_max);
 
-    view_zoom_to_window_xy_3d(vod->region, zfac, zoom_to_pos ? vod->prev.event_xy : NULL);
+    view_zoom_to_win_xy_3d(vod->rgn, zfac, zoom_to_pos ? vod->prev.ev_xy : NULL);
   }
 
   /* these limits were in old code too */
   CLAMP(vod->rv3d->dist, dist_range[0], dist_range[1]);
 
   if (RV3D_LOCK_FLAGS(vod->rv3d) & RV3D_BOXVIEW) {
-    view3d_boxview_sync(vod->area, vod->region);
+    view3d_boxview_sync(vod->area, vod->rgn);
   }
 
-  ED_view3d_camera_lock_sync(vod->depsgraph, vod->v3d, vod->rv3d);
+  ed_view3d_camera_lock_sync(vod->graph, vod->v3d, vod->rv3d);
 
-  ED_region_tag_redraw(vod->region);
+  ed_rgn_tag_redraw(vod->rgn);
 }
 
 static void viewzoom_apply(ViewOpsData *vod,
@@ -334,7 +329,7 @@ static void viewzoom_apply(ViewOpsData *vod,
                            const bool zoom_to_pos)
 {
   if ((vod->rv3d->persp == RV3D_CAMOB) &&
-      (vod->rv3d->is_persp && ED_view3d_camera_lock_check(vod->v3d, vod->rv3d)) == 0) {
+      (vod->rv3d->is_persp && ed_view3d_camera_lock_check(vod->v3d, vod->rv3d)) == 0) {
     viewzoom_apply_camera(vod, xy, viewzoom, zoom_invert, zoom_to_pos);
   }
   else {
@@ -342,7 +337,7 @@ static void viewzoom_apply(ViewOpsData *vod,
   }
 }
 
-static int viewzoom_modal(duneContext *C, wmOperator *op, const wmEvent *event)
+static int viewzoom_modal(Cxt *C, WinOp *op, const WinEv *ev)
 {
   ViewOpsData *vod = op->customdata;
   short event_code = VIEW_PASS;
