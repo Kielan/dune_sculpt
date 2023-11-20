9a71fb5a28daac8976ea52f3630e55c7039fd550
Kielan <ki.lemons@gmail.com>
2023-11-19 22:02:40 -0800

    Create action_btns.cc

---
+++ source/dune/editors/space_action/action_btns.cc
@@ -0,0 +1,46 @@
+
+#include <cfloat>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+
+#include "lib_utildefines.h"
+
+#include "dune_cxt.hh"
+#include "dune_screen.hh"
+
+#include "action_intern.hh" /* own include */
+
+/* action editor space & btns */
+/* general */
+void action_btns_register(ARgnType * /*art*/)
+{
+#if 0
+  PnlType *pt;
+
+  /* TODO: AnimData / Actions List */
+  pt = mem_cnew<PnlType>("spacetype action pnl props");
+  STRNCPY(pt->idname, "ACTION_PT_props");
+  STRNCPY(pt->label, N_("Active F-Curve"));
+  STRNCPY(pt->lang_cxt, LANG_CXT_DEFAULT_API);
+  pt->draw = action_anim_pnl_props;
+  pt->poll = action_anim_pnl_poll;
+  lib_addtail(&art->pnltypes, pt);
+
+  pt = mem_cnew<PnlType>("spacetype action pnl props");
+  STRNCPY(pt->idname, "ACTION_PT_key_props");
+  STRNCPY(pt->label, N_("Active Keyframe"));
+  STRNCPY(pt->lang_cxt, LANG_CXT_DEFAULT_API);
+  pt->draw = action_anim_pnl_key_props;
+  pt->poll = action_anim_pnl_poll;
+  lob_addtail(&art->pnltypes, pt);
+
+  pt = mem_calloc(sizeof(PnlType), "spacetype action pnl mods");
+  STRNCPY(pt->idname, "ACTION_PT_mods");
+  STRNCPY(pt->label, N_("Mods"));
+  STRNCPY(pt->lang_cxt, LANG_CXT_DEFAULT_API);
+  pt->draw = action_anim_pnl_mods;
+  pt->poll = action_anim_pnl_poll;
+  lib_addtail(&art->pnltypes, pt);
+#endif
+}
