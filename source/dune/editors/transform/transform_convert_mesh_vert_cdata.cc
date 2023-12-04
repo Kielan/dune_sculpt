343c14e2fc77e7a85e14ce0fbad1ced78e8b5427
Kielan <ki.lemons@gmail.com>
2023-12-04 02:20:19 -0800

    Update transform_convert_mesh_vert_cdata.cc

--- source/dune/editors/transform/transform_convert_mesh_vert_cdata.cc
+++ source/dune/editors/transform/transform_convert_mesh_vert_cdata.cc
@@ -1,23 +1,20 @@
-#include "BLI_math_matrix.h"
-#include "BLI_math_vector.h"
+#include "lib_math_matrix.h"
+#include "lib_math_vector.h"
 
-#include "BKE_context.hh"
-#include "BKE_editmesh.hh"
+#include "dune_cxt.hh"
+#include "dune_meshedit.hh"
 
-#include "DEG_depsgraph.hh"
+#include "graph.hh"
 
 #include "transform.hh"
 #include "transform_orientations.hh"
 
 #include "transform_convert.hh"
 
-/* -------------------------------------------------------------------- */
-/** \name Edit Mesh Bevel Weight and Crease Transform Creation
- * \{ */
-
+/* MeshEdit Bevel Weight and Crease Transform Creation */
 static float *mesh_cdata_transdata_center(const TransIslandData *island_data,
                                           const int island_index,
-                                          BMVert *eve)
+                                          MeshVert *eve)
 {
   if (island_data->center && island_index != -1) {
     return island_data->center[island_index];
@@ -26,17 +23,17 @@ static float *mesh_cdata_transdata_center(const TransIslandData *island_data,
 }
 
 static void mesh_cdata_transdata_create(TransDataBasic *td,
-                                        BMVert *eve,
+                                        MeshVert *mvert,
                                         float *weight,
                                         const TransIslandData *island_data,
                                         const int island_index)
 {
-  BLI_assert(BM_elem_flag_test(eve, BM_ELEM_HIDDEN) == 0);
+  lib_assert(mesh_elem_flag_test(eve, MESH_ELEM_HIDDEN) == 0);
 
   td->val = weight;
   td->ival = *weight;
 
-  if (BM_elem_flag_test(eve, BM_ELEM_SELECT)) {
+  if (mesh_elem_flag_test(eve, MESH_ELEM_SEL)) {
     td->flag |= TD_SELECTED;
   }
 
@@ -44,14 +41,14 @@ static void mesh_cdata_transdata_create(TransDataBasic *td,
   td->extra = eve;
 }
 
-static void createTransMeshVertCData(bContext * /*C*/, TransInfo *t)
+static void createTransMeshVertCData(Cxt * /*C*/, TransInfo *t)
 {
-  BLI_assert(ELEM(t->mode, TFM_BWEIGHT, TFM_VERT_CREASE));
+  lib_assert(ELEM(t->mode, TFM_BWEIGHT, TFM_VERT_CREASE));
   FOREACH_TRANS_DATA_CONTAINER (t, tc) {
-    BMEditMesh *em = BKE_editmesh_from_object(tc->obedit);
-    BMesh *bm = em->bm;
-    BMVert *eve;
-    BMIter iter;
+    MeshEdit *me = dune_meshedit_from_ob(tc->obedit);
+    Mesh *mesh = me->mesh;
+    MeshVert *eve;
+    MeshIter iter;
     float mtx[3][3], smtx[3][3];
     int a;
     const int prop_mode = (t->flag & T_PROP_EDIT) ? (t->flag & T_PROP_EDIT_ALL) : 0;
@@ -59,24 +56,24 @@ static void createTransMeshVertCData(bContext * /*C*/, TransInfo *t)
     TransIslandData island_data = {nullptr};
     TransMeshDataCrazySpace crazyspace_data = {nullptr};
 
-    /* Support other objects using proportional editing to adjust these, unless connected is
+    /* Support other obs using proportional editing to adjust these, unless connected is
      * enabled. */
-    if ((!prop_mode || (prop_mode & T_PROP_CONNECTED)) && (bm->totvertsel == 0)) {
+    if ((!prop_mode || (prop_mode & T_PROP_CONNECTED)) && (mesh->totvertsel == 0)) {
       continue;
     }
 
     int cd_offset = -1;
     if (t->mode == TFM_BWEIGHT) {
-      if (!CustomData_has_layer_named(&bm->vdata, CD_PROP_FLOAT, "bevel_weight_vert")) {
-        BM_data_layer_add_named(bm, &bm->vdata, CD_PROP_FLOAT, "bevel_weight_vert");
+      if (!CustomData_has_layer_named(&mesh->vdata, CD_PROP_FLOAT, "bevel_weight_vert")) {
+        mesh_data_layer_add_named(mesh, &mesh->vdata, CD_PROP_FLOAT, "bevel_weight_vert");
       }
       cd_offset = CustomData_get_offset_named(&bm->vdata, CD_PROP_FLOAT, "bevel_weight_vert");
     }
     else {
-      if (!CustomData_has_layer_named(&bm->vdata, CD_PROP_FLOAT, "crease_vert")) {
-        BM_data_layer_add_named(bm, &bm->vdata, CD_PROP_FLOAT, "crease_vert");
+      if (!CustomData_has_layer_named(&mesh->vdata, CD_PROP_FLOAT, "crease_vert")) {
+        mesh_data_layer_add_named(mesh, &mesh->vdata, CD_PROP_FLOAT, "crease_vert");
       }
-      cd_offset = CustomData_get_offset_named(&bm->vdata, CD_PROP_FLOAT, "crease_vert");
+      cd_offset = CustomData_get_offset_named(&mesh->vdata, CD_PROP_FLOAT, "crease_vert");
     }
 
     if (cd_offset == -1) {
@@ -85,14 +82,14 @@ static void createTransMeshVertCData(bContext * /*C*/, TransInfo *t)
 
     int data_len = 0;
     if (prop_mode) {
-      BM_ITER_MESH (eve, &iter, bm, BM_VERTS_OF_MESH) {
-        if (!BM_elem_flag_test(eve, BM_ELEM_HIDDEN)) {
+      MESH_ITER (eve, &iter, mesh, MESH_VERTS) {
+        if (!mesh_elem_flag_test(eve, MESH_ELEM_HIDDEN)) {
           data_len++;
         }
       }
     }
     else {
-      data_len = bm->totvertsel;
+      data_len = mesh->totvertsel;
     }
 
     if (data_len == 0) {
@@ -101,44 +98,44 @@ static void createTransMeshVertCData(bContext * /*C*/, TransInfo *t)
 
     const bool is_island_center = (t->around == V3D_AROUND_LOCAL_ORIGINS);
     if (is_island_center) {
-      /* In this specific case, near-by vertices will need to know
-       * the island of the nearest connected vertex. */
+      /* In this specific case, near-by verts will need to know
+       * the island of the nearest connected vert. */
       const bool calc_single_islands = ((prop_mode & T_PROP_CONNECTED) &&
                                         (t->around == V3D_AROUND_LOCAL_ORIGINS) &&
-                                        (em->selectmode & SCE_SELECT_VERTEX));
+                                        (me->selmode & SCE_SEL_VERT));
 
       const bool calc_island_center = false;
       const bool calc_island_axismtx = false;
 
       transform_convert_mesh_islands_calc(
-          em, calc_single_islands, calc_island_center, calc_island_axismtx, &island_data);
+          me, calc_single_islands, calc_island_center, calc_island_axismtx, &island_data);
     }
 
-    copy_m3_m4(mtx, tc->obedit->object_to_world);
+    copy_m3_m4(mtx, tc->obedit->ob_to_world);
     /* we use a pseudo-inverse so that when one of the axes is scaled to 0,
      * matrix inversion still works and we can still moving along the other */
     pseudoinverse_m3_m3(smtx, mtx, PSEUDOINVERSE_EPSILON);
 
-    /* Original index of our connected vertex when connected distances are calculated.
-     * Optional, allocate if needed. */
+    /* Original index of our connected vert when connected distances are calcd.
+     * Optional, alloc if needed. */
     int *dists_index = nullptr;
     float *dists = nullptr;
     if (prop_mode & T_PROP_CONNECTED) {
-      dists = static_cast<float *>(MEM_mallocN(bm->totvert * sizeof(float), __func__));
+      dists = static_cast<float *>(mem_malloc(mesh->totvert * sizeof(float), __func__));
       if (is_island_center) {
-        dists_index = static_cast<int *>(MEM_mallocN(bm->totvert * sizeof(int), __func__));
+        dists_index = static_cast<int *>(mem_malloc(mesh->totvert * sizeof(int), __func__));
       }
-      transform_convert_mesh_connectivity_distance(em->bm, mtx, dists, dists_index);
+      transform_convert_mesh_connectivity_distance(mesh->mesh, mtx, dists, dists_index);
     }
 
     /* Detect CrazySpace [tm]. */
-    transform_convert_mesh_crazyspace_detect(t, tc, em, &crazyspace_data);
+    transform_convert_mesh_crazyspace_detect(t, tc, me, &crazyspace_data);
 
     /* Create TransData. */
-    BLI_assert(data_len >= 1);
+    lib_assert(data_len >= 1);
     tc->data_len = data_len;
     tc->data = static_cast<TransData *>(
-        MEM_callocN(data_len * sizeof(TransData), "TransObData(Mesh EditMode)"));
+        mem_calloc(data_len * sizeof(TransData), "TransObData(Mesh EditMode)"));
 
     TransData *td = tc->data;
     MESH_ITER_INDEX (eve, &iter, mesh, MESHA_VERTS_OF_MESH, a) {
@@ -152,8 +149,8 @@ static void createTransMeshVertCData(bContext * /*C*/, TransInfo *t)
         island_index = island_data.island_vert_map[connected_index];
       }
 
-      float *weight = static_cast<float *>(BM_ELEM_CD_GET_VOID_P(eve, cd_offset));
-      if (prop_mode || mesh_elem_flag_test(eve, BM_ELEM_SELECT)) {
+      float *weight = static_cast<float *>(MESH_ELEM_CD_GET_VOID_P(eve, cd_offset));
+      if (prop_mode || mesh_elem_flag_test(eve, MESH_ELEM_SEL)) {
         mesh_cdata_transdata_create((TransDataBasic *)td, eve, weight, &island_data, island_index);
 
         if (t->around == V3D_AROUND_LOCAL_ORIGINS) {
@@ -204,13 +201,11 @@ static void createTransMeshVertCData(bContext * /*C*/, TransInfo *t)
 static void recalcData_mesh_cdata(TransInfo *t)
 {
   FOREACH_TRANS_DATA_CONTAINER (t, tc) {
-    DEG_id_tag_update(static_cast<ID *>(tc->obedit->data), ID_RECALC_GEOMETRY);
+    graph_id_tag_update(static_cast<Id *>(tc->obedit->data), ID_RECALC_GEOMETRY);
   }
 }
 
-/** \} */
-
-TransConvertTypeInfo TransConvertType_MeshVertCData = {
+TransConvertTypeInfo TransConvertTypeMeshVertCData = {
     /*flags*/ (T_EDIT | T_POINTS),
     /*create_trans_data*/ createTransMeshVertCData,
     /*recalc_data*/ recalcData_mesh_cdata,
