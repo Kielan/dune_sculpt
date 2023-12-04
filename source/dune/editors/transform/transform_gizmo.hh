f58cbe263871f42fcb36b05a972bd0f9a58c3cae
Kielan <ki.lemons@gmail.com>
2023-12-04 10:28:26 -0800

    Update transform_gizmo.hh

--- source/dune/editors/transform/transform_gizmo.hh
+++ source/dune/editors/transform/transform_gizmo.hh
@@ -1,59 +1,40 @@
 #pragma once
 
-/* -------------------------------------------------------------------- */
-/** \name Types/
- * \{ */
-
-struct ARegion;
-struct bContext;
-struct bScreen;
-struct RegionView3D;
+struct ARgn;
+struct Cxt;
+struct Screen;
+struct RgnView3D;
 struct Scene;
 struct ScrArea;
 struct TransformBounds;
 struct TransInfo;
-struct wmGizmoGroup;
-struct wmGizmoGroupType;
-struct wmMsgBus;
+struct WinGizmoGroup;
+struct WinGizmoGroupType;
+struct WinMsgBus;
 
-/** \} */
-
-/* -------------------------------------------------------------------- */
-/** \name Gizmo
- * \{ */
-
+/* Gizmo */
 /* `transform_gizmo_3d.cc` */
-
 #define GIZMO_AXIS_LINE_WIDTH 2.0f
 
-void gizmo_prepare_mat(const bContext *C, RegionView3D *rv3d, const TransformBounds *tbounds);
-void gizmo_xform_message_subscribe(wmGizmoGroup *gzgroup,
-                                   wmMsgBus *mbus,
+void gizmo_prepare_mat(const Cxt *C, RgnView3D *rv3d, const TransformBounds *tbounds);
+void gizmo_xform_msg_sub(WinGizmoGroup *gzgroup,
+                                   WinMsgBus *mbus,
                                    Scene *scene,
-                                   bScreen *screen,
+                                   Screen *screen,
                                    ScrArea *area,
-                                   ARegion *region,
-                                   void (*type_fn)(wmGizmoGroupType *));
+                                   ARgn *rgn,
+                                   void (*type_fn)(WinGizmoGroupType *));
 
-/**
- * Set the #T_NO_GIZMO flag.
- *
- * \note This maintains the conventional behavior of not displaying the gizmo if the operator has
- * been triggered by shortcuts.
- */
+/* Set the T_NO_GIZMO flag.
+ * This maintains the conventional behavior of not displaying the gizmo if the op has
+ * been triggered by shortcuts */
 void transform_gizmo_3d_model_from_constraint_and_mode_init(TransInfo *t);
 
-/**
- * Change the gizmo and its orientation to match the transform state.
- *
- * \note This used while the modal operator is running so changes to the constraint or mode show
- * the gizmo associated with that state, as if it had been the initial gizmo dragged.
- */
+/* Change the gizmo and its orientation to match the transform state.
+ * This used while the modal op is running so changes to the constraint or mode show
+ * the gizmo assoc. w that state, as if it had been the initial gizmo dragged. */
 void transform_gizmo_3d_model_from_constraint_and_mode_set(TransInfo *t);
 
-/**
- * Restores the non-modal state of the gizmo.
- */
+/* Restores the non-modal state of the gizmo. */
 void transform_gizmo_3d_model_from_constraint_and_mode_restore(TransInfo *t);
 
-/** \} */
