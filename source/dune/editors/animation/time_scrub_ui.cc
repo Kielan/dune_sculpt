43ff024e5d3eb30ae19834c7b086463981b14790
Kielan <ki.lemons@gmail.com>
2023-12-06 18:59:34 -0800

    Update time_scrub_ui.cc

--- source/dune/editors/animation/time_scrub_ui.cc
+++ source/dune/editors/animation/time_scrub_ui.cc
@@ -1,54 +1,54 @@
-#include "BKE_context.hh"
-#include "BKE_scene.h"
+#include "dune_cxt.hh"
+#include "dune_scene.h"
 
-#include "GPU_immediate.h"
-#include "GPU_matrix.h"
-#include "GPU_state.h"
+#include "gpu_immediate.h"
+#include "gpu_matrix.h"
+#include "gpu_state.h"
 
-#include "ED_time_scrub_ui.hh"
+#include "ed_time_scrub_ui.hh"
 
-#include "WM_api.hh"
-#include "WM_types.hh"
+#include "win_api.hh"
+#include "win_types.hh"
 
-#include "UI_interface.hh"
-#include "UI_interface_icons.hh"
-#include "UI_resources.hh"
-#include "UI_view2d.hh"
+#include "ui.hh"
+#include "ui_icons.hh"
+#include "ui_resources.hh"
+#include "ui_view2d.hh"
 
-#include "DNA_scene_types.h"
+#include "types_scene.h"
 
-#include "BLI_rect.h"
-#include "BLI_string.h"
-#include "BLI_timecode.h"
+#include "lib_rect.h"
+#include "lib_string.h"
+#include "lib_timecode.h"
 
-#include "RNA_access.hh"
-#include "RNA_prototypes.h"
+#include "api_access.hh"
+#include "api_prototypes.h"
 
-void ED_time_scrub_region_rect_get(const ARegion *region, rcti *rect)
+void ed_time_scrub_rgn_rect_get(const ARgn *rgn, rcti *rect)
 {
   rect->xmin = 0;
-  rect->xmax = region->winx;
-  rect->ymax = region->winy;
+  rect->xmax = rgn->winx;
+  rect->ymax = rgn->winy;
   rect->ymin = rect->ymax - UI_TIME_SCRUB_MARGIN_Y;
 }
 
-static int get_centered_text_y(const rcti *rect)
+static int get_centered_txt_y(const rcti *rect)
 {
-  return BLI_rcti_cent_y(rect) - UI_SCALE_FAC * 4;
+  return lib_rcti_cent_y(rect) - UI_SCALE_FAC * 4;
 }
 
-static void draw_background(const rcti *rect)
+static void drw_background(const rcti *rect)
 {
-  uint pos = GPU_vertformat_attr_add(immVertexFormat(), "pos", GPU_COMP_F32, 2, GPU_FETCH_FLOAT);
+  uint pos = gpu_vertformat_attr_add(immVertexFormat(), "pos", GPU_COMP_F32, 2, GPU_FETCH_FLOAT);
   immBindBuiltinProgram(GPU_SHADER_3D_UNIFORM_COLOR);
 
   immUniformThemeColor(TH_TIME_SCRUB_BACKGROUND);
 
-  GPU_blend(GPU_BLEND_ALPHA);
+  gpu_blend(GPU_BLEND_ALPHA);
 
   immRectf(pos, rect->xmin, rect->ymin, rect->xmax, rect->ymax);
 
-  GPU_blend(GPU_BLEND_NONE);
+  gpu_blend(GPU_BLEND_NONE);
 
   immUnbindProgram();
 }
@@ -57,36 +57,36 @@ static void get_current_time_str(
     const Scene *scene, bool display_seconds, int frame, char *r_str, uint str_maxncpy)
 {
   if (display_seconds) {
-    BLI_timecode_string_from_time(r_str, str_maxncpy, 0, FRA2TIME(frame), FPS, U.timecode_style);
+    lib_timecode_string_from_time(r_str, str_maxncpy, 0, FRA2TIME(frame), FPS, U.timecode_style);
   }
   else {
-    BLI_snprintf(r_str, str_maxncpy, "%d", frame);
+    lib_snprintf(r_str, str_maxncpy, "%d", frame);
   }
 }
 
-static void draw_current_frame(const Scene *scene,
+static void drw_current_frame(const Scene *scene,
                                bool display_seconds,
                                const View2D *v2d,
-                               const rcti *scrub_region_rect,
+                               const rcti *scrub_rgn_rect,
                                int current_frame)
 {
   const uiFontStyle *fstyle = UI_FSTYLE_WIDGET;
-  int frame_x = UI_view2d_view_to_region_x(v2d, current_frame);
+  int frame_x = ui_view2d_view_to_rgn_x(v2d, current_frame);
 
   char frame_str[64];
   get_current_time_str(scene, display_seconds, current_frame, frame_str, sizeof(frame_str));
-  float text_width = UI_fontstyle_string_width(fstyle, frame_str);
+  float txt_width = ui_fontstyle_string_width(fstyle, frame_str);
   float box_width = std::max(text_width + 8 * UI_SCALE_FAC, 24 * UI_SCALE_FAC);
   float box_padding = 3 * UI_SCALE_FAC;
   const int line_outline = max_ii(1, round_fl_to_int(1 * UI_SCALE_FAC));
 
   float bg_color[4];
-  UI_GetThemeColorShade4fv(TH_CFRAME, -5, bg_color);
+  ui_GetThemeColorShade4fv(TH_CFRAME, -5, bg_color);
 
-  /* Draw vertical line from the bottom of the current frame box to the bottom of the screen. */
-  const float subframe_x = UI_view2d_view_to_region_x(v2d, BKE_scene_ctime_get(scene));
+  /* Drw vert line from the bottom of the current frame box to the bottom of the screen. */
+  const float subframe_x = ui_view2d_view_to_rgn_x(v2d, dune_scene_ctime_get(scene));
   GPUVertFormat *format = immVertexFormat();
-  uint pos = GPU_vertformat_attr_add(format, "pos", GPU_COMP_F32, 2, GPU_FETCH_FLOAT);
+  uint pos = gpu_vertformat_attr_add(format, "pos", GPU_COMP_F32, 2, GPU_FETCH_FLOAT);
 
   hou_blend(GPU_BLEND_ALPHA);
   immBindBuiltinProgram(GPU_SHADER_3D_UNIFORM_COLOR);
@@ -125,10 +125,10 @@ static void draw_current_frame(const Scene *scene,
   uchar txt_color[4];
   ui_GetThemeColor4ubv(TH_HEADER_TXT_HI, txt_color);
   ui_fontstyle_drw_simple(fstyle,
-                           frame_x - txt_width / 2 + U.pixelsize / 2,
-                           get_centered_txt_y(scrub_rgn_rect),
-                           frame_str,
-                           txt_color);
+                          frame_x - txt_width / 2 + U.pixelsize / 2,
+                          get_centered_txt_y(scrub_rgn_rect),
+                          frame_str,
+                          txt_color);
 }
 
 void ed_time_scrub_drw_current_frame(const ARgn *rgn,
@@ -205,9 +205,9 @@ void ed_time_scrub_channel_search_drw(const Cxt *C, ARgn *rgn, DopeSheet *dopesh
   const float padding_x = 2 * UI_SCALE_FAC;
   const float padding_y = UI_SCALE_FAC;
 
-  uiBlock *block = ui_block_begin(C, region, __func__, UI_EMBOSS);
+  uiBlock *block = ui_block_begin(C, rgn, __func__, UI_EMBOSS);
   uiLayout *layout = ui_block_layout(block,
-                                     UI_LAYOUT_VERTICAL,
+                                     UI_LAYOUT_VERT,
                                      UI_LAYOUT_HEADER,
                                      rect.xmin + padding_x,
                                      rect.ymin + UI_UNIT_Y + padding_y,
@@ -223,12 +223,12 @@ void ed_time_scrub_channel_search_drw(const Cxt *C, ARgn *rgn, DopeSheet *dopesh
   ui_block_align_end(block);
   ui_block_layout_resolve(block, nullptr, nullptr);
 
-  /* Make sure the events are consumed from the search and don't reach other UI blocks since this
-   * is drawn on top of animation-channels. */
-  UI_block_flag_enable(block, UI_BLOCK_CLIP_EVENTS);
-  UI_block_bounds_set_normal(block, 0);
-  UI_block_end(C, block);
-  UI_block_draw(C, block);
+  /* Make sure the evs are consumed from the search and don't reach other UI blocks since this
+   * is drwn on top of animation-channels. */
+  ui_block_flag_enable(block, UI_BLOCK_CLIP_EVENTS);
+  ui_block_bounds_set_normal(block, 0);
+  ui_block_end(C, block);
+  ui_block_drw(C, block);
 
-  GPU_matrix_pop_projection();
+  gpu_matrix_pop_projection();
 }
