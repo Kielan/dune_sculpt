e2821f49af6d590aeee3bb1833fc1db987c0f6f7
Kielan <ki.lemons@gmail.com>
2023-12-09 04:25:17 -0800

    Update pose_utils.cc

--- source/dune/editors/armature/pose_utils.cc
+++ source/dune/editors/armature/pose_utils.cc
@@ -1,57 +1,53 @@
-#include "MEM_guardedalloc.h"
+#include "mem_guardedalloc.h"
 
-#include "BLI_blenlib.h"
-#include "BLI_math_rotation.h"
-#include "BLI_math_vector.h"
+#include "lib_dunelib.h"
+#include "lib_math_rotation.h"
+#include "lib_math_vector.h"
 
-#include "DNA_anim_types.h"
-#include "DNA_armature_types.h"
-#include "DNA_object_types.h"
-#include "DNA_scene_types.h"
+#include "types_anim.h"
+#include "types_armature.h"
+#include "types_ob.h"
+#include "types_scene.h"
 
-#include "BKE_action.h"
-#include "BKE_anim_data.h"
-#include "BKE_idprop.h"
-#include "BKE_layer.h"
-#include "BKE_object.hh"
+#include "dune_action.h"
+#include "dune_anim_data.h"
+#include "dune_idprop.h"
+#include "dune_layer.h"
+#include "dune_ob.hh"
 
-#include "BKE_context.hh"
+#include "dune_cxt.hh"
 
-#include "DEG_depsgraph.hh"
+#include "graph.hh"
 
-#include "RNA_access.hh"
-#include "RNA_path.hh"
-#include "RNA_prototypes.h"
+#include "api_access.hh"
+#include "api_path.hh"
+#include "api_prototypes.h"
 
-#include "WM_api.hh"
-#include "WM_types.hh"
+#include "win_api.hh"
+#include "win_types.hh"
 
-#include "ED_armature.hh"
-#include "ED_keyframing.hh"
+#include "ed_armature.hh"
+#include "ed_keyframing.hh"
 
-#include "ANIM_keyframing.hh"
+#include "anim_keyframing.hh"
 
 #include "armature_intern.h"
 
-/* *********************************************** */
 /* Contents of this File:
- *
  * This file contains methods shared between Pose Slide and Pose Lib;
- * primarily the functions in question concern Animato <-> Pose
- * convenience functions, such as applying/getting pose values
- * and/or inserting keyframes for these.
- */
-/* *********************************************** */
-/* FCurves <-> PoseChannels Links */
+ * primarily the fns in question concern Anim <-> Pose
+ * convenience fns, such as applying/getting pose vals
+ * and/or inserting keyframes for these */
 
+/* FCurves <-> PoseChannels Links */
 /* helper for poseAnim_mapping_get() -> get the relevant F-Curves per PoseChannel */
-static void fcurves_to_pchan_links_get(ListBase *pfLinks,
-                                       Object *ob,
-                                       bAction *act,
-                                       bPoseChannel *pchan)
+static void fcurves_to_pchan_links_get(List *pfLinks,
+                                       Ob *ob,
+                                       Action *act,
+                                       PoseChannel *pchan)
 {
-  ListBase curves = {nullptr, nullptr};
-  const eAction_TransformFlags transFlags = BKE_action_get_item_transform_flags(
+  List curves = {nullptr, nullptr};
+  const eActionTransformFlags transFlags = dune_action_get_item_transform_flags(
       act, ob, pchan, &curves);
 
   pchan->flag &= ~(POSE_LOC | POSE_ROT | POSE_SIZE | POSE_BBONE_SHAPE);
@@ -60,18 +56,18 @@ static void fcurves_to_pchan_links_get(ListBase *pfLinks,
   if (transFlags) {
     /* make new linkage data */
     tPChanFCurveLink *pfl = static_cast<tPChanFCurveLink *>(
-        MEM_callocN(sizeof(tPChanFCurveLink), "tPChanFCurveLink"));
+        mem_calloc(sizeof(tPChanFCurveLink), "tPChanFCurveLink"));
 
     pfl->ob = ob;
     pfl->fcurves = curves;
     pfl->pchan = pchan;
 
-    /* get the RNA path to this pchan - this needs to be freed! */
-    PointerRNA ptr = RNA_pointer_create((ID *)ob, &RNA_PoseBone, pchan);
-    pfl->pchan_path = RNA_path_from_ID_to_struct(&ptr);
+    /* get the api path to this pchan this needs to be freed! */
+    ApiPtr ptr = api_ptr_create((Id *)ob, &ApiPoseBone, pchan);
+    pfl->pchan_path = api_path_from_id_to_struct(&ptr);
 
-    /* add linkage data to operator data */
-    BLI_addtail(pfLinks, pfl);
+    /* add linkage data to op data */
+    lib_addtail(pfLinks, pfl);
 
     /* set pchan's transform flags */
     if (transFlags & ACT_TRANS_LOC) {
@@ -95,7 +91,7 @@ static void fcurves_to_pchan_links_get(ListBase *pfLinks,
     copy_v3_v3(pfl->oldaxis, pchan->rotAxis);
     pfl->oldangle = pchan->rotAngle;
 
-    /* store current bbone values */
+    /* store current bbone vals */
     pfl->roll1 = pchan->roll1;
     pfl->roll2 = pchan->roll2;
     pfl->curve_in_x = pchan->curve_in_x;
@@ -108,35 +104,34 @@ static void fcurves_to_pchan_links_get(ListBase *pfLinks,
     copy_v3_v3(pfl->scale_in, pchan->scale_in);
     copy_v3_v3(pfl->scale_out, pchan->scale_out);
 
-    /* make copy of custom properties */
+    /* make copy of custom props */
     if (pchan->prop && (transFlags & ACT_TRANS_PROP)) {
-      pfl->oldprops = IDP_CopyProperty(pchan->prop);
+      pfl->oldprops = IDPCopyProp(pchan->prop);
     }
   }
 }
 
-Object *poseAnim_object_get(Object *ob_)
+Ob *poseAnim_ob_get(Ob *ob_)
 {
-  Object *ob = BKE_object_pose_armature_get(ob_);
+  Ob *ob = dune_ob_pose_armature_get(ob_);
   if (!ELEM(nullptr, ob, ob->data, ob->adt, ob->adt->action)) {
     return ob;
   }
   return nullptr;
 }
 
-void poseAnim_mapping_get(bContext *C, ListBase *pfLinks)
+void poseAnim_mapping_get(Cxt *C, List *pfLinks)
 {
   /* for each Pose-Channel which gets affected, get the F-Curves for that channel
-   * and set the relevant transform flags...
-   */
-  Object *prev_ob, *ob_pose_armature;
+   * and set the relevant transform flags... */
+  Ob *prev_ob, *ob_pose_armature;
 
   prev_ob = nullptr;
   ob_pose_armature = nullptr;
-  CTX_DATA_BEGIN_WITH_ID (C, bPoseChannel *, pchan, selected_pose_bones, Object *, ob) {
+  CXT_DATA_BEGIN_WITH_ID (C, PoseChannel *, pchan, sel_pose_bones, Ob *, ob) {
     if (ob != prev_ob) {
       prev_ob = ob;
-      ob_pose_armature = poseAnim_object_get(ob);
+      ob_pose_armature = poseAnim_ob_get(ob);
     }
 
     if (ob_pose_armature == nullptr) {
@@ -145,18 +140,17 @@ void poseAnim_mapping_get(bContext *C, ListBase *pfLinks)
 
     fcurves_to_pchan_links_get(pfLinks, ob_pose_armature, ob_pose_armature->adt->action, pchan);
   }
-  CTX_DATA_END;
+  CXT_DATA_END;
 
   /* if no PoseChannels were found, try a second pass, doing visible ones instead
-   * i.e. if nothing selected, do whole pose
-   */
-  if (BLI_listbase_is_empty(pfLinks)) {
+   * i.e. if nothing sel, do whole pose */
+  if (lib_list_is_empty(pfLinks)) {
     prev_ob = nullptr;
     ob_pose_armature = nullptr;
-    CTX_DATA_BEGIN_WITH_ID (C, bPoseChannel *, pchan, visible_pose_bones, Object *, ob) {
+    CXT_DATA_BEGIN_WITH_ID (C, PoseChannel *, pchan, visible_pose_bones, Ob *, ob) {
       if (ob != prev_ob) {
         prev_ob = ob;
-        ob_pose_armature = poseAnim_object_get(ob);
+        ob_pose_armature = poseAnim_ob_get(ob);
       }
 
       if (ob_pose_armature == nullptr) {
@@ -165,35 +159,35 @@ void poseAnim_mapping_get(bContext *C, ListBase *pfLinks)
 
       fcurves_to_pchan_links_get(pfLinks, ob_pose_armature, ob_pose_armature->adt->action, pchan);
     }
-    CTX_DATA_END;
+    CXT_DATA_END;
   }
 }
 
-void poseAnim_mapping_free(ListBase *pfLinks)
+void poseAnim_mapping_free(List *pfLinks)
 {
   tPChanFCurveLink *pfl, *pfln = nullptr;
 
-  /* free the temp pchan links and their data */
+  /* free the tmp pchan links and their data */
   for (pfl = static_cast<tPChanFCurveLink *>(pfLinks->first); pfl; pfl = pfln) {
     pfln = pfl->next;
 
-    /* free custom properties */
+    /* free custom props */
     if (pfl->oldprops) {
-      IDP_FreeProperty(pfl->oldprops);
+      IDP_FreeProp(pfl->oldprops);
     }
 
-    /* free list of F-Curve reference links */
-    BLI_freelistN(&pfl->fcurves);
+    /* free list of F-Curve ref links */
+    lib_freelist(&pfl->fcurves);
 
-    /* free pchan RNA Path */
-    MEM_freeN(pfl->pchan_path);
+    /* free pchan api Path */
+    mem_free(pfl->pchan_path);
 
     /* free link itself */
-    BLI_freelinkN(pfLinks, pfl);
+    lib_freelink(pfLinks, pfl);
   }
 }
 
-void poseAnim_mapping_refresh(Cxt *C, Scene * /*scene*/, Object *ob)
+void poseAnim_mapping_refresh(Cxt *C, Scene * /*scene*/, Ob *ob)
 {
   graph_id_tag_update(&ob->id, ID_RECALC_GEOMETRY);
   win_ev_add_notifier(C, NC_OB | ND_POSE, ob);
@@ -206,7 +200,7 @@ void poseAnim_mapping_refresh(Cxt *C, Scene * /*scene*/, Object *ob)
 
 void poseAnim_mapping_reset(List *pfLinks)
 {
-  /* iter over each pose-channel affected, restoring all channels to their original values */
+  /* iter over each pose-channel affected, restoring all channels to their original vals */
   LIST_FOREACH (tPChanFCurveLink *, pfl, pfLinks) {
     PoseChannel *pchan = pfl->pchan;
 
@@ -231,7 +225,7 @@ void poseAnim_mapping_reset(List *pfLinks)
     copy_v3_v3(pchan->scale_in, pfl->scale_in);
     copy_v3_v3(pchan->scale_out, pfl->scale_out);
 
-    /* just overwrite values of props from the stored copies (there should be some) */
+    /* just overwrite vals of props from the stored copies (there should be some) */
     if (pfl->oldprops) {
       IDP_SyncGroupVals(pfl->pchan->prop, pfl->oldprops);
     }
@@ -248,7 +242,7 @@ void poseAnim_mapping_autoKeyframe(Cxt *C, Scene *scene, List *pfLinks, float cf
     ob->id.tag &= ~LIB_TAG_DOIT;
     ob = poseAnim_ob_get(ob);
 
-    /* Ensure validity of the settings from the context. */
+    /* Ensure validity of the settings from the cxt. */
     if (ob == nullptr) {
       continue;
     }
@@ -269,7 +263,7 @@ void poseAnim_mapping_autoKeyframe(Cxt *C, Scene *scene, List *pfLinks, float cf
   dune::Vector<ApiPtr> sources;
 
   /* iter over each pose-channel affected, tagging bones to be keyed */
-  /* Here we altdy have the info about what transforms exist, though
+  /* Here we alrdy have the info about what transforms exist, though
    * it might be easier to just overwrite all using normal mechanisms  */
   LIST_FOREACH (tPChanFCurveLink *, pfl, pfLinks) {
     PoseChannel *pchan = pfl->pchan;
@@ -278,7 +272,7 @@ void poseAnim_mapping_autoKeyframe(Cxt *C, Scene *scene, List *pfLinks, float cf
       continue;
     }
 
-    /* Add data-source override for the PoseChannel, to be used later. */
+    /* Add data-src override for the PoseChannel, to be used later. */
     anim_relative_keyingset_add_source(sources, &pfl->ob->id, &ApiPoseBone, pchan);
   }
 
@@ -291,7 +285,7 @@ void poseAnim_mapping_autoKeyframe(Cxt *C, Scene *scene, List *pfLinks, float cf
   FOREACH_OB_IN_MODE_BEGIN (scene, view_layer, v3d, OB_ARMATURE, OB_MODE_POSE, ob) {
     if (ob->id.tag & LIB_TAG_DOIT) {
       if (ob->pose->avs.path_bakeflag & MOTIONPATH_BAKE_HAS_PATHS) {
-        // ed_pose_clear_paths(C, ob); /* XXX for now, don't need to clear. */
+        // ed_pose_clear_paths(C, ob); /* for nowno need to clear. */
         /* TODO: Should ensure we can use more narrow update range here. */
         ed_pose_recalc_paths(C, scene, ob, POSE_PATH_CALC_RANGE_FULL);
       }
