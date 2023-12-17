af1d21716e77e65bee52c019e4423d5cbdc3d334
Kielan <ki.lemons@gmail.com>
2023-12-16 23:24:53 -0800

    Update keyframes_keylist_test.cc

--- source/dune/editors/animation/keyframes_keylist_test.cc
+++ source/dune/editors/animation/keyframes_keylist_test.cc
@@ -1,20 +1,20 @@
 #include "testing/testing.h"
 
-#include "BLI_utildefines.h"
+#include "lib_utildefines.h"
 
-#include "ED_keyframes_keylist.hh"
+#include "ed_keyframes_keylist.hh"
 
-#include "DNA_anim_types.h"
-#include "DNA_curve_types.h"
+#include "types_anim.h"
+#include "types_curve.h"
 
-#include "MEM_guardedalloc.h"
+#include "mem_guardedalloc.h"
 
-#include "BKE_fcurve.h"
+#include "dune_fcurve.h"
 
 #include <functional>
 #include <optional>
 
-namespace blender::editor::animation::tests {
+namespace dune::editor::animation::tests {
 
 const float KEYLIST_NEAR_ERROR = 0.1;
 const float FRAME_STEP = 0.005;
@@ -24,7 +24,7 @@ static void build_fcurve(FCurve &fcurve)
 {
   fcurve.totvert = 3;
   fcurve.bezt = static_cast<BezTriple *>(
-      MEM_callocN(sizeof(BezTriple) * fcurve.totvert, "BezTriples"));
+      mem_calloc(sizeof(BezTriple) * fcurve.totvert, "BezTriples"));
   fcurve.bezt[0].vec[1][0] = 10.0f;
   fcurve.bezt[0].vec[1][1] = 1.0f;
   fcurve.bezt[1].vec[1][0] = 20.0f;
@@ -35,21 +35,21 @@ static void build_fcurve(FCurve &fcurve)
 
 static AnimKeylist *create_test_keylist()
 {
-  FCurve *fcurve = BKE_fcurve_create();
+  FCurve *fcurve = dune_fcurve_create();
   build_fcurve(*fcurve);
 
-  AnimKeylist *keylist = ED_keylist_create();
+  AnimKeylist *keylist = ed_keylist_create();
   fcurve_to_keylist(nullptr, fcurve, keylist, 0, {-FLT_MAX, FLT_MAX});
-  BKE_fcurve_free(fcurve);
+  dune_fcurve_free(fcurve);
 
-  ED_keylist_prepare_for_direct_access(keylist);
+  ed_keylist_prepare_for_direct_access(keylist);
   return keylist;
 }
 
 static void assert_act_key_column(const ActKeyColumn *column,
                                   const std::optional<float> expected_frame)
 {
-  if (expected_frame.has_value()) {
+  if (expected_frame.has_val()) {
     ASSERT_NE(column, nullptr) << "Expected a frame to be found at " << *expected_frame;
     EXPECT_NEAR(column->cfra, *expected_frame, KEYLIST_NEAR_ERROR);
   }
@@ -58,17 +58,17 @@ static void assert_act_key_column(const ActKeyColumn *column,
   }
 }
 
-using KeylistFindFunction = std::function<const ActKeyColumn *(const AnimKeylist *, float)>;
+using KeylistFindFn = std::function<const ActKeyColumn *(const AnimKeylist *, float)>;
 
 static void check_keylist_find_range(const AnimKeylist *keylist,
-                                     KeylistFindFunction keylist_find_func,
+                                     KeylistFindFn keylist_find_fn,
                                      const float frame_from,
                                      const float frame_to,
                                      const std::optional<float> expected_frame)
 {
   float cfra = frame_from;
   for (; cfra < frame_to; cfra += FRAME_STEP) {
-    const ActKeyColumn *found = keylist_find_func(keylist, cfra);
+    const ActKeyColumn *found = keylist_find_fn(keylist, cfra);
     assert_act_key_column(found, expected_frame);
   }
 }
@@ -78,7 +78,7 @@ static void check_keylist_find_next_range(const AnimKeylist *keylist,
                                           const float frame_to,
                                           const std::optional<float> expected_frame)
 {
-  check_keylist_find_range(keylist, ED_keylist_find_next, frame_from, frame_to, expected_frame);
+  check_keylist_find_range(keylist, ed_keylist_find_next, frame_from, frame_to, expected_frame);
 }
 
 TEST(keylist, find_next)
@@ -110,7 +110,7 @@ TEST(keylist, find_prev)
   check_keylist_find_prev_range(keylist, 20.01f, 30.00f, 20.0f);
   check_keylist_find_prev_range(keylist, 30.01f, 49.99f, 30.0f);
 
-  ED_keylist_free(keylist);
+  ed_keylist_free(keylist);
 }
 
 static void check_keylist_find_exact_range(const AnimKeylist *keylist,
