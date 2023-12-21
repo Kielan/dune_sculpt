afcf74e74bd7e3ce82006ec64c8d763d876915de
Kielan <ki.lemons@gmail.com>
2023-12-21 04:00:23 -0800

    Update types_fileglobal.h

--- source/dune/types/types_fileglobal.h
+++ source/dune/types/types_fileglobal.h
@@ -31,7 +31,7 @@ typedef struct FileGlobal {
 /* minversion: in file, the oldest past dune version you can use compliant */
 /* example: if in 2.43 the meshes lose mesh data, minversion is 2.43 then too */
 /* or: in 2.42, subversion 1, same as above, minversion then is 2.42, min subversion 1 */
-/* (defines for version are in the dune_dune_version.h file, for historic reasons) */
+/* (defines for version are in the dune_version.h file, for historic reasons) */
 
 #ifdef __cplusplus
 }
