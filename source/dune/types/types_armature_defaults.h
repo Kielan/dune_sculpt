c55b965821a7df9bddcffe6f10a0e51a50808396
Kielan <ki.lemons@gmail.com>
2023-12-21 02:14:04 -0800

    Update types_armature_defaults.h

--- source/dune/types/types_armature_defaults.h
+++ source/dune/types/types_armature_defaults.h
@@ -3,13 +3,11 @@
 /* Struct members on own line. */
 /* clang-format off */
 
-/* -------------------------------------------------------------------- */
-/** Armature Struct **/
-
+/* Armature Struct */
 #define _TYPES_DEFAULT_Armature \
   { \
     .deformflag = ARM_DEF_VGROUP | ARM_DEF_ENVELOPE, \
     .flag = ARM_COL_CUSTOM,  /* custom bone-group colors */ \
     .layer = 1, \
-    .drawtype = ARM_OCTA, \
+    .drwtype = ARM_OCTA, \
   }
