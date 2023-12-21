698f20a0f0b17ce95d3461613239e5db32c6a200
Kielan <ki.lemons@gmail.com>
2023-12-20 21:13:40 -0800

    Update overlay_sculpt.c

--- source/dune/draw/overlay_sculpt.c
+++ source/dune/draw/overlay_sculpt.c
@@ -1,6 +1,6 @@
-#include "draw_render.h"
+#include "drw_render.h"
 
-#include "draw_cache_impl.h"
+#include "drw_cache_impl.h"
 #include "overlay_private.h"
 
 #include "dune_paint.h"
@@ -9,28 +9,28 @@
 
 void overlay_sculpt_cache_init(OverlayData *vedata)
 {
-  OVERLAY_PassList *psl = vedata->psl;
-  OVERLAY_PrivateData *pd = vedata->stl->pd;
-  DRWShadingGroup *grp;
+  OverlayPassList *psl = vedata->psl;
+  OverlayPrivateData *pd = vedata->stl->pd;
+  DrwShadingGroup *grp;
 
-  DRWState state = DRW_STATE_WRITE_COLOR | DRW_STATE_DEPTH_EQUAL | DRW_STATE_BLEND_MUL;
+  DrwState state = DRW_STATE_WRITE_COLOR | DRW_STATE_DEPTH_EQUAL | DRW_STATE_BLEND_MUL;
   DRW_PASS_CREATE(psl->sculpt_mask_ps, state | pd->clipping_state);
 
-  GPUShader *sh = OVERLAY_shader_sculpt_mask();
-  pd->sculpt_mask_grp = grp = DRW_shgroup_create(sh, psl->sculpt_mask_ps);
-  DRW_shgroup_uniform_float_copy(grp, "maskOpacity", pd->overlay.sculpt_mode_mask_opacity);
-  DRW_shgroup_uniform_float_copy(
+  GPUShader *sh = overlay_shader_sculpt_mask();
+  pd->sculpt_mask_grp = grp = drw_shgroup_create(sh, psl->sculpt_mask_ps);
+  drw_shgroup_uniform_float_copy(grp, "maskOpacity", pd->overlay.sculpt_mode_mask_opacity);
+  drw_shgroup_uniform_float_copy(
       grp, "faceSetsOpacity", pd->overlay.sculpt_mode_face_sets_opacity);
 }
 
-void overlay_sculpt_cache_populate(OverlayData *vedata, Object *ob)
+void overlay_sculpt_cache_populate(OverlayData *vedata, Ob *ob)
 {
   OverlayPrivateData *pd = vedata->stl->pd;
-  const DrawCtxState *draw_ctx = draw_ctx_state_get();
+  const DrwCxtState *drw_cxt = drw_cxt_state_get();
   struct GPUBatch *sculpt_overlays;
   PBVH *pbvh = ob->sculpt->pbvh;
 
-  const bool use_pbvh = dune_sculptsession_use_pbvh_draw(ob, draw_ctx->v3d);
+  const bool use_pbvh = dune_sculptsession_use_pbvh_drw(ob, drw_cxt->v3d);
 
   if (!pbvh) {
     /* It is possible to have SculptSession without PBVH. This happens, for example, when toggling
@@ -46,25 +46,25 @@ void overlay_sculpt_cache_populate(OverlayData *vedata, Object *ob)
   }
 
   if (use_pbvh) {
-    draw_shgroup_call_sculpt(pd->sculpt_mask_grp, ob, false, true);
+    drw_shgroup_call_sculpt(pd->sculpt_mask_grp, ob, false, true);
   }
   else {
-    sculpt_overlays = draw_mesh_batch_cache_get_sculpt_overlays(ob->data);
+    sculpt_overlays = drw_mesh_batch_cache_get_sculpt_overlays(ob->data);
     if (sculpt_overlays) {
-      draw_shgroup_call(pd->sculpt_mask_grp, sculpt_overlays, ob);
+      drw_shgroup_call(pd->sculpt_mask_grp, sculpt_overlays, ob);
     }
   }
 }
 
-void overlay_sculpt_draw(OverlayData *vedata)
+void overlay_sculpt_drw(OverlayData *vedata)
 {
   OverlayPassList *psl = vedata->psl;
   OverlatPrivateData *pd = vedata->stl->pd;
-  DefaultFramebufferList *dfbl = draw_viewport_framebuffer_list_get();
+  DefaultFramebufList *dfbl = drw_viewport_framebuf_list_get();
 
-  if (draw_state_is_fbo()) {
-    gpu_framebuffer_bind(pd->painting.in_front ? dfbl->in_front_fb : dfbl->default_fb);
+  if (drw_state_is_fbo()) {
+    gpu_framebuf_bind(pd->painting.in_front ? dfbl->in_front_fb : dfbl->default_fb);
   }
 
-  draw_pass(psl->sculpt_mask_ps);
+  drw_pass(psl->sculpt_mask_ps);
 }
