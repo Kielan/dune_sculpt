84a4dd844d8fd4feed196153304896672694efbc
Kielan <ki.lemons@gmail.com>
2023-12-21 02:13:38 -0800

    Update types_armature.h

--- source/dune/types/types_armature.h
+++ source/dune/types/types_armature.h
@@ -10,19 +10,18 @@ extern "C" {
 
 struct AnimData;
 
-/* this system works on different transformation space levels;
- *
+/* this sys works on different transformation space levels;
  * 1) Bone Space;      with each Bone having own (0,0,0) origin
- * 2) Armature Space;  the rest position, in Object space, Bones Spaces are applied hierarchical
- * 3) Pose Space;      the animation position, in Object space
+ * 2) Armature Space;  the rest position, in Ob space, Bones Spaces are applied hierarchical
+ * 3) Pose Space;      the animation position, in Ob space
  * 4) World Space;     Object matrix applied to Pose or Armature space */
 
 typedef struct Bone {
-  /** Next/previous elements within this list. */
+  /* Next/prev elements within this list. */
   struct Bone *next, *prev;
   /* User-Defined Properties on this Bone. */
   IdProp *prop;
-  /** Parent (IK parent if appropriate flag is set). */
+  /* Parent (IK parent if appropriate flag is set). */
   struct Bone *parent;
   /* Children. */
   List childbase;
@@ -113,12 +112,12 @@ typedef struct Armature {
   /* Active edit-bone (in edit-mode). */
   struct EditBone *act_edbone;
 
-  /* ID data is older than edit-mode data (TODO: move to edit-mode struct). */
+  /* Id data is older than edit-mode data (TODO: move to edit-mode struct). */
   char needs_flush_to_id;
   char _pad0[3];
 
   int flag;
-  int drawtype;
+  int drwtype;
 
   short deformflag;
   short pathflag;
@@ -134,7 +133,7 @@ typedef struct Armature {
 
 /* armature->flag */
 /* don't use bit 7, was saved in files to disable stuff */
-typedef enum eArmature_Flag {
+typedef enum eArmatureFlag {
   ARM_RESTPOS = (1 << 0),
   /** XRAY is here only for backwards converting */
   ARM_FLAG_UNUSED_1 = (1 << 1), /* cleared */
@@ -146,108 +145,108 @@ typedef enum eArmature_Flag {
   ARM_FLAG_UNUSED_7 = (1 << 7),
   ARM_MIRROR_EDIT = (1 << 8),
   ARM_FLAG_UNUSED_9 = (1 << 9),
-  /** Made option negative, for backwards compatibility. */
+  /* Made option negative, for backwards compatibility. */
   ARM_NO_CUSTOM = (1 << 10),
-  /** Draw custom colors. */
+  /* Drw custom colors. */
   ARM_COL_CUSTOM = (1 << 11),
-  /** When ghosting, only show selected bones (this should belong to ghostflag instead). */
+  /* When ghosting, only show selected bones (this should belong to ghostflag instead). */
   ARM_FLAG_UNUSED_12 = (1 << 12), /* cleared */
-  /** Dope-sheet channel is expanded */
+  /* Dope-sheet channel is expanded */
   ARM_DS_EXPAND = (1 << 13),
-  /** Other objects are used for visualizing various states (hack for efficient updates). */
+  /* Other obs are used for visualizing various states (hack for efficient updates). */
   ARM_HAS_VIZ_DEPS = (1 << 14),
-} eArmature_Flag;
+} eArmatureFlag;
 
-/* armature->drawtype */
-typedef enum eArmature_Drawtype {
+/* armature->drwtype */
+typedef enum eArmatureDrwtype {
   ARM_OCTA = 0,
   ARM_LINE = 1,
   ARM_B_BONE = 2,
   ARM_ENVELOPE = 3,
   ARM_WIRE = 4,
-} eArmature_Drawtype;
+} eArmatureDrwtype;
 
 /* armature->deformflag */
-typedef enum eArmature_DeformFlag {
+typedef enum eArmatureDeformFlag {
   ARM_DEF_VGROUP = (1 << 0),
   ARM_DEF_ENVELOPE = (1 << 1),
   ARM_DEF_QUATERNION = (1 << 2),
-#ifdef DNA_DEPRECATED_ALLOW
+#ifdef TYPES_DEPRECATED_ALLOW
   ARM_DEF_B_BONE_REST = (1 << 3), /* deprecated */
 #endif
   ARM_DEF_INVERT_VGROUP = (1 << 4),
-} eArmature_DeformFlag;
+} eArmatureDeformFlag;
 
 #ifdef TYPES_DEPRECATED_ALLOW /* Old animation system (armature only viz). */
-/** Armature.pathflag */
-typedef enum eArmature_PathFlag {
+/* Armature.pathflag */
+typedef enum eArmaturePathFlag {
   ARM_PATH_FNUMS = (1 << 0),
   ARM_PATH_KFRAS = (1 << 1),
   ARM_PATH_HEADS = (1 << 2),
   ARM_PATH_ACFRA = (1 << 3),
   ARM_PATH_KFNOS = (1 << 4),
-} eArmature_PathFlag;
+} eArmaturePathFlag;
 #endif
 
 /* bone->flag */
-typedef enum eBone_Flag {
+typedef enum eBoneFlag {
   BONE_SELECTED = (1 << 0),
   BONE_ROOTSEL = (1 << 1),
   BONE_TIPSEL = (1 << 2),
-  /* Used instead of BONE_SELECTED during transform (clear before use) */
+  /* Used instead of BONE_SEL during transform (clear before use) */
   BONE_TRANSFORM = (1 << 3),
-  /** When bone has a parent, connect head of bone to parent's tail. */
+  /* When bone has a parent, connect head of bone to parent's tail. */
   BONE_CONNECTED = (1 << 4),
   /* 32 used to be quatrot, was always set in files, do not reuse unless you clear it always */
-  /** hidden Bones when drawing PoseChannels */
+  /* hidden Bones when drawing PoseChannels */
   BONE_HIDDEN_P = (1 << 6),
-  /** For detecting cyclic dependencies */
+  /* For detecting cyclic dependencies */
   BONE_DONE = (1 << 7),
-  /** active is on mouse clicks only - deprecated, ONLY USE FOR DRAWING */
-  BONE_DRAW_ACTIVE = (1 << 8),
-  /** No parent rotation or scale */
+  /* active is on mouse clicks only - deprecated, ONLY USE FOR DRAWING */
+  BONE_DRW_ACTIVE = (1 << 8),
+  /* No parent rotation or scale */
   BONE_HINGE = (1 << 9),
-  /** hidden Bones when drawing Armature Editmode */
+  /* hidden Bones when drawing Armature Editmode */
   BONE_HIDDEN_A = (1 << 10),
   /* multiplies vgroup with envelope */
   BONE_MULT_VG_ENV = (1 << 11),
   /* bone doesn't deform geometry */
   BONE_NO_DEFORM = (1 << 12),
-#ifdef DNA_DEPRECATED_ALLOW
+#ifdef TYPES_DEPRECATED_ALLOW
   /* set to prevent destruction of its unkeyframed pose (after transform) */
   BONE_UNKEYED = (1 << 13),
 #endif
   /* set to prevent hinge child bones from influencing the transform center */
   BONE_HINGE_CHILD_TRANSFORM = (1 << 14),
-#ifdef DNA_DEPRECATED_ALLOW
-  /** No parent scale */
+#ifdef TYPES_DEPRECATED_ALLOW
+  /* No parent scale */
   BONE_NO_SCALE = (1 << 15),
 #endif
-  /** hidden bone when drawing PoseChannels (for ghost drawing) */
+  /* hidden bone when drawing PoseChannels (for ghost drawing) */
   BONE_HIDDEN_PG = (1 << 16),
-  /** bone should be drawn as OB_WIRE, regardless of draw-types of view+armature */
-  BONE_DRAWWIRE = (1 << 17),
-  /** when no parent, bone will not get cyclic offset */
+  /* bone should be drawn as OB_WIRE, regardless of draw-types of view+armature */
+  BONE_DRWWIRE = (1 << 17),
+  /* when no parent, bone will not get cyclic offset */
   BONE_NO_CYCLICOFFSET = (1 << 18),
-  /** bone transforms are locked in EditMode */
+  /* bone transforms are locked in EditMode */
   BONE_EDITMODE_LOCKED = (1 << 19),
-  /** Indicates that a parent is also being transformed */
+  /* Indicates that a parent is also being transformed */
   BONE_TRANSFORM_CHILD = (1 << 20),
-  /** bone cannot be selected */
-  BONE_UNSELECTABLE = (1 << 21),
-  /** bone location is in armature space */
+  /* bone cannot be sel */
+  BONE_UNSELECTABLR = (1 << 21),
+  /* bone location is in armature space */
   BONE_NO_LOCAL_LOCATION = (1 << 22),
-  /** object child will use relative transform (like deform) */
+  /* ob child will use relative transform (like deform) */
   BONE_RELATIVE_PARENTING = (1 << 23),
-#ifdef DNA_DEPRECATED_ALLOW
+#ifdef TYPES_DEPRECATED_ALLOW
   /** it will add the parent end roll to the inroll */
   BONE_ADD_PARENT_END_ROLL = (1 << 24),
 #endif
-  /** this bone was transformed by the mirror function */
+  /* this bone was transformed by the mirror function */
   BONE_TRANSFORM_MIRROR = (1 << 25),
-  /** this bone is associated with a locked vertex group, ONLY USE FOR DRAWING */
-  BONE_DRAW_LOCKED_WEIGHT = (1 << 26),
-} eBone_Flag;
+  /* this bone is associated with a locked vertex group, ONLY USE FOR DRAWING */
+  BONE_DRW_LOCKED_WEIGHT = (1 << 26),
+} eBoneFlag;
 
 /* bone->inherit_scale_mode */
 typedef enum eBone_InheritScaleMode {
@@ -255,46 +254,46 @@ typedef enum eBone_InheritScaleMode {
   BONE_INHERIT_SCALE_FULL = 0,
   /* Inherit scale, but remove final shear. */
   BONE_INHERIT_SCALE_FIX_SHEAR = 1,
-  /* Inherit average scale. */
-  BONE_INHERIT_SCALE_AVERAGE = 2,
+  /* Inherit avg scale. */
+  BONE_INHERIT_SCALE_AVG = 2,
   /* Inherit no scale or shear. */
   BONE_INHERIT_SCALE_NONE = 3,
   /* Inherit effects of shear on parent (same as old disabled Inherit Scale). */
   BONE_INHERIT_SCALE_NONE_LEGACY = 4,
   /* Inherit parent X scale as child X scale etc. */
   BONE_INHERIT_SCALE_ALIGNED = 5,
-} eBone_InheritScaleMode;
+} eBoneInheritScaleMode;
 
 /* bone->bbone_prev_type, bbone_next_type */
-typedef enum eBone_BBoneHandleType {
+typedef enum eBoneBBoneHandleType {
   BBONE_HANDLE_AUTO = 0,     /* Default mode based on parents & children. */
   BBONE_HANDLE_ABSOLUTE = 1, /* Custom handle in absolute position mode. */
   BBONE_HANDLE_RELATIVE = 2, /* Custom handle in relative position mode. */
   BBONE_HANDLE_TANGENT = 3,  /* Custom handle in tangent mode (use direction, not location). */
-} eBone_BBoneHandleType;
+} eBoneBBoneHandleType;
 
 /* bone->bbone_flag */
-typedef enum eBone_BBoneFlag {
+typedef enum eBoneBBoneFlag {
   /** Add the parent Out roll to the In roll. */
   BBONE_ADD_PARENT_END_ROLL = (1 << 0),
   /** Multiply B-Bone easing values with Scale Length. */
   BBONE_SCALE_EASING = (1 << 1),
-} eBone_BBoneFlag;
+} eBoneBBoneFlag;
 
 /* bone->bbone_prev/next_flag */
-typedef enum eBone_BBoneHandleFlag {
+typedef enum eBoneBBoneHandleFlag {
   /* Use handle bone scaling for scale X. */
   BBONE_HANDLE_SCALE_X = (1 << 0),
   /* Use handle bone scaling for scale Y (length). */
   BBONE_HANDLE_SCALE_Y = (1 << 1),
-  /** Use handle bone scaling for scale Z. */
+  /* Use handle bone scaling for scale Z. */
   BBONE_HANDLE_SCALE_Z = (1 << 2),
-  /** Use handle bone scaling for easing. */
+  /* Use handle bone scaling for easing. */
   BBONE_HANDLE_SCALE_EASE = (1 << 3),
-  /** Is handle scale required? */
+  /* Is handle scale required? */
   BBONE_HANDLE_SCALE_ANY = BBONE_HANDLE_SCALE_X | BBONE_HANDLE_SCALE_Y | BBONE_HANDLE_SCALE_Z |
                            BBONE_HANDLE_SCALE_EASE,
-} eBone_BBoneHandleFlag;
+} eBoneBBoneHandleFlag;
 
 #define MAXBONENAME 64
 
