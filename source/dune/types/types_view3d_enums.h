11d4f1c364b4562aad27756472a24a61b0a3b847
Kielan <ki.lemons@gmail.com>
2023-12-23 15:26:32 -0800

    Update types_view3d_enums.h

--- source/dune/types/types_view3d_enums.h
+++ source/dune/types/types_view3d_enums.h
@@ -4,32 +4,32 @@
 extern "C" {
 #endif
 
-/** Settings for off-screen rendering. */
-typedef enum eV3DOffscreenDrawFlag {
-  V3D_OFSDRAW_NONE = (0),
-  V3D_OFSDRAW_SHOW_ANNOTATION = (1 << 0),
-  V3D_OFSDRAW_OVERRIDE_SCENE_SETTINGS = (1 << 1),
-  V3D_OFSDRAW_SHOW_GRIDFLOOR = (1 << 2),
-  V3D_OFSDRAW_SHOW_SELECTION = (1 << 3),
-  V3D_OFSDRAW_XR_SHOW_CONTROLLERS = (1 << 4),
-  V3D_OFSDRAW_XR_SHOW_CUSTOM_OVERLAYS = (1 << 5),
-} eV3DOffscreenDrawFlag;
+/* Settings for off-screen rendering. */
+typedef enum eV3DOffscreenDrwFlag {
+  V3D_OFSDRW_NONE = (0),
+  V3D_OFSDRW_SHOW_ANNOTATION = (1 << 0),
+  V3D_OFSDRW_OVERRIDE_SCENE_SETTINGS = (1 << 1),
+  V3D_OFSDRW_SHOW_GRIDFLOOR = (1 << 2),
+  V3D_OFSDRW_SHOW_SEL = (1 << 3),
+  V3D_OFSDRW_XR_SHOW_CTRLRS = (1 << 4),
+  V3D_OFSDRW_XR_SHOW_CUSTOM_OVERLAYS = (1 << 5),
+} eV3DOffscreenDrwFlag;
 
-/** #View3DShading.light */
+/* View3DShading.light */
 typedef enum eV3DShadingLightingMode {
   V3D_LIGHTING_FLAT = 0,
   V3D_LIGHTING_STUDIO = 1,
   V3D_LIGHTING_MATCAP = 2,
 } eV3DShadingLightingMode;
 
-/** #View3DShading.color_type, #View3DShading.wire_color_type */
+/* View3DShading.color_type, View3DShading.wire_color_type */
 typedef enum eV3DShadingColorType {
   V3D_SHADING_MATERIAL_COLOR = 0,
   V3D_SHADING_RANDOM_COLOR = 1,
   V3D_SHADING_SINGLE_COLOR = 2,
   V3D_SHADING_TEXTURE_COLOR = 3,
-  V3D_SHADING_OBJECT_COLOR = 4,
-  V3D_SHADING_VERTEX_COLOR = 5,
+  V3D_SHADING_OB_COLOR = 4,
+  V3D_SHADING_VERT_COLOR = 5,
 } eV3DShadingColorType;
 
 /** #View3DShading.background_type */
