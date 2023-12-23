4e4a5d1e9b2b306e5a7915138c80e2cabc0fdd63
Kielan <ki.lemons@gmail.com>
2023-12-23 15:41:22 -0800

    Update types_vec_defaults.h

--- source/dune/types/types_vec_defaults.h
+++ source/dune/types/types_vec_defaults.h
@@ -3,10 +3,8 @@
 /* Struct members on own line. */
 /* clang-format off */
 
-/* -------------------------------------------------------------------- */
-/** Generic Defaults **/
-
-/** See #unit_m4. */
+/* Generic Defaults */
+/* See unit_m4. */
 #define _TYPES_DEFAULT_UNIT_M4 \
   { \
     {1, 0, 0, 0}, \
@@ -22,7 +20,7 @@
     {0, 0, 1}, \
   }
 
-/** See #unit_qt. */
+/* See unit_qt. */
 #define _TYPES_DEFAULT_UNIT_QT \
   {1, 0, 0, 0}
 
