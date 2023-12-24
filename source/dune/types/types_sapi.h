64823d702263b2755e30fbfcc17a0dd9a0e637f8
Kielan <ki.lemons@gmail.com>
2023-12-24 02:37:53 -0800

    Update types_sapi.h

--- source/dune/types/types_sapi.h
+++ source/dune/types/types_sapi.h
@@ -8,97 +8,97 @@ struct MemArena;
 
 #
 #
-typedef struct TYPES_StructMember {
-  /** This struct must not change, it's only a convenience view for raw data stored in SDNA. */
+typedef struct TypesStructMember {
+  /* This struct must not change, it's only a convenience view for raw data stored in Types structs. */
 
-  /** An index into STYPES->types. */
+  /* An index into Types->types. */
   short type;
-  /** An index into STYPES->names. */
+  /* An index into Types->names. */
   short name;
-} TYPES_StructMember;
+} TypesStructMember;
 
 #
 #
-typedef struct TYPES_Struct {
-  /** This struct must not change, it's only a convenience view for raw data stored in SDNA. */
+typedef TypesStruct {
+  /* This struct must not change, it's only a convenience view for raw data stored in Types structs. */
 
-  /** An index into TYPES->types. */
+  /* An index into TYPES->types. */
   short type;
-  /** The amount of members in this struct. */
+  /* The amount of members in this struct. */
   short members_len;
-  /** "Flexible array member" that contains information about all members of this struct. */
-  TYPES_StructMember members[];
-} TYPES_Struct;
+  /* "Flexible array member" that contains info about all members of this struct. */
+  TypesStructMember members[];
+} TypesStruct;
 
 #
 #
-typedef struct TYPES {
-  /** Full copy of 'encoded' data (when data_alloc is set, otherwise borrowed). */
+typedef struct Types {
+  /* Full copy of 'encoded' data (when data_alloc is set, otherwise borrowed). */
   const char *data;
-  /** Length of data. */
+  /* Length of data. */
   int data_len;
   bool data_alloc;
 
-  /** Total number of struct members. */
+  /* Total num of struct members. */
   int names_len, names_len_alloc;
-  /** Struct member names. */
+  /* Struct member names. */
   const char **names;
-  /** Result of #types_elem_array_size (aligned with #names). */
+  /* Result of types_elem_array_size (aligned with #names). */
   short *names_array_len;
 
-  /** Size of a ptr in bytes. */
+  /* Size of a ptr in bytes. */
   int ptr_size;
 
-  /** Type names. */
+  /* Type names. */
   const char **types;
-  /** Number of basic types + struct types. */
+  /* Num of basic types + struct types. */
   int types_len;
 
-  /** Type lengths. */
+  /* Type lengths. */
   short *types_size;
 
-  /** Information about structs and their members. */
-  SDNA_Struct **structs;
-  /** Number of struct types. */
+  /* Info about structs and their members. */
+  TypesStruct **structs;
+  /* Num of struct types. */
   int structs_len;
 
-  /** #GHash for faster lookups, requires WITH_DNA_GHASH to be used for now. */
+  /* GHash for faster lookups, requires WITH_TYPES_GHASH to be used for now. */
   struct GHash *structs_map;
 
-  /** Temporary memory currently only used for version patching DNA. */
+  /* Tmp mem currently only used for version patching DNA. */
   struct MemArena *mem_arena;
-  /** Runtime versions of data stored in DNA, lazy initialized,
-   * only different when renaming is done. */
+  /* Runtime versions of data stored in Types, lazy init,
+   * only diff when renaming is done. */
   struct {
-    /** Aligned with #SDNA.names, same pointers when unchanged. */
+    /* Aligned w Types.names, same ptrs when unchanged. */
     const char **names;
-    /** Aligned with #SDNA.types, same pointers when unchanged. */
+    /* Aligned w Types.types, same ptr when unchanged. */
     const char **types;
-    /** A version of #SDNA.structs_map that uses #SDNA.alias.types for its keys. */
+    /* A version of Types.structs_map that uses Types.alias.types for its keys. */
     struct GHash *structs_map;
   } alias;
-} TYPES;
+} Types;
 
 #
 #
 typedef struct BHead {
   int code, len;
   const void *old;
-  int TYPESnr, nr;
+  int Typesnr, nr;
 } BHead;
 #
 #
 typedef struct BHead4 {
   int code, len;
   uint old;
-  int TYPESnr, nr;
+  int Typesnr, nr;
 } BHead4;
 #
 #
 typedef struct BHead8 {
   int code, len;
   uint64_t old;
-  int SDNAnr, nr;
+  int Typesnr, nr;
 } BHead8;
 
 #ifdef __cplusplus
