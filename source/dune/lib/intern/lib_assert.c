30122914567320cd1215976b8ef273e7c1652e03
Kielan <ki.lemons@gmail.com>
2023-12-24 17:41:44 -0800

    Create lib_assert.c

---
+++ source/dune/lib/intern/lib_assert.c
@@ -0,0 +1,40 @@
+/* lib
+ * Helper fns for lib_assert.h header. */
+
+#include "lib_assert.h" /* Own include. */
+#include "lib_system.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void _(stderr, "lib_assert failed: %s:%d, %s(), at \'%s\'\n", file, line, function, id);
+}
+
+void _lib_assert_print_extra(const char *str)
+{
+  fprintf(stderr, "  %s\n", str);
+}
+
+void _lib_assert_unreachable_print(const char *file, const int line, const char *function)
+{
+  fprintf(stderr, "Code marked as unreachable has been executed. Please report this as a bug.\n");
+  fprintf(stderr, "Error found at %s:%d in %s.\n", file, line, function);
+}
+
+void _lib_assert_print_backtrace(void)
+{
+#ifndef NDEBUG
+  lib_system_backtrace(stderr);
+#endif
+}
+
+void _lib_assert_abort(void)
+{
+  /* Wrap to remove 'noreturn' attribute since this suppresses missing return statements,
+   * allowing changes to debug builds to accidentally to break release builds.
+   *
+   * For example `lib_assert_unreachable();` at the end of a fn that returns a val,
+   * will hide that it's missing a return. */
+
+  abort();
+}
