e642d79629e3c1d783fec1a574ee784666ca0fbe
Kielan <ki.lemons@gmail.com>
2023-12-25 15:10:20 -0800

    Create fileops.cc

---
+++ source/dune/lib/intern/fileops.cc
@@ -0,0 +1,31 @@
+#include "lib_fileops.hh"
+
+#ifdef WIN32
+#  include "utfconv.hh"
+#endif
+
+namespace dune {
+fstream::fstream(const char *filepath, std::ios_base::openmode mode)
+{
+  this->open(filepath, mode);
+}
+
+fstream::fstream(const std::string &filepath, std::ios_base::openmode mode)
+{
+  this->open(filepath, mode);
+}
+
+void fstream::open(StringRefNull filepath, ios_base::openmode mode)
+{
+#ifdef WIN32
+  const char *filepath_cstr = filepath.c_str();
+  UTF16_ENCODE(filepath_cstr);
+  std::wstring filepath_wstr(filepath_cstr_16);
+  std::fstream::open(filepath_wstr.c_str(), mode);
+  UTF16_UN_ENCODE(filepath_cstr);
+#else
+  std::fstream::open(filepath, mode);
+#endif
+}
+
+}  // namespace dune
