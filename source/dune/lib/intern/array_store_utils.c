1e35be363d55fe868c8a011ac44ff2b365201ee3
Kielan <ki.lemons@gmail.com>
2023-12-26 14:34:19 -0800

    Update array_store_utils.c

--- source/dune/lib/intern/array_store_utils.c
+++ source/dune/lib/intern/array_store_utils.c
@@ -1,5 +1,4 @@
 /* Helper fns for lib_array_store API. */
-
 #include "mem_guardedalloc.h"
 
 #include "lib_utildefines.h"
@@ -9,19 +8,19 @@
 
 #include "lib_math_base.h"
 
-BArrayStore *lib_array_store_at_size_ensure(struct BArrayStore_AtSize *bs_stride,
+ArrayStore *lib_array_store_at_size_ensure(struct ArrayStoreAtSize *bs_stride,
                                             const int stride,
                                             const int chunk_size)
 {
   if (bs_stride->stride_table_len < stride) {
     bs_stride->stride_table_len = stride;
-    bs_stride->stride_table = mem_recallocN(bs_stride->stride_table,
+    bs_stride->stride_table = mem_recalloc(bs_stride->stride_table,
                                             sizeof(*bs_stride->stride_table) * stride);
   }
-  BArrayStore **bs_p = &bs_stride->stride_table[stride - 1];
+  ArrayStore **bs_p = &bs_stride->stride_table[stride - 1];
 
   if ((*bs_p) == NULL) {
-    /* calculate best chunk-count to fit a power of two */
+    /* calc best chunk-count to fit a power of two */
     uint chunk_count = chunk_size;
     {
       uint size = chunk_count * stride;
@@ -35,7 +34,7 @@ BArrayStore *lib_array_store_at_size_ensure(struct BArrayStore_AtSize *bs_stride
   return *bs_p;
 }
 
-BArrayStore *lib_array_store_at_size_get(struct BArrayStore_AtSize *bs_stride, const int stride)
+ArrayStore *lib_array_store_at_size_get(struct ArrayStoreAtSize *bs_stride, const int stride)
 {
   lib_assert(stride > 0 && stride <= bs_stride->stride_table_len);
   return bs_stride->stride_table[stride - 1];
@@ -54,14 +53,14 @@ void lib_array_store_at_size_clear(struct BArrayStore_AtSize *bs_stride)
   bs_stride->stride_table_len = 0;
 }
 
-void lib_array_store_at_size_calc_mem_usage(struct BArrayStore_AtSize *bs_stride,
+void lib_array_store_at_size_calc_mem_usage(struct ArrayStoreAtSize *bs_stride,
                                             size_t *r_size_expanded,
                                             size_t *r_size_compacted)
 {
   size_t size_compacted = 0;
   size_t size_expanded = 0;
   for (int i = 0; i < bs_stride->stride_table_len; i++) {
-    BArrayStore *bs = bs_stride->stride_table[i];
+    ArrayStore *bs = bs_stride->stride_table[i];
     if (bs) {
       size_compacted += lib_array_store_calc_size_compacted_get(bs);
       size_expanded += lib_array_store_calc_size_expanded_get(bs);
