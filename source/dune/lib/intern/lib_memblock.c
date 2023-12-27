5566e5c4f30dfea401622396537a5cf90db24176
Kielan <ki.lemons@gmail.com>
2023-12-27 09:05:53 -0800

    Update lib_memblock.c

--- source/dune/lib/intern/lib_memblock.c
+++ source/dune/lib/intern/lib_memblock.c
@@ -1,57 +1,53 @@
-/** \file
- * \ingroup bli
- *
- * Dead simple, fast memory allocator for allocating many elements of the same size.
- */
+/* Dead simple, fast mem allocator for alloc many elements of the same size. */
 
 #include <stdlib.h>
 #include <string.h>
 
 #include "atomic_ops.h"
 
-#include "BLI_utildefines.h"
+#include "lib_utildefines.h"
 
-#include "BLI_memblock.h" /* own include */
+#include "lib_memblock.h" /* own include */
 
-#include "MEM_guardedalloc.h"
+#include "mem_guardedalloc.h"
 
-#include "BLI_strict_flags.h" /* keep last */
+#include "lib_strict_flags.h" /* keep last */
 
 #define CHUNK_LIST_SIZE 16
 
-struct BLI_memblock {
+struct LibMemblock {
   void **chunk_list;
 
-  /** Element size in bytes. */
+  /* Element size in bytes. */
   int elem_size;
-  /** First unused element index. */
+  /* First unused element index. */
   int elem_next;
-  /** Last "touched" element. */
+  /* Last "touched" element. */
   int elem_last;
-  /** Offset in a chunk of the next elem. */
+  /* Offset in a chunk of the next elem. */
   int elem_next_ofs;
-  /** Max offset in a chunk. */
+  /* Max offset in a chunk. */
   int chunk_max_ofs;
-  /** Id of the chunk used for the next allocation. */
+  /* Id of the chunk used for the next allocation. */
   int chunk_next;
-  /** Chunk size in bytes. */
+  /* Chunk size in bytes. */
   int chunk_size;
-  /** Number of allocated chunk. */
+  /* Num of alloc'd chunk. */
   int chunk_len;
 };
 
-BLI_memblock *BLI_memblock_create_ex(uint elem_size, uint chunk_size)
+LibMemblock *lib_memblock_create_ex(uint elem_size, uint chunk_size)
 {
-  BLI_assert(elem_size < chunk_size);
+  lib_assert(elem_size < chunk_size);
 
-  BLI_memblock *mblk = MEM_mallocN(sizeof(BLI_memblock), "BLI_memblock");
+  LibMemblock *mblk = mem_malloc(sizeof(LibMemblock), "lib_memblock");
   mblk->elem_size = (int)elem_size;
   mblk->elem_next = 0;
   mblk->elem_last = -1;
   mblk->chunk_size = (int)chunk_size;
   mblk->chunk_len = CHUNK_LIST_SIZE;
-  mblk->chunk_list = MEM_callocN(sizeof(void *) * (uint)mblk->chunk_len, "chunk list");
-  mblk->chunk_list[0] = MEM_mallocN_aligned((uint)mblk->chunk_size, 32, "BLI_memblock chunk");
+  mblk->chunk_list = mem_calloc(sizeof(void *) * (uint)mblk->chunk_len, "chunk list");
+  mblk->chunk_list[0] = mem_malloc_aligned((uint)mblk->chunk_size, 32, "lib_memblock chunk");
   memset(mblk->chunk_list[0], 0x0, (uint)mblk->chunk_size);
   mblk->chunk_max_ofs = (mblk->chunk_size / mblk->elem_size) * mblk->elem_size;
   mblk->elem_next_ofs = 0;
@@ -59,16 +55,16 @@ BLI_memblock *BLI_memblock_create_ex(uint elem_size, uint chunk_size)
   return mblk;
 }
 
-void BLI_memblock_destroy(BLI_memblock *mblk, MemblockValFreeFP free_callback)
+void lib_memblock_destroy(LibMemblock *mblk, MemblockValFreeFP free_cb)
 {
   int elem_per_chunk = mblk->chunk_size / mblk->elem_size;
 
-  if (free_callback) {
+  if (free_cb) {
     for (int i = 0; i <= mblk->elem_last; i++) {
       int chunk_idx = i / elem_per_chunk;
       int elem_idx = i - elem_per_chunk * chunk_idx;
       void *val = (char *)(mblk->chunk_list[chunk_idx]) + mblk->elem_size * elem_idx;
-      free_callback(val);
+      free_cb(val);
     }
   }
 
@@ -76,15 +72,15 @@ void BLI_memblock_destroy(BLI_memblock *mblk, MemblockValFreeFP free_callback)
     MEM_SAFE_FREE(mblk->chunk_list[i]);
   }
   MEM_SAFE_FREE(mblk->chunk_list);
-  MEM_freeN(mblk);
+  mem_free(mblk);
 }
 
-void BLI_memblock_clear(BLI_memblock *mblk, MemblockValFreeFP free_callback)
+void lib_memblock_clear(LibMemblock *mblk, MemblockValFreeFP free_cb)
 {
   int elem_per_chunk = mblk->chunk_size / mblk->elem_size;
   int last_used_chunk = mblk->elem_next / elem_per_chunk;
 
-  if (free_callback) {
+  if (free_cb) {
     for (int i = mblk->elem_last; i >= mblk->elem_next; i--) {
       int chunk_idx = i / elem_per_chunk;
       int elem_idx = i - elem_per_chunk * chunk_idx;
@@ -108,7 +104,7 @@ void BLI_memblock_clear(BLI_memblock *mblk, MemblockValFreeFP free_callback)
   mblk->chunk_next = 0;
 }
 
-void *BLI_memblock_alloc(BLI_memblock *mblk)
+void *lib_memblock_alloc(LibMemblock *mblk)
 {
   /* Bookkeeping. */
   if (mblk->elem_last < mblk->elem_next) {
@@ -126,19 +122,19 @@ void *BLI_memblock_alloc(BLI_memblock *mblk)
 
     if (UNLIKELY(mblk->chunk_next >= mblk->chunk_len)) {
       mblk->chunk_len += CHUNK_LIST_SIZE;
-      mblk->chunk_list = MEM_recallocN(mblk->chunk_list, sizeof(void *) * (uint)mblk->chunk_len);
+      mblk->chunk_list = mem_recalloc(mblk->chunk_list, sizeof(void *) * (uint)mblk->chunk_len);
     }
 
     if (UNLIKELY(mblk->chunk_list[mblk->chunk_next] == NULL)) {
-      mblk->chunk_list[mblk->chunk_next] = MEM_mallocN_aligned(
-          (uint)mblk->chunk_size, 32, "BLI_memblock chunk");
+      mblk->chunk_list[mblk->chunk_next] = mem_malloc_aligned(
+          (uint)mblk->chunk_size, 32, "lib_memblock chunk");
       memset(mblk->chunk_list[mblk->chunk_next], 0x0, (uint)mblk->chunk_size);
     }
   }
   return ptr;
 }
 
-void BLI_memblock_iternew(BLI_memblock *mblk, BLI_memblock_iter *iter)
+void lib_memblock_iternew(LibMemblock *mblk, lib_memblock_iter *iter)
 {
   /* Small copy of the memblock used for better cache coherence. */
   iter->chunk_list = mblk->chunk_list;
@@ -150,7 +146,7 @@ void BLI_memblock_iternew(BLI_memblock *mblk, BLI_memblock_iter *iter)
   iter->chunk_max_ofs = mblk->chunk_max_ofs;
 }
 
-void *lib_memblock_iterstep(BLI_memblock_iter *iter)
+void *lib_memblock_iterstep(LibMemblockIter *iter)
 {
   if (iter->cur_index == iter->end_index) {
     return NULL;
@@ -169,9 +165,9 @@ void *lib_memblock_iterstep(BLI_memblock_iter *iter)
   return ptr;
 }
 
-void *lib_memblock_elem_get(BLI_memblock *mblk, int chunk, int elem)
+void *lib_memblock_elem_get(LibMemblock *mblk, int chunk, int elem)
 {
-  BLI_assert(chunk < mblk->chunk_len);
+  lib_assert(chunk < mblk->chunk_len);
   int elem_per_chunk = mblk->chunk_size / mblk->elem_size;
   chunk += elem / elem_per_chunk;
   elem = elem % elem_per_chunk;
