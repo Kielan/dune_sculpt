7a30ea6038a3bb1d0bc2299a64edf2ad3b438c3b
Kielan <ki.lemons@gmail.com>
2023-12-27 08:40:40 -0800

    Update filereader_file.c

--- source/dune/lib/intern/filereader_file.c
+++ source/dune/lib/intern/filereader_file.c
@@ -1,15 +1,15 @@
 #ifndef WIN32
 #  include <unistd.h> /* for read close */
 #else
-#  include "BLI_winstuff.h"
+#  include "lib_winstuff.h"
 #  include "winsock2.h"
 #  include <io.h> /* for open close read */
 #endif
 
-#include "BLI_blenlib.h"
-#include "BLI_filereader.h"
+#include "lib_dunelib.h"
+#include "lib_filereader.h"
 
-#include "MEM_guardedalloc.h"
+#include "mem_guardedalloc.h"
 
 typedef struct {
   FileReader reader;
@@ -20,7 +20,7 @@ typedef struct {
 static int64_t file_read(FileReader *reader, void *buffer, size_t size)
 {
   RawFileReader *rawfile = (RawFileReader *)reader;
-  int64_t readsize = BLI_read(rawfile->filedes, buffer, size);
+  int64_t readsize = lib_read(rawfile->filedes, buffer, size);
 
   if (readsize >= 0) {
     rawfile->reader.offset += readsize;
@@ -32,7 +32,7 @@ static int64_t file_read(FileReader *reader, void *buffer, size_t size)
 static off64_t file_seek(FileReader *reader, off64_t offset, int whence)
 {
   RawFileReader *rawfile = (RawFileReader *)reader;
-  rawfile->reader.offset = BLI_lseek(rawfile->filedes, offset, whence);
+  rawfile->reader.offset = lib_lseek(rawfile->filedes, offset, whence);
   return rawfile->reader.offset;
 }
 
@@ -40,12 +40,12 @@ static void file_close(FileReader *reader)
 {
   RawFileReader *rawfile = (RawFileReader *)reader;
   close(rawfile->filedes);
-  MEM_freeN(rawfile);
+  mem_free(rawfile);
 }
 
 FileReader *lib_filereader_new_file(int filedes)
 {
-  RawFileReader *rawfile = MEM_callocN(sizeof(RawFileReader), __func__);
+  RawFileReader *rawfile = mem_calloc(sizeof(RawFileReader), __func__);
 
   rawfile->filedes = filedes;
 
