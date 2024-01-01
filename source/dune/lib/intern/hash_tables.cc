574334818b7298d550f48933e8de07b4b13e6608
Kielan <ki.lemons@gmail.com>
2024-01-01 11:26:26 -0800

    Update hash_tables.cc

--- source/dune/lib/intern/hash_tables.cc
+++ source/dune/lib/intern/hash_tables.cc
@@ -1,9 +1,9 @@
-#include "BLI_hash_tables.hh"
-#include "BLI_string.h"
+#include "lib_hash_tables.hh"
+#include "lib_string.h"
 
 #include <iostream>
 
-void blender::HashTableStats::print(const char *name) const
+void dune::HashTableStats::print(const char *name) const
 {
   std::cout << "Hash Table Stats: " << name << "\n";
   std::cout << "  Address: " << address_ << "\n";
@@ -12,12 +12,12 @@ void blender::HashTableStats::print(const char *name) const
   std::cout << "  Removed Slots: " << removed_amount_ << " (" << removed_load_factor_ * 100.0f
             << " %)\n";
 
-  char memory_size_str[BLI_STR_FORMAT_INT64_BYTE_UNIT_SIZE];
-  BLI_str_format_byte_unit(memory_size_str, size_in_bytes_, true);
-  std::cout << "  Size: ~" << memory_size_str << "\n";
+  char mem_size_str[LIB_STR_FORMAT_INT64_BYTE_UNIT_SIZE];
+  lib_str_format_byte_unit(mem_size_str, size_in_bytes_, true);
+  std::cout << "  Size: ~" << mem_size_str << "\n";
   std::cout << "  Size per Slot: " << size_per_element_ << " bytes\n";
 
-  std::cout << "  Average Collisions: " << average_collisions_ << "\n";
+  std::cout << "  Avg Collisions: " << avg_collisions_ << "\n";
   for (int64_t collision_count : keys_by_collision_count_.index_range()) {
     std::cout << "  " << collision_count
               << " Collisions: " << keys_by_collision_count_[collision_count] << "\n";
