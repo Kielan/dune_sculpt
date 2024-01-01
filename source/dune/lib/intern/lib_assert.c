d125aa441fdc2c1a1d3ed62abe6fadfd7e56b615
Kielan <ki.lemons@gmail.com>
2024-01-01 12:46:16 -0800

    Update lib_assert.c

--- source/dune/lib/intern/lib_assert.c
+++ source/dune/lib/intern/lib_assert.c
@@ -1,13 +1,12 @@
 /* lib
  * Helper fns for lib_assert.h header. */
-
 #include "lib_assert.h" /* Own include. */
 #include "lib_system.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void _(stderr, "lib_assert failed: %s:%d, %s(), at \'%s\'\n", file, line, function, id);
+void _(stderr, "lib_assert failed: %s:%d, %s(), at \'%s\'\n", file, line, fn, id);
 }
 
 void _lib_assert_print_extra(const char *str)
@@ -17,8 +16,8 @@ void _lib_assert_print_extra(const char *str)
 
 void _lib_assert_unreachable_print(const char *file, const int line, const char *function)
 {
-  fprintf(stderr, "Code marked as unreachable has been executed. Please report this as a bug.\n");
-  fprintf(stderr, "Error found at %s:%d in %s.\n", file, line, function);
+  fprintf(stderr, "Code marked as unreachable has been ex. Please report this as a bug.\n");
+  fprintf(stderr, "Err found at %s:%d in %s.\n", file, line, function);
 }
 
 void _lib_assert_print_backtrace(void)
@@ -32,9 +31,7 @@ void _lib_assert_abort(void)
 {
   /* Wrap to remove 'noreturn' attribute since this suppresses missing return statements,
    * allowing changes to debug builds to accidentally to break release builds.
-   *
    * For example `lib_assert_unreachable();` at the end of a fn that returns a val,
    * will hide that it's missing a return. */
-
   abort();
 }
