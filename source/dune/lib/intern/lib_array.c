99adc2e08280c6e89378cfc8a7aae9e9d3397b94
Kielan <ki.lemons@gmail.com>
2024-01-01 12:08:55 -0800

    Update lib_array.c

--- source/dune/lib/intern/lib_array.c
+++ source/dune/lib/intern/lib_array.c
@@ -1,43 +1,35 @@
 /* A (mainly) macro array lib.
  *
- * This is an array lib, used to manage array (re)allocation.
- *
- * This is primarily accessed via macros,
- * fns are used to implement some of the internals.
- *
+ * An array lib: used to manage array (re)alloc.
+ * Primarily accessed via macros,
+ * fns used to implement some of the internals.
  * Example usage:
- *
- * \code{.c}
  * int *arr = NULL;
  * lib_array_declare(arr);
  * int i;
  *
  * for (i = 0; i < 10; i++) {
- *     lib_array_grow_one(arr);
+ *     lib_arr_grow_one(arr);
  *     arr[i] = something;
  * }
- * lib_array_free(arr);
- * \endcode
+ * lib_arr_free(arr);
  *
- * Arrays are over allocated, so each reallocation the array size is doubled.
- * In situations where contiguous array access isn't needed,
- * other solutions for alloc are available.
+ * Arrs are over alloc, so each realloc the arr size is doubled.
+ * In situations where contiguous arr access isn't needed,
+ * other solutions for alloc are avail.
  * Consider using on of: lib_memarena.c, lib_mempool.c, lib_stack.c */
 
 #include <string.h>
-
 #include "lib_array.h"
-
 #include "lib_sys_types.h"
-
 #include "mem_guardedalloc.h"
 
 void _lib_array_grow_fn(void **arr_p,
-                          const void *arr_static,
-                          const int sizeof_arr_p,
-                          const int arr_len,
-                          const int num,
-                          const char *alloc_str)
+                        const void *arr_static,
+                        const int sizeof_arr_p,
+                        const int arr_len,
+                        const int num,
+                        const char *alloc_str)
 {
   void *arr = *arr_p;
   void *arr_tmp;
