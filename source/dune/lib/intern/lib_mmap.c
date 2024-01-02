d6921574cf1b1880e409333a3a891b2482daa2fa
Kielan <ki.lemons@gmail.com>
2024-01-02 06:53:34 -0800

    Update lib_mmap.c

--- source/dune/lib/intern/lib_mmap.c
+++ source/dune/lib/intern/lib_mmap.c
@@ -16,58 +16,54 @@
 #endif
 
 struct lib_mmap_file {
-  /* The address to which the file was mapped. */
+  /* Address to which the file was mapped. */
   char *mem;
-
-  /* The length of the file (and therefore the mapped rgn). */
+  /* Length of the file (and therefore the mapped rgn). */
   size_t length;
-
   /* Platform-specific handle for the mapping. */
   void *handle;
-
-  /* Flag to indicate IO errors. Needs to be volatile since it's being set from
-   * within the signal handler, which is not part of the normal ex flow. */
+  /* Flag to indicate IO errs. Must be volatile bc it's being set from
+   * inside of the signal handler, not part of the normal ex flow. */
   volatile bool io_error;
 };
 
 #ifndef WIN32
-/* When using mem-mapped files, any IO errors will result in a SIGBUS signal.
- * Therefore, we need to catch that signal and stop reading the file in question.
- * To do so, we keep a list of all current FileDatas that use mem-mapped files,
- * and if a SIGBUS is caught, we check if the failed address is inside one of the
- * mapped rgns.
- * If it is, we set a flag to indicate a failed read and remap the mem in
- * question to a zero-backed region in order to avoid additional signals.
- * The code that actually reads the mem area has to check whether the flag was
- * set after it's done reading.
- * If the error occurred outside of a mem-mapped region, we call the prev
- * handler if one was configured and abort the process otherwise. */
-
-static struct error_handler_data {
+/* When using mem-mapped files IO errs result in a SIGBUS signal.
+ * Must catch that signal and stop reading the file in question.
+ * Keep a list of all current FileDatas using mem-mapped files,
+ * and if a SIGBUS is caught, check if the failed address
+ * is inside 1 of the mapped rgns.
+ * If yes: set a flag to indicate a failed read and remap the mem in
+ * question to a zero-backed rgn to avoid additional signals.
+ * The code that actually reads the mem area must
+ * check if flag was set after it's done reading.
+ * If the err occurred outside of a mem-mapped rgn, call the prev
+ * handler if 1 was config'd and abort the proc otherwise. */
+static struct err_handler_data {
   List open_mmaps;
   char configured;
   void (*next_handler)(int, siginfo_t *, void *);
-} error_handler = {0};
+} err_handler = {0};
 
 static void sigbus_handler(int sig, siginfo_t *siginfo, void *ptr)
 {
   /* We only handle SIGBUS here for now. */
   lib_assert(sig == SIGBUS);
 
-  char *error_addr = (char *)siginfo->si_addr;
-  /* Find the file that this error belongs to. */
-  LIST_FOREACH (LinkData *, link, &error_handler.open_mmaps) {
+  char *err_addr = (char *)siginfo->si_addr;
+  /* Find the file that this err belongs to. */
+  LIST_FOREACH (LinkData *, link, &err_handler.open_mmaps) {
     lib_mmap_file *file = link->data;
 
-    /* Is the address where the error occurred in this file's mapped range? */
-    if (error_addr >= file->mem && error_addr < file->mem + file->length) {
-      file->io_error = true;
+    /* Is the address where the err occurred in this file's mapped range? */
+    if (error_addr >= file->mem && err_addr < file->mem + file->length) {
+      file->io_err = true;
 
-      /* Replace the mapped mem with zeroes. */
+      /* Replace the mapped mem w zeroes. */
       const void *mapped_mem = mmap(
           file->mem, file->length, PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
       if (mapped_mem == MAP_FAILED) {
-        fprintf(stderr, "SIGBUS handler: Error replacing mapped file with zeros\n");
+        fprintf(stderr, "SIGBUS handler: Err replacing mapped file w zeros\n");
       }
 
       return;
@@ -75,8 +71,8 @@ static void sigbus_handler(int sig, siginfo_t *siginfo, void *ptr)
   }
 
   /* Fall back to other handler if there was one. */
-  if (error_handler.next_handler) {
-    error_handler.next_handler(sig, siginfo, ptr);
+  if (err_handler.next_handler) {
+    err_handler.next_handler(sig, siginfo, ptr);
   }
   else {
     fprintf(stderr, "Unhandled SIGBUS caught\n");
@@ -84,7 +80,7 @@ static void sigbus_handler(int sig, siginfo_t *siginfo, void *ptr)
   }
 }
 
-/* Ensures that the error handler is set up and ready. */
+/* Ensures that the err handler is set up and rdy. */
 static bool sigbus_handler_setup(void)
 {
   if (!error_handler.configured) {
@@ -97,26 +93,26 @@ static bool sigbus_handler_setup(void)
       return false;
     }
 
-    /* Remember the prev configured handler to fall back to it if the error
+    /* Remember the prev configured handler to fall back to it if the err
      * does not belong to any of the mapped files. */
-    error_handler.next_handler = oldact.sa_sigaction;
-    error_handler.configured = 1;
+    err_handler.next_handler = oldact.sa_sigaction;
+    err_handler.configured = 1;
   }
 
   return true;
 }
 
-/* Adds a file to the list that the error handler checks. */
+/* Adds a file to the list that the err handler checks. */
 static void sigbus_handler_add(lib_mmap_file *file)
 {
-  lib_addtail(&error_handler.open_mmaps, lib_genericNode(file));
+  lib_addtail(&err_handler.open_mmaps, lib_genericNode(file));
 }
 
-/* Removes a file from the list that the error handler checks. */
+/* Removes a file from the list that the err handler checks. */
 static void sigbus_handler_remove(lib_mmap_file *file)
 {
-  LinkData *link = lib_findptr(&error_handler.open_mmaps, file, offsetof(LinkData, data));
-  lib_freelinkN(&error_handler.open_mmaps, link);
+  LinkData *link = lib_findptr(&err_handler.open_mmaps, file, offsetof(LinkData, data));
+  lib_freelink(&err_handler.open_mmaps, link);
 }
 #endif
 
@@ -129,7 +125,7 @@ lib_mmap_file *lib_mmap_open(int fd)
   }
 
 #ifndef WIN32
-  /* Ensure that the SIGBUS handler is configured. */
+  /* Ensure that the SIGBUS handler is config'd. */
   if (!sigbus_handler_setup()) {
     return NULL;
   }
@@ -142,9 +138,9 @@ lib_mmap_file *lib_mmap_open(int fd)
 #else
   /* Convert the POSIX-style file descriptor to a Windows handle. */
   void *file_handle = (void *)_get_osfhandle(fd);
-  /* Mem mapping on Windows is a two-step process - first we create a mapping,
-   * then we create a view into that mapping.
-   * In our case, one view that spans the entire file is enough. */
+  /* Mem mapping on Windows is a 2-step proc: 1st create a mapping,
+   * then create a view into that mapping.
+   * Our case 1 view that spans the entire file is enough. */
   handle = CreateFileMapping(file_handle, NULL, PAGE_READONLY, 0, 0, NULL);
   if (handle == NULL) {
     return NULL;
@@ -156,60 +152,62 @@ lib_mmap_file *lib_mmap_open(int fd)
   }
 #endif
 
-  /* Now that the mapping was successful, allocate memory and set up the BLI_mmap_file. */
-  lib_mmap_file *file = mem_calloc(sizeof(BLI_mmap_file), __func__);
+  /* The mapping was successful,
+   * alloc mem and set up the lib_mmap_file. */
+  lib_mmap_file *file = mem_calloc(sizeof(MmapFile), __func__);
   file->mem = mem;
   file->handle = handle;
   file->length = length;
 
 #ifndef WIN32
-  /* Register the file with the error handler. */
+  /* Register file w the err handler. */
   sigbus_handler_add(file);
 #endif
 
   return file;
 }
 
-bool lib_mmap_read(lib_mmap_file *file, void *dest, size_t offset, size_t length)
+bool lib_mmap_read(MmapFile *file, void *dest, size_t offset, size_t length)
 {
-  /* If a prev read has already failed or we try to read past the end,
-   * don't even attempt to read any further. */
+  /* If a prev read has alrdy failed or we try to read past the end,
+   * don't attempt to read any further. */
   if (file->io_error || (offset + length > file->length)) {
     return false;
   }
 
 #ifndef WIN32
-  /* If an error occurs in this call, sigbus_handler will be called and will set
-   * file->io_error to true. */
+  /* If an err occurs in this call,
+   * sigbus_handler is called and set
+   * file->io_err to true. */
   memcpy(dest, file->mem + offset, length);
 #else
-  /* On Windows, we use exception handling to be notified of errors. */
+  /* On Windows use exception handling to be notified of errs. */
   __try
   {
     memcpy(dest, file->mem + offset, length);
   }
-  __except (GetExceptionCode() == EXCEPTION_IN_PAGE_ERROR ? EXCEPTION_EXECUTE_HANDLER :
+  __except (GetExceptionCode() == EXCEPTION_IN_PAGE_ERR ? EXCEPTION_EX_HANDLER :
                                                             EXCEPTION_CONTINUE_SEARCH)
   {
-    file->io_error = true;
+    file->io_err = true;
     return false;
   }
 #endif
 
-  return !file->io_error;
+  return !file->io_err;
 }
 
-void *lib_mmap_get_ptr(lib_mmap_file *file)
+void *lib_mmap_get_ptr(MmapFile *file)
 {
-  return file->memory;
+  return file->mem;
 }
 
-size_t lib_mmap_get_length(const lib_mmap_file *file)
+size_t lib_mmap_get_length(const MmapFile *file)
 {
   return file->length;
 }
 
-void lib_mmap_free(lib_mmap_file *file)
+void lib_mmap_free(MmapFile *file)
 {
 #ifndef WIN32
   munmap((void *)file->mem, file->length);
