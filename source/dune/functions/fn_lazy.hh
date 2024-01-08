c01ef51f136f5e645910e5b67baead425438d8d8
Kielan <ki.lemons@gmail.com>
2024-01-08 02:49:59 -0800

    Update fn_lazy.hh

--- source/dune/functions/fn_lazy.hh
+++ source/dune/functions/fn_lazy.hh
@@ -1,116 +1,96 @@
 #pragma once
 
 /* A `LazyFn` encapsulates a computation which has inputs, outputs and potentially side
- * effects. Most importantly, a `LazyFn` supports laziness in its inputs and outputs:
+ * effects. Most importantly aa `LazyFn` supports laziness in its inputs and outputs:
  * - Only outputs that are actually used have to be computed.
  * - Inputs can be requested lazily based on which 
  *   outputs are used or what side effects the fn has.
  *
  * A lazy-fn that uses laziness may be ex more than once. The most common example is
  * the geometry nodes switch node. Depending on a condition input, it decides which 1 of the
- * other inputs is actually used. From the perspective of the switch node, its ex works as
+ * other inputs is actually used.
+ * From the perspective of the switch node, its ex works as
  * follows:
- * 1. The switch node is first executed. It sees that the output is used. Now it requests the
+ * 1. The switch node is 1st ex. It sees that the output is used. Now it requests the
  *    condition input from the caller and exits.
- * 2. Once the caller is able to provide the condition input the switch node is executed again.
- *    This time it retrieves the condition and requests one of the other inputs. Then the node
- *    exits again, giving back control to the caller.
- * 3. When the caller computed the second requested input the switch node executes a last time.
+ * 2. Once the caller is able to provide the condition input the switch node is ex again.
+ *    This time it retrieves the condition and requests  of the other inputs. Then the node
+ *    exits again, giving back ctrl to the caller.
+ * 3. When the caller computed the 2nd requested input the switch node ex's a last time.
  *    This time it retrieves the new input and forwards it to the output.
  *
- * In some sense, a lazy-function can be thought of like a state machine. Every time it is
- * executed, it advances its state until all required outputs are ready.
+ * abstract:, a lazy-fn can be thought of like a state machine.
+ * Every time it is executed, it
+ * advances its state until all required outputs are rdy.
  *
- * The lazy-function interface is designed to support composition of many such functions into a new
- * lazy-functions, all while keeping the laziness working. For example, in geometry nodes a switch
+ * The lazy-fn interface is designed to support composition of many such fns into a new
+ * lazy-fns, all while keeping the laziness working. Example, in geometry nodes a switch
  * node in a node group should still be able to decide whether a node in the parent group will be
- * executed or not. This is essential to avoid doing unnecessary work.
+ * ex or not. This is essential to avoid doing unnecessary work.
  *
- * The lazy-function system consists of multiple core components:
- * - The interface of a lazy-function itself including its calling convention.
- * - A graph data structure that allows composing many lazy-functions by connecting their inputs
+ * The lazy-fn sys consists of multiple core components:
+ * - The interface of a lazy-fn itself including its calling convention.
+ * - A graph data struct allows composing many lazy-fns by connecting their inputs
  *   and outputs.
- * - An executor that allows multi-threaded execution or such a graph.
- */
+ * - An executor that allows multi-threaded ex or such a graph. */
 
-#include "BLI_cpp_type.hh"
-#include "BLI_function_ref.hh"
-#include "BLI_generic_pointer.hh"
-#include "BLI_linear_allocator.hh"
-#include "BLI_vector.hh"
+#include "lib_cpp_type.hh"
+#include "lib_fn_ref.hh"
+#include "lib_generic_ptr.hh"
+#include "lib_linear_allocator.hh"
+#include "lib_vector.hh"
 
 #include <atomic>
 #include <thread>
 
 #ifndef NDEBUG
-#  define FN_LAZY_FUNCTION_DEBUG_THREADS
-#endif
+#  define FN_LAZY_FN_DEBUG_THREADS
+#endi
+namespace dune::fn::lazy_fn {
 
-namespace blender::fn::lazy_function {
-
-enum class ValueUsage : uint8_t {
-  /**
-   * The value is definitely used and therefore has to be computed.
-   */
+enum class ValUsage : uint8_t {
+  /* The val is definitely used and therefore has to be computed. */
   Used,
-  /**
-   * It's unknown whether this value will be used or not. Computing it is ok but the result may be
-   * discarded.
-   */
+  /* It's unknown whether this val will be used or not. Computing it is ok but the result may be
+   * discarded. */
   Maybe,
-  /**
-   * The value will definitely not be used. It can still be computed but the result will be
-   * discarded in all cases.
-   */
+  /* The val will definitely not be used. It can still be computed but the result will be
+   * discarded in all cases */
   Unused,
 };
 
-class LazyFunction;
+class LazyF!;
 
-/**
- * Extension of #UserData that is thread-local. This avoids accessing e.g.
- * `EnumerableThreadSpecific.local()` in every nested lazy-function because the thread local
- * data is passed in by the caller.
- */
+/* Extension of UserData that is thread-local. This avoids accessing e.g.
+ * `EnumThreadSpecific.local()` in every nested lazy-fn bc the thread local
+ * data is passed in by the caller. */
 class LocalUserData {
  public:
   virtual ~LocalUserData() = default;
 };
 
-/**
- * This allows passing arbitrary data into a lazy-function during execution. For that, #UserData
- * has to be subclassed. This mainly exists because it's more type safe than passing a `void *`
- * with no type information attached.
+/* This allows passing arbitrary data into a lazy-fn during ex. For that, UserData
+ * has to be subclassed. This mainly exists bc it's more type safe than passing a `void *
+ * w no type info attached.
  *
- * Some lazy-functions may expect to find a certain type of user data when executed.
- */
+ * Some lazy-fns may expect to find a certain type of user data when ex. */
 class UserData {
  public:
   virtual ~UserData() = default;
 
-  /**
-   * Get thread local data for this user-data and the current thread.
-   */
+  /* Get thread local data for this user-data and the current thread. */
   virtual destruct_ptr<LocalUserData> get_local(LinearAllocator<> &allocator);
 };
 
-/**
- * Passed to the lazy-function when it is executed.
- */
+/* Passed to the lazy-fn when it is ex. */
 struct Context {
-  /**
-   * If the lazy-function has some state (which only makes sense when it is executed more than once
-   * to finish its job), the state is stored here. This points to memory returned from
-   * #LazyFunction::init_storage.
-   */
+  /* If the lazy-fn has some state (which only makes sense when it is ex more than once
+   * to finish its job), the state is stored here. This points to mem returned from
+   * LazyFn::init_storage. */
   void *storage;
-  /**
-   * Custom user data that can be used in the function.
-   */
+  /* Custom user data that can be used in the fn */
   UserData *user_data;
-  /**
-   * Custom user data that is local to the thread that executes the lazy-function.
-   */
+  /* Custom user data that is local to the thread that ex the lazy-fn. */
   LocalUserData *local_user_data;
 
   Context(void *storage, UserData *user_data, LocalUserData *local_user_data)
@@ -119,91 +99,66 @@ struct Context {
   }
 };
 
-/**
- * Defines the calling convention for a lazy-function. During execution, a lazy-function retrieves
- * its inputs and sets the outputs through #Params.
- */
+/* Defines the calling convention for a lazy-fn. During ex, a lazy-fn retrieves
+ * its inputs and sets the outputs through Params. */
 class Params {
  public:
-  /**
-   * The lazy-function this #Params has been prepared for.
-   */
-  const LazyFunction &fn_;
-#ifdef FN_LAZY_FUNCTION_DEBUG_THREADS
+  /* The lazy-fn this Params has been prepared for. */
+  const LazyFn &fn_;
+#ifdef FN_LAZY_FN_DEBUG_THREADS
   std::thread::id main_thread_id_;
   std::atomic<bool> allow_multi_threading_;
 #endif
 
  public:
-  Params(const LazyFunction &fn, bool allow_multi_threading_initially);
+  Params(const LazyFn &fn, bool allow_multi_threading_initially);
 
-  /**
-   * Get a pointer to an input value if the value is available already. Otherwise null is returned.
-   *
-   * The #LazyFunction must leave returned object in an initialized state, but can move from it.
-   */
+  /* Get a ptr to an input val if the value is available alrdy. Otherwise null is returned.
+   * The LazyFn must leave returned ob in an initialized state, but can move from it. */
   void *try_get_input_data_ptr(int index) const;
 
-  /**
-   * Same as #try_get_input_data_ptr, but if the data is not yet available, request it. This makes
-   * sure that the data will be available in a future execution of the #LazyFunction.
-   */
+  /* Same as try_get_input_data_ptr, but if the data is not yet available, request it. This makes
+   * sure that the data will be available in a future ex of the LazyFn. */
   void *try_get_input_data_ptr_or_request(int index);
 
-  /**
-   * Get a pointer to where the output value should be stored.
-   * The value at the pointer is in an uninitialized state at first.
-   * The #LazyFunction is responsible for initializing the value.
-   * After the output has been initialized to its final value, #output_set has to be called.
-   */
+  /* Get a ptr to where the output val should be stored.
+   * The val at the ptr is in an uninit state at first.
+   * The LazyFn is responsible for init the val.
+   * After the output has been init to its final val, output_set has to be called. */
   void *get_output_data_ptr(int index);
 
-  /**
-   * Call this after the output value is initialized. After this is called, the value must not be
-   * touched anymore. It may be moved or destructed immediately.
-   */
-  void output_set(int index);
+  /* Call this after the output val is init. After this is called the val must not be
+   * touched anymore. It may be moved or destructed immediately. */
+  void output_set(int 
 
-  /**
-   * Allows the #LazyFunction to check whether an output was computed already without keeping
-   * track of it itself.
-   */
+  /* Allows the LazyFn to check whether an output was computed alrdy wo keeping
+   * track of it itself. */
   bool output_was_set(int index) const;
 
-  /**
-   * Can be used to detect which outputs have to be computed.
-   */
-  ValueUsage get_output_usage(int index) const;
+  /* Can be used to detect which outputs have to be computed. */
+  ValUsage get_output_usage(int index) const;
 
-  /**
-   * Tell the caller of the #LazyFunction that a specific input will definitely not be used.
-   * Only an input that was not #ValueUsage::Used can become unused.
-   */
+  /* Tell the caller of the LazyFn that a specific input will definitely not be used.
+   * Only an input that was not ValUsage::Used can become unused. */
   void set_input_unused(int index);
 
-  /**
-   * Typed utility methods that wrap the methods above.
-   */
+  /* Typed util methods that wrap the methods above. */
   template<typename T> T extract_input(int index);
   template<typename T> T &get_input(int index) const;
   template<typename T> T *try_get_input_data_ptr(int index) const;
   template<typename T> T *try_get_input_data_ptr_or_request(int index);
-  template<typename T> void set_output(int index, T &&value);
+  template<typename T> void set_output(int index, T &&val);
 
-  /**
-   * Returns true when the lazy-function is now allowed to use multi-threading when interacting
-   * with this #Params. That means, it is allowed to call non-const methods from different threads.
-   */
+  /* Returns true when the lazy-fn is now allowed to use multi-threading when interacting
+   * w this Params. That means, it is allowed to call non-const methods from diff threads. */
   bool try_enable_multi_threading();
 
  private:
   void assert_valid_thread() const;
 
-  /**
-   * Methods that need to be implemented by subclasses. Those are separate from the non-virtual
+  /* Methods that need to be implemented by subclasses. Those are separate from the non-virtual
    * methods above to make it easy to insert additional debugging logic on top of the
-   * implementations.
-   */
+   * implementations. */
   virtual void *try_get_input_data_ptr_impl(int index) const = 0;
   virtual void *try_get_input_data_ptr_or_request_impl(int index) = 0;
   virtual void *get_output_data_ptr_impl(int index) = 0;
@@ -221,13 +176,13 @@ struct Input {
   const char *debug_name;
   /* Data type of this input. */
   const CPPType *type;
-  /* Can be used to indicate a caller or this function if this input is used statically before
-   * executing it the first time. This is technically not needed but can improve efficiency because
-   * a round-trip through the `execute` method can be avoided.
+  /* Can be used to indicate a caller or this fn if this input is used statically before
+   * ex it the 1st time. Technically not needed but can improve efficiency bc
+   * a round-trip through the `ex` method can be avoided.
    *
-   * When this is ValUsage::Used, the caller has to ensure that the input is definitely
-   * available when the execute method is first called. The #execute method does not have to check
-   * whether the value is actually available.  */
+   * When this is ValUsage::Used, caller must ensure that the input is definitely
+   * available when the ex method is first called. The ex method does not have to check
+   * whether the val is actually available.  */
   ValUsage usage;
 
   Input(const char *debug_name, const CPPType &type, const ValUsage usage = ValueUsage::Used)
@@ -237,7 +192,7 @@ struct Input {
 };
 
 struct Output {
-  /* Name used for debugging purposes. The string has to be static or has to be owned by something
+  /* Name used for debugging purposes. The string must be static or must be owned by something
    * else. */
   const char *debug_name;
   /* Data type of this output. */
@@ -259,14 +214,14 @@ class LazyFn {
  public:
   virtual ~LazyFn() = default;
 
-  /* Get a name of the function or an input or output. This is mainly used for debugging.
-   * These are virtual functions because the names are often not used outside of debugging
+  /* Get a name of the fn or an input or output. This is mainly used for debugging.
+   * These are virtual fns because the names are often not used outside of debugging
    * workflows. This way the names are only generated when they are actually needed. */
   virtual std::string name() const;
   virtual std::string input_name(int index) const;
   virtual std::string output_name(int index) const;
 
-  /* Alloc storage for this fn. The storage will be passed to every call to #execute.
+  /* Alloc storage for this fn. The storage will be passed to every call to ex.
    * If the fn does not keep track of any state, this does not have to be implemented. */
   virtual void *init_storage(LinearAllocator<> &allocator) const;
 
@@ -278,30 +233,30 @@ class LazyFn {
   virtual void possible_output_dependencies(int output_index,
                                             FnRef<void(Span<int>)> fn) const;
 
-  /* Inputs of the function */
+  /* Inputs of the fn */
   Span<Input> inputs() const;
   /* Outputs of the fn  */
   Span<Output> outputs() const;
 
-  /* During execution the function retrieves inputs and sets outputs in #params. For some
-   * functions, this method is called more than once. After execution, the function either has
+  /* During ex the fn retrieves inputs and sets outputs in params. For some
+   * fns, this method is called more than once. After ex, the fn either has
    * computed all required outputs or is waiting for more inputs. */
-  void ex(Params &params, const Context &context) const;
+  void ex(Params &params, const Cxt &cxt) const;
 
-  /* Util to check that the guarantee by #Input::usage is followed. */
+  /* Util to check that the guarantee by Input::usage is followed. */
   bool always_used_inputs_available(const Params &params) const;
 
-  /* If true, the function can be executed even when some requested inputs are not available yet.
+  /* If true, the fn can be ex even when some requested inputs are not available yet.
    * This allows the fn to make some progress and maybe to compute some outputs that are
    * passed into this fn again (lazy-fn graphs may contain cycles as long as there
-   * aren't actually data dependencies). */
+   * aren't actually data deps). */
   bool allow_missing_requested_inputs() const
   {
     return allow_missing_requested_inputs_;
   }
 
  private:
-  /* Needs to be implemented by subclasses. This is separate from #execute so that additional
+  /* Must be implemented by subclasses. This is separate from ex so that additional
    * debugging logic can be implemented in ex. */
   virtual void ex_impl(Params &params, const Cxt &cxt) const = 0;
 };
@@ -317,10 +272,10 @@ inline Span<Output> LazyFn::outputs() const
   return outputs_;
 }
 
-inline void LazyFn::execute(Params &params, const Context &context) const
+inline void LazyFn::ex(Params &params, const Cxt &cxt) const
 {
   lib_assert(this->always_used_inputs_available(params));
-  this->execute_impl(params, cxt);
+  this->ex_impl(params, cxt);
 }
 
 /* Params Inline Methods */
@@ -437,15 +392,13 @@ inline void Params::assert_valid_thread() const
     return;
   }
   if (main_thread_id_ != std::this_thread::get_id()) {
-    BLI_assert_unreachable();
+    lib_assert_unreachable();
   }
 #endif
 }
 
-/** \} */
+}  // namespace dune::fn::lazy_fn
 
-}  // namespace blender::fn::lazy_function
-
-namespace blender {
-namespace lf = fn::lazy_function;
+namespace dune {
+namespace lf = fn::lazy_fn;
 }
