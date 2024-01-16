cc5a399ba0bed607fc3548bf7c2093f34e54e607
Kielan <ki.lemons@gmail.com>
2024-01-15 22:28:00 -0800

    Create node_fn_util.cc

---
+++ source/dune/ nodes/fn/node_fn_util.cc
@@ -0,0 +1,24 @@
+#include "node_fn_util.hh"
+#include "node_util.hh"
+
+#include "NOD_socket_search_link.hh"
+
+static bool fn_node_poll_default(const NodeType * /*ntype*/,
+                                 const NodeTree *ntree,
+                                 const char **r_disabled_hint)
+{
+  /* Fn nodes are only supported in simulation node trees so far. */
+  if (!STREQ(ntree->idname, "GeometryNodeTree")) {
+    *r_disabled_hint = RPT_("Not a geometry node tree");
+    return false;
+  }
+  return true;
+}
+
+void fn_node_type_base(NodeType *ntype, int type, const char *name, short nclass)
+{
+  dune::node_type_base(ntype, type, name, nclass);
+  ntype->poll = fn_node_poll_default;
+  ntype->insert_link = node_insert_link_default;
+  ntype->gather_link_search_ops = dune::nodes::search_link_ops_for_basic_node;
+}
