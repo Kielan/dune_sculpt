ea3d5229f2821470f9a5b86c5fadb806d90aefff
Kielan <ki.lemons@gmail.com>
2024-06-12 16:44:54 -0700

    Update node_geo_bounding_box.cc

--- source/dune/ nodes/geometry/nodes/node_geo_bounding_box.cc
+++ source/dune/ nodes/geometry/nodes/node_geo_bounding_box.cc
@@ -1,12 +1,12 @@
-#include "GEO_mesh_primitive_cuboid.hh"
+#include "geo_mesh_primitive_cuboid.hh"
 
-#include "node_geometry_util.hh"
+#include "node_geo_util.hh"
 
 namespace dune::nodes::node_geo_bounding_box_cc {
 
-static void node_declare(NodeDeclarationBuilder &b)
+static void node_decl(NodeDeclBuilder &b)
 {
-  b.add_input<decl::Geometry>("Geometry");
+  b.add_input<decl::Geometry>("Geo");
   b.add_output<decl::Geometry>("Bounding Box");
   b.add_output<decl::Vector>("Min");
   b.add_output<decl::Vector>("Max");
@@ -14,11 +14,11 @@ static void node_declare(NodeDeclarationBuilder &b)
 
 static void node_geo_ex(GeoNodeExParams params)
 {
-  GeometrySet geometry_set = params.extract_input<GeometrySet>("Geometry");
+  GeoSet geo_set = params.extract_input<GeoSet>("Geo");
 
-  /* Compute the min and max of all realized geometry for the two
-   * vector outputs, which are only meant to consider real geometry. */
-  const std::optional<Bounds<float3>> bounds = geometry_set.compute_boundbox_without_instances();
+  /* Compute the min and max of all realized geo for the two
+   * vector outputs, which are only meant to consider real geo. */
+  const std::optional<Bounds<float3>> bounds = geo_set.compute_boundbox_wo_instances();
   if (!bounds) {
     params.set_output("Min", float3(0));
     params.set_output("Max", float3(0));
@@ -28,35 +28,35 @@ static void node_geo_ex(GeoNodeExParams params)
     params.set_output("Max", bounds->max);
   }
 
-  /* Generate the bounding box meshes inside each unique geometry set (including individually for
-   * every instance). Bc geometry components are reference counted anyway, we can just
-   * repurpose the original geometry sets for the output. */
+  /* Generate the bounding box meshes inside each unique geo set (including individually for
+   * every instance). Bc geo components are ref counted anyway, we can just
+   * repurpose the original geo sets for the output. */
   if (params.output_is_required("Bounding Box")) {
-    geometry_set.modify_geometry_sets([&](GeometrySet &sub_geometry) {
+    geo_set.modify_geo_sets([&](GeoSet &sub_geo) {
       std::optional<Bounds<float3>> sub_bounds;
 
       /* Reuse the min and max calc if this is the main "real" geometry set. */
-      if (&sub_geometry == &geometry_set) {
+      if (&sub_geo == &geom_set) {
         sub_bounds = bounds;
       }
       else {
-        sub_bounds = sub_geometry.compute_boundbox_without_instances();
+        sub_bounds = sub_geo.compute_boundbox_without_instances();
       }
 
       if (!sub_bounds) {
-        sub_geometry.remove_geometry_during_modify();
+        sub_geo.remove_geo_during_modify();
       }
       else {
         const float3 scale = sub_bounds->max - sub_bounds->min;
         const float3 center = sub_bounds->min + scale / 2.0f;
-        Mesh *mesh = geometry::create_cuboid_mesh(scale, 2, 2, 2, "uv_map");
-        transform_mesh(*mesh, center, math::Quaternion::identity(), float3(1));
-        sub_geometry.replace_mesh(mesh);
-        sub_geometry.keep_only_during_modify({GeometryComponent::Type::Mesh});
+        Mesh *mesh = geo::create_cuboid_mesh(scale, 2, 2, 2, "uv_map");
+        transform_mesh(*mesh, center, math::Quaternion::id(), float3(1));
+        sub_geo.replace_mesh(mesh);
+        sub_geo.keep_only_during_mod({GeoComponent::Type::Mesh});
       }
     });
 
-    params.set_output("Bounding Box", std::move(geometry_set));
+    params.set_output("Bounding Box", std::move(geo_set));
   }
 }
 
@@ -64,11 +64,11 @@ static void node_register()
 {
   static NodeType ntype;
 
-  geo_node_type_base(&ntype, GEO_NODE_BOUNDING_BOX, "Bounding Box", NODE_CLASS_GEOMETRY);
-  ntype.declare = node_declare;
-  ntype.geometry_node_ex = node_geo_ex;
+  geo_node_type_base(&ntype, GEO_NODE_BOUNDING_BOX, "Bounding Box", NODE_CLASS_GEO));
+  ntype.decl = node_decl;
+  ntype.geo_node_ex = node_geo_ex;
   nodeRegisterType(&ntype);
 }
-NOD_REGISTER_NODE(node_register)
+REGISTER_NODE(node_register)
 
-}  // namespace dube::nodes::node_geo_bounding_box_cc
+}  // namespace dune::nodes::node_geo_bounding_box_cc
