8a23baffd9a827cab9388bdfccc6c638a03b02bf
Kielan <ki.lemons@gmail.com>
2024-06-12 07:47:24 -0700

    Update node_geo_attribute_statistic.cc

--- source/dune/ nodes/geometry/nodes/node_geo_attribute_statistic.cc
+++ source/dune/ nodes/geometry/nodes/node_geo_attribute_statistic.cc
@@ -1,32 +1,32 @@
 #include <algorithm>
 #include <numeric>
 
-#include "NOD_rna_define.hh"
+#include "node_api_define.hh"
 
-#include "UI_interface.hh"
-#include "UI_resources.hh"
+#include "ui_interface.hh"
+#include "ui_resources.hh"
 
-#include "BLI_array_utils.hh"
-#include "BLI_math_base_safe.h"
+#include "lib_array_utils.hh"
+#include "lib_math_base_safe.h"
 
-#include "NOD_socket_search_link.hh"
+#include "api_socket_search_link.hh"
 
-#include "RNA_enum_types.hh"
+#include "api_enum_types.hh"
 
 #include "node_geometry_util.hh"
 
-namespace blender::nodes::node_geo_attribute_statistic_cc {
+namespace dune::nodes::node_geo_attr_statistic_cc {
 
-static void node_declare(NodeDeclarationBuilder &b)
+static void node_decl(NodeDeclBuilder &b)
 {
-  const bNode *node = b.node_or_null();
+  const Node *node = b.node_or_null();
 
-  b.add_input<decl::Geometry>("Geometry");
-  b.add_input<decl::Bool>("Selection").default_value(true).field_on_all().hide_value();
+  b.add_input<decl::Geo>("Geo");
+  b.add_input<decl::Bool>("Selection").default_val(true).field_on_all().hide_val();
 
   if (node != nullptr) {
     const eCustomDataType data_type = eCustomDataType(node->custom1);
-    b.add_input(data_type, "Attribute").hide_value().field_on_all();
+    b.add_input(data_type, "Attr").hide_val().field_on_all();
 
     b.add_output(data_type, "Mean");
     b.add_output(data_type, "Median");
@@ -39,19 +39,19 @@ static void node_declare(NodeDeclarationBuilder &b)
   }
 }
 
-static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
+static void node_layout(uiLayout *layout, Cxt * /*C*/, ApiPtr *ptr)
 {
   uiItemR(layout, ptr, "data_type", UI_ITEM_NONE, "", ICON_NONE);
   uiItemR(layout, ptr, "domain", UI_ITEM_NONE, "", ICON_NONE);
 }
 
-static void node_init(bNodeTree * /*tree*/, bNode *node)
+static void node_init(NodeTree * /*tree*/, Node *node)
 {
   node->custom1 = CD_PROP_FLOAT;
   node->custom2 = int16_t(AttrDomain::Point);
 }
 
-static std::optional<eCustomDataType> node_type_from_other_socket(const bNodeSocket &socket)
+static std::optional<eCustomDataType> node_type_from_other_socket(const NodeSocket &socket)
 {
   switch (socket.type) {
     case SOCK_FLOAT:
@@ -68,8 +68,8 @@ static std::optional<eCustomDataType> node_type_from_other_socket(const bNodeSoc
 
 static void node_gather_link_searches(GatherLinkSearchOpParams &params)
 {
-  const bNodeType &node_type = params.node_type();
-  const NodeDeclaration &declaration = *params.node_type().static_declaration;
+  const NodeType &node_type = params.node_type();
+  const NodeDeclaration &decl = *params.node_type().static_declaration;
   search_link_ops_for_declarations(params, declaration.inputs);
 
   const std::optional<eCustomDataType> type = node_type_from_other_socket(params.other_socket());
@@ -78,10 +78,10 @@ static void node_gather_link_searches(GatherLinkSearchOpParams &params)
   }
 
   if (params.in_out() == SOCK_IN) {
-    params.add_item(IFACE_("Attribute"), [node_type, type](LinkSearchOpParams &params) {
-      bNode &node = params.add_node(node_type);
+    params.add_item(IFACE_("Attr"), [node_type, type](LinkSearchOpParams &params) {
+      Node &node = params.add_node(node_type);
       node.custom1 = *type;
-      params.update_and_connect_available_socket(node, "Attribute");
+      params.update_and_connect_available_socket(node, "Attr");
     });
   }
   else {
@@ -89,7 +89,7 @@ static void node_gather_link_searches(GatherLinkSearchOpParams &params)
          {"Mean", "Median", "Sum", "Min", "Max", "Range", "Standard Deviation", "Variance"})
     {
       params.add_item(IFACE_(name.c_str()), [node_type, name, type](LinkSearchOpParams &params) {
-        bNode &node = params.add_node(node_type);
+        Node &node = params.add_node(node_type);
         node.custom1 = *type;
         params.update_and_connect_available_socket(node, name);
       });
@@ -108,13 +108,13 @@ static float compute_variance(const Span<float> data, const float mean)
     return 0.0f;
   }
 
-  float sum_of_squared_differences = std::accumulate(
-      data.begin(), data.end(), 0.0f, [mean](float accumulator, float value) {
-        float difference = mean - value;
-        return accumulator + difference * difference;
+  float sum_of_squared_diffs = std::accumulate(
+      data.begin(), data.end(), 0.0f, [mean](float accumulator, float val) {
+        float diff = mean - val;
+        return accumulator + diff * diff;
       });
 
-  return sum_of_squared_differences / data.size();
+  return sum_of_squared_diffs / data.size();
 }
 
 static float median_of_sorted_span(const Span<float> data)
@@ -125,46 +125,46 @@ static float median_of_sorted_span(const Span<float> data)
 
   const float median = data[data.size() / 2];
 
-  /* For spans of even length, the median is the average of the middle two elements. */
+  /* For spans of even length, the median is the avg of the middle 2 elems. */
   if (data.size() % 2 == 0) {
     return (median + data[data.size() / 2 - 1]) * 0.5f;
   }
   return median;
 }
 
-static void node_geo_exec(GeoNodeExecParams params)
+static void node_geo_ex(GeoNodeExParams params)
 {
-  GeometrySet geometry_set = params.get_input<GeometrySet>("Geometry");
-  const bNode &node = params.node();
+  GeometrySet geometry_set = params.get_input<GeoSet>("Geo");
+  const Node &node = params.node();
   const eCustomDataType data_type = eCustomDataType(node.custom1);
   const AttrDomain domain = AttrDomain(node.custom2);
-  Vector<const GeometryComponent *> components = geometry_set.get_components();
+  Vector<const GeometryComponent *> components = geo_set.get_components();
 
-  const Field<bool> selection_field = params.get_input<Field<bool>>("Selection");
+  const Field<bool> sel_field = params.get_input<Field<bool>>("Sel");
 
   switch (data_type) {
     case CD_PROP_FLOAT: {
-      const Field<float> input_field = params.get_input<Field<float>>("Attribute");
+      const Field<float> input_field = params.get_input<Field<float>>("Attr");
       Vector<float> data;
       for (const GeometryComponent *component : components) {
-        const std::optional<AttributeAccessor> attributes = component->attributes();
-        if (!attributes.has_value()) {
+        const std::optional<AttrAccessor> attrs = component->attrs();
+        if (!attrs.has_val()) {
           continue;
         }
-        if (attributes->domain_supported(domain)) {
-          const bke::GeometryFieldContext field_context{*component, domain};
-          fn::FieldEvaluator data_evaluator{field_context, attributes->domain_size(domain)};
-          data_evaluator.add(input_field);
-          data_evaluator.set_selection(selection_field);
-          data_evaluator.evaluate();
-          const VArray<float> component_data = data_evaluator.get_evaluated<float>(0);
-          const IndexMask selection = data_evaluator.get_evaluated_selection_as_mask();
+        if (attrs->domain_supported(domain)) {
+          const dune::GeoFieldCxt field_cxt{*component, domain};
+          fn::FieldEval data_eval{field_cxt, attrs->domain_size(domain)};
+          data_eval.add(input_field);
+          data_eval.set_sel(sel_field);
+          data_eval.eval();
+          const VArray<float> component_data = data_eval.get_eval<float>(0);
+          const IndexMask sel = data_eval.get_eval_sel_as_mask();
 
           const int next_data_index = data.size();
-          data.resize(next_data_index + selection.size());
-          MutableSpan<float> selected_data = data.as_mutable_span().slice(next_data_index,
-                                                                          selection.size());
-          array_utils::gather(component_data, selection, selected_data);
+          data.resize(next_data_index + sel.size());
+          MutableSpan<float> sel_data = data.as_mutable_span().slice(next_data_index,
+                                                                     sel.size());
+          array_utils::gather(component_data, sel, sel_data);
         }
       }
 
@@ -224,25 +224,25 @@ static void node_geo_exec(GeoNodeExecParams params)
     case CD_PROP_FLOAT3: {
       const Field<float3> input_field = params.get_input<Field<float3>>("Attribute");
       Vector<float3> data;
-      for (const GeometryComponent *component : components) {
-        const std::optional<AttributeAccessor> attributes = component->attributes();
-        if (!attributes.has_value()) {
+      for (const GeoComponent *component : components) {
+        const std::optional<AttrAccessor> attrs = component->attributes();
+        if (!attrs.has_val()) {
           continue;
         }
-        if (attributes->domain_supported(domain)) {
-          const bke::GeometryFieldContext field_context{*component, domain};
-          fn::FieldEvaluator data_evaluator{field_context, attributes->domain_size(domain)};
-          data_evaluator.add(input_field);
-          data_evaluator.set_selection(selection_field);
-          data_evaluator.evaluate();
-          const VArray<float3> component_data = data_evaluator.get_evaluated<float3>(0);
-          const IndexMask selection = data_evaluator.get_evaluated_selection_as_mask();
+        if (attrs->domain_supported(domain)) {
+          const dune::GeoFieldCxt field_cxt{*component, domain};
+          fn::FieldEval data_eval{field_cxt, attrs->domain_size(domain)};
+          data_eval.add(input_field);
+          data_eval.set_sel(sel_field);
+          data_eval.eval();
+          const VArray<float3> component_data = data_eval.get_eval<float3>(0);
+          const IndexMask sel = data_eval.get_eval_sel_as_mask();
 
           const int next_data_index = data.size();
-          data.resize(data.size() + selection.size());
-          MutableSpan<float3> selected_data = data.as_mutable_span().slice(next_data_index,
-                                                                           selection.size());
-          array_utils::gather(component_data, selection, selected_data);
+          data.resize(data.size() + sel.size());
+          MutableSpan<float3> sel_data = data.as_mutable_span().slice(next_data_index,
+                                                                      sel.size());
+          array_utils::gather(component_data, sel, sel_data);
         }
       }
 
@@ -328,20 +328,20 @@ static void node_geo_exec(GeoNodeExecParams params)
   }
 }
 
-static void node_rna(StructRNA *srna)
+static void node_api(ApiStruct *sapi)
 {
-  RNA_def_node_enum(
-      srna,
+  api_def_node_enum(
+      sapi,
       "data_type",
       "Data Type",
-      "The data type the attribute is converted to before calculating the results",
-      api_enum_attribute_type_items,
-      NOD_inline_enum_accessors(custom1),
+      "The data type the attr is converted to before calculating the results",
+      api_enum_attr_type_items,
+      node_inline_enum_accessors(custom1),
       CD_PROP_FLOAT,
-      [](Cxt * /*C*/, ApiPtr * /*ptr*/, PropertyRNA * /*prop*/, bool *r_free) {
+      [](Cxt * /*C*/, ApiPtr * /*ptr*/, ApiProp * /*prop*/, bool *r_free) {
         *r_free = true;
         return enum_items_filter(api_enum_attribute_type_items, [](const EnumPropItem &item) {
-          return ELEM(item.val, CD_PROP_FLOAT, CD_PROP_FLOAT3);
+          return elem(item.val, CD_PROP_FLOAT, CD_PROP_FLOAT3);
         });
       });
 
@@ -349,28 +349,28 @@ static void node_rna(StructRNA *srna)
                     "domain",
                     "Domain",
                     "Which domain to read the data from",
-                    api_enum_attribute_domain_items,
-                    NOD_inline_enum_accessors(custom2),
+                    api_enum_attr_domain_items,
+                    node_inline_enum_accessors(custom2),
                     int(AttrDomain::Point),
-                    enums::domain_experimental_pen_version3_fn);
+                    enums::domain_experimental_pen_v3_fn);
 }
 
 static void node_register()
 {
-  static bNodeType ntype;
+  static NodeType ntype;
 
   geo_node_type_base(
-      &ntype, GEO_NODE_ATTRIBUTE_STATISTIC, "Attribute Statistic", NODE_CLASS_ATTRIBUTE);
+      &ntype, GEO_NODE_ATTR_STATISTIC, "Attr Statistic", NODE_CLASS_ATTR);
 
-  ntype.initfunc = node_init;
-  ntype.declare = node_declare;
-  ntype.geometry_node_execute = node_geo_exec;
-  ntype.draw_buttons = node_layout;
+  ntype.initfn = node_init;
+  ntype.decl = node_decl;
+  ntype.geo_node_ex = node_geo_ex;
+  ntype.drw_btns = node_layout;
   ntype.gather_link_search_ops = node_gather_link_searches;
   nodeRegisterType(&ntype);
 
-  node_rna(ntype.rna_ext.srna);
+  node_api(ntype.api_ext.sapi);
 }
-NOD_REGISTER_NODE(node_register)
+REGISTER_NODE(node_register)
 
 }  // namespace blender::nodes::node_geo_attribute_statistic_cc
