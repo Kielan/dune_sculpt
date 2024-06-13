f162c182b1dca7c2611dd6a6ba31918cb3f690db
Kielan <ki.lemons@gmail.com>
2024-06-13 12:14:26 -0700

    Update node_geo_curve_length.cc

--- source/dune/ nodes/geometry/nodes/node_geo_curve_length.cc
+++ source/dune/ nodes/geometry/nodes/node_geo_curve_length.cc
@@ -1,43 +1,43 @@
-#include "BKE_curves.hh"
-#include "BKE_grease_pencil.hh"
+#include "dune_curves.hh"
+#include "dune_pen.hh"
 
-#include "node_geometry_util.hh"
+#include "node_geo_util.hh"
 
-namespace blender::nodes::node_geo_curve_length_cc {
+namespace dune::nodes::node_geo_curve_length_cc {
 
-static void node_declare(NodeDeclarationBuilder &b)
+static void node_decl(NodeDeclBuilder &b)
 {
-  b.add_input<decl::Geometry>("Curve").supported_type(
-      {GeometryComponent::Type::Curve, GeometryComponent::Type::GreasePencil});
+  b.add_input<decl::Geo>("Curve").supported_type(
+      {GeoComponent::Type::Curve, GeoComponent::Type::Pen});
   b.add_output<decl::Float>("Length");
 }
 
-static float curves_total_length(const bke::CurvesGeometry &curves)
+static float curves_total_length(const dune::CurvesGeo &curves)
 {
   const VArray<bool> cyclic = curves.cyclic();
-  curves.ensure_evaluated_lengths();
+  curves.ensure_eval_lengths();
 
   float total_length = 0.0f;
   for (const int i : curves.curves_range()) {
-    total_length += curves.evaluated_length_total_for_curve(i, cyclic[i]);
+    total_length += curves.eval_length_total_for_curve(i, cyclic[i]);
   }
   return total_length;
 }
 
-static void node_geo_exec(GeoNodeExecParams params)
+static void node_geo_ex(GeoNodeExParams params)
 {
-  GeometrySet geometry_set = params.extract_input<GeometrySet>("Curve");
+  GeoSet geo_set = params.extract_input<GeoSet>("Curve");
   float length = 0.0f;
   if (geometry_set.has_curves()) {
-    const Curves &curves_id = *geometry_set.get_curves();
-    const bke::CurvesGeometry &curves = curves_id.geometry.wrap();
+    const Curves &curves_id = *geo_set.get_curves();
+    const dune::CurvesGeo &curves = curves_id.geo.wrap();
     length += curves_total_length(curves);
   }
-  else if (geometry_set.has_grease_pencil()) {
-    using namespace bke::greasepencil;
-    const GreasePencil &grease_pencil = *geometry_set.get_grease_pencil();
-    for (const int layer_index : grease_pencil.layers().index_range()) {
-      const Drawing *drawing = get_eval_grease_pencil_layer_drawing(grease_pencil, layer_index);
+  else if (geo_set.has_pen()) {
+    using namespace dune::pen;
+    const Pen &pen = *geo_set.get_pen();
+    for (const int layer_index : pen.layers().index_range()) {
+      const Drawing *drawing = get_eval_pen_layer_drawing(pen, layer_index);
       if (drawing == nullptr) {
         continue;
       }
