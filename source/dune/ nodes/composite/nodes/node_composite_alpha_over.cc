c223cef0c29d9c1dc9df85770307ace831c11162
Kielan <ki.lemons@gmail.com>
2024-06-13 13:07:40 -0700

    Update node_composite_alpha_over.cc

--- source/dune/ nodes/composite/nodes/node_composite_alpha_over.cc
+++ source/dune/ nodes/composite/nodes/node_composite_alpha_over.cc
@@ -1,41 +1,41 @@
-#include "UI_interface.hh"
-#include "UI_resources.hh"
+#include "ui_interface.hh"
+#include "ui_resources.hh"
 
-#include "GPU_material.h"
+#include "gpu_material.h"
 
-#include "COM_shader_node.hh"
+#include "com_shader_node.hh"
 
 #include "node_composite_util.hh"
 
-/* **************** ALPHAOVER ******************** */
+/* ALPHAOVER */
 
-namespace blender::nodes::node_composite_alpha_over_cc {
+namespace dune::nodes::node_composite_alpha_over_cc {
 
-NODE_STORAGE_FUNCS(NodeTwoFloats)
+NODE_STORAGE_FNS(NodeTwoFloats)
 
-static void cmp_node_alphaover_declare(NodeDeclarationBuilder &b)
+static void cmp_node_alphaover_declare(NodeDeclBuilder &b)
 {
   b.add_input<decl::Float>("Fac")
-      .default_value(1.0f)
+      .default_val(1.0f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR)
       .compositor_domain_priority(2);
-  b.add_input<decl::Color>("Image")
-      .default_value({1.0f, 1.0f, 1.0f, 1.0f})
+  b.add_input<decl::Color>("Img")
+      .default_val({1.0f, 1.0f, 1.0f, 1.0f})
       .compositor_domain_priority(0);
-  b.add_input<decl::Color>("Image", "Image_001")
-      .default_value({1.0f, 1.0f, 1.0f, 1.0f})
+  b.add_input<decl::Color>("Img", "Img_001")
+      .default_val({1.0f, 1.0f, 1.0f, 1.0f})
       .compositor_domain_priority(1);
-  b.add_output<decl::Color>("Image");
+  b.add_output<decl::Color>("Img");
 }
 
-static void node_alphaover_init(bNodeTree * /*ntree*/, bNode *node)
+static void node_alphaover_init(NodeTree * /*ntree*/, Node *node)
 {
-  node->storage = MEM_cnew<NodeTwoFloats>(__func__);
+  node->storage = mem_cnew<NodeTwoFloats>(__func__);
 }
 
-static void node_composit_buts_alphaover(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
+static void node_composit_buts_alphaover(uiLayout *layout, Cxt * /*C*/, ApiPtr *ptr)
 {
   uiLayout *col;
 
@@ -57,35 +57,35 @@ class AlphaOverShaderNode : public ShaderNode {
 
     const float premultiply_factor = get_premultiply_factor();
     if (premultiply_factor != 0.0f) {
-      GPU_stack_link(material,
+      gpu_stack_link(material,
                      &bnode(),
                      "node_composite_alpha_over_mixed",
                      inputs,
                      outputs,
-                     GPU_uniform(&premultiply_factor));
+                     gpu_uniform(&premultiply_factor));
       return;
     }
 
     if (get_use_premultiply()) {
-      GPU_stack_link(material, &bnode(), "node_composite_alpha_over_key", inputs, outputs);
+      gpu_stack_link(material, &bnode(), "node_composite_alpha_over_key", inputs, outputs);
       return;
     }
 
-    GPU_stack_link(material, &bnode(), "node_composite_alpha_over_premultiply", inputs, outputs);
+    gpu_stack_link(material, &bnode(), "node_composite_alpha_over_premultiply", inputs, outputs);
   }
 
   bool get_use_premultiply()
   {
-    return bnode().custom1;
+    return node().custom1;
   }
 
   float get_premultiply_factor()
   {
-    return node_storage(bnode()).x;
+    return node_storage(node()).x;
   }
 };
 
-static ShaderNode *get_compositor_shader_node(DNode node)
+static ShaderNode *get_compositor_shader_node(Node node)
 {
   return new AlphaOverShaderNode(node);
 }
@@ -96,12 +96,12 @@ void register_node_type_cmp_alphaover()
 {
   namespace file_ns = dune::nodes::node_composite_alpha_over_cc;
 
-  static bNodeType ntype;
+  static NodeType ntype;
 
   cmp_node_type_base(&ntype, CMP_NODE_ALPHAOVER, "Alpha Over", NODE_CLASS_OP_COLOR);
-  ntype.declare = file_ns::cmp_node_alphaover_declare;
-  ntype.draw_buttons = file_ns::node_composit_buts_alphaover;
-  ntype.initfunc = file_ns::node_alphaover_init;
+  ntype.decl = file_ns::cmp_node_alphaover_decl;
+  ntype.draw_btns = file_ns::node_composit_buts_alphaover;
+  ntype.initfn = file_ns::node_alphaover_init;
   node_type_storage(
       &ntype, "NodeTwoFloats", node_free_standard_storage, node_copy_standard_storage);
   ntype.get_compositor_shader_node = file_ns::get_compositor_shader_node;
