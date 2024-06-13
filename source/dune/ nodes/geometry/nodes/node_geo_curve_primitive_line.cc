756a1d0507eba2832e98ef7f53b41a6b29a08415
Kielan <ki.lemons@gmail.com>
2024-06-13 13:03:28 -0700

    Update node_geo_curve_primitive_line.cc

--- source/dune/ nodes/geometry/nodes/node_geo_curve_primitive_line.cc
+++ source/dune/ nodes/geometry/nodes/node_geo_curve_primitive_line.cc
@@ -1,19 +1,19 @@
-#include "BKE_curves.hh"
+#include "funr_curves.hh"
 
-#include "NOD_rna_define.hh"
+#include "node_api_define.hh"
 
-#include "UI_interface.hh"
-#include "UI_resources.hh"
+#include "ui_interface.hh"
+#include "ui_resources.hh"
 
-#include "node_geometry_util.hh"
+#include "node_geo_util.hh"
 
-namespace blender::nodes::node_geo_curve_primitive_line_cc {
+namespace dune::nodes::node_geo_curve_primitive_line_cc {
 
-NODE_STORAGE_FUNCS(NodeGeometryCurvePrimitiveLine)
+NODE_STORAGE_FNS(NodeGeoCurvePrimitiveLine)
 
-static void node_declare(NodeDeclarationBuilder &b)
+static void node_decl(NodeDeclBuilder &b)
 {
-  auto enable_direction = [](bNode &node) {
+  auto enable_direction = [](Node &node) {
     node_storage(node).mode = GEO_NODE_CURVE_PRIMITIVE_LINE_MODE_DIRECTION;
   };
 
@@ -21,58 +21,58 @@ static void node_declare(NodeDeclarationBuilder &b)
       .subtype(PROP_TRANSLATION)
       .description("Position of the first control point");
   b.add_input<decl::Vector>("End")
-      .default_value({0.0f, 0.0f, 1.0f})
+      .default_val({0.0f, 0.0f, 1.0f})
       .subtype(PROP_TRANSLATION)
       .description("Position of the second control point")
-      .make_available([](bNode &node) {
+      .make_available([](Node &node) {
         node_storage(node).mode = GEO_NODE_CURVE_PRIMITIVE_LINE_MODE_POINTS;
       });
   b.add_input<decl::Vector>("Direction")
-      .default_value({0.0f, 0.0f, 1.0f})
+      .default_val({0.0f, 0.0f, 1.0f})
       .description("Direction the line is going in. The length of this vector does not matter")
       .make_available(enable_direction);
   b.add_input<decl::Float>("Length")
-      .default_value(1.0f)
+      .default_val(1.0f)
       .subtype(PROP_DISTANCE)
       .description("Distance between the two points")
       .make_available(enable_direction);
-  b.add_output<decl::Geometry>("Curve");
+  b.add_output<decl::Geo>("Curve");
 }
 
-static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
+static void node_layout(uiLayout *layout, Cxt * /*C*/, ApiPtr *ptr)
 {
   uiItemR(layout, ptr, "mode", UI_ITEM_R_EXPAND, nullptr, ICON_NONE);
 }
 
-static void node_init(bNodeTree * /*tree*/, bNode *node)
+static void node_init(NodeTree * /*tree*/, Node *node)
 {
-  NodeGeometryCurvePrimitiveLine *data = MEM_cnew<NodeGeometryCurvePrimitiveLine>(__func__);
+  NodeGeoCurvePrimitiveLine *data = mem_cnew<NodeGeoCurvePrimitiveLine>(__func__);
 
   data->mode = GEO_NODE_CURVE_PRIMITIVE_LINE_MODE_POINTS;
   node->storage = data;
 }
 
-static void node_update(bNodeTree *ntree, bNode *node)
+static void node_update(NodeTree *ntree, Node *node)
 {
-  const NodeGeometryCurvePrimitiveLine &storage = node_storage(*node);
-  const GeometryNodeCurvePrimitiveLineMode mode = (GeometryNodeCurvePrimitiveLineMode)storage.mode;
+  const NodeGeoCurvePrimitiveLine &storage = node_storage(*node);
+  const GeoNodeCurvePrimitiveLineMode mode = (GeoNodeCurvePrimitiveLineMode)storage.mode;
 
-  bNodeSocket *p2_socket = static_cast<bNodeSocket *>(node->inputs.first)->next;
-  bNodeSocket *direction_socket = p2_socket->next;
-  bNodeSocket *length_socket = direction_socket->next;
+  NodeSocket *p2_socket = static_cast<NodeSocket *>(node->inputs.first)->next;
+  NodeSocket *direction_socket = p2_socket->next;
+  NodeSocket *length_socket = direction_socket->next;
 
-  bke::nodeSetSocketAvailability(
+  dune::nodeSetSocketAvailability(
       ntree, p2_socket, mode == GEO_NODE_CURVE_PRIMITIVE_LINE_MODE_POINTS);
-  bke::nodeSetSocketAvailability(
+  dune::nodeSetSocketAvailability(
       ntree, direction_socket, mode == GEO_NODE_CURVE_PRIMITIVE_LINE_MODE_DIRECTION);
-  bke::nodeSetSocketAvailability(
+  dune::nodeSetSocketAvailability(
       ntree, length_socket, mode == GEO_NODE_CURVE_PRIMITIVE_LINE_MODE_DIRECTION);
 }
 
 static Curves *create_point_line_curve(const float3 start, const float3 end)
 {
-  Curves *curves_id = bke::curves_new_nomain_single(2, CURVE_TYPE_POLY);
-  bke::CurvesGeometry &curves = curves_id->geometry.wrap();
+  Curves *curves_id = dune::curves_new_nomain_single(2, CURVE_TYPE_POLY);
+  dune::CurvesGeo &curves = curves_id->geo.wrap();
 
   curves.positions_for_write().first() = start;
   curves.positions_for_write().last() = end;
@@ -84,8 +84,8 @@ static Curves *create_direction_line_curve(const float3 start,
                                            const float3 direction,
                                            const float length)
 {
-  Curves *curves_id = bke::curves_new_nomain_single(2, CURVE_TYPE_POLY);
-  bke::CurvesGeometry &curves = curves_id->geometry.wrap();
+  Curves *curves_id = dune::curves_new_nomain_single(2, CURVE_TYPE_POLY);
+  dune::CurvesGeo &curves = curves_id->geo.wrap();
 
   curves.positions_for_write().first() = start;
   curves.positions_for_write().last() = math::normalize(direction) * length + start;
@@ -93,10 +93,10 @@ static Curves *create_direction_line_curve(const float3 start,
   return curves_id;
 }
 
-static void node_geo_exec(GeoNodeExecParams params)
+static void node_geo_ex(GeoNodeExParams params)
 {
-  const NodeGeometryCurvePrimitiveLine &storage = node_storage(params.node());
-  const GeometryNodeCurvePrimitiveLineMode mode = (GeometryNodeCurvePrimitiveLineMode)storage.mode;
+  const NodeGeoCurvePrimitiveLine &storage = node_storage(params.node());
+  const GeoNodeCurvePrimitiveLineMode mode = (GeoNodeCurvePrimitiveLineMode)storage.mode;
 
   Curves *curves = nullptr;
   if (mode == GEO_NODE_CURVE_PRIMITIVE_LINE_MODE_POINTS) {
@@ -109,12 +109,12 @@ static void node_geo_exec(GeoNodeExecParams params)
                                          params.extract_input<float>("Length"));
   }
 
-  params.set_output("Curve", GeometrySet::from_curves(curves));
+  params.set_output("Curve", GeoSet::from_curves(curves));
 }
 
-static void node_rna(StructRNA *srna)
+static void node_api(ApiStruct *sapi)
 {
-  static const EnumPropertyItem mode_items[] = {
+  static const EnumPropItem mode_items[] = {
       {GEO_NODE_CURVE_PRIMITIVE_LINE_MODE_POINTS,
        "POINTS",
        ICON_NONE,
@@ -128,32 +128,32 @@ static void node_rna(StructRNA *srna)
       {0, nullptr, 0, nullptr, nullptr},
   };
 
-  RNA_def_node_enum(srna,
+  api_def_node_enum(sapi,
                     "mode",
                     "Mode",
                     "Method used to determine radius and placement",
                     mode_items,
-                    NOD_storage_enum_accessors(mode),
+                    node_storage_enum_accessors(mode),
                     GEO_NODE_CURVE_PRIMITIVE_LINE_MODE_POINTS);
 }
 
 static void node_register()
 {
-  static bNodeType ntype;
-  geo_node_type_base(&ntype, GEO_NODE_CURVE_PRIMITIVE_LINE, "Curve Line", NODE_CLASS_GEOMETRY);
-  ntype.initfunc = node_init;
-  ntype.updatefunc = node_update;
+  static NodeType ntype;
+  geo_node_type_base(&ntype, GEO_NODE_CURVE_PRIMITIVE_LINE, "Curve Line", NODE_CLASS_GEO);
+  ntype.initfn = node_init;
+  ntype.updatefn = node_update;
   node_type_storage(&ntype,
-                    "NodeGeometryCurvePrimitiveLine",
+                    "NodeGeoCurvePrimitiveLine",
                     node_free_standard_storage,
                     node_copy_standard_storage);
-  ntype.declare = node_declare;
-  ntype.geometry_node_execute = node_geo_exec;
-  ntype.draw_buttons = node_layout;
+  ntype.decl = node_decl;
+  ntype.geo_node_ex = node_geo_ex;
+  ntype.drw_btns = node_layout;
   nodeRegisterType(&ntype);
 
-  node_rna(ntype.rna_ext.srna);
+  node_api(ntype.api_ext.sapi);
 }
-NOD_REGISTER_NODE(node_register)
+REGISTER_NODE(node_register)
 
-}  // namespace blender::nodes::node_geo_curve_primitive_line_cc
+}  // namespace dune::nodes::node_geo_curve_primitive_line_cc
