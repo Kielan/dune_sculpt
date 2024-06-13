28c243dbfb312e239d81d2b25de027c15d89e5cd
Kielan <ki.lemons@gmail.com>
2024-06-13 12:09:34 -0700

    Update node_geo_curve_handle_type_selection.cc

--- source/dune/ nodes/geometry/nodes/node_geo_curve_handle_type_selection.cc
+++ source/dune/ nodes/geometry/nodes/node_geo_curve_handle_type_selection.cc
@@ -1,35 +1,35 @@
-#include "BKE_curves.hh"
+#include "dune_curves.hh"
 
-#include "UI_interface.hh"
-#include "UI_resources.hh"
+#include "ui_interface.hh"
+#include "ui_resources.hh"
 
-#include "node_geometry_util.hh"
+#include "node_geo_util.hh"
 
-namespace blender::nodes::node_geo_curve_handle_type_selection_cc {
+namespace dune::nodes::node_geo_curve_handle_type_sel_cc {
 
-NODE_STORAGE_FUNCS(NodeGeometryCurveSelectHandles)
+NODE_STORAGE_FNS(NodeGeoCurveSelHandles)
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_output<decl::Bool>("Selection").field_source();
 }
 
-static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
+static void node_layout(uiLayout *layout, Cxt * /*C*/, ApiPtr *ptr)
 {
   uiItemR(layout, ptr, "mode", UI_ITEM_R_EXPAND, nullptr, ICON_NONE);
   uiItemR(layout, ptr, "handle_type", UI_ITEM_NONE, "", ICON_NONE);
 }
 
-static void node_init(bNodeTree * /*tree*/, bNode *node)
+static void node_init(NodeTree * /*tree*/, Node *node)
 {
-  NodeGeometryCurveSelectHandles *data = MEM_cnew<NodeGeometryCurveSelectHandles>(__func__);
+  NodeGeoCurveSelHandles *data = mem_cnew<NodeGeoCurveSelHandles>(__func__);
 
   data->handle_type = GEO_NODE_CURVE_HANDLE_AUTO;
   data->mode = GEO_NODE_CURVE_HANDLE_LEFT | GEO_NODE_CURVE_HANDLE_RIGHT;
   node->storage = data;
 }
 
-static HandleType handle_type_from_input_type(const GeometryNodeCurveHandleType type)
+static HandleType handle_type_from_input_type(const GeoNodeCurveHandleType type)
 {
   switch (type) {
     case GEO_NODE_CURVE_HANDLE_AUTO:
@@ -41,14 +41,14 @@ static HandleType handle_type_from_input_type(const GeometryNodeCurveHandleType 
     case GEO_NODE_CURVE_HANDLE_VECTOR:
       return BEZIER_HANDLE_VECTOR;
   }
-  BLI_assert_unreachable();
+  lib_assert_unreachable();
   return BEZIER_HANDLE_AUTO;
 }
 
-static void select_by_handle_type(const bke::CurvesGeometry &curves,
-                                  const HandleType type,
-                                  const GeometryNodeCurveHandleMode mode,
-                                  const MutableSpan<bool> r_selection)
+static void sel_by_handle_type(const dune::CurvesGeo &curves,
+                               const HandleType type,
+                               const GeoNodeCurveHandleMode mode,
+                               const MutableSpan<bool> r_sel)
 {
   const OffsetIndices points_by_curve = curves.points_by_curve();
   VArray<int8_t> curve_types = curves.curve_types();
@@ -58,40 +58,40 @@ static void select_by_handle_type(const bke::CurvesGeometry &curves,
   for (const int i_curve : curves.curves_range()) {
     const IndexRange points = points_by_curve[i_curve];
     if (curve_types[i_curve] != CURVE_TYPE_BEZIER) {
-      r_selection.slice(points).fill(false);
+      r_sel.slice(points).fill(false);
     }
     else {
       for (const int i_point : points) {
-        r_selection[i_point] = (mode & GEO_NODE_CURVE_HANDLE_LEFT && left[i_point] == type) ||
-                               (mode & GEO_NODE_CURVE_HANDLE_RIGHT && right[i_point] == type);
+        r_sel[i_point] = (mode & GEO_NODE_CURVE_HANDLE_LEFT && left[i_point] == type) ||
+                         (mode & GEO_NODE_CURVE_HANDLE_RIGHT && right[i_point] == type);
       }
     }
   }
 }
 
-class HandleTypeFieldInput final : public bke::CurvesFieldInput {
+class HandleTypeFieldInput final : public dune::CurvesFieldInput {
   HandleType type_;
-  GeometryNodeCurveHandleMode mode_;
+  GeoNodeCurveHandleMode mode_;
 
  public:
   HandleTypeFieldInput(HandleType type, GeometryNodeCurveHandleMode mode)
-      : bke::CurvesFieldInput(CPPType::get<bool>(), "Handle Type Selection node"),
+      : dune::CurvesFieldInput(CPPType::get<bool>(), "Handle Type Selection node"),
         type_(type),
         mode_(mode)
   {
     category_ = Category::Generated;
   }
 
-  GVArray get_varray_for_context(const bke::CurvesGeometry &curves,
+  GVArray get_varray_for_cxt(const dune::CurvesGeo &curves,
                                  const AttrDomain domain,
                                  const IndexMask &mask) const final
   {
     if (domain != AttrDomain::Point) {
       return {};
     }
-    Array<bool> selection(mask.min_array_size());
-    select_by_handle_type(curves, type_, mode_, selection);
-    return VArray<bool>::ForContainer(std::move(selection));
+    Array<bool> sel(mask.min_array_size());
+    sel_by_handle_type(curves, type_, mode_, sel);
+    return VArray<bool>::ForContainer(std::move(sel));
   }
 
   uint64_t hash() const override
@@ -101,15 +101,15 @@ class HandleTypeFieldInput final : public bke::CurvesFieldInput {
 
   bool is_equal_to(const fn::FieldNode &other) const override
   {
-    if (const HandleTypeFieldInput *other_handle_selection =
+    if (const HandleTypeFieldInput *other_handle_sel =
             dynamic_cast<const HandleTypeFieldInput *>(&other))
     {
-      return mode_ == other_handle_selection->mode_ && type_ == other_handle_selection->type_;
+      return mode_ == other_handle_sel->mode_ && type_ == other_handle_sel->type_;
     }
     return false;
   }
 
-  std::optional<AttrDomain> preferred_domain(const CurvesGeometry & /*curves*/) const
+  std::optional<AttrDomain> preferred_domain(const CurvesGeo & /*curves*/) const
   {
     return AttrDomain::Point;
   }
@@ -117,32 +117,32 @@ class HandleTypeFieldInput final : public bke::CurvesFieldInput {
 
 static void node_geo_ex(GeoNodeExParams params)
 {
-  const NodeGeometryCurveSelectHandles &storage = node_storage(params.node());
+  const NodeGeoCurveSelHandles &storage = node_storage(params.node());
   const HandleType handle_type = handle_type_from_input_type(
-      (GeometryNodeCurveHandleType)storage.handle_type);
-  const GeometryNodeCurveHandleMode mode = (GeometryNodeCurveHandleMode)storage.mode;
+      (GeoNodeCurveHandleType)storage.handle_type);
+  const GeoNodeCurveHandleMode mode = (GeoNodeCurveHandleMode)storage.mode;
 
-  Field<bool> selection_field{std::make_shared<HandleTypeFieldInput>(handle_type, mode)};
-  params.set_output("Selection", std::move(selection_field));
+  Field<bool> sel_field{std::make_shared<HandleTypeFieldInput>(handle_type, mode)};
+  params.set_output("Sel", std::move(sel_field));
 }
 
 static void node_register()
 {
-  static bNodeType ntype;
+  static NodeType ntype;
 
   geo_node_type_base(
-      &ntype, GEO_NODE_CURVE_HANDLE_TYPE_SELECTION, "Handle Type Selection", NODE_CLASS_INPUT);
-  ntype.declare = node_declare;
-  ntype.geometry_node_execute = node_geo_exec;
-  ntype.initfunc = node_init;
+      &ntype, GEO_NODE_CURVE_HANDLE_TYPE_SEL, "Handle Type Selection", NODE_CLASS_INPUT);
+  ntype.decl = node_declare;
+  ntype.geo_node_ex = node_geo_ex;
+  ntype.initfn = node_init;
   node_type_storage(&ntype,
-                    "NodeGeometryCurveSelectHandles",
+                    "NodeGeoCurveSelHandles",
                     node_free_standard_storage,
                     node_copy_standard_storage);
-  ntype.draw_buttons = node_layout;
+  ntype.draw_btns = node_layout;
 
   nodeRegisterType(&ntype);
 }
 NOD_REGISTER_NODE(node_register)
 
-}  // namespace blender::nodes::node_geo_curve_handle_type_selection_cc
+}  // namespace dune::nodes::node_geo_curve_handle_type_sel_cc
