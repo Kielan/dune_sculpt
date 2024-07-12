d4dbbd6e9f80983f2080cdec56a622f5f969fdc5
Kielan <ki.lemons@gmail.com>
2024-07-12 11:03:34 -0700

    Update action_ops.cc

--- source/dune/editors/space_action/action_ops.cc
+++ source/dune/editors/space_action/action_ops.cc
@@ -18,62 +18,62 @@ void action_optypes()
 {
   /* keyframes */
   /* sel */
-  win_optype_append(ACTION_OT_clickselect);
-  win_optype_append(ACTION_OT_select_all);
-  win_optype_append(ACTION_OT_select_box);
-  win_optype_append(ACTION_OT_select_lasso);
-  win_optype_append(ACTION_OT_select_circle);
-  win_optype_append(ACTION_OT_select_column);
-  win_optype_append(ACTION_OT_select_linked);
-  win_optype_append(ACTION_OT_select_more);
-  win_optype_append(ACTION_OT_select_less);
-  win_optype_append(ACTION_OT_select_leftright);
+  win_optype_append(act_OT_clicksel);
+  win_optype_append(act_OT_sel_all);
+  win_optype_append(act_OT_sel_box);
+  win_optype_append(act_OT_sel_lasso);
+  win_optype_append(act_OT_sel_circle);
+  win_optype_append(act_OT_sel_column);
+  win_optype_append(act_OT_sel_linked);
+  win_optype_append(act_OT_select_more);
+  win_optype_append(act_OT_sel_less);
+  win_optype_append(act_OT_sel_leftright);
 
   /* editing */
-  win_optype_append(ACTION_OT_snap);
-  win_optype_append(ACTION_OT_mirror);
-  win_optype_append(ACTION_OT_frame_jump);
-  win_optype_append(ACTION_OT_handle_type);
-  win_optype_append(ACTION_OT_interpolation_type);
-  win_optype_append(ACTION_OT_extrapolation_type);
-  win_optype_append(ACTION_OT_easing_type);
-  win_optype_append(ACTION_OT_keyframe_type);
-  win_optype_append(ACTION_OT_bake_keys);
-  win_optype_append(ACTION_OT_clean);
-  win_optype_append(ACTION_OT_delete);
-  win_optype_append(ACTION_OT_dup);
-  win_optype_append(ACTION_OT_keyframe_insert);
-  win_optype_append(ACTION_OT_copy);
-  win_optype_append(ACTION_OT_paste);
+  win_optype_append(act_OT_snap);
+  win_optype_append(act_OT_mirror);
+  win_optype_append(act_OT_frame_jump);
+  win_optype_append(act_OT_handle_type);
+  win_optype_append(act_OT_interpolation_type);
+  win_optype_append(act_OT_extrapolation_type);
+  win_optype_append(act_OT_easing_type);
+  win_optype_append(act_OT_keyframe_type);
+  win_optype_append(act_OT_bake_keys);
+  win_optype_append(act_OT_clean);
+  win_optype_append(act_OT_del);
+  win_optype_append(act_OT_dup);
+  win_optype_append(act_OT_keyframe_insert);
+  win_optype_append(act_OT_copy);
+  win_optype_append(act_OT_paste);
 
-  win_optype_append(ACTION_OT_new);
-  win_optype_append(ACTION_OT_unlink);
+  win_optype_append(act_OT_new);
+  win_optype_append(act_OT_unlink);
 
-  win_optype_append(ACTION_OT_push_down);
-  win_optype_append(ACTION_OT_stash);
-  win_optype_append(ACTION_OT_stash_and_create);
+  win_optype_append(act_OT_push_down);
+  win_optype_append(act_OT_stash);
+  win_optype_append(act_OT_stash_and_create);
 
-  win_optype_append(ACTION_OT_layer_next);
-  win_optype_append(ACTION_OT_layer_prev);
+  win_optype_append(act_OT_layer_next);
+  win_optype_append(act_OT_layer_prev);
 
-  win_optype_append(ACTION_OT_previewrange_set);
-  win_optype_append(ACTION_OT_view_all);
-  win_optype_append(ACTION_OT_view_selected);
-  win_optype_append(ACTION_OT_view_frame);
+  win_optype_append(act_OT_previewrange_set);
+  win_optype_append(act_OT_view_all);
+  win_optype_append(act_OT_view_sel);
+  win_optype_append(act_OT_view_frame);
 
-  win_optype_append(ACTION_OT_markers_make_local);
+  win_optype_append(act_OT_markers_make_local);
 }
 
-void ed_opmacros_action()
+void ed_opmacros_act()
 {
   WinOpType *ot;
   WinOpTypeMacro *otmacro;
 
-  ot = win_optype_append_macro("ACTION_OT_dup_move",
+  ot = win_optype_append_macro("act_OT_dup_move",
                                "Duplicate",
                                "Make a copy of all sel keyframes and move them",
                                OPTYPE_UNDO | OPTYPE_REGISTER);
-  win_optype_macro_define(ot, "ACTION_OT_dup");
+  win_optype_macro_define(ot, "act_ot_dup");
   otmacro = win_optype_macro_define(ot, "TRANSFORM_OT_transform");
   api_enum_set(otmacro->ptr, "mode", TFM_TIME_TRANSLATE);
   api_bool_set(otmacro->ptr, "use_dup_keyframes", true);
