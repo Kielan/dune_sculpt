1a270884af78268789f624507d43b3adba48457c
Kielan <ki.lemons@gmail.com>
2025-11-04 11:35:16 -0800

    Update lazy_fn_graph.cc

--- source/dune/functions/intern/lazy_fn_graph.cc
+++ source/dune/functions/intern/lazy_fn_graph.cc
@@ -8,8 +8,8 @@ namespace dune::fn::lazy_fn {
 
 Graph::Graph()
 {
-  graph_input_node_ = allocator_.construct<InterfaceNode>().release();
-  graph_output_node_ = allocator_.construct<InterfaceNode>().release();
+  graph_input_node_ = allocator_.construct<NodeI>().release();
+  graph_output_node_ = allocator_.construct<NodeI>().release();
   nodes_.append(graph_input_node_);
   nodes_.append(graph_output_node_);
 }
@@ -25,7 +25,7 @@ Graph::~Graph()
     }
     std::destroy_at(node);
   }
-  for (const InterfaceNode *node : {graph_input_node_, graph_output_node_}) {
+  for (const NodeI *node : {graph_input_node_, graph_output_node_}) {
     for (InputSocket *socket : node->inputs_) {
       std::destroy_at(socket);
     }
@@ -41,21 +41,21 @@ FnNode &Graph::add_fn(const LazyFn &fn)
   const Span<Input> inputs = fn.inputs();
   const Span<Output> outputs = fn.outputs();
 
-  FnNode &node = *allocator_.construct<FunctionNode>().release();
+  FnNode &node = *allocator_.construct<FnNode>().release();
   node.fn_ = &fn;
-  node.inputs_ = allocator_.construct_elements_and_ptr_array<InputSocket>(inputs.size());
-  node.outputs_ = allocator_.construct_elements_and_ptr_array<OutputSocket>(outputs.size());
+  node.inputs_ = allocator_.construct_elements_and_ptr_arr<InputSocket>(inputs.size());
+  node.outputs_ = allocator_.construct_elements_and_ptr_arr<OutputSocket>(outputs.size());
 
-  for (const int i : inputs.index_range()) {
+  for (const int i : inputs.idx_range()) {
     InputSocket &socket = *node.inputs_[i];
-    socket.index_in_node_ = i;
+    socket.idx_in_node_ = i;
     socket.is_input_ = true;
     socket.node_ = &node;
     socket.type_ = inputs[i].type;
   }
-  for (const int i : outputs.index_range()) {
+  for (const int i : outputs.idx_range()) {
     OutputSocket &socket = *node.outputs_[i];
-    socket.index_in_node_ = i;
+    socket.idx_in_node_ = i;
     socket.is_input_ = false;
     socket.node_ = &node;
     socket.type_ = outputs[i].type;
@@ -71,7 +71,7 @@ GraphInputSocket &Graph::add_input(const CPPType &type, std::string name)
   socket.is_input_ = false;
   socket.node_ = graph_input_node_;
   socket.type_ = &type;
-  socket.index_in_node_ = graph_inputs_.append_and_get_index(&socket);
+  socket.idx_in_node_ = graph_inputs_.append_and_get_idx(&socket);
   graph_input_node_->outputs_ = graph_inputs_;
 
   graph_input_node_->socket_names_.append(std::move(name));
@@ -84,7 +84,7 @@ GraphOutputSocket &Graph::add_output(const CPPType &type, std::string name)
   socket.is_input_ = true;
   socket.node_ = graph_output_node_;
   socket.type_ = &type;
-  socket.index_in_node_ = graph_outputs_.append_and_get_index(&socket);
+  socket.idx_in_node_ = graph_outputs_.append_and_get_index(&socket);
   graph_output_node_->inputs_ = graph_outputs_;
 
   graph_output_node_->socket_names_.append(std::move(name));
@@ -109,20 +109,20 @@ void Graph::clear_origin(InputSocket &socket)
 
 void Graph::update_node_indices()
 {
-  for (const int i : nodes_.index_range()) {
-    nodes_[i]->index_in_graph_ = i;
+  for (const int i : nodes_.idx_range()) {
+    nodes_[i]->idx_in_graph_ = i;
   }
 }
 
 void Graph::update_socket_indices()
 {
   int socket_counter = 0;
-  for (const int i : nodes_.index_range()) {
+  for (const int i : nodes_.idx_range()) {
     for (InputSocket *socket : nodes_[i]->inputs()) {
-      socket->index_in_graph_ = socket_counter++;
+      socket->idx_in_graph_ = socket_counter++;
     }
     for (OutputSocket *socket : nodes_[i]->outputs()) {
-      socket->index_in_graph_ = socket_counter++;
+      socket->idx_in_graph_ = socket_counter++;
     }
   }
   socket_num_ = socket_counter;
@@ -130,8 +130,8 @@ void Graph::update_socket_indices()
 
 bool Graph::node_indices_are_valid() const
 {
-  for (const int i : nodes_.index_range()) {
-    if (nodes_[i]->index_in_graph_ != i) {
+  for (const int i : nodes_.idx_range()) {
+    if (nodes_[i]->idx_in_graph_ != i) {
       return false;
     }
   }
@@ -141,15 +141,15 @@ bool Graph::node_indices_are_valid() const
 std::string Socket::name() const
 {
   if (node_->is_fn()) {
-    const FnNode &fn_node = static_cast<const FunctionNode &>(*node_);
-    const LazyFn &fn = fn_node.function();
+    const FnNode &fn_node = static_cast<const FnNode &>(*node_);
+    const LazyFn &fn = fn_node.fn();
     if (is_input_) {
-      return fn.input_name(index_in_node_);
+      return fn.input_name(idx_in_node_);
     }
-    return fn.output_name(index_in_node_);
+    return fn.output_name(idx_in_node_);
   }
   const InterfaceNode &interface_node = *static_cast<const InterfaceNode *>(node_);
-  return interface_node.socket_names_[index_in_node_];
+  return interface_node.socket_names_[idx_in_node_];
 }
 
 std::string Socket::detailed_name() const
@@ -189,7 +189,7 @@ std::string Graph::to_dot(const ToDotOptions &options) const
   dot::DirectedGraph digraph;
   digraph.set_rankdir(dot::Attr_rankdir::LeftToRight);
 
-  Map<const Node *, dot::NodeWithSocketsRef> dot_nodes;
+  Map<const Node *, dot::NodeWSocketsRef> dot_nodes;
 
   for (const Node *node : nodes_) {
     dot::Node &dot_node = digraph.new_node("");
@@ -200,29 +200,29 @@ std::string Graph::to_dot(const ToDotOptions &options) const
       dot_node.set_background_color("white");
     }
 
-    dot::NodeWithSockets dot_node_with_sockets;
-    dot_node_with_sockets.node_name = node->name();
+    dot::NodeWSockets dot_node_w_sockets;
+    dot_node_w_sockets.node_name = node->name();
     for (const InputSocket *socket : node->inputs()) {
-      dot::NodeWithSockets::Input &dot_input = dot_node_with_sockets.add_input(
+      dot::NodeWSockets::Input &dot_input = dot_node_with_sockets.add_input(
           options.socket_name(*socket));
       dot_input.fontcolor = options.socket_font_color(*socket);
     }
     for (const OutputSocket *socket : node->outputs()) {
-      dot::NodeWithSockets::Output &dot_output = dot_node_w_sockets.add_output(
+      dot::NodeWSockets::Output &dot_output = dot_node_w_sockets.add_output(
           options.socket_name(*socket));
       dot_output.fontcolor = options.socket_font_color(*socket);
     }
 
-    dot_nodes.add_new(node, dot::NodeWithSocketsRef(dot_node, dot_node_w_sockets));
+    dot_nodes.add_new(node, dot::NodeWSocketsRef(dot_node, dot_node_w_sockets));
   }
 
   for (const Node *node : nodes_) {
     for (const InputSocket *socket : node->inputs()) {
-      const dot::NodeWithSocketsRef &to_dot_node = dot_nodes.lookup(&socket->node());
-      const dot::NodePort to_dot_port = to_dot_node.input(socket->index());
+      const dot::NodeWSocketsRef &to_dot_node = dot_nodes.lookup(&socket->node());
+      const dot::NodePort to_dot_port = to_dot_node.input(socket->idx());
 
       if (const OutputSocket *origin = socket->origin()) {
-        dot::NodeWithSocketsRef &from_dot_node = dot_nodes.lookup(&origin->node());
+        dot::NodeWSocketsRef &from_dot_node = dot_nodes.lookup(&origin->node());
         dot::DirectedEdge &dot_edge = digraph.new_edge(from_dot_node.output(origin->index()),
                                                        to_dot_port);
         options.add_edge_attributes(*origin, *socket, dot_edge);
@@ -247,4 +247,4 @@ std::string Graph::to_dot(const ToDotOptions &options) const
   return digraph.to_dot_string();
 }
 
-}  // namespace blender::fn::lazy_function
+}  // namespace dune::fn::lazy_fn
